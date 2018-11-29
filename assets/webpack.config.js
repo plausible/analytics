6e794c915b3e42167bcd21296f212eae27a26d28
Uku Taht <uku.taht@gmail.com>
2018-11-29 15:47:43 +0000

    Use tailwind properly

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -30,7 +30,7 @@ module.exports = (env, options) => ({
       },
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader']
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
       }
     ]
   },
