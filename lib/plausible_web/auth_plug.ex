452bbdab2cd6f535f60bdc6ed223d9b96b663d90
Uku Taht <uku.taht@gmail.com>
2019-02-26 13:10:40 +0000

    Remove session email support

--- lib/plausible_web/auth_plug.ex
+++ lib/plausible_web/auth_plug.ex
@@ -6,24 +6,12 @@ defmodule PlausibleWeb.AuthPlug do
   end
 
   def call(conn, _opts) do
-    case get_session(conn, :current_user_email) do
-      nil ->
-        case get_session(conn, :current_user_id) do
-          nil -> conn
-          id ->
-            user = Plausible.Auth.find_user_by(id: id)
-            if user do
-              assign(conn, :current_user, user)
-            else
-              conn
-            end
-        end
-      email ->
-        user = Plausible.Auth.find_user_by(email: email)
+    case get_session(conn, :current_user_id) do
+      nil -> conn
+      id ->
+        user = Plausible.Auth.find_user_by(id: id)
         if user do
-          conn
-          |> put_session(:current_user_id, user.id)
-          |> assign(:current_user, user)
+          assign(conn, :current_user, user)
         else
           conn
         end
