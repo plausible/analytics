f35acc3a2faca92a78c89d3f040346b618d398cc
Uku Taht <uku.taht@gmail.com>
2019-04-12 10:51:32 +0300

    Increase timeout to 10 seconds

--- config/prod.exs
+++ config/prod.exs
@@ -76,6 +76,7 @@ config :plausible, Plausible.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  timeout: 10_000,
   ssl: true
 
 config :plausible, :amplitude,
