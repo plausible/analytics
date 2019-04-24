66dc964225e051ebe9b11917a6b76e7e7c722107
Uku Taht <uku.taht@gmail.com>
2019-04-24 19:08:24 +0100

    Add url to bot image

--- lib/plausible/slack.ex
+++ lib/plausible/slack.ex
@@ -6,7 +6,10 @@ defmodule Plausible.Slack do
     Task.start(fn ->
       case @app_env do
         "prod" ->
-          HTTPoison.post!(@feed_channel_url, Poison.encode!(%{text: text}))
+          HTTPoison.post!(@feed_channel_url, Poison.encode!(%{
+            text: text,
+            icon_url: "https://plausible.io/images/icon/plausible_favicon.png"
+          }))
         _ ->
           nil
       end
