6f99e060e81d624f713cc5ac89045243e31a5d66
Uku Taht <uku.taht@gmail.com>
2019-05-01 16:31:30 +0100

    Paddle plan id is a string

--- lib/plausible_web/views/auth_view.ex
+++ lib/plausible_web/views/auth_view.ex
@@ -2,8 +2,8 @@ defmodule PlausibleWeb.AuthView do
   use PlausibleWeb, :view
 
   @subscription_names %{
-    558018 => "Personal",
-    558156 => "TEST - Personal"
+    "558018" => "Personal",
+    "558156" => "TEST - Personal"
   }
 
   def subscription_name(subscription) do
