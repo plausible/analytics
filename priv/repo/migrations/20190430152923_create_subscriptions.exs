7eb22c435c422cb7a10d0efec594cb8b059ab2e9
Uku Taht <uku.taht@gmail.com>
2019-05-01 11:31:36 +0100

    Make urls text instead of varchar

--- priv/repo/migrations/20190430152923_create_subscriptions.exs
+++ priv/repo/migrations/20190430152923_create_subscriptions.exs
@@ -6,8 +6,8 @@ defmodule Plausible.Repo.Migrations.CreateSubscriptions do
       add :paddle_subscription_id, :string, null: false
       add :paddle_plan_id, :string, null: false
       add :user_id, references(:users), null: false
-      add :update_url, :string, null: false
-      add :cancel_url, :string, null: false
+      add :update_url, :text, null: false
+      add :cancel_url, :text, null: false
       add :status, :string, null: false
 
       timestamps()
