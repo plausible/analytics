ac423c60380f8e37b0e5aeaf0abe35c39174bb8e
Uku Taht <uku.taht@gmail.com>
2019-05-16 12:33:37 +0100

    Refactor test file

--- test/mix/tasks/send_intro_emails_test.exs
+++ test/mix/tasks/send_intro_emails_test.exs
@@ -4,7 +4,7 @@ defmodule Mix.Tasks.SendIntroEmailsTest do
 
   describe "when user has not managed to set up a site" do
     test "does not send an email 5 hours after signup" do
-      {:ok, _user} = create_user(%{inserted_at: hours_ago(5)})
+      _user = insert(:user, inserted_at: hours_ago(5))
 
       Mix.Tasks.SendIntroEmails.execute()
 
@@ -12,7 +12,7 @@ defmodule Mix.Tasks.SendIntroEmailsTest do
     end
 
     test "sends a setup help email 6 hours after signup" do
-      {:ok, user} = create_user(%{inserted_at: hours_ago(6)})
+      user = insert(:user, inserted_at: hours_ago(6))
 
       Mix.Tasks.SendIntroEmails.execute()
 
@@ -23,7 +23,7 @@ defmodule Mix.Tasks.SendIntroEmailsTest do
     end
 
     test "sends a setup help email 23 hours after signup" do
-      {:ok, user} = create_user(%{inserted_at: hours_ago(23)})
+      user = insert(:user, inserted_at: hours_ago(23))
 
       Mix.Tasks.SendIntroEmails.execute()
 
@@ -34,7 +34,7 @@ defmodule Mix.Tasks.SendIntroEmailsTest do
     end
 
     test "does not send an email 24 hours after signup" do
-      {:ok, _user} = create_user(%{inserted_at: hours_ago(24)})
+      _user = insert(:user, inserted_at: hours_ago(24))
 
       Mix.Tasks.SendIntroEmails.execute()
 
@@ -44,7 +44,7 @@ defmodule Mix.Tasks.SendIntroEmailsTest do
 
   describe "when user has managed to set up their first site" do
     test "does not send an email 5 hours after signup" do
-      {:ok, _user} = create_user(%{inserted_at: hours_ago(5)})
+      _user = insert(:user, inserted_at: hours_ago(5))
 
       Mix.Tasks.SendIntroEmails.execute()
 
@@ -52,8 +52,8 @@ defmodule Mix.Tasks.SendIntroEmailsTest do
     end
 
     test "sends a setup help email 6 hours after signup if the user has created a site but has not received a pageview yet" do
-      {:ok, user} = create_user(%{inserted_at: hours_ago(6)})
-      create_site(user.id)
+      user = insert(:user, inserted_at: hours_ago(6))
+      insert(:site, members: [user])
 
       Mix.Tasks.SendIntroEmails.execute()
 
@@ -64,9 +64,9 @@ defmodule Mix.Tasks.SendIntroEmailsTest do
     end
 
     test "sends a welcome email 6 hours after signup if the user has created a site and has received a pageview" do
-      {:ok, user} = create_user(%{inserted_at: hours_ago(6)})
-      site = create_site(user.id)
-      create_pageview(site.domain)
+      user = insert(:user, inserted_at: hours_ago(6))
+      site = insert(:site, members: [user])
+      insert(:pageview, hostname: site.domain)
 
       Mix.Tasks.SendIntroEmails.execute()
 
@@ -77,9 +77,9 @@ defmodule Mix.Tasks.SendIntroEmailsTest do
     end
 
     test "sends a welcome email 23 hours after signup" do
-      {:ok, user} = create_user(%{inserted_at: hours_ago(23)})
-      site = create_site(user.id)
-      create_pageview(site.domain)
+      user = insert(:user, inserted_at: hours_ago(23))
+      site = insert(:site, members: [user])
+      insert(:pageview, hostname: site.domain)
 
       Mix.Tasks.SendIntroEmails.execute()
 
@@ -90,9 +90,9 @@ defmodule Mix.Tasks.SendIntroEmailsTest do
     end
 
     test "does not send a welcome email 24 hours after signup" do
-      {:ok, user} = create_user(%{inserted_at: hours_ago(24)})
-      site = create_site(user.id)
-      create_pageview(site.domain)
+      user = insert(:user, inserted_at: hours_ago(24))
+      site = insert(:site, members: [user])
+      insert(:pageview, hostname: site.domain)
 
       Mix.Tasks.SendIntroEmails.execute()
 
@@ -101,49 +101,17 @@ defmodule Mix.Tasks.SendIntroEmailsTest do
   end
 
   test "does not send two intro emails to the same person" do
-    {:ok, user} = create_user(%{inserted_at: hours_ago(12)})
+    user = insert(:user, inserted_at: hours_ago(12))
 
     Mix.Tasks.SendIntroEmails.execute()
 
-    site = create_site(user.id)
-    create_pageview(site.domain)
+    site = insert(:site, members: [user])
+    insert(:pageview, hostname: site.domain)
 
     Mix.Tasks.SendIntroEmails.execute()
 
     assert_delivered_email(PlausibleWeb.Email.help_email(user))
     refute_delivered_email(PlausibleWeb.Email.welcome_email(user))
-  end
-
-  defp create_user(attrs) do
-    %Plausible.Auth.User{
-      name: "Jane Doe",
-      email: "user@example.com"
-    }
-    |> Map.merge(attrs)
-    |> Repo.insert()
-  end
-
-  defp create_site(user_id) do
-    site = %Plausible.Site{
-      domain: "example.com",
-      timezone: "Etc/Greenwich"
-    } |> Repo.insert!()
-
-    %Plausible.Site.Membership{
-      user_id: user_id,
-      site_id: site.id
-    } |> Repo.insert!
-    site
-  end
-
-  defp create_pageview(domain) do
-    %Plausible.Pageview{
-      hostname: domain,
-      pathname: "/",
-      new_visitor: true,
-      session_id: "123",
-      user_id: "321"
-    } |> Repo.insert!
   end
 
   defp hours_ago(hours) do
