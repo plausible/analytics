1ed546a0ee7cc75961bfc2d91a1ba3cb61f4fbdd
Uku Taht <uku.taht@gmail.com>
2019-07-02 17:25:05 +0300

    Better UUID function

--- assets/js/plausible.js
+++ assets/js/plausible.js
@@ -24,9 +24,14 @@
     }
 
     function pseudoUUIDv4() {
-      return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-        return v.toString(16);
+      var d = new Date().getTime();
+      if (typeof performance !== 'undefined' && typeof performance.now === 'function'){
+        d += performance.now(); //use high-precision timer if available
+      }
+      return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = (d + Math.random() * 16) % 16 | 0;
+        d = Math.floor(d / 16);
+        return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
       });
     }
 
