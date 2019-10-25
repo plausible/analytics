9dd08ddfa6706ad7e81c55a75fabcd8d9784e624
Uku Taht <uku.taht@gmail.com>
2019-10-25 14:51:54 +0800

    Add test for change plan page

--- test/plausible_web/controllers/billing_controller_test.exs
+++ test/plausible_web/controllers/billing_controller_test.exs
@@ -5,6 +5,13 @@ defmodule PlausibleWeb.BillingControllerTest do
   describe "GET /change-plan" do
     setup [:create_user, :log_in]
 
+    test "shows change plan page if user has subsription", %{conn: conn, user: user} do
+      insert(:subscription, user: user)
+      conn = get(conn, "/billing/change-plan")
+
+      assert html_response(conn, 200) =~ "Change subscription plan"
+    end
+
     test "redirects to /upgrade if user does not have a subscription", %{conn: conn} do
       conn = get(conn, "/billing/change-plan")
 
