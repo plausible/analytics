55bca892d13ae1bcbb39ab2772f7d33634db8798
Uku Taht <uku.taht@gmail.com>
2020-01-16 14:12:12 +0200

    Configure twitter api

--- config/prod.exs
+++ config/prod.exs
@@ -89,3 +89,10 @@ config :plausible, :google,
 config :plausible, Plausible.Mailer,
   adapter: Bamboo.PostmarkAdapter,
   api_key: System.get_env("POSTMARK_API_KEY")
+
+config :plausible, :twitter, [
+   consumer_key: System.get_env("TWITTER_CONSUMER_KEY"),
+   consumer_secret: System.get_env("TWITTER_CONSUMER_SECRET"),
+   token: System.get_env("TWITTER_ACCESS_TOKEN"),
+   token_secret: System.get_env("TWITTER_ACCESS_TOKEN_SECRET")
+]
