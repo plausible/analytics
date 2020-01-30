ad2e5b6b281d77a1b60c35d376b6c2529e56cd80
Uku Taht <uku.taht@gmail.com>
2020-01-30 11:03:55 +0200

    Remove unused code

--- test/mix/tasks/send_trial_notifications_test.exs
+++ test/mix/tasks/send_trial_notifications_test.exs
@@ -34,26 +34,6 @@ defmodule Mix.Tasks.SendTrialNotificationsTest do
       assert_delivered_email(PlausibleWeb.Email.trial_upgrade_email(user, "tomorrow", 1))
     end
 
-    #    test "sends an upgrade email the day the trial ends" do
-    #      user = insert(:user, inserted_at: Timex.now |> Timex.shift(days: -30))
-    #      site = insert(:site, members: [user])
-    #      insert(:pageview, hostname: site.domain)
-    #
-    #      Mix.Tasks.SendTrialNotifications.execute()
-    #
-    #      assert_delivered_email(PlausibleWeb.Email.trial_upgrade_email(user, "today", 1))
-    #    end
-    #
-    #test "sends a trial over email on the day after the trial ends" do
-    #  user = insert(:user, inserted_at: Timex.now |> Timex.shift(days: -31))
-    #  site = insert(:site, members: [user])
-    #  insert(:pageview, hostname: site.domain)
-
-    #  Mix.Tasks.SendTrialNotifications.execute()
-
-    #  assert_delivered_email(PlausibleWeb.Email.trial_over_email(user))
-    #end
-
     test "does not send a notification if user has a subscription" do
       user1 = insert(:user, inserted_at: Timex.now |> Timex.shift(days: -14))
       site1 = insert(:site, members: [user1])
