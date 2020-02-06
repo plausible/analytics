pramsky/plausible

WIP: New UI idea

assets/js/dashboard/stats/countries.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Datamap from 'datamaps'
 
 import Bar from './bar'
 import MoreLink from './more-link'
@@ -7,6 +8,7 @@ import * as api from '../api'
 export default class Countries extends React.Component {
   constructor(props) {
     super(props)
+    this.resizeMap = this.resizeMap.bind(this)
     this.state = {
       loading: true
     }
@@ -14,6 +16,11 @@ export default class Countries extends React.Component {
 
   componentDidMount() {
     this.fetchCountries()
+    window.addEventListener('resize', this.resizeMap);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.resizeMap);
   }
 
   componentDidUpdate(prevProps) {
@@ -26,38 +33,64 @@ export default class Countries extends React.Component {
   fetchCountries() {
     api.get(`/api/stats/${this.props.site.domain}/countries`, this.props.query)
       .then((res) => this.setState({loading: false, countries: res}))
+      .then(() => this.drawMap())
   }
 
-  renderCountry(country) {
-    return (
-      <React.Fragment key={country.name}>
-        <div className="flex items-center justify-between my-2">
-          <span className="truncate" style={{maxWidth: '80%'}}>{country.name}</span>
-          <span tooltip={`${country.count} visitors`}>{country.percentage}%</span>
-        </div>
-        <Bar count={country.count} all={this.state.countries} color="indigo" />
-      </React.Fragment>
-    )
+  resizeMap() {
+    this.map && this.map.resize()
+  }
+
+  drawMap() {
+    var dataset = {};
+
+    var onlyValues = this.state.countries.map(function(obj){ return obj.count });
+    var minValue = Math.min.apply(null, onlyValues),
+      maxValue = Math.max.apply(null, onlyValues);
+
+    var paletteScale = d3.scale.linear()
+      .domain([minValue,maxValue])
+      .range(["#b2b7ff","#5661b3"]);
+
+    this.state.countries.forEach(function(item){
+      dataset[item.name] = {numberOfThings: item.count, fillColor: paletteScale(item.count)};
+    });
+
+    this.map = new Datamap({
+      element: document.getElementById('map-container'),
+      responsive: true,
+      projection: 'mercator',
+      fills: { defaultFill: '#F5F5F5' },
+      data: dataset,
+      geographyConfig: {
+        borderColor: '#dae1e7',
+        highlightBorderWidth: 2,
+        highlightFillColor: function(geo) {
+          return geo['fillColor'] || '#F5F5F5';
+        },
+        highlightBorderColor: '#b8c2cc',
+        popupTemplate: function(geo, data) {
+          if (!data) { return ; }
+          return ['<div class="hoverinfo">',
+            '<strong>', geo.properties.name, '</strong>',
+            '<br><strong>', data.numberOfThings, '</strong> Visitors',
+            '</div>'].join('');
+        }
+      }
+    });
   }
 
   render() {
     if (this.state.loading) {
       return (
-        <div className="w-full md:w-31percent bg-white shadow-md rounded mt-4 p-4" style={{height: '405px'}}>
+        <div className="stats-item bg-white shadow-md rounded p-4" style={{height: '424px'}}>
           <div className="loading my-32 mx-auto"><div></div></div>
         </div>
       )
     } else if (this.state.countries) {
       return (
-        <div className="w-full md:w-31percent bg-white shadow-md rounded mt-4 p-4" style={{height: '405px'}}>
-          <div className="text-center">
-            <h2>Top Countries</h2>
-            <div className="text-grey-darker mt-1">by visitors</div>
-          </div>
-
-          <div className="mt-8">
-            { this.state.countries.map(this.renderCountry.bind(this)) }
-          </div>
+        <div className="stats-item bg-white shadow-md rounded p-4" style={{height: '424px'}}>
+          <h3>Countries</h3>
+          <div className="mt-6 mx-auto" style={{width: '100%', maxWidth: '475px', height: '320px'}} id="map-container"></div>
           <MoreLink site={this.props.site} list={this.state.countries} endpoint="countries" />
         </div>
       )

plausible/analytics

assets/js/dashboard/stats/countries.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Datamap from 'datamaps'
 
 import Bar from './bar'
 import MoreLink from './more-link'
@@ -7,6 +8,7 @@ import * as api from '../api'
 export default class Countries extends React.Component {
   constructor(props) {
     super(props)
+    this.resizeMap = this.resizeMap.bind(this)
     this.state = {
       loading: true
     }
@@ -14,6 +16,11 @@ export default class Countries extends React.Component {
 
   componentDidMount() {
     this.fetchCountries()
+    window.addEventListener('resize', this.resizeMap);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.resizeMap);
   }
 
   componentDidUpdate(prevProps) {
@@ -26,39 +33,65 @@ export default class Countries extends React.Component {
   fetchCountries() {
     api.get(`/api/stats/${encodeURIComponent(this.props.site.domain)}/countries`, this.props.query)
       .then((res) => this.setState({loading: false, countries: res}))
+      .then(() => this.drawMap())
   }
 
-  renderCountry(country) {
-    return (
-      <React.Fragment key={country.name}>
-        <div className="flex items-center justify-between my-2">
-          <span className="truncate" style={{maxWidth: '80%'}}>{country.name}</span>
-          <span tooltip={`${country.count} visitors`}>{country.percentage}%</span>
-        </div>
-        <Bar count={country.count} all={this.state.countries} color="indigo" />
-      </React.Fragment>
-    )
+  resizeMap() {
+    this.map && this.map.resize()
+  }
+
+  drawMap() {
+    var dataset = {};
+
+    var onlyValues = this.state.countries.map(function(obj){ return obj.count });
+    var minValue = Math.min.apply(null, onlyValues),
+      maxValue = Math.max.apply(null, onlyValues);
+
+    var paletteScale = d3.scale.linear()
+      .domain([minValue,maxValue])
+      .range(["#f3ebff","#a779e9"]);
+
+    this.state.countries.forEach(function(item){
+      dataset[item.name] = {numberOfThings: item.count, fillColor: paletteScale(item.count)};
+    });
+
+    this.map = new Datamap({
+      element: document.getElementById('map-container'),
+      responsive: true,
+      projection: 'mercator',
+      fills: { defaultFill: '#f8fafc' },
+      data: dataset,
+      geographyConfig: {
+        borderColor: '#dae1e7',
+        highlightBorderWidth: 2,
+        highlightFillColor: function(geo) {
+          return geo['fillColor'] || '#F5F5F5';
+        },
+        highlightBorderColor: '#a779e9',
+        popupTemplate: function(geo, data) {
+          if (!data) { return ; }
+          return ['<div class="hoverinfo">',
+            '<strong>', geo.properties.name, '</strong>',
+            '<br><strong>', data.numberOfThings, '</strong> Visitors',
+            '</div>'].join('');
+        }
+      }
+    });
   }
 
   render() {
     if (this.state.loading) {
       return (
-        <div className="w-full md:w-31percent bg-white shadow-md rounded mt-4 p-4 relative" style={{height: '405px'}}>
+        <div className="stats-item bg-white shadow-xl rounded p-4" style={{height: '436px'}}>
           <div className="loading my-32 mx-auto"><div></div></div>
         </div>
       )
     } else if (this.state.countries) {
       return (
-        <div className="w-full md:w-31percent bg-white shadow-md rounded mt-4 p-4 relative" style={{height: '405px'}}>
-          <div className="text-center">
-            <h2>Top Countries</h2>
-            <div className="text-grey-darker mt-1">by visitors</div>
-          </div>
-
-          <div className="mt-8">
-            { this.state.countries.map(this.renderCountry.bind(this)) }
-          </div>
-          <MoreLink site={this.props.site} endpoint="countries" />
+        <div className="stats-item bg-white shadow-xl rounded p-4" style={{height: '436px'}}>
+          <h3>Countries</h3>
+          <div className="mt-6 mx-auto" style={{width: '100%', maxWidth: '475px', height: '320px'}} id="map-container"></div>
+          <MoreLink site={this.props.site} list={this.state.countries} endpoint="countries" />
         </div>
       )
     }