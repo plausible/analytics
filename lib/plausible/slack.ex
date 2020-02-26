1be43a6ce6e2a6c1ac2282aa43682f582c0ad308
Uku Taht <uku.taht@gmail.com>
2020-02-26 11:40:16 +0200

    Update webhook

--- lib/plausible/slack.ex
+++ lib/plausible/slack.ex
@@ -1,6 +1,6 @@
 defmodule Plausible.Slack do
   @app_env System.get_env("APP_ENV") || "dev"
-  @feed_channel_url "https://hooks.slack.com/services/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"
+  @feed_channel_url "https://hooks.slack.com/services/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"
   require Logger
 
   def notify(text) do
