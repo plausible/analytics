45bb8a637e168db0c418b039b3bc6ae644276009
Uku Taht <uku.taht@gmail.com>
2020-02-26 15:45:36 +0200

    Code golf, makes getCookie function smaller

--- assets/js/plausible.js
+++ assets/js/plausible.js
@@ -17,14 +17,10 @@
     }
 
     function getCookie(name) {
-        var nameEQ = name + "=";
-        var ca = document.cookie.split(';');
-        for(var i=0;i < ca.length;i++) {
-            var c = ca[i];
-            while (c.charAt(0)==' ') c = c.substring(1,c.length);
-            if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-        }
-        return null;
+      let matches = document.cookie.match(new RegExp(
+        "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+      ));
+      return matches ? decodeURIComponent(matches[1]) : null;
     }
 
     function pseudoUUIDv4() {
