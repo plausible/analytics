07209dfeaa1d317a67b50f922129d55401acf5a7
Uku Taht <uku.taht@gmail.com>
2020-03-05 11:33:03 +0200

    Upgrade config file

--- assets/js/dashboard/stats/more-link.js
+++ assets/js/dashboard/stats/more-link.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export default function MoreLink({site, list, endpoint}) {
   if (list.length > 0) {
     return (
-      <div className="text-center w-full absolute pin-b pin-l p-4">
+      <div className="text-center w-full absolute bottom-0 left-0 p-4">
         <Link to={`/${encodeURIComponent(site.domain)}/${endpoint}${window.location.search}`}className="font-bold text-sm text-grey-dark hover:text-red transition tracking-wide">
           <svg className="feather mr-1"><use xlinkHref="#feather-maximize" /></svg>
           MORE
--- assets/postcss.config.js
+++ assets/postcss.config.js
@@ -1,8 +1,6 @@
-var tailwindcss = require('tailwindcss');
-
 module.exports = {
   plugins: [
-    tailwindcss('./tailwind.js'),
+    require('tailwindcss'),
     require('autoprefixer')
   ]
 }
