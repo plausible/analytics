ae99f4eafd9ad090172e41958170c39657c16224
Chandra Tungathurthi <tckb@tgrthi.me>
2020-05-07 12:24:54 +0200

    removed the default comments
    Signed-off-by: Chandra Tungathurthi <tckb@tgrthi.me>

--- config/dev.exs
+++ config/dev.exs
@@ -1,11 +1,5 @@
 use Mix.Config
 
-# For development, we disable any cache and enable
-# debugging and code reloading.
-#
-# The watchers configuration can be used to run external
-# watchers to your application. For example, we use it
-# with webpack to recompile .js and .css sources.
 config :plausible, PlausibleWeb.Endpoint,
   server: true,
   debug_errors: true,
