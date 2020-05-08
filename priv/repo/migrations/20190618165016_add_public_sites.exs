071c08a538f879ad37a78d1852d5a5b17195f2c5
Chandra Tungathurthi <tckb@tgrthi.me>
2020-05-08 15:27:10 +0200

    Initial base domain update - phase#1
    These changes are only meant for correct operating it under self-hosting. There are many other cosmetic changes, that require updates to email, site and other places where the original website and author is used.
    
    Signed-off-by: Chandra Tungathurthi <tckb@tgrthi.me>

--- priv/repo/migrations/20190618165016_add_public_sites.exs
+++ priv/repo/migrations/20190618165016_add_public_sites.exs
@@ -1,11 +1,12 @@
 defmodule Plausible.Repo.Migrations.AddPublicSites do
   use Ecto.Migration
+  @host PlausibleWeb.Endpoint.host()
 
   def change do
     alter table(:sites) do
       add :public, :boolean, null: false, default: false
     end
 
-    execute "update sites set public=true where domain='plausible.io'"
+    execute "update sites set public=true where domain='#{@host}'"
   end
 end
