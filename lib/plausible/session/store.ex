25e33d9f00ea447134b9397ae598f9028c75cf5e
Uku Taht <uku.taht@gmail.com>
2020-05-13 10:19:25 +0300

    Add garbage collection to session store

--- lib/plausible/session/store.ex
+++ lib/plausible/session/store.ex
@@ -2,22 +2,26 @@ defmodule Plausible.Session.Store do
   use GenServer
   use Plausible.Repo
   alias Plausible.Session.WriteBuffer
+  require Logger
 
   @session_length_seconds Application.get_env(:plausible, :session_length_minutes) * 60
+  @forget_session_after @session_length_seconds * 2 # Remember session for longer in case of upstream latency
+  @garbage_collect_interval_milliseconds 60 * 1000
 
   def start_link(opts) do
     GenServer.start_link(__MODULE__, opts, name: __MODULE__)
   end
 
   def init(_opts) do
-    {:ok, %{}}
+    timer = Process.send_after(self(), :garbage_collect, @garbage_collect_interval_milliseconds)
+    {:ok, %{timer: timer, sessions: %{}}}
   end
 
   def on_event(event) do
     GenServer.cast(__MODULE__, {:on_event, event})
   end
 
-  def handle_cast({:on_event, event}, sessions) do
+  def handle_cast({:on_event, event}, %{sessions: sessions} = state) do
     found_session = sessions[event.fingerprint]
     active = is_active?(found_session, event)
 
@@ -36,7 +40,7 @@ defmodule Plausible.Session.Store do
         Map.put(sessions, event.fingerprint, new_session)
     end
 
-    {:noreply, updated_sessions}
+    {:noreply, %{ state | sessions: updated_sessions }}
   end
 
   defp is_active?(session, event) do
@@ -74,5 +78,29 @@ defmodule Plausible.Session.Store do
       timestamp: event.timestamp,
       start: event.timestamp
     }
+  end
+
+  def handle_info(:garbage_collect, state) do
+    Logger.debug("Session store collecting garbage")
+
+    now = Timex.now()
+    new_sessions = Enum.reduce(state[:sessions], %{}, fn {key, session}, acc ->
+      if Timex.diff(now, session.timestamp, :second) <= @forget_session_after do
+        Map.put(acc, key, session)
+      else
+        acc # forget the session
+      end
+    end)
+
+    Process.cancel_timer(state[:timer])
+    new_timer = Process.send_after(self(), :garbage_collect, @garbage_collect_interval_milliseconds)
+
+    Logger.debug(fn ->
+      n_old = Enum.count(state[:sessions])
+      n_new = Enum.count(new_sessions)
+      "Removed #{n_old - n_new} sessions from store"
+    end)
+
+    {:noreply, %{state | sessions: new_sessions, timer: new_timer}}
   end
 end
