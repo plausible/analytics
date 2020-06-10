449d591a18154f536e85274582928bdbf7844aca
Uku Taht <uku.taht@gmail.com>
2020-06-10 17:59:20 +0300

    Cache tracker script for 1 hour

--- lib/plausible_web/controllers/tracker_controller.ex
+++ lib/plausible_web/controllers/tracker_controller.ex
@@ -3,19 +3,24 @@ defmodule PlausibleWeb.TrackerController do
   require EEx
   EEx.function_from_file(:defp, :render_plausible, Application.app_dir(:plausible, "priv/tracker/js/plausible.js"), [:base_url])
   EEx.function_from_file(:defp, :render_p, Application.app_dir(:plausible, "priv/tracker/js/p.js"), [:base_url])
-
-  # TODO: Cache headers
+  @max_age 3600 # 1 hour
 
   def plausible(conn, _params) do
-    conn |> send_resp(200, render_plausible(base_url()))
+    conn
+    |> put_resp_header("cache-control", "max-age=#{@max_age},public")
+    |> send_resp(200, render_plausible(base_url()))
   end
 
   def analytics(conn, _params) do
-    conn |> send_resp(200, render_plausible(base_url()))
+    conn
+    |> put_resp_header("cache-control", "max-age=#{@max_age},public")
+    |> send_resp(200, render_plausible(base_url()))
   end
 
   def p(conn, _params) do
-    conn |> send_resp(200, render_p(base_url()))
+    conn
+    |> put_resp_header("cache-control", "max-age=#{@max_age},public")
+    |> send_resp(200, render_p(base_url()))
   end
 
   defp base_url() do
