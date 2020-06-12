d5e8d91420059948963e2878376d152dc1f51018
Uku Taht <uku.taht@gmail.com>
2020-06-12 16:38:43 +0300

    Add content-type to tracker script. Fixes #79

--- lib/plausible_web/controllers/tracker_controller.ex
+++ lib/plausible_web/controllers/tracker_controller.ex
@@ -6,21 +6,22 @@ defmodule PlausibleWeb.TrackerController do
   @max_age 3600 # 1 hour
 
   def plausible(conn, _params) do
-    conn
-    |> put_resp_header("cache-control", "max-age=#{@max_age},public")
-    |> send_resp(200, render_plausible(base_url()))
+    send_js(conn, render_plausible(base_url()))
   end
 
   def analytics(conn, _params) do
-    conn
-    |> put_resp_header("cache-control", "max-age=#{@max_age},public")
-    |> send_resp(200, render_plausible(base_url()))
+    send_js(conn, render_plausible(base_url()))
   end
 
   def p(conn, _params) do
+    send_js(conn, render_p(base_url()))
+  end
+
+  defp send_js(conn, file) do
     conn
     |> put_resp_header("cache-control", "max-age=#{@max_age},public")
-    |> send_resp(200, render_p(base_url()))
+    |> put_resp_header("content-type", "application/javascript")
+    |> send_resp(200, file)
   end
 
   defp base_url() do
