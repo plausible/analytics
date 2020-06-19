0bcc389e73c6080b97251ffdbd593bc72e496f28
Uku Taht <uku.taht@gmail.com>
2020-06-19 14:35:09 +0300

    Change 1m plan

--- lib/plausible/billing/plans.ex
+++ lib/plausible/billing/plans.ex
@@ -5,7 +5,7 @@ defmodule Plausible.Billing.Plans do
       "100k": %{product_id: "558745", due_now: "$12"},
       "200k": %{product_id: "597485", due_now: "$18"},
       "500k": %{product_id: "597487", due_now: "$27"},
-      "1m": %{product_id: "558746", due_now: "$36"},
+      "1m": %{product_id: "597642", due_now: "$48"},
       "2m": %{product_id: "597309", due_now: "$69"},
       "5m": %{product_id: "597311", due_now: "$99"}
     },
@@ -14,7 +14,7 @@ defmodule Plausible.Billing.Plans do
       "100k": %{product_id: "590752", due_now: "$96"},
       "200k": %{product_id: "597486", due_now: "$114"},
       "500k": %{product_id: "597488", due_now: "$216"},
-      "1m": %{product_id: "590753", due_now: "$288"},
+      "1m": %{product_id: "597643", due_now: "$384"},
       "2m": %{product_id: "597310", due_now: "$552"},
       "5m": %{product_id: "597312", due_now: "$792"}
     }
