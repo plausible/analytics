00d946e9cf11debeb4e0e0d0149ce916c84ea824
Uku Taht <uku.taht@gmail.com>
2020-07-15 10:16:27 +0300

    Rotate salts for every site

--- lib/plausible_web/controllers/api/external_controller.ex
+++ lib/plausible_web/controllers/api/external_controller.ex
@@ -42,10 +42,7 @@ defmodule PlausibleWeb.Api.ExternalController do
 
       ref = parse_referrer(uri, params["referrer"])
       country_code = visitor_country(conn)
-      rotating_salts = Plausible.Session.Salts.fetch()
-      static_salt =
-        Keyword.fetch!(Application.get_env(:plausible, PlausibleWeb.Endpoint), :secret_key_base)
-        |> binary_part(0, 16)
+      salts = Plausible.Session.Salts.fetch()
 
       event_attrs = %{
         timestamp: NaiveDateTime.utc_now(),
@@ -53,7 +50,7 @@ defmodule PlausibleWeb.Api.ExternalController do
         hostname: strip_www(uri && uri.host),
         domain: strip_www(params["domain"]) || strip_www(uri && uri.host),
         pathname: uri && (uri.path || "/"),
-        user_id: generate_user_id(conn, params, static_salt),
+        user_id: generate_user_id(conn, params, salts[:current]),
         country_code: country_code,
         operating_system: ua && os_name(ua),
         browser: ua && browser_name(ua),
@@ -65,22 +62,12 @@ defmodule PlausibleWeb.Api.ExternalController do
       changeset = Plausible.ClickhouseEvent.changeset(%Plausible.ClickhouseEvent{}, event_attrs)
 
       if changeset.valid? do
+        previous_user_id = salts[:previous] && generate_user_id(conn, params, salts[:previous])
         event = struct(Plausible.ClickhouseEvent, event_attrs)
-        session_id = Plausible.Session.Store.on_event(event, nil)
+        session_id = Plausible.Session.Store.on_event(event, previous_user_id)
 
-        res = Map.put(event, :session_id, session_id)
+        Map.put(event, :session_id, session_id)
         |> Plausible.Event.WriteBuffer.insert()
-
-        if params["domain"] == "plausible.io" do # Test salt rotation
-          user_id = generate_user_id(conn, params, rotating_salts[:current])
-          previous_user_id = rotating_salts[:previous] && generate_user_id(conn, params, rotating_salts[:previous])
-          event = %{event | domain: "plausible-test.io", user_id: user_id}
-          session_id = Plausible.Session.Store.on_event(event, previous_user_id)
-
-          Map.put(event, :session_id, session_id)
-          |> Plausible.Event.WriteBuffer.insert()
-        end
-        res
       else
         {:error, changeset}
       end
