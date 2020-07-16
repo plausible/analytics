91f5f054b2dfbba6f60bf51e77ec3c0987cef7d0
Uku Taht <uku.taht@gmail.com>
2020-07-16 14:49:10 +0300

    Better format for duration

--- assets/js/dashboard/number-formatter.js
+++ assets/js/dashboard/number-formatter.js
@@ -32,9 +32,9 @@ export function durationFormatter(duration) {
   const minutes = Math.floor(duration / 60) % 60
   const seconds = Math.floor(duration - (minutes * 60) - (hours * 60 * 60))
   if (hours > 0) {
-    return `${hours}h${minutes}m${seconds}s`
+    return `${hours}h ${minutes}m ${seconds}s`
   } else if (minutes > 0) {
-    return `${minutes}m${pad(seconds, 2)}s`
+    return `${minutes}m ${pad(seconds, 2)}s`
   } else {
     return `${seconds}s`
   }
