0b1c90a1928e380772411a3370f9082f7dfec028
Caleb Woodbine <calebwoodbine.public@gmail.com>
2020-10-10 16:40:24 +1300

    Update container image to run as non root

--- .gitlab/build-scripts/docker-entrypoint.sh
+++ .gitlab/build-scripts/docker-entrypoint.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 set -e
 
-chmod a+x /app/*.sh
-
 if [[ "$1" = 'run' ]]; then
-      exec gosu plausibleuser /app/bin/plausible start
+      /app/bin/plausible start
 
 elif [[ "$1" = 'db' ]]; then
-      exec gosu plausibleuser /app/"$2".sh
- else
-      exec "$@"
-
+      /app/"$2".sh
 fi
 
 exec "$@"
