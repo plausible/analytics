5e500fef59ae78d83f9a27100af5f25bfa02aa69
Uku Taht <uku.taht@gmail.com>
2020-10-14 15:59:19 +0300

    Revert "Update container image to run as non root (#352)"
    This reverts commit 59acd4c76d6ce77e00d3af46d4aea20c1d33586e.

--- .gitlab/build-scripts/docker-entrypoint.sh
+++ .gitlab/build-scripts/docker-entrypoint.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 set -e
 
+chmod a+x /app/*.sh
+
 if [[ "$1" = 'run' ]]; then
-      /app/bin/plausible start
+      exec gosu plausibleuser /app/bin/plausible start
 
 elif [[ "$1" = 'db' ]]; then
-      /app/"$2".sh
+      exec gosu plausibleuser /app/"$2".sh
+ else
+      exec "$@"
+
 fi
 
 exec "$@"
