b0cecd8065d23291cd67b4034d6ecc59928cb272
Uku Taht <uku.taht@gmail.com>
2020-10-30 15:58:17 +0200

    Precompute templates and allow aliases

--- lib/plausible_web/router.ex
+++ lib/plausible_web/router.ex
@@ -36,11 +36,7 @@ defmodule PlausibleWeb.Router do
     forward "/sent-emails", Bamboo.SentEmailViewerPlug
   end
 
-  get "/js/plausible.js", PlausibleWeb.TrackerController, :plausible
-  get "/js/plausible.hash.js", PlausibleWeb.TrackerController, :plausible_hash
-  get "/js/plausible.outbound-links.js", PlausibleWeb.TrackerController, :plausible_outbound_links
   get "/js/analytics.js", PlausibleWeb.TrackerController, :plausible
-  get "/js/p.js", PlausibleWeb.TrackerController, :p
 
   scope "/api/stats", PlausibleWeb.Api do
     pipe_through :stats_api
