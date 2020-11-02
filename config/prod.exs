6bfd12b5b760e63896235a40736461f34d4e9cbc
Oliver Kriska <oliver.kriska@payout.one>
2020-11-02 19:39:39 +0100

    Payout  - removed wrong releases import

--- config/prod.exs
+++ config/prod.exs
@@ -3,5 +3,3 @@ use Mix.Config
 # For the actual-production deployments we will use releases,
 # i.e., "releases.exs" is the _actual_ production config
 # see "releases.exs"
-
-import_config "releases.exs"
