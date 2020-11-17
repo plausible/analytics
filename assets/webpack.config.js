ebf40b475b8d705bec4cd35260b5dd9827551917
Uku Taht <uku.taht@gmail.com>
2020-11-17 14:01:49 +0200

    Update JS deps

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const glob = require('glob');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const DefinePlugin = require('webpack').DefinePlugin;
@@ -9,7 +9,7 @@ const DefinePlugin = require('webpack').DefinePlugin;
 module.exports = (env, options) => ({
   optimization: {
     minimizer: [
-      new UglifyJsPlugin({ cache: true, parallel: true, sourceMap: false }),
+      new TerserPlugin(),
       new OptimizeCSSAssetsPlugin({})
     ]
   },
