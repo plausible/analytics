00d39c5c13c899ee2d75c2a604c360f3edf3a8f0
Uku Taht <uku.taht@gmail.com>
2020-11-26 16:01:14 +0200

    Halt when redirecting to settings

--- lib/plausible_web/plugs/upgrade_billing_plug.ex
+++ lib/plausible_web/plugs/upgrade_billing_plug.ex
@@ -1,5 +1,6 @@
 defmodule PlausibleWeb.UpgradeBillingPlug do
   import Phoenix.Controller
+  import Plug.Conn
   use Plausible.Repo
 
   def init(options) do
@@ -12,6 +13,7 @@ defmodule PlausibleWeb.UpgradeBillingPlug do
     if user && Plausible.Billing.needs_to_upgrade?(conn.assigns[:current_user]) do
       conn
       |> redirect(to: "/settings")
+      |> halt
     else
       conn
     end
