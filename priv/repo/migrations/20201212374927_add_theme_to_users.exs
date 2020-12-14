b823d1f51646b727b9c42c478b17cc705a4b1dcc
Vignesh Joglekar <rexvigasaurus@gmail.com>
2020-12-14 07:17:09 +0000

    Fixes naming of migration and removes static alter

--- priv/repo/migrations/20201212374927_add_theme_to_users.exs
+++ priv/repo/migrations/20201212374927_add_theme_to_users.exs
@@ -1,12 +1,10 @@
-defmodule Plausible.Repo.Migrations.AddTrialExpiryToUsers do
+defmodule Plausible.Repo.Migrations.AddThemePrefToUsers do
   use Ecto.Migration
 
   def change do
     alter table(:users) do
       add :theme, :string
     end
-
-    execute("UPDATE users SET theme='system'")
 
     alter table(:users) do
       modify :theme, :string, null: false
