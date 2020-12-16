4e09b37e80c424c28f28ec3d97ae94bd04f644ac
Vignesh Joglekar <rexvigasaurus@gmail.com>
2020-12-16 09:37:59 +0000

    Formatting fixes

--- assets/js/dashboard/theme-consumer-hoc.js
+++ assets/js/dashboard/theme-consumer-hoc.js
@@ -2,15 +2,15 @@ import React from 'react';
 import { ThemeContext } from './theme-context'
 
 export const withThemeConsumer = (WrappedComponent) => {
-	return class CountriesWrapper extends React.Component {
-		render() {
-			return (
-				<ThemeContext.Consumer>
-					{theme => (
-						<WrappedComponent darkTheme={theme} {...this.props} />
-					)}
-				</ThemeContext.Consumer>
-			);
-		}
-	}
+  return class extends React.Component {
+    render() {
+      return (
+        <ThemeContext.Consumer>
+          {theme => (
+            <WrappedComponent darkTheme={theme} {...this.props} />
+          )}
+        </ThemeContext.Consumer>
+      );
+    }
+  }
 }
--- assets/js/dashboard/theme-provider-hoc.js
+++ assets/js/dashboard/theme-provider-hoc.js
@@ -2,36 +2,36 @@ import React from 'react';
 import { ThemeContext } from './theme-context'
 
 export const withThemeProvider = (WrappedComponent) => {
-	return class extends React.Component {
-		constructor(props) {
-			super(props)
-			this.state = {
-				dark: document.querySelector('html').classList.contains('dark') || false
-			};
+  return class extends React.Component {
+    constructor(props) {
+      super(props)
+      this.state = {
+        dark: document.querySelector('html').classList.contains('dark') || false
+      };
 
-			this.mutationObserver = new MutationObserver((mutationsList, observer) => {
-				mutationsList.forEach(mutation => {
-					if (mutation.attributeName === 'class') {
-						this.setState({ dark: mutation.target.classList.contains('dark') });
-					}
-				});
-			});
-		}
+      this.mutationObserver = new MutationObserver((mutationsList, observer) => {
+        mutationsList.forEach(mutation => {
+          if (mutation.attributeName === 'class') {
+            this.setState({ dark: mutation.target.classList.contains('dark') });
+          }
+        });
+      });
+    }
 
-		componentDidMount() {
-			this.mutationObserver.observe(document.querySelector('html'), { attributes: true });
-		}
+    componentDidMount() {
+      this.mutationObserver.observe(document.querySelector('html'), { attributes: true });
+    }
 
-		componentWillUnmount() {
-			this.mutationObserver.disconnect();
-		}
+    componentWillUnmount() {
+      this.mutationObserver.disconnect();
+    }
 
-		render() {
-			return (
-				<ThemeContext.Provider value={this.state.dark}>
-					<WrappedComponent {...this.props}/>
-				</ThemeContext.Provider>
-			);
-		}
-	}
+    render() {
+      return (
+        <ThemeContext.Provider value={this.state.dark}>
+          <WrappedComponent {...this.props}/>
+        </ThemeContext.Provider>
+      );
+    }
+  }
 }
