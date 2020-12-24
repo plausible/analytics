5c9f5acc19cbd31d9ae978212483716ecf41b972
Vignesh Joglekar <rexvigasaurus@gmail.com>
2020-12-24 01:43:32 -0600

    Refactors, comments, cleans up filters code

--- assets/js/dashboard/filters.js
+++ assets/js/dashboard/filters.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect, useLayoutEffect } from 'react';
 import { withRouter } from 'react-router-dom'
-import {countFilters, navigateToQuery, removeQueryParam} from './query'
+import { countFilters, navigateToQuery, removeQueryParam } from './query'
 import Datamap from 'datamaps'
 import Transition from "../transition.js";
 import { useScreenClass } from './screenclass-hook';
 
-function filterText(key, value, query) {
+const filterText = (key, value, query) => {
   if (key === "goal") {
     return <span className="inline-block max-w-2xs truncate">Completed goal <b>{value}</b></span>
   }
@@ -56,58 +56,77 @@ function filterText(key, value, query) {
   }
 }
 
-function renderFilter(history, [key, value], query, wrapped) {
-  function removeFilter() {
-    const newOpts = {
-      [key]: false
-    }
-    if (key === 'goal') { newOpts.props = false }
-    navigateToQuery(
-      history,
-      query,
-      newOpts
-    )
+const removeFilter = (key, history, query) => {
+  const newOpts = {
+    [key]: false
   }
+  if (key === 'goal') { newOpts.props = false }
+  navigateToQuery(
+    history,
+    query,
+    newOpts
+  )
+}
 
-  return wrapped ? (
-    <div className="px-4 py-2 text-sm leading-tight flex items-center justify-between" key={key+value}>
+const renderDropdownFilter = (history, [key, value], query) => {
+  return (
+    <div className="px-4 py-2 text-sm leading-tight flex items-center justify-between" key={key + value}>
       {filterText(key, value, query)}
-      <b className="ml-1 cursor-pointer hover:text-indigo-500" onClick={removeFilter}>✕</b>
+      <b className="ml-1 cursor-pointer hover:text-indigo-500" onClick={() => removeFilter(key, history, query)}>✕</b>
     </div>
-  ) : (
+  )
+}
+
+const renderListFilter = (history, [key, value], query) => {
+  return (
     <span key={key} title={value} className="inline-flex bg-white dark:bg-gray-800 text-gray-700 dark:text-gray-300 shadow text-sm rounded py-2 px-3 mr-2">
-      {filterText(key, value, query)} <b className="ml-1 cursor-pointer hover:text-indigo-500" onClick={removeFilter}>✕</b>
+      {filterText(key, value, query)} <b className="ml-1 cursor-pointer hover:text-indigo-500" onClick={() => removeFilter(key, history, query)}>✕</b>
     </span>
   )
 }
 
-function Filters({query, history, location}) {
-  const [open, setOpen] = useState(false);
-  const [wrapped, setWrapped] = useState(false);
-  const [recheck, setRecheck] = useState(false);
-  const screenClass = useScreenClass();
+const clearAllFilters = (history, query) => {
+  const newOpts = Object.keys(query.filters).reduce((acc, red) => ({ ...acc, [red]: false }), {});
+  navigateToQuery(
+    history,
+    query,
+    newOpts
+  );
+}
+
+const Filters = ({ query, history, location }) => {
   let dropDownNode;
   const appliedFilters = Object.keys(query.filters)
     .map((key) => [key, query.filters[key]])
     .filter(([key, value]) => !!value)
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [wrapped, setWrapped] = useState(false); // Filter wrap state (true=dropdown, false=list)
+  const [recheck, setRecheck] = useState(false); // If there is a pending rewrap check
+  const screenClass = useScreenClass();
 
+  // Dropdown auto-close listener
   useEffect(() => {
+    const handleClick = (e) => {
+      if (dropDownNode && dropDownNode.contains(e.target)) return;
+
+      setDropdownOpen(false)
+    };
+
     document.addEventListener('mousedown', handleClick, false);
     return () => {
       document.removeEventListener('mousedown', handleClick, false);
     }
   }, [])
 
+  // Checks if the filter container is wrapping items
   const rewrapFilters = () => {
-    // console.log('attempting');
-    if (wrapped) { return };
-    // console.log('rewrapping');
     let currItem, prevItem, items = document.getElementById('filters_row');
-    if (!items) { return };
 
-    if (appliedFilters.length === 1) { return };
-    // console.log('checking');
+    if (wrapped) { return }; // Don't rewrap if we're already wrapped
+    if (!items) { return }; // Don't rewrap if there are no filters
+    if (appliedFilters.length === 1) { return }; // Don't rewrap if there is only one filter
 
+    // For every filter DOM Node, check if its y value is higher than the previous (this indicates a wrap)
     [...(items.childNodes)].forEach(item => {
       currItem = item.getBoundingClientRect();
       if (prevItem && prevItem.top < currItem.top) {
@@ -117,68 +136,25 @@ function Filters({query, history, location}) {
     });
   };
 
+  // On query change or viewport resize, force a check for if the filters can fit
   useLayoutEffect(() => {
     setWrapped(false);
     setRecheck(true);
   }, [JSON.stringify(query), screenClass]);
 
+  // When a refresh is forced, wait for state changes to propogate, then check if a wrap is needed
   useLayoutEffect(() => {
     setRecheck(false);
     rewrapFilters();
   }, [wrapped, recheck]);
 
-  const handleClick = (e) => {
-    if (dropDownNode && dropDownNode.contains(e.target)) return;
-
-    setOpen(false)
-  }
-
-  const renderDropDown = () => {
+  const renderDropDownContent = () => {
     return (
-      <div id="filters_row">
-        { wrapped || screenClass === 'sm' ? (
-          <div className="relative" style={{ height: '35.5px', width: '160px' }}>
-            <div onClick={() => setOpen(!open)} className="flex items-center justify-between rounded bg-white dark:bg-gray-800 shadow px-4 pr-3 py-2 leading-tight cursor-pointer text-sm font-medium text-gray-800 dark:text-gray-200 h-full">
-              <span className="mr-2">Active Filters</span>
-              <svg className="text-pink-500 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                <polyline points="6 9 12 15 18 9"></polyline>
-              </svg>
-            </div>
-            <Transition
-              show={open}
-              enter="transition ease-out duration-100 transform"
-              enterFrom="opacity-0 scale-95"
-              enterTo="opacity-100 scale-100"
-              leave="transition ease-in duration-75 transform"
-              leaveFrom="opacity-100 scale-100"
-              leaveTo="opacity-0 scale-95"
-            >
-              {renderDropDownContent(wrapped || screenClass === 'sm')}
-            </Transition>
-          </div>) :
-          (appliedFilters.map((filter) => renderFilter(history, filter, query, wrapped || screenClass === 'sm')))
-        }
-      </div>
-    )
-  }
-
-  const clearFilters = () => {
-    const newOpts = Object.keys(query.filters).reduce((acc, red) => ({...acc, [red]: false}), {});
-    navigateToQuery(
-      history,
-      query,
-      newOpts
-    );
-  }
-
-  const renderDropDownContent = (wrapped) => {
-    return (
-      <div className="absolute mt-2 rounded shadow-md z-10" style={{width: '235px', right: '-14px'}} ref={node => dropDownNode = node}>
+      <div className="absolute mt-2 rounded shadow-md z-10" style={{ width: '235px', right: '-14px' }} ref={node => dropDownNode = node}>
         <div className="rounded bg-white dark:bg-gray-800 ring-1 ring-black ring-opacity-5 font-medium text-gray-800 dark:text-gray-200 flex flex-col">
-          { appliedFilters.map((filter) => renderFilter(history, filter, query, wrapped)) }
-
+          {appliedFilters.map((filter) => renderDropdownFilter(history, filter, query))}
           <div className="border-t border-gray-200 dark:border-gray-500" />
-          <div className="px-4 py-2 text-sm leading-tight hover:text-indigo-500 hover:cursor-pointer" onClick={clearFilters}>
+          <div className="px-4 py-2 text-sm leading-tight hover:text-indigo-500 hover:cursor-pointer" onClick={() => clearAllFilters(history, query)}>
             Clear All Filters
           </div>
         </div>
@@ -186,13 +162,49 @@ function Filters({query, history, location}) {
     )
   }
 
-  if (appliedFilters.length > 0) {
+  const renderDropDown = () => {
     return (
-      renderDropDown()
-    )
+      <div id="filters_row">
+        <div className="relative" style={{ height: '35.5px', width: '160px' }}>
+          <div onClick={() => setDropdownOpen(!dropdownOpen)} className="flex items-center justify-between rounded bg-white dark:bg-gray-800 shadow px-4 pr-3 py-2 leading-tight cursor-pointer text-sm font-medium text-gray-800 dark:text-gray-200 h-full">
+            <span className="mr-2">Active Filters</span>
+            <svg className="text-pink-500 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+              <polyline points="6 9 12 15 18 9"></polyline>
+            </svg>
+          </div>
+          <Transition
+            show={dropdownOpen}
+            enter="transition ease-out duration-100 transform"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="transition ease-in duration-75 transform"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
+          >
+            {renderDropDownContent()}
+          </Transition>
+        </div>
+      </div>
+    );
   }
 
-  return null
+  const renderFilterList = () => {
+    return (
+      <div id="filters_row">
+        {(appliedFilters.map((filter) => renderListFilter(history, filter, query)))}
+      </div>
+    );
+  }
+
+  if (appliedFilters.length > 0) {
+    if (wrapped || screenClass === 'sm') {
+      return renderDropDown();
+    }
+
+    return renderFilterList();
+  }
+
+  return null;
 }
 
-export default withRouter(Filters)
+export default withRouter(Filters);
