f9373e4d218d1a42d4b686d35caeabbbbc65285c
Uku Taht <uku.taht@gmail.com>
2020-12-29 10:22:12 +0200

    Convert datetime to date
    Fixes #518

--- lib/plausible/stats/query.ex
+++ lib/plausible/stats/query.ex
@@ -9,7 +9,7 @@ defmodule Plausible.Stats.Query do
   def shift_back(%__MODULE__{period: "month"} = query, site) do
     {new_first, new_last} = if Timex.compare(Timex.now(site.timezone), query.date_range.first, :month) == 0  do # Querying current month to date
       diff = Timex.diff(Timex.beginning_of_month(Timex.now(site.timezone)), Timex.now(site.timezone), :days) - 1
-      {query.date_range.first |> Timex.shift(days: diff), Timex.now(site.timezone) |> Timex.shift(days: diff)}
+      {query.date_range.first |> Timex.shift(days: diff), Timex.now(site.timezone) |> Timex.to_date |> Timex.shift(days: diff)}
     else
       diff = Timex.diff(query.date_range.first, query.date_range.last, :days) - 1
       {query.date_range.first |> Timex.shift(days: diff), query.date_range.last |> Timex.shift(days: diff)}
