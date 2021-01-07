f260f9269d9bd97ceb4c51d8fa17d9393a873b4a
Oliver Kriska <oliver.kriska@payout.one>
2021-01-07 11:02:43 +0100

    Dialyzer
     - fixed test env
     - renamed GitHub actions steps

--- mix.exs
+++ mix.exs
@@ -23,7 +23,7 @@ defmodule Plausible.MixProject do
       ],
       dialyzer: [
         plt_file: {:no_warn, "priv/plts/dialyzer.plt"},
-        plt_add_apps: [:mix]
+        plt_add_apps: [:mix, :ex_unit]
       ]
     ]
   end
