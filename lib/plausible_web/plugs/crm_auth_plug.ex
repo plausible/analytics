d1c2cc13ce08aa6cd1ec5d0ece85c98b9505d5d8
Oliver Kriska <oliver.kriska@payout.one>
2021-01-07 12:47:00 +0100

    Credo
     - fixed CRM plug

--- lib/plausible_web/plugs/crm_auth_plug.ex
+++ lib/plausible_web/plugs/crm_auth_plug.ex
@@ -1,7 +1,6 @@
 defmodule PlausibleWeb.CRMAuthPlug do
   import Plug.Conn
   use Plausible.Repo
-  @admin_emails Application.get_env(:plausible, :admin_emails)
 
   def init(options) do
     options
@@ -15,11 +14,13 @@ defmodule PlausibleWeb.CRMAuthPlug do
       id ->
         user = Repo.get_by(Plausible.Auth.User, id: id)
 
-        if user && user.email in @admin_emails do
+        if user && user.email in admin_emails() do
           assign(conn, :current_user, user)
         else
           conn |> send_resp(403, "Not allowed") |> halt
         end
     end
   end
+
+  defp admin_emails(), do: Application.get_env(:plausible, :admin_emails)
 end
