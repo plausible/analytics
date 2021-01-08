e692afc20e18846fab3b5d6b9c3bba2f6a16742b
Uku Taht <uku.taht@gmail.com>
2021-01-08 15:36:26 +0200

    Search sites by their members

--- lib/plausible/site/admin.ex
+++ lib/plausible/site/admin.ex
@@ -1,6 +1,13 @@
 defmodule Plausible.SiteAdmin do
   use Plausible.Repo
 
+  def search_fields(_schema) do
+    [
+      :domain,
+      members: [:name, :email]
+    ]
+  end
+
   def custom_index_query(_conn, _schema, query) do
     from(r in query, preload: [:members])
   end
@@ -18,7 +25,7 @@ defmodule Plausible.SiteAdmin do
       domain: nil,
       timezone: nil,
       public: nil,
-      members: %{value: fn s -> Enum.map(s.members, & &1.email) end}
+      members: %{value: fn s -> Enum.map(s.members, & &1.email) |> Enum.join(", ") end}
     ]
   end
 end
