0b90bc31263764b7d97f32e10de765c73fd4c7a6
Uku Taht <uku.taht@gmail.com>
2021-01-14 15:33:59 +0200

    Enfore BASE_URL

--- config/runtime.exs
+++ config/runtime.exs
@@ -6,11 +6,21 @@ end
 
 port = System.get_env("PORT") || 8000
 
-base_url =
-  System.get_env("BASE_URL", "http://localhost:8000")
-  |> URI.parse()
+base_url = System.get_env("BASE_URL")
 
-secret_key_base = System.get_env("SECRET_KEY_BASE")
+if !base_url do
+  raise "BASE_URL configuration option is required. See https://plausible.io/docs/self-hosting-configuration#server"
+end
+
+base_url = URI.parse(base_url)
+
+if base_url.scheme not in ["http", "https"] do
+  raise "BASE_URL must start with `http` or `https`. Currently configured as `#{
+          System.get_env("BASE_URL")
+        }`"
+end
+
+secret_key_base = System.fetch_env!("SECRET_KEY_BASE")
 
 db_url =
   System.get_env(
