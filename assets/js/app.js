e8cb4ddefb0d7ad603932754a7becbdd881e37af
Uku Taht <uku.taht@gmail.com>
2021-03-16 15:06:58 +0200

    Fix syntax error in JS

--- assets/js/app.js
+++ assets/js/app.js
@@ -108,10 +108,11 @@ if (embedButton) {
         embedLink.searchParams.set('background', background)
       }
 
-      embedCode.value = `<iframe plausible-embed ${backgroundAttr} src="${embedLink.toString()}" scrolling="no" frameborder="0" loading="lazy" style="width: 1px; min-width: 100%; height: 1600px;"></iframe>
+      embedCode.value = `<iframe plausible-embed src="${embedLink.toString()}" scrolling="no" frameborder="0" loading="lazy" style="width: 1px; min-width: 100%; height: 1600px;"></iframe>
 <div style="font-size: 14px; padding-bottom: 14px;">Stats powered by <a target="_blank" style="color: #4F46E5; text-decoration: underline;" href="https://plausible.io">Plausible Analytics</a></div>
 <script async src="${baseUrl}/js/embed.host.js"></script>`
     } catch (e) {
+      console.error(e)
       embedCode.value = 'ERROR: Please enter a valid URL in the shared link field'
     }
   })
