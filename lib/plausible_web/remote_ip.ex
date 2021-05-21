2cf0c1a4561212134cfe8a6dd24e4f55db7a87f1
Uku Taht <uku.taht@gmail.com>
2021-05-21 14:57:00 +0300

    Use `forwarded` header when X-Forwarded-For is missing

--- lib/plausible_web/remote_ip.ex
+++ lib/plausible_web/remote_ip.ex
@@ -2,6 +2,7 @@ defmodule PlausibleWeb.RemoteIp do
   def get(conn) do
     cf_connecting_ip = List.first(Plug.Conn.get_req_header(conn, "cf-connecting-ip"))
     forwarded_for = List.first(Plug.Conn.get_req_header(conn, "x-forwarded-for"))
+    forwarded = List.first(Plug.Conn.get_req_header(conn, "forwarded"))
 
     cond do
       cf_connecting_ip ->
@@ -11,6 +12,14 @@ defmodule PlausibleWeb.RemoteIp do
         String.split(forwarded_for, ",")
         |> Enum.map(&String.trim/1)
         |> List.first()
+
+      forwarded ->
+        Regex.named_captures(~r/for=(?<for>[^;,]+).*$/, forwarded)
+        |> Map.get("for")
+        # IPv6 addresses are enclosed in quote marks and square brackets: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Forwarded
+        |> String.trim("\"")
+        |> String.trim_leading("[")
+        |> String.trim_trailing("]")
 
       true ->
         to_string(:inet_parse.ntoa(conn.remote_ip))
