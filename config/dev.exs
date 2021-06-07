97b4b477554fa3202aa7e7be674585a4ebfd500c
Uku Taht <uku.taht@gmail.com>
2021-06-07 11:49:43 +0300

    Update development command

--- config/dev.exs
+++ config/dev.exs
@@ -14,7 +14,8 @@ config :plausible, PlausibleWeb.Endpoint,
       "node_modules/webpack/bin/webpack.js",
       "--mode",
       "development",
-      "--watch-stdin",
+      "--watch",
+      "--watch-options-stdin",
       cd: Path.expand("../assets", __DIR__)
     ]
   ],
