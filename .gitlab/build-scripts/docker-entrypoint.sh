a61e661938b15709c8d14562ac70dc979a055f49
Niklas Hambüchen <mail@nh2.me>
2021-07-13 03:54:16 +0200

    Fix shell scripts.
    There were various mistakes in the shell scripts, such as lack of
    quoting; they would break them immediately e.g. if `BIN_DIR` contained a space.
    
    Pointed out by `shellcheck`.

--- .gitlab/build-scripts/docker-entrypoint.sh
+++ .gitlab/build-scripts/docker-entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 set -e
 
-if [[ "$1" = 'run' ]]; then
+if [ "$1" = 'run' ]; then
       exec /app/bin/plausible start
 
-elif [[ "$1" = 'db' ]]; then
+elif [ "$1" = 'db' ]; then
       exec /app/"$2".sh
  else
       exec "$@"
