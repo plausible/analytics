7c34a956ebeca6e00108053e352c76b299eb9cc1
Uku Taht <uku.taht@gmail.com>
2021-08-25 15:28:31 +0300

    Correct clickhouse migration

--- priv/clickhouse_repo/migrations/20210712214034_add_more_location_details.exs
+++ priv/clickhouse_repo/migrations/20210712214034_add_more_location_details.exs
@@ -3,17 +3,15 @@ defmodule Plausible.ClickhouseRepo.Migrations.AddMoreLocationDetails do
 
   def change do
     alter table(:events) do
-      add :country_geoname_id, :UInt32
-      add :subdivision1_geoname_id, :UInt32
-      add :subdivision2_geoname_id, :UInt32
-      add :city_geoname_id, :UInt32
+      add(:subdivision1_code, :"LowCardinality(String)")
+      add(:subdivision2_code, :"LowCardinality(String)")
+      add(:city_geoname_id, :UInt32)
     end
 
     alter table(:sessions) do
-      add :country_geoname_id, :UInt32
-      add :subdivision1_geoname_id, :UInt32
-      add :subdivision2_geoname_id, :UInt32
-      add :city_geoname_id, :UInt32
+      add(:subdivision1_code, :"LowCardinality(String)")
+      add(:subdivision2_code, :"LowCardinality(String)")
+      add(:city_geoname_id, :UInt32)
     end
   end
 end
