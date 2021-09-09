1bedfd3a933875c3734f0a9fab63111df5be0e67
feld <40271278+feld@users.noreply.github.com>
2021-09-09 02:33:29 -0500

    Update minimum elixir version to 1.11 (#1310)
    Required by the use of config_env() in config/config.exs

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Plausible.MixProject do
     [
       app: :plausible,
       version: System.get_env("APP_VERSION", "0.0.1"),
-      elixir: "~> 1.10",
+      elixir: "~> 1.11",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
