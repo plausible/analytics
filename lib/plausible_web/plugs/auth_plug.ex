a7fd23a8040165ce2723ad0cf8a667f5aeb44af1
Uku Taht <uku.taht@gmail.com>
2021-09-30 09:45:58 +0200

    Revert "Reduce database roundtrips on critical path for authorization"
    This reverts commit 71d1988ee8e023f54cfc4bbd5fdb7e576b4f269e.

--- lib/plausible_web/plugs/auth_plug.ex
+++ lib/plausible_web/plugs/auth_plug.ex
@@ -12,19 +12,12 @@ defmodule PlausibleWeb.AuthPlug do
         conn
 
       id ->
-        subscription_query =
-          from(s in Plausible.Billing.Subscription, order_by: [desc: s.inserted_at], limit: 1)
-
-        {user, subscription} =
-          Repo.one(
-            from u in Plausible.Auth.User,
-              left_join: s in subquery(subscription_query),
-              on: s.user_id == u.id,
-              where: u.id == ^id,
-              select: {u, s}
+        user =
+          Repo.get_by(Plausible.Auth.User, id: id)
+          |> Repo.preload(
+            subscription:
+              from(s in Plausible.Billing.Subscription, order_by: [desc: s.inserted_at])
           )
-
-        user = Map.put(user, :subscription, subscription)
 
         if user do
           Sentry.Context.set_user_context(%{id: user.id, name: user.name, email: user.email})
