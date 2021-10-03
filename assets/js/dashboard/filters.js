e3a342c5e4354dc4dc74889e526ab6a4c5c838b0
Vignesh Joglekar <rexvigasaurus@gmail.com>
2021-10-03 06:36:13 -0500

    Couple more dark mode issues

--- assets/js/dashboard/filters.js
+++ assets/js/dashboard/filters.js
@@ -112,7 +112,7 @@ function filterDropdownOption(site, option) {
         <Link
           to={{ pathname: `/${encodeURIComponent(site.domain)}/filter/${option}`, search: window.location.search }}
           className={classNames(
-            active ? 'bg-gray-100 text-gray-900' : 'text-gray-800 dark:text-gray-300',
+            active ? 'bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100' : 'text-gray-800 dark:text-gray-300',
             'block px-4 py-2 text-sm font-medium'
           )}
         >
