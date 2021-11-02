deb0ef46c384424eb7f3f90b1e354f06f02f11d3
mcol <mcol@posteo.net>
2021-11-02 11:14:48 +0000

    Use spinner to indicate when export has started

--- assets/js/dashboard/stats/visitor-graph.js
+++ assets/js/dashboard/stats/visitor-graph.js
@@ -101,6 +101,9 @@ class LineGraph extends React.Component {
     super(props);
     this.regenerateChart = this.regenerateChart.bind(this);
     this.updateWindowDimensions =  this.updateWindowDimensions.bind(this);
+    this.state = {
+      exported: false
+    };
   }
 
   regenerateChart() {
@@ -301,15 +304,33 @@ class LineGraph extends React.Component {
     return stats
   }
 
+  downloadSpinner() {
+    this.setState({exported: true});
+    setTimeout(
+      () => this.setState({exported: false}),
+      4000
+    );
+  }
+
   downloadLink() {
     if (this.props.query.period !== 'realtime') {
-      const endpoint = `/${encodeURIComponent(this.props.site.domain)}/export${api.serializeQuery(this.props.query)}`
 
-      return (
-        <a href={endpoint} download>
-          <svg className="absolute w-4 h-5 text-gray-700 feather dark:text-gray-300 -top-8 right-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path><polyline points="7 10 12 15 17 10"></polyline><line x1="12" y1="15" x2="12" y2="3"></line></svg>
-        </a>
-      )
+      if (this.state.exported) {
+        return (
+          <svg className="animate-spin h-4 w-4 text-indigo-500 absolute -top-8 right-8" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+            <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+            <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+          </svg>
+        )
+      } else {
+        const endpoint = `/${encodeURIComponent(this.props.site.domain)}/export${api.serializeQuery(this.props.query)}`
+
+        return (
+          <a href={endpoint} download onClick={this.downloadSpinner.bind(this)}>
+            <svg className="absolute w-4 h-5 text-gray-700 feather dark:text-gray-300 -top-8 right-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path><polyline points="7 10 12 15 17 10"></polyline><line x1="12" y1="15" x2="12" y2="3"></line></svg>
+          </a>
+        )
+      }
     }
   }
 
