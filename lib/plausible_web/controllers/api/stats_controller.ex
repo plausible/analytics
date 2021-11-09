0ada34d1982320d8d1db7854123f6ced960b1b18
mcol <mcol@posteo.net>
2021-11-09 16:33:22 +0000

    Export custom props of current goal when filtering dashboard for goal
    This makes the CSV export also output a `prop_breakdown.csv` file which,
    for the currently filtered goal, contains the conversion data for each
    of its configured properties.

--- lib/plausible_web/controllers/api/stats_controller.ex
+++ lib/plausible_web/controllers/api/stats_controller.ex
@@ -622,7 +622,38 @@ defmodule PlausibleWeb.Api.StatsController do
         )
       end)
 
-    json(conn, props)
+    if params["csv"] do
+      props
+    else
+      json(conn, props)
+    end
+  end
+
+  def all_props_breakdown(conn, params) do
+    site = conn.assigns[:site]
+    query = Query.from(site.timezone, params) |> Filters.add_prefix()
+
+    headers = ["prop", "name", "unique_conversions", "total_conversions"]
+
+    prop_names =
+      if query.filters["event:goal"] do
+        {_, _, goal} = query.filters["event:goal"]
+
+        Stats.props(site, query)
+        |> Map.get(goal)
+      else
+        []
+      end
+
+    values =
+      prop_names
+      |> Enum.map(fn prop ->
+        prop_breakdown(conn, Map.put(params, "prop_name", prop))
+        |> Enum.map(&Map.put(&1, "prop", prop))
+      end)
+      |> Enum.concat()
+
+    to_csv(values, headers)
   end
 
   def current_visitors(conn, _) do
