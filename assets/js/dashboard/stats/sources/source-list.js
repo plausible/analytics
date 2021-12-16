7ab3de9708059defe88d679a1101d86cfc776483
Uku Taht <uku.taht@gmail.com>
2021-12-16 15:22:08 +0200

    Rename 'UTM params' to 'Campaigns'

--- assets/js/dashboard/stats/sources/source-list.js
+++ assets/js/dashboard/stats/sources/source-list.js
@@ -294,7 +294,7 @@ export default class SourceList extends React.Component {
     const activeClass = 'inline-block h-5 text-indigo-700 dark:text-indigo-500 font-bold active-prop-heading truncate text-left'
     const defaultClass = 'hover:text-indigo-600 cursor-pointer truncate text-left'
     const dropdownOptions = ['utm_medium', 'utm_source', 'utm_campaign']
-    let buttonText = UTM_TAGS[this.state.tab] ? UTM_TAGS[this.state.tab].label : 'UTM Params'
+    let buttonText = UTM_TAGS[this.state.tab] ? UTM_TAGS[this.state.tab].label : 'Campaigns'
 
     return (
       <div className="flex text-xs font-medium text-gray-500 dark:text-gray-400 space-x-2">
@@ -303,7 +303,7 @@ export default class SourceList extends React.Component {
         <Menu as="div" className="relative inline-block text-left">
           <div>
             <Menu.Button className="inline-flex justify-between focus:outline-none">
-              <span style={{width: '4.6rem'}} className={this.state.tab.startsWith('utm_') ? activeClass : defaultClass}>{buttonText}</span>
+              <span style={{width: '4.2rem'}} className={this.state.tab.startsWith('utm_') ? activeClass : defaultClass}>{buttonText}</span>
               <ChevronDownIcon className="-mr-1 ml-px h-4 w-4" aria-hidden="true" />
             </Menu.Button>
           </div>
