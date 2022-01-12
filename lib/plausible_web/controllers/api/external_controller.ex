8673fc29e05c03e4f56354221b23676725153891
Marko Saric <34340819+metmarkosaric@users.noreply.github.com>
2022-01-12 18:34:29 +0100

    Couple of changes to the cities (#1599)
    * Couple of changes to the cities
    
    - list of city overrides now alphabetical
    - added new overrides for switzerland and canada
    
    * Changes for Bucharest

--- lib/plausible_web/controllers/api/external_controller.ex
+++ lib/plausible_web/controllers/api/external_controller.ex
@@ -222,28 +222,72 @@ defmodule PlausibleWeb.Api.ExternalController do
   end
 
   @city_overrides %{
-    # UK
-    # Shadwell -> London
-    6_690_595 => 2_643_743,
-    # City of London -> London
-    2_643_741 => 2_643_743,
-    # South Bank -> London
-    6_545_251 => 2_643_743,
-    # Soho -> London
-    6_545_173 => 2_643_743,
-    # Whitechapel -> London
-    2_634_112 => 2_643_743,
-    # King's Cross -> London
-    6_690_589 => 2_643_743,
-    # Poplar -> London
-    2_640_091 => 2_643_743,
-    # Hackney -> London
-    2_647_694 => 2_643_743,
-
-    # India
-    # Navi Mumbai -> Mumbai
-    6_619_347 => 1_275_339,
-
+    # Austria
+    # Gemeindebezirk Floridsdorf -> Vienna
+    2_779_467 => 2_761_369,
+    # Gemeindebezirk Leopoldstadt -> Vienna
+    2_772_614 => 2_761_369,
+    # Gemeindebezirk Landstrasse -> Vienna
+    2_773_040 => 2_761_369,
+    # Urfahr -> Linz
+    2_762_518 => 2_772_400,
+    
+    # Canada
+    # Old Toronto -> Toronto
+    8_436_019 => 6_167_865,
+    # Etobicoke -> Toronto
+    5_950_267 => 6_167_865,
+    # East York -> Toronto
+    5_946_235 => 6_167_865,
+    # Scarborough -> Toronto
+    6_948_711 => 6_167_865,
+    # North York -> Toronto
+    6_091_104 => 6_167_865,
+    
+    # Czech republic
+    # Praha 5 -> Prague
+    11_951_220 => 3_067_696,
+    # Praha 4 -> Prague
+    11_951_218 => 3_067_696,
+    # Praha 11 -> Prague
+    11_951_232 => 3_067_696,
+    # Praha 10 -> Prague
+    11_951_210 => 3_067_696,
+    # Praha 4 -> Prague
+    8_378_772 => 3_067_696,
+    
+    # Denmark
+    # København SV -> Copenhagen
+    11_747_123 => 2_618_425,
+    # København NV -> Copenhagen
+    11_746_894 => 2_618_425,
+    # Odense S -> Odense
+    11_746_825 => 2_615_876,
+    # Odense M -> Odense
+    11_746_974 => 2_615_876,
+    # Odense SØ -> Odense
+    11_746_888 => 2_615_876,
+    # Aarhus C -> Aarhus
+    11_746_746 => 2_624_652,
+    # Aarhus N -> Aarhus
+    11_746_890 => 2_624_652,
+    
+    # Estonia
+    # Kristiine linnaosa -> Tallinn
+    11_050_530 => 588_409,
+    # Kesklinna linnaosa -> Tallinn
+    11_053_706 => 588_409,
+    # Lasnamäe linnaosa -> Tallinn
+    11_050_526 => 588_409,
+    # Põhja-Tallinna linnaosa -> Tallinn
+    11_049_594 => 588_409,
+    # Mustamäe linnaosa -> Tallinn
+    11_050_531 => 588_409,
+    # Haabersti linnaosa -> Tallinn
+    11_053_707 => 588_409,
+    # Viimsi -> Tallinn
+    587_629 => 588_409,
+    
     # Germany
     # Bezirk Tempelhof-Schöneberg -> Berlin
     3_336_297 => 2_950_159,
@@ -267,73 +311,11 @@ defmodule PlausibleWeb.Api.ExternalController do
     6_947_481 => 2_886_242,
     # Beuel -> Bonn
     2_949_619 => 2_946_447,
-
-    # Austria
-    # Gemeindebezirk Floridsdorf -> Vienna
-    2_779_467 => 2_761_369,
-    # Gemeindebezirk Leopoldstadt -> Vienna
-    2_772_614 => 2_761_369,
-    # Gemeindebezirk Landstrasse -> Vienna
-    2_773_040 => 2_761_369,
-    # Urfahr -> Linz
-    2_762_518 => 2_772_400,
-
-    # Netherlands
-    # Schiphol-Rijk -> Amsterdam
-    10_173_838 => 2_759_794,
-
-    # Czech republic
-    # Praha 5 -> Prague
-    11_951_220 => 3_067_696,
-    # Praha 4 -> Prague
-    11_951_218 => 3_067_696,
-    # Praha 11 -> Prague
-    11_951_232 => 3_067_696,
-    # Praha 10 -> Prague
-    11_951_210 => 3_067_696,
-    # Praha 4 -> Prague
-    8_378_772 => 3_067_696,
-
-    # Estonia
-    # Kristiine linnaosa -> Tallinn
-    11_050_530 => 588_409,
-    # Kesklinna linnaosa -> Tallinn
-    11_053_706 => 588_409,
-    # Lasnamäe linnaosa -> Tallinn
-    11_050_526 => 588_409,
-    # Põhja-Tallinna linnaosa -> Tallinn
-    11_049_594 => 588_409,
-    # Mustamäe linnaosa -> Tallinn
-    11_050_531 => 588_409,
-    # Haabersti linnaosa -> Tallinn
-    11_053_707 => 588_409,
-    # Viimsi -> Tallinn
-    587_629 => 588_409,
-
-    # Sweden
-    # Södermalm -> Stockholm
-    2_676_209 => 2_673_730,
-
-    # Slovakia
-    # Bratislava -> Bratislava
-    3_343_955 => 3_060_972,
-
-    # Denmark
-    # København SV -> Copenhagen
-    11_747_123 => 2_618_425,
-    # København NV -> Copenhagen
-    11_746_894 => 2_618_425,
-    # Odense S -> Odense
-    11_746_825 => 2_615_876,
-    # Odense M -> Odense
-    11_746_974 => 2_615_876,
-    # Odense SØ -> Odense
-    11_746_888 => 2_615_876,
-    # Aarhus C -> Aarhus
-    11_746_746 => 2_624_652,
-    # Aarhus N -> Aarhus
-    11_746_890 => 2_624_652,
-
+    
+    # India
+    # Navi Mumbai -> Mumbai
+    6_619_347 => 1_275_339,
+    
     # Mexico
     # Miguel Hidalgo Villa Olímpica -> Mexico city
     11_561_026 => 3_530_597,
@@ -344,7 +326,57 @@ defmodule PlausibleWeb.Api.ExternalController do
     #  Centro -> Mexico city
     9_179_691 => 3_530_597,
     #  Cuauhtémoc-> Mexico city
-    12_266_959 => 3_530_597
+    12_266_959 => 3_530_597,
+    
+    # Netherlands
+    # Schiphol-Rijk -> Amsterdam
+    10_173_838 => 2_759_794,
+    
+    # Romania
+    # Sector 1 -> Bucharest,
+    11_055_041 => 683_506,
+    # Sector 2 -> Bucharest
+    11_055_040 => 683_506,
+    # Sector 3 -> Bucharest
+    11_055_044 => 683_506,
+    # Sector 4 -> Bucharest
+    11_055_042 => 683_506,
+    # Sector 5 -> Bucharest
+    11_055_043    => 683_506,
+    # Sector 6 -> Bucharest
+    11_055_039 => 683_506,
+    # Bucuresti -> Bucharest
+    6_691_781 => 683_506,
+    
+    # Slovakia
+    # Bratislava -> Bratislava
+    3_343_955 => 3_060_972,
+    
+    # Sweden
+    # Södermalm -> Stockholm
+    2_676_209 => 2_673_730,
+    
+    # Switzerland
+    # Vorstädte -> Basel
+    11_789_440 => 2_661_604,
+    
+    # UK
+    # Shadwell -> London
+    6_690_595 => 2_643_743,
+    # City of London -> London
+    2_643_741 => 2_643_743,
+    # South Bank -> London
+    6_545_251 => 2_643_743,
+    # Soho -> London
+    6_545_173 => 2_643_743,
+    # Whitechapel -> London
+    2_634_112 => 2_643_743,
+    # King's Cross -> London
+    6_690_589 => 2_643_743,
+    # Poplar -> London
+    2_640_091 => 2_643_743,
+    # Hackney -> London
+    2_647_694 => 2_643_743
   }
 
   @decorate trace("ingest.geolocation")
