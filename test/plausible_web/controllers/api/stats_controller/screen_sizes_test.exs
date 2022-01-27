d369ba31bc7ab531115a66bc874bcda3475886ac
mcol <mcol@posteo.net>
2022-01-27 23:24:28 +0000

    Add imported data to devices tests

--- test/plausible_web/controllers/api/stats_controller/screen_sizes_test.exs
+++ test/plausible_web/controllers/api/stats_controller/screen_sizes_test.exs
@@ -20,6 +20,34 @@ defmodule PlausibleWeb.Api.StatsController.ScreenSizesTest do
              ]
     end
 
+    test "returns screen sizes by new visitors with imported data", %{conn: conn, site: site} do
+      populate_stats(site, [
+        build(:pageview, screen_size: "Desktop"),
+        build(:pageview, screen_size: "Desktop"),
+        build(:pageview, screen_size: "Laptop")
+      ])
+
+      populate_stats(site, [
+        build(:imported_devices, device: "Mobile"),
+        build(:imported_devices, device: "Laptop")
+      ])
+
+      conn = get(conn, "/api/stats/#{site.domain}/screen-sizes?period=day")
+
+      assert json_response(conn, 200) == [
+               %{"name" => "Desktop", "visitors" => 2, "percentage" => 67},
+               %{"name" => "Laptop", "visitors" => 1, "percentage" => 33}
+             ]
+
+      conn = get(conn, "/api/stats/#{site.domain}/screen-sizes?period=day&with_imported=true")
+
+      assert json_response(conn, 200) == [
+               %{"name" => "Desktop", "visitors" => 2, "percentage" => 40},
+               %{"name" => "Laptop", "visitors" => 2, "percentage" => 40},
+               %{"name" => "Mobile", "visitors" => 1, "percentage" => 20}
+             ]
+    end
+
     test "calculates conversion_rate when filtering for goal", %{conn: conn, site: site} do
       populate_stats(site, [
         build(:pageview, user_id: 1, screen_size: "Desktop"),
