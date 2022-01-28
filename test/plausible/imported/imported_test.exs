5ac5ba65637037874ad0278898fe258de7b104d5
mcol <mcol@posteo.net>
2022-01-28 01:05:30 +0000

    Add new test module for the data import step

---
+++ test/plausible/imported/imported_test.exs
@@ -0,0 +1,48 @@
+defmodule Plausible.ImportedTest do
+  use PlausibleWeb.ConnCase
+  use Timex
+  import Plausible.TestUtils
+
+  @utc Timezone.get("UTC")
+
+  describe "Parse and import third party data fetched from Google Analytics" do
+    setup [:create_user, :log_in, :create_new_site]
+
+    test "Visitors data imported from Google Analytics", %{conn: conn, site: site} do
+      populate_stats(site, [
+        build(:pageview, timestamp: ~N[2021-01-01 00:00:00]),
+        build(:pageview, timestamp: ~N[2021-01-31 00:00:00])
+      ])
+
+      assert {:ok, _} =
+               Plausible.Imported.from_google_analytics(
+                 [
+                   %{
+                     "dimensions" => ["2021010100"],
+                     "metrics" => [%{"values" => ["1", "1", "0", "1", "60"]}]
+                   },
+                   %{
+                     "dimensions" => ["2021013100"],
+                     "metrics" => [%{"values" => ["1", "1", "1", "1", "60"]}]
+                   }
+                 ],
+                 site.id,
+                 "visitors",
+                 @utc
+               )
+
+      conn =
+        get(
+          conn,
+          "/api/stats/#{site.domain}/main-graph?period=month&date=2021-01-01&with_imported=true"
+        )
+
+      assert %{"plot" => plot, "imported_source" => "Google Analytics"} = json_response(conn, 200)
+
+      assert Enum.count(plot) == 31
+      assert List.first(plot) == 2
+      assert List.last(plot) == 2
+      assert Enum.sum(plot) == 4
+    end
+  end
+end
