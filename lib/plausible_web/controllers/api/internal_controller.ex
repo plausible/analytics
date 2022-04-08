e855f6f30de4bdc21cbb0393447ea0fd590c06ec
andrea mazzarella <andrea.mazzarella89@gmail.com>
2022-04-08 17:03:24 +0100

    Update the internal /sites api to paginate results and adapts site-switcher to it

--- lib/plausible_web/controllers/api/internal_controller.ex
+++ lib/plausible_web/controllers/api/internal_controller.ex
@@ -2,6 +2,8 @@ defmodule PlausibleWeb.Api.InternalController do
   use PlausibleWeb, :controller
   use Plausible.Repo
   alias Plausible.Stats.Clickhouse, as: Stats
+  import PlausibleWeb.Api.Helpers
+  import Phoenix.Pagination.JSON
 
   def domain_status(conn, %{"domain" => domain}) do
     if Stats.has_pageviews?(%Plausible.Site{domain: domain}) do
@@ -11,14 +13,37 @@ defmodule PlausibleWeb.Api.InternalController do
     end
   end
 
-  def sites(conn, _) do
-    if conn.assigns[:current_user] do
-      user = Repo.preload(conn.assigns[:current_user], :sites)
-      json(conn, Enum.map(user.sites, & &1.domain))
+  def sites(conn, params) do
+    current_user = conn.assigns[:current_user]
+
+    if current_user do
+      sites =
+        sitesFor(current_user, params)
+        |> buildResponse(conn)
+      json(conn, sites)
     else
-      conn
-      |> put_status(401)
-      |> json(%{error: "You need to be logged in to request a list of sites"})
+      unauthorized(conn, "You need to be logged in to request a list of sites")
     end
   end
+
+  defp sitesFor(user, params) do
+    Repo.paginate(
+      from(
+        s in Plausible.Site,
+        join: sm in Plausible.Site.Membership,
+        on: sm.site_id == s.id,
+        where: sm.user_id == ^user.id,
+        order_by: s.domain
+      ),
+      params
+    )
+  end
+
+  defp buildResponse({sites, pagination}, conn) do
+    %{
+      data: Enum.map(sites, &%{domain: &1.domain}),
+      pagination: paginate(conn, pagination)
+    }
+  end
+
 end
