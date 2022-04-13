d28b973f65b2f16c6603fcda6003b134acd1a2c9
Vignesh Joglekar <rexvigasaurus@gmail.com>
2022-04-12 21:29:12 -0500

    Fixes another tooltip visibility edge case + cleans up boolean algebra

--- assets/js/dashboard/stats/graph/visitor-graph.js
+++ assets/js/dashboard/stats/graph/visitor-graph.js
@@ -152,15 +152,17 @@ class LineGraph extends React.Component {
       this.chart.update();
 
       if (tooltip) {
-        tooltip.style.opacity = 0;
+        tooltip.style.display = 'none';
       }
     }
 
-    if (this.chart && ((metric && !graphData) || !metric)) {
-      this.chart.destroy();
+    if (!graphData || !metric) {
+      if (this.chart) {
+        this.chart.destroy();
+      }
 
       if (tooltip) {
-        tooltip.style.opacity = 0;
+        tooltip.style.display = 'none';
       }
     }
   }
@@ -170,6 +172,7 @@ class LineGraph extends React.Component {
     const tooltip = document.getElementById('chartjs-tooltip');
     if (tooltip) {
       tooltip.style.opacity = 0;
+      tooltip.style.display = 'none';
     }
     window.removeEventListener('mousemove', this.repositionTooltip)
   }
