71fd276d6c1e7f7be63af17df03abf9c05165cad
Uku Taht <uku.taht@gmail.com>
2022-05-03 15:23:32 +0300

    Mkdir priv stats

--- rel/prepare_release.sh
+++ rel/prepare_release.sh
@@ -10,19 +10,10 @@ fi
 
 if [ "$GITHUB_WORKSPACE" != "" ]
 then
-  TARGET=$GITHUB_WORKSPACE/priv/static/version.json
-
-  echo "===================="
-  echo "> ls GITHUB_WORKSPACE"
-  ls $GITHUB_WORKSPACE
-  echo "> ls GITHUB_WORKSPACE/priv"
-  ls $GITHUB_WORKSPACE/priv
-  echo "> ls GITHUB_WORKSPACE/priv/static"
-  ls $GITHUB_WORKSPACE/priv/static
-  echo "===================="
+  TARGET_FOLDER=$GITHUB_WORKSPACE/priv/static
 else
-  TARGET=$(pwd)/priv/static/version.json
+  TARGET_FOLDER=$(pwd)/priv/static
 fi
 
-echo $TARGET
-echo "{\"version\": \"$VERSION\", \"commit\": \"$COMMIT\"}" > $TARGET
+mkdir -p $TARGET_FOLDER
+echo "{\"version\": \"$VERSION\", \"commit\": \"$COMMIT\"}" > $TARGET_FOLDER/version.json
