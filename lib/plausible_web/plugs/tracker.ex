439697f6cfe763de425fe84eaf617f1eb318bccc
Robert Joonas <robertjoonas16@gmail.com>
2022-05-20 11:05:10 +0300

    reorder extensions in filename when serving the plausible script

--- lib/plausible_web/plugs/tracker.ex
+++ lib/plausible_web/plugs/tracker.ex
@@ -13,8 +13,6 @@ defmodule PlausibleWeb.Tracker do
     "dimensions"
   ]
 
-  base_filenames = ["plausible", "script"]
-
   # Generates Power Set of all variants
   variants =
     1..Enum.count(base_variants)
@@ -28,43 +26,29 @@ defmodule PlausibleWeb.Tracker do
   files_available =
     ["plausible.js", "p.js"] ++ Enum.map(variants, fn v -> "plausible.#{v}.js" end)
 
-  # Computes permutations for every power set elements, formats them as alias filenames
-  aliases_available =
-    Enum.map(variants, fn x ->
-      variants =
-        String.split(x, ".")
-        |> Combination.permutate()
-        |> Enum.map(fn p -> Enum.join(p, ".") end)
-        |> Enum.map(fn v -> Enum.map(base_filenames, fn filename -> "#{filename}.#{v}.js" end) end)
-        |> List.flatten()
-
-      if Enum.count(variants) > 0 do
-        {"plausible.#{x}.js", variants}
-      end
-    end)
-    |> Enum.reject(fn x -> x == nil end)
-    |> Enum.into(%{})
-    |> Map.put("plausible.js", ["analytics.js", "script.js"])
-
-  @templates files_available
-  @aliases aliases_available
+  @script_aliases ["plausible.js", "script.js", "analytics.js"]
+  @base_filenames ["plausible", "script"]
+  @files_available files_available
 
   def init(_) do
-    all_files =
-      Enum.reduce(@templates, %{}, fn template_filename, all_files ->
-        aliases = Map.get(@aliases, template_filename, [])
-
-        [template_filename | aliases]
-        |> Enum.map(fn filename -> {"/js/" <> filename, template_filename} end)
-        |> Enum.into(%{})
-        |> Map.merge(all_files)
-      end)
-
-    [files: all_files]
+    [files: @files_available]
   end
 
   def call(conn, files: files) do
-    case files[conn.request_path] do
+    filename = case conn.request_path do
+      "/js/p.js" ->
+        "p.js"
+
+      "/js/" <> script_alias when script_alias in @script_aliases ->
+        "plausible.js"
+
+      "/js/" <> requested_filename ->
+        sorted_script_variant(requested_filename)
+
+      _ -> nil
+    end
+
+    case filename && Enum.find(files, &(&1 == filename)) do
       nil ->
         conn
 
@@ -79,6 +63,21 @@ defmodule PlausibleWeb.Tracker do
         |> put_resp_header("cache-control", "public, max-age=86400, must-revalidate")
         |> send_file(200, location)
         |> halt()
+    end
+  end
+
+  defp sorted_script_variant(requested_filename) do
+    case String.split(requested_filename, ".") do
+      [base_filename | rest] when base_filename in @base_filenames ->
+        sorted_variants =
+          rest
+          |> List.delete("js")
+          |> Enum.sort()
+          |> Enum.join(".")
+
+        "plausible.#{sorted_variants}.js"
+
+      _ -> nil
     end
   end
 end
