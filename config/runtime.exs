2b8e3ea62af5972d5e2a8e5b301eef438fdae922
Uku Taht <Uku.taht@gmail.com>
2022-07-08 11:14:52 +0300

    Use finch in sentry client (#1996)
    * Introduce Finch for Sentry integration
    
    * Make sure the DummyAgent can be started
    
    * No need to sanitize the dsn, finch takes care of that
    
    * Simplify the dummy child spec
    
    * Annotate redirects clause
    
    * Make use of new `get_int_from_path_or_env`
    
    * Actually use finch in Sentry config
    
    * Configure `excluded_domains` correctly for Sentry
    
    The way sentry is configured currently, when we get an HTTP error it
    will be logged twice - once from Sentry.PlugCapture and once from
    Sentry.LoggerBackend. The logger backend module does the right thing
    by default but for some reason we've been overriding the config
    parameter that by default stops double-counting errors. This commit
    returns to the default configuration which is better.
    
    * Default to 15s timeout
    
    * Attempt to send twice at most
    
    * Warn in sentry client
    
    * Use warn level in sentry client
    
    Co-authored-by: Adam Rutkowski <hq@mtod.org>

--- config/runtime.exs
+++ config/runtime.exs
@@ -218,7 +218,7 @@ config :fun_with_flags, :persistence,
   adapter: FunWithFlags.Store.Persistent.Ecto,
   repo: Plausible.Repo
 
-included_environments = if sentry_dsn, do: ["prod", "staging"], else: []
+included_environments = if sentry_dsn, do: ["prod", "staging", "dev"], else: []
 
 config :sentry,
   dsn: sentry_dsn,
@@ -228,13 +228,12 @@ config :sentry,
   tags: %{app_version: app_version},
   enable_source_code_context: true,
   root_source_code_path: [File.cwd!()],
-  hackney_pool_max_connections: get_int_from_path_or_env(config_dir, "SENTRY_POOL_SIZE", 50),
-  sample_rate: 1.0
+  client: Plausible.Sentry.Client,
+  send_max_attempts: 2
 
 config :logger, Sentry.LoggerBackend,
   capture_log_messages: true,
-  level: :error,
-  excluded_domains: []
+  level: :error
 
 config :plausible, :paddle,
   vendor_auth_code: paddle_auth_code,
@@ -366,6 +365,18 @@ end
 config :plausible, :hcaptcha,
   sitekey: hcaptcha_sitekey,
   secret: hcaptcha_secret
+
+config :plausible, Plausible.Finch,
+  default_pool_size: get_int_from_path_or_env(config_dir, "DEFAULT_FINCH_POOL_SIZE", 50),
+  default_pool_count: get_int_from_path_or_env(config_dir, "DEFAULT_FINCH_POOL_COUNT", 1),
+  sentry_pool_size: get_int_from_path_or_env(config_dir, "SENTRY_FINCH_POOL_SIZE", 50),
+  sentry_pool_count: get_int_from_path_or_env(config_dir, "SENTRY_FINCH_POOL_COUNT", 1)
+
+config :plausible, Plausible.Sentry.Client,
+  finch_request_opts: [
+    pool_timeout: get_int_from_path_or_env(config_dir, "SENTRY_FINCH_POOL_TIMEOUT", 5000),
+    receive_timeout: get_int_from_path_or_env(config_dir, "SENTRY_FINCH_RECEIVE_TIMEOUT", 15000)
+  ]
 
 config :ref_inspector,
   init: {Plausible.Release, :configure_ref_inspector}
