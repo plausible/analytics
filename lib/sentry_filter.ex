4a33cd0a259e5632fe049e4d2bcbfca85ca5639d
Uku Taht <uku.taht@gmail.com>
2022-07-14 03:26:59 +0300

    Delegate to default Sentry filter

--- lib/sentry_filter.ex
+++ lib/sentry_filter.ex
@@ -2,5 +2,8 @@ defmodule Plausible.SentryFilter do
   @behaviour Sentry.EventFilter
 
   def exclude_exception?(%Sentry.CrashError{}, _source), do: true
-  def exclude_exception?(exception, source), do: false
+
+  def exclude_exception?(exception, source) do
+    Sentry.DefaultEventFilter.exclude_exception?(exception, source)
+  end
 end
