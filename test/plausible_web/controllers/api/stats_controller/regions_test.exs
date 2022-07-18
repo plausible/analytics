8f9f7c597a27b3a0c1dfb57dfe79fd67c12d4916
Vinicius Brasil <vini@hey.com>
2022-07-18 12:54:51 -0300

    Keep user filter when listing cities, countries, and regions stats
    This commit fixes a bug where location filters were filtering stats but
    not the locations list. This was caused by a `Map.put/3` call that
    overrides the user filter. This commit rollbacks 5b571432737fb121e1b2a6c9e977500cb70f8880
    changes and removes the `Map.put/3` call.
    
    Closes #1982

---
+++ test/plausible_web/controllers/api/stats_controller/regions_test.exs
@@ -0,0 +1,36 @@
+defmodule PlausibleWeb.Api.StatsController.RegionsTest do
+  use PlausibleWeb.ConnCase
+  import Plausible.TestUtils
+
+  describe "GET /api/stats/:domain/regions" do
+    defp seed(%{site: site}) do
+      populate_stats(site, [
+        build(:pageview, country_code: "EE", subdivision1_code: "EE-37", city_geoname_id: 588_409),
+        build(:pageview, country_code: "EE", subdivision1_code: "EE-37", city_geoname_id: 588_409),
+        build(:pageview, country_code: "EE", subdivision1_code: "EE-37", city_geoname_id: 588_409),
+        build(:pageview, country_code: "EE", subdivision1_code: "EE-39", city_geoname_id: 591_632),
+        build(:pageview, country_code: "EE", subdivision1_code: "EE-39", city_geoname_id: 591_632)
+      ])
+    end
+
+    setup [:create_user, :log_in, :create_new_site, :add_imported_data, :seed]
+
+    test "returns top cities by new visitors", %{conn: conn, site: site} do
+      conn = get(conn, "/api/stats/#{site.domain}/regions?period=day")
+
+      assert json_response(conn, 200) == [
+               %{"code" => "EE-37", "country_flag" => "🇪🇪", "name" => "Harjumaa", "visitors" => 3},
+               %{"code" => "EE-39", "country_flag" => "🇪🇪", "name" => "Hiiumaa", "visitors" => 2}
+             ]
+    end
+
+    test "when list is filtered returns one city only", %{conn: conn, site: site} do
+      filters = Jason.encode!(%{region: "EE-39"})
+      conn = get(conn, "/api/stats/#{site.domain}/regions?period=day&filters=#{filters}")
+
+      assert json_response(conn, 200) == [
+               %{"code" => "EE-39", "country_flag" => "🇪🇪", "name" => "Hiiumaa", "visitors" => 2}
+             ]
+    end
+  end
+end
