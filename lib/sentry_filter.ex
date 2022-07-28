ca15b605b7f00ea2f4815d46a48100bf6641111e
Manu S Ajith <neo@codingarena.in>
2022-07-28 07:17:47 -0500

    Filter out noisy exception from Sentry (#2058)
    Signed-off-by: Manu S Ajith <neo@codingarena.in>

--- lib/sentry_filter.ex
+++ lib/sentry_filter.ex
@@ -2,6 +2,7 @@ defmodule Plausible.SentryFilter do
   @behaviour Sentry.EventFilter
 
   def exclude_exception?(%Sentry.CrashError{}, _source), do: true
+  def exclude_exception?(%Phoenix.NotAcceptableError{}, _), do: true
 
   def exclude_exception?(exception, source) do
     Sentry.DefaultEventFilter.exclude_exception?(exception, source)
