5eff70efa7eb1a188492301d6b1d770c6e490294
Vinicius Brasil <vini@hey.com>
2022-08-16 08:20:44 -0300

    Remove stash options from ingestion

--- test/plausible/ingestion/event_test.exs
+++ test/plausible/ingestion/event_test.exs
@@ -35,7 +35,7 @@ defmodule Plausible.Ingestion.EventTest do
   }
 
   test "build_and_buffer/3 creates an event" do
-    assert {:ok, _stash} =
+    assert :ok ==
              @valid_request
              |> Map.put(:domain, "plausible-ingestion-event-basic.test")
              |> Plausible.Ingestion.Event.build_and_buffer()
@@ -70,41 +70,5 @@ defmodule Plausible.Ingestion.EventTest do
 
     assert is_integer(session_id)
     assert is_integer(user_id)
-  end
-
-  test "build_and_buffer/3 stashes user_agent and geolocation" do
-    assert {:ok, stash} =
-             @valid_request
-             |> Map.put(:domain, "plausible-ingestion-event-stash-1.test")
-             |> Plausible.Ingestion.Event.build_and_buffer([:user_agent, :geolocation])
-
-    assert %Plausible.ClickhouseEvent{
-             browser: "Safari",
-             browser_version: "",
-             operating_system: "iOS",
-             operating_system_version: "3.2",
-             subdivision1_code: "FR-IDF",
-             subdivision2_code: "FR-75",
-             city_geoname_id: 2_988_507,
-             country_code: "FR"
-           } = get_event("plausible-ingestion-event-stash-1.test")
-
-    assert {:ok, _stash} =
-             @valid_request
-             |> Map.put(:domain, "plausible-ingestion-event-stash-2.test")
-             |> Map.put(:user_agent, "Dummy UA")
-             |> Map.put(:remote_ip, "127.0.0.1")
-             |> Plausible.Ingestion.Event.build_and_buffer([:user_agent, :geolocation], stash)
-
-    assert %Plausible.ClickhouseEvent{
-             browser: "Safari",
-             browser_version: "",
-             operating_system: "iOS",
-             operating_system_version: "3.2",
-             subdivision1_code: "FR-IDF",
-             subdivision2_code: "FR-75",
-             city_geoname_id: 2_988_507,
-             country_code: "FR"
-           } = get_event("plausible-ingestion-event-stash-2.test")
   end
 end
