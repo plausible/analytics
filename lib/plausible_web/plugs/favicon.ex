57c758cff9e4a187f813b50f5c140a35d9612d5a
Uku Taht <uku.taht@gmail.com>
2022-08-16 14:31:01 +0300

    Fix favicon HTTP response handling

--- lib/plausible_web/plugs/favicon.ex
+++ lib/plausible_web/plugs/favicon.ex
@@ -20,19 +20,27 @@ defmodule PlausibleWeb.Favicon do
       ["favicon", "sources", source] ->
         clean_source = URI.decode_www_form(source)
         domain = Map.get(favicon_domains, clean_source, clean_source)
-        res = HTTPClient.get("https://icons.duckduckgo.com/ip3/#{domain}.ico")
 
-        conn =
-          Enum.filter(res.headers, fn {key, _val} -> key != "Transfer-Encoding" end)
-          |> Enum.reduce(conn, fn {key, val}, conn ->
-            put_resp_header(conn, key, val)
-          end)
+        case HTTPClient.get("https://icons.duckduckgo.com/ip3/#{domain}") do
+          {:ok, res} ->
+            send_response(conn, res)
 
-        send_resp(conn, 200, res.body)
-        |> halt
+          e ->
+            send_resp(conn, 503, "") |> halt
+        end
 
       _ ->
         conn
     end
+  end
+
+  defp send_response(conn, res) do
+    conn =
+      Enum.filter(res.headers, fn {key, _val} -> key != "Transfer-Encoding" end)
+      |> Enum.reduce(conn, fn {key, val}, conn ->
+        put_resp_header(conn, key, val)
+      end)
+
+    send_resp(conn, 200, res.body) |> halt
   end
 end
