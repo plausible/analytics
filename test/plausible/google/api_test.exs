bc68235d0479a57b7d2d6bc15c14380fe642ff75
Vinicius Brasil <vini@hey.com>
2022-08-19 07:14:49 -0300

    Add fallback for GA reports without rows (#2121)

--- test/plausible/google/api_test.exs
+++ test/plausible/google/api_test.exs
@@ -84,6 +84,33 @@ defmodule Plausible.Google.ApiTest do
       assert_receive({Finch, :request, [_, _]})
       assert_receive({Finch, :request, [_, _]})
     end
+
+    test "does not fail when report does not have rows key", %{site: site, buffer: buffer} do
+      finch_double =
+        Finch
+        |> stub(:request, fn _, _ ->
+          {:ok,
+           %Finch.Response{status: 200, body: File.read!("fixture/ga_report_empty_rows.json")}}
+        end)
+
+      request = %Plausible.Google.ReportRequest{
+        dataset: "imported_exit_pages",
+        view_id: "123",
+        date_range: Date.range(~D[2022-01-01], ~D[2022-02-01]),
+        dimensions: ["ga:date", "ga:exitPagePath"],
+        metrics: ["ga:users", "ga:exits"],
+        access_token: "fake-token",
+        page_token: nil,
+        page_size: 10_000
+      }
+
+      assert :ok ==
+               Api.fetch_and_persist(site, request,
+                 http_client: finch_double,
+                 sleep_time: 0,
+                 buffer: buffer
+               )
+    end
   end
 
   describe "fetch_stats/3" do
