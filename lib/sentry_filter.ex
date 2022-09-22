3f7c1ce549d955741d6fabe6b7401c8a55e462b6
Adam Rutkowski <hq@mtod.org>
2022-09-22 17:24:54 +0200

    Aggregate DBConnection.ConnectionError in Sentry (#2260)

--- lib/sentry_filter.ex
+++ lib/sentry_filter.ex
@@ -1,4 +1,7 @@
 defmodule Plausible.SentryFilter do
+  @moduledoc """
+  Sentry callbacks for filtering and grouping events
+  """
   @behaviour Sentry.EventFilter
 
   def exclude_exception?(%Sentry.CrashError{}, _source), do: true
@@ -6,5 +9,17 @@ defmodule Plausible.SentryFilter do
 
   def exclude_exception?(exception, source) do
     Sentry.DefaultEventFilter.exclude_exception?(exception, source)
+  end
+
+  @spec before_send(Sentry.Event.t()) :: Sentry.Event.t()
+  def before_send(event)
+
+  # https://hexdocs.pm/sentry/readme.html#fingerprinting
+  def before_send(%{exception: [%{type: DBConnection.ConnectionError}]} = event) do
+    %{event | fingerprint: ["ecto", "db_connection", "timeout"]}
+  end
+
+  def before_send(event) do
+    event
   end
 end
