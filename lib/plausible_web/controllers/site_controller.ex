d5bd0d69ae4c89dcdaf3520738586436df8765f2
Vinicius Brasil <vini@hey.com>
2022-10-05 11:04:36 -0300

    Use memoized stats_start_date instead of querying CH

--- lib/plausible_web/controllers/site_controller.ex
+++ lib/plausible_web/controllers/site_controller.ex
@@ -736,9 +736,7 @@ defmodule PlausibleWeb.SiteController do
     site = conn.assigns[:site]
 
     start_date = Plausible.Google.HTTP.get_analytics_start_date(view_id, access_token)
-
-    end_date =
-      Plausible.Stats.Clickhouse.pageview_start_date_local(site) || Timex.today(site.timezone)
+    end_date = Plausible.Sites.stats_start_date(site) || Timex.today(site.timezone)
 
     {:ok, {view_name, view_id}} = Plausible.Google.Api.get_view(access_token, view_id)
 
