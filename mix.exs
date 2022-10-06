641ec3e0f45bc12a769408f8144945819b7eaefb
Robert <robertjoonas16@gmail.com>
2022-10-06 19:31:02 +0300

    generate tracker files for elixir tests/dev/CI

--- mix.exs
+++ mix.exs
@@ -123,7 +123,13 @@ defmodule Plausible.MixProject do
     [
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate", "test", "clean_clickhouse"],
+      test: [
+        "cmd npm run deploy --prefix ./tracker",
+        "ecto.create --quiet",
+        "ecto.migrate",
+        "test",
+        "clean_clickhouse"
+      ],
       sentry_recompile: ["compile", "deps.compile sentry --force"]
     ]
   end
