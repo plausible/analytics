f2322c5c11a567a347f52a867ab8027a82e19e1f
Vignesh Joglekar <rexvigasaurus@gmail.com>
2022-10-12 11:39:25 -0500

    Formatting

--- lib/plausible_web/controllers/api/stats_controller.ex
+++ lib/plausible_web/controllers/api/stats_controller.ex
@@ -36,36 +36,36 @@ defmodule PlausibleWeb.Api.StatsController do
       labels = Enum.map(timeseries_result, fn row -> row[:date] end)
       present_index = present_index_for(site, query, labels)
 
-    full_intervals =
-      if query.interval == "week" do
-        Enum.map(labels, fn label ->
-          {label,
-           Enum.member?(query.date_range, Timex.beginning_of_week(label)) &&
-             Enum.member?(query.date_range, Timex.end_of_week(label))}
-        end)
-        |> Enum.into(%{})
-      else
-        if query.interval == "month" do
+      full_intervals =
+        if query.interval == "week" do
           Enum.map(labels, fn label ->
             {label,
-             Enum.member?(query.date_range, Timex.beginning_of_month(label)) &&
-               Enum.member?(query.date_range, Timex.end_of_month(label))}
+             Enum.member?(query.date_range, Timex.beginning_of_week(label)) &&
+               Enum.member?(query.date_range, Timex.end_of_week(label))}
           end)
           |> Enum.into(%{})
         else
-          nil
+          if query.interval == "month" do
+            Enum.map(labels, fn label ->
+              {label,
+               Enum.member?(query.date_range, Timex.beginning_of_month(label)) &&
+                 Enum.member?(query.date_range, Timex.end_of_month(label))}
+            end)
+            |> Enum.into(%{})
+          else
+            nil
+          end
         end
-      end
 
-    json(conn, %{
-      plot: plot,
-      labels: labels,
-      present_index: present_index,
-      interval: query.interval,
-      with_imported: query.include_imported,
-      imported_source: site.imported_data && site.imported_data.source,
-      full_intervals: full_intervals
-    })
+      json(conn, %{
+        plot: plot,
+        labels: labels,
+        present_index: present_index,
+        interval: query.interval,
+        with_imported: query.include_imported,
+        imported_source: site.imported_data && site.imported_data.source,
+        full_intervals: full_intervals
+      })
     else
       _ ->
         bad_request(conn)
