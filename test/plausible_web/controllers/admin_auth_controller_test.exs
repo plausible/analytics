39ed18037029b9624ce4cf8524252b1487d257f9
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2022-10-18 17:50:23 +0700

    add failing test

--- test/plausible_web/controllers/admin_auth_controller_test.exs
+++ test/plausible_web/controllers/admin_auth_controller_test.exs
@@ -29,6 +29,37 @@ defmodule PlausibleWeb.AdminAuthControllerTest do
       assert redirected_to(conn) == "/sites"
     end
 
+    # https://github.com/plausible/analytics/issues/1271
+    test "allows editing admin user email when authentication is disabled", ctx do
+      %{conn: conn} = ctx
+      set_config(disable_authentication: true)
+
+      og_admin_user =
+        insert(:user,
+          email: Application.get_env(:plausible, :admin_email),
+          password: Application.get_env(:plausible, :admin_pwd)
+        )
+
+      # change admin user email
+      {:ok, admin_user} =
+        og_admin_user
+        |> Ecto.Changeset.change(%{email: "new-admin@email.com"})
+        |> Plausible.Repo.update()
+
+      refute og_admin_user.email == admin_user.email
+
+      # goto landing page
+      conn = get(conn, "/")
+      assert get_session(conn, :current_user_id) == admin_user.id
+      assert redirected_to(conn) == "/sites"
+
+      # trying logging out
+      conn = get(conn, "/logout")
+      assert redirected_to(conn) == "/"
+      conn = get(conn, "/")
+      assert redirected_to(conn) == "/sites"
+    end
+
     test "disable registration", %{conn: conn} do
       set_config(disable_registration: true)
       conn = get(conn, "/register")
@@ -37,6 +68,9 @@ defmodule PlausibleWeb.AdminAuthControllerTest do
   end
 
   def set_config(config) do
+    prev_env = Application.get_env(:plausible, :selfhost)
+    on_exit(fn -> Application.put_env(:plausible, :selfhost, prev_env) end)
+
     updated_config =
       Keyword.merge(
         [disable_authentication: false, disable_registration: false],
