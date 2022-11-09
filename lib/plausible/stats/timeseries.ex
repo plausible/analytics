b480f8e8f910419ea0291b037be5230e8b63c942
Vinicius Brasil <vini@hey.com>
2022-11-09 16:42:17 -0300

    fixup! Add API support for intervals

--- lib/plausible/stats/timeseries.ex
+++ lib/plausible/stats/timeseries.ex
@@ -51,12 +51,7 @@ defmodule Plausible.Stats.Timeseries do
   end
 
   defp buckets(%Query{interval: "month"} = query) do
-    n_buckets =
-      Timex.diff(
-        query.date_range.last |> Timex.end_of_month(),
-        query.date_range.first |> Timex.beginning_of_month(),
-        :months
-      )
+    n_buckets = Timex.diff(query.date_range.last, query.date_range.first, :months)
 
     Enum.map(n_buckets..0, fn shift ->
       query.date_range.last
