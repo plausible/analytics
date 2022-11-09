7dd3291b5987e76c15f161a3816fb5ffc2ead793
Adam Rutkowski <hq@mtod.org>
2022-11-09 10:11:58 +0100

    Add ingest rate limit meta-data migration

--- priv/repo/migrations/20221109082503_add_rate_limiting_to_sites.exs
+++ priv/repo/migrations/20221109082503_add_rate_limiting_to_sites.exs
@@ -2,6 +2,9 @@ defmodule Plausible.Repo.Migrations.AddRateLimitingToSites do
   use Ecto.Migration
 
   def change do
-
+    alter table(:sites) do
+      add :ingest_rate_limit_scale_seconds, :integer, null: true
+      add :ingest_rate_limit_threshold, :integer, null: true
+    end
   end
 end
