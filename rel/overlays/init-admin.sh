7cb00f79f6326f03791de1cab93cd4d8a269a435
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2022-11-09 17:08:54 +0800

    remove TODO

--- rel/overlays/init-admin.sh
+++ rel/overlays/init-admin.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 # Create an admin user
 
-# TODO 1.6.0 remove this custom command
 echo "init-admin is deprecated and is no-op now"
 echo "user registration on first launch happens via Web UI instead"
