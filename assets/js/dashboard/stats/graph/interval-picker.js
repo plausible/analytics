80d9fc9ed37061310d703f6f422b80ca9eeba248
Vinicius Brasil <vini@hey.com>
2022-11-18 16:39:05 -0300

    Remove unnecessary imports

--- assets/js/dashboard/stats/graph/interval-picker.js
+++ assets/js/dashboard/stats/graph/interval-picker.js
@@ -1,6 +1,6 @@
-import { Menu, Transition } from '@headlessui/react';
+import { Menu } from '@headlessui/react';
 import { ChevronDownIcon } from '@heroicons/react/20/solid'
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import classNames from 'classnames'
 import * as storage from '../../util/storage'
 
