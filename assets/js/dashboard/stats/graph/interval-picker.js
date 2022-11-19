df2486c1e937809ef97817e0405d82325e520783
Vinicius Brasil <vini@hey.com>
2022-11-18 22:30:06 -0300

    Convert IntervalPicker to function component

--- assets/js/dashboard/stats/graph/interval-picker.js
+++ assets/js/dashboard/stats/graph/interval-picker.js
@@ -20,45 +20,38 @@ export const storeInterval = function(period, domain, interval) {
   storage.setItem(`interval__${period}__${domain}`, interval)
 }
 
-export class IntervalPicker extends Component {
-  constructor(props) {
-    super(props)
-    this.renderDropdownOption = this.renderDropdownOption.bind(this)
-  }
+function DropdownItem({ option, currentInterval, updateInterval }) {
+  return (
+    <Menu.Item onClick={() => updateInterval(option)} key={option}>
+      {({ active }) => (
+        <span className={classNames({
+                'bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-200 cursor-pointer': active,
+                'text-gray-700 dark:text-gray-200': !active,
+                'font-bold': option == currentInterval,
+              }, 'block px-4 py-2 text-sm')}>
+          { INTERVAL_LABELS[option] }
+        </span>
+      )}
+    </Menu.Item>
+  )
+}
 
-  render() {
-    if (this.props.query.period == 'realtime') return null
+export function IntervalPicker({ graphData, query, site, updateInterval }) {
+  if (query.period == 'realtime') return null
 
-    const currentInterval = this.props.graphData?.interval || this.props.query.interval || "all"
-    let possibleIntervals = this.props.site.allowedIntervalsForPeriod[this.props.query.period]
-    possibleIntervals = possibleIntervals.filter(interval => interval !== currentInterval)
+  const currentInterval = graphData?.interval || query.interval || "all"
+  const options = site.allowedIntervalsForPeriod[query.period]
 
-    return (
-      <Menu as="div" className="relative inline-block">
-        <Menu.Button className="inline-flex focus:outline-none text-gray-700 dark:text-gray-300 hover:text-indigo-600 dark:hover:text-indigo-600 items-center">
-          { INTERVAL_LABELS[currentInterval] }
-          <ChevronDownIcon className="h-5 w-5" aria-hidden="true" />
-        </Menu.Button>
+  return (
+    <Menu as="div" className="relative inline-block">
+      <Menu.Button className="inline-flex focus:outline-none text-gray-700 dark:text-gray-300 hover:text-indigo-600 dark:hover:text-indigo-600 items-center">
+        { INTERVAL_LABELS[currentInterval] }
+        <ChevronDownIcon className="h-5 w-5" aria-hidden="true" />
+      </Menu.Button>
 
-        <Menu.Items className="py-1 text-left origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white dark:bg-gray-800 ring-1 ring-black ring-opacity-5 focus:outline-none z-10">
-          { possibleIntervals.map(this.renderDropdownOption) }
-        </Menu.Items>
-      </Menu>
-    )
-  }
-
-  renderDropdownOption(interval) {
-    return (
-      <Menu.Item onClick={() => { this.props.updateInterval(interval) }} key={interval}>
-        {({ active }) => (
-          <span className={classNames({
-                  'bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-200 cursor-pointer': active,
-                  'text-gray-700 dark:text-gray-200': !active
-                }, 'block px-4 py-2 text-sm')}>
-            { INTERVAL_LABELS[interval] }
-          </span>
-        )}
-      </Menu.Item>
-    )
-  }
+      <Menu.Items className="py-1 text-left origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white dark:bg-gray-800 ring-1 ring-black ring-opacity-5 focus:outline-none z-10">
+        { options.map((option) => DropdownItem({ option, currentInterval, updateInterval })) }
+      </Menu.Items>
+    </Menu>
+  )
 }
