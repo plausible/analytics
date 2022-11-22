d45f19d0894ce9f8f6388c9ac19342ecc0216407
Adam Rutkowski <hq@mtod.org>
2022-11-22 16:16:35 +0100

    Reduce function complexity

--- lib/plausible_web/controllers/api/external_controller.ex
+++ lib/plausible_web/controllers/api/external_controller.ex
@@ -11,47 +11,30 @@ defmodule PlausibleWeb.Api.ExternalController do
   alias Plausible.Ingestion
 
   def event(conn, _params) do
-    case Ingestion.Request.build(conn) do
-      {:ok, request} ->
-        Sentry.Context.set_extra_context(%{request: request})
+    with {:ok, request} <- Ingestion.Request.build(conn),
+         _ = Sentry.Context.set_extra_context(%{request: request}) do
+      case Ingestion.Event.build_and_buffer(request) do
+        {:ok, %{dropped: [], buffered: _buffered}} ->
+          conn
+          |> put_status(202)
+          |> text("ok")
 
-        case Ingestion.Event.build_and_buffer(request) do
-          {:ok, %{dropped: [], buffered: _buffered}} ->
+        {:ok, %{dropped: dropped, buffered: _}} ->
+          first_invalid_changeset = find_first_invalid_changeset(dropped)
+
+          if first_invalid_changeset do
             conn
+            |> put_resp_header("x-plausible-dropped", "#{Enum.count(dropped)}")
+            |> put_status(400)
+            |> json(%{errors: traverse_errors(first_invalid_changeset)})
+          else
+            conn
+            |> put_resp_header("x-plausible-dropped", "#{Enum.count(dropped)}")
             |> put_status(202)
             |> text("ok")
-
-          {:ok, %{dropped: dropped, buffered: _}} ->
-            first_invalid_changeset =
-              Enum.find_value(dropped, nil, fn dropped_event ->
-                case dropped_event.drop_reason do
-                  {:error, %Ecto.Changeset{} = changeset} -> changeset
-                  _ -> false
-                end
-              end)
-
-            if first_invalid_changeset do
-              user_facing_errors =
-                Ecto.Changeset.traverse_errors(first_invalid_changeset, fn {msg, opts} ->
-                  Regex.replace(~r"%{(\w+)}", msg, fn _, key ->
-                    opts
-                    |> Keyword.get(String.to_existing_atom(key), key)
-                    |> to_string()
-                  end)
-                end)
-
-              conn
-              |> put_resp_header("x-plausible-dropped", "#{Enum.count(dropped)}")
-              |> put_status(400)
-              |> json(%{errors: user_facing_errors})
-            else
-              conn
-              |> put_resp_header("x-plausible-dropped", "#{Enum.count(dropped)}")
-              |> put_status(202)
-              |> text("ok")
-            end
-        end
-
+          end
+      end
+    else
       {:error, :invalid_json} ->
         conn
         |> put_status(400)
@@ -110,5 +93,24 @@ defmodule PlausibleWeb.Api.ExternalController do
       geo_database: geo_database,
       build: build
     })
+  end
+
+  defp find_first_invalid_changeset(dropped) do
+    Enum.find_value(dropped, nil, fn dropped_event ->
+      case dropped_event.drop_reason do
+        {:error, %Ecto.Changeset{} = changeset} -> changeset
+        _ -> false
+      end
+    end)
+  end
+
+  defp traverse_errors(changeset) do
+    Ecto.Changeset.traverse_errors(changeset, fn {msg, opts} ->
+      Regex.replace(~r"%{(\w+)}", msg, fn _, key ->
+        opts
+        |> Keyword.get(String.to_existing_atom(key), key)
+        |> to_string()
+      end)
+    end)
   end
 end
