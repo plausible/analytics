c747514a0138452b255fd92edac7d2b1330befff
Adam Rutkowski <hq@mtod.org>
2022-12-23 13:45:43 +0100

    Exclude slow tests by default

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,6 +1,6 @@
 {:ok, _} = Application.ensure_all_started(:ex_machina)
 Plausible.Test.ClickhouseSetup.run()
 Mox.defmock(Plausible.HTTPClient.Mock, for: Plausible.HTTPClient.Interface)
-ExUnit.start()
+ExUnit.start(exclude: :slow)
 Application.ensure_all_started(:double)
 Ecto.Adapters.SQL.Sandbox.mode(Plausible.Repo, :manual)
