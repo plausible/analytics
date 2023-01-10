6177f4166f56f560d360167ba6fcf22e0a021739
Adam Rutkowski <hq@mtod.org>
2023-01-10 15:03:56 +0100

    On 500s, load our self-hosted sentry bundle

--- lib/plausible_web/views/error_view.ex
+++ lib/plausible_web/views/error_view.ex
@@ -38,7 +38,7 @@ defmodule PlausibleWeb.ErrorView do
           |> Jason.encode!()
 
         ~E"""
-        <script src="https://browser.sentry-cdn.com/5.9.1/bundle.min.js" integrity="sha384-/x1aHz0nKRd6zVUazsV6CbQvjJvr6zQL2CHbQZf3yoLkezyEtZUpqUNnOLW9Nt3v" crossorigin="anonymous"></script>
+        <script src="<%= Routes.static_path(@conn, "/js/sentry-bundle-5.9.1-min.js") %>" integrity="sha384-/x1aHz0nKRd6zVUazsV6CbQvjJvr6zQL2CHbQZf3yoLkezyEtZUpqUNnOLW9Nt3v" crossorigin="anonymous"></script>
         <script>
         Sentry.init({ dsn: '<%= Sentry.Config.dsn() %>' });
         Sentry.showReportDialog(<%= raw opts %>)
