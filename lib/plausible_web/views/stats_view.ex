4ad2d409a66af4370a123fb0bbff0de258a173b6
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2023-01-18 22:51:39 +0700

    fix class name typo and include it in safelist

--- lib/plausible_web/views/stats_view.ex
+++ lib/plausible_web/views/stats_view.ex
@@ -52,7 +52,7 @@ defmodule PlausibleWeb.StatsView do
   def stats_container_class(conn) do
     cond do
       conn.assigns[:embedded] && conn.assigns[:width] == "manual" -> ""
-      conn.assigns[:embedded] -> "max-width-screen-lg mx-auto"
+      conn.assigns[:embedded] -> "max-w-screen-lg mx-auto"
       !conn.assigns[:embedded] -> "container"
     end
   end
