3afa11ed5e4457cf44b06217469640a99886d144
Robert Joonas <robertjoonas16@gmail.com>
2023-01-19 16:26:48 +0200

    fix subquery for sessions in base_event_query/2
    As the 'sessions' table is using the CollapsingMergeTree engine, we have
    to select session_id's distinctively. Otherwise we will get multiple rows
    (with sign -1 and 1) as long as the background merge hasn't happened.

--- lib/plausible/stats/base.ex
+++ lib/plausible/stats/base.ex
@@ -14,7 +14,7 @@ defmodule Plausible.Stats.Base do
       sessions_q =
         from(
           s in query_sessions(site, query),
-          select: %{session_id: s.session_id}
+          select: %{session_id: fragment("DISTINCT ?", s.session_id)}
         )
 
       from(
