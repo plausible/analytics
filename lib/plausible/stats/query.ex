4b69d3354209eff194d58c00fddb84a613badf3a
David Janda <djanda97@gmail.com>
2023-01-27 21:50:08 -0700

    Add case to shift_back function to handle day periods

--- lib/plausible/stats/query.ex
+++ lib/plausible/stats/query.ex
@@ -56,6 +56,29 @@ defmodule Plausible.Stats.Query do
     Map.put(query, :date_range, Date.range(new_first, new_last))
   end
 
+  def shift_back(%__MODULE__{period: "day"} = query, site) do
+    # Querying current day to date
+    {new_first, new_last} =
+      if Timex.compare(Timex.now(site.timezone), query.date_range.first, :day) == 0 do
+        diff =
+          Timex.diff(
+            Timex.beginning_of_day(Timex.now(site.timezone)),
+            Timex.now(site.timezone),
+            :days
+          ) - 1
+
+        {query.date_range.first |> Timex.shift(days: diff),
+         Timex.now(site.timezone) |> Timex.to_date() |> Timex.shift(days: diff)}
+      else
+        diff = Timex.diff(query.date_range.first, query.date_range.last, :days) - 1
+
+        {query.date_range.first |> Timex.shift(days: diff),
+         query.date_range.last |> Timex.shift(days: diff)}
+      end
+
+    Map.put(query, :date_range, Date.range(new_first, new_last))
+  end
+
   def shift_back(query, _site) do
     diff = Timex.diff(query.date_range.first, query.date_range.last, :days) - 1
     new_first = query.date_range.first |> Timex.shift(days: diff)
