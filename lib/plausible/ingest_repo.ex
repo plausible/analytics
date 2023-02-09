9b2ca521592fe4e7ac791d23b0a3df2fc1700d01
Adam Rutkowski <hq@mtod.org>
2023-02-09 08:39:14 +0100

    Add IngestRepo

---
+++ lib/plausible/ingest_repo.ex
@@ -0,0 +1,13 @@
+defmodule Plausible.IngestRepo do
+  use Ecto.Repo,
+    otp_app: :plausible,
+    adapter: ClickhouseEcto
+
+  defmacro __using__(_) do
+    quote do
+      alias Plausible.ClickhouseRepo
+      import Ecto
+      import Ecto.Query, only: [from: 1, from: 2]
+    end
+  end
+end
