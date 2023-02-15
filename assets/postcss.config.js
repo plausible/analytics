9eedbce04706bcc64fff2b5e9da6599dba68c515
Uku Taht <uku.taht@gmail.com>
2023-02-15 13:55:30 +0200

    Require autoprefixer in postcss config

--- assets/postcss.config.js
+++ assets/postcss.config.js
@@ -1,6 +1,9 @@
+const tailwindcss = require('tailwindcss');
+const autoprefixer = require('autoprefixer');
+
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  }
-}
+  plugins: [
+    tailwindcss,
+    autoprefixer
+  ],
+};
