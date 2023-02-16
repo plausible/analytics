6283dd61c50dfd766ea8280c70dcfa329ff428c4
Uku Taht <uku.taht@gmail.com>
2023-02-16 12:49:10 +0200

    Validate property key in breakdown API endpoint

--- lib/plausible/stats/props.ex
+++ lib/plausible/stats/props.ex
@@ -1,6 +1,33 @@
 defmodule Plausible.Stats.Props do
   use Plausible.ClickhouseRepo
   import Plausible.Stats.Base
+  @event_props ["event:page", "event:page_match", "event:name"]
+  @session_props [
+    "visit:source",
+    "visit:country",
+    "visit:region",
+    "visit:city",
+    "visit:entry_page",
+    "visit:exit_page",
+    "visit:referrer",
+    "visit:utm_medium",
+    "visit:utm_source",
+    "visit:utm_campaign",
+    "visit:utm_content",
+    "visit:utm_term",
+    "visit_device",
+    "visit:os",
+    "visit:os_version",
+    "visit:browser",
+    "visit:browser_version"
+  ]
+
+  def event_props(), do: @event_props
+
+  def valid_prop?(prop) when prop in @event_props, do: true
+  def valid_prop?(prop) when prop in @session_props, do: true
+  def valid_prop?("event:props:" <> prop) when is_binary(prop), do: true
+  def valid_prop?(_), do: false
 
   def props(site, query) do
     prop_filter =
