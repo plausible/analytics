e50072995b699d60d447e457cd6a2a4eb15471fe
Vinicius Brasil <vini@hey.com>
2023-02-22 10:00:21 -0300

    Set graph z-index to zero

--- assets/js/dashboard/stats/graph/visitor-graph.js
+++ assets/js/dashboard/stats/graph/visitor-graph.js
@@ -348,7 +348,7 @@ class LineGraph extends React.Component {
             <IntervalPicker site={site} query={query} graphData={graphData} metric={metric} updateInterval={updateInterval}/>
           </div>
           <FadeIn show={graphData}>
-            <div className="relative h-96 w-full">
+            <div className="relative h-96 w-full z-0">
               <canvas id="main-graph-canvas" className={canvasClass}></canvas>
             </div>
           </FadeIn>
