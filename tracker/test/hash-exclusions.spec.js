a0bba5f749e5382882f60dad68692e3e0c933a3c
Uku Taht <uku.taht@gmail.com>
2023-02-28 12:14:49 +0200

    Remove browserstack harness, run playwright directly

--- tracker/test/hash-exclusions.spec.js
+++ tracker/test/hash-exclusions.spec.js
@@ -1,6 +1,5 @@
-const { test } = require('./support/harness');
 const { mockRequest } = require('./support/test-utils')
-const { expect } = require('@playwright/test');
+const { expect, test } = require('@playwright/test');
 
 test.describe('combination of hash and exclusions script extensions', () => {
   test('excludes by hash part of the URL', async ({ page }) => {
