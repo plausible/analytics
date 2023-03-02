7df8834c0bf93455a70c765ed30b2f49da00e399
Vinicius Brasil <vini@hey.com>
2023-03-02 12:20:19 -0300

    Fix CI warning

--- priv/repo/migrations/20190618165016_add_public_sites.exs
+++ priv/repo/migrations/20190618165016_add_public_sites.exs
@@ -1,12 +1,13 @@
 defmodule Plausible.Repo.Migrations.AddPublicSites do
   use Ecto.Migration
-  @host Application.get_env(:plausible, :url, :host)
 
   def change do
+    host = Application.get_env(:plausible, :url, :host)
+
     alter table(:sites) do
       add :public, :boolean, null: false, default: false
     end
 
-    execute "update sites set public=true where domain='#{@host}'"
+    execute "update sites set public=true where domain='#{host}'"
   end
 end
