3f593761ddb9128e66de496770024c946e8444ea
Vinicius Brasil <vini@hey.com>
2023-03-07 10:53:56 -0300

    Refactor previous period functions to use earliest/2

--- lib/plausible/stats/comparisons.ex
+++ lib/plausible/stats/comparisons.ex
@@ -28,13 +28,13 @@ defmodule Plausible.Stats.Comparisons do
       ) do
     if valid_mode?(source_query, mode) do
       now = now || Timex.now(site.timezone)
-      {:ok, do_compare(site, source_query, mode, now)}
+      {:ok, do_compare(source_query, mode, now)}
     else
       {:error, :not_supported}
     end
   end
 
-  defp do_compare(_site, source_query, "year_over_year", now) do
+  defp do_compare(source_query, "year_over_year", now) do
     start_date = Date.add(source_query.date_range.first, -365)
     end_date = earliest(source_query.date_range.last, now) |> Date.add(-365)
 
@@ -42,57 +42,15 @@ defmodule Plausible.Stats.Comparisons do
     %Stats.Query{source_query | date_range: range}
   end
 
-  defp do_compare(_site, %Stats.Query{period: "year"} = query, "previous_period", now) do
-    # Querying current year to date
-    {new_first, new_last} =
-      if Timex.compare(now, query.date_range.first, :year) == 0 do
-        diff =
-          Timex.diff(
-            Timex.beginning_of_year(now),
-            now,
-            :days
-          ) - 1
+  defp do_compare(source_query, "previous_period", now) do
+    last = earliest(source_query.date_range.last, now)
+    diff_in_days = Date.diff(source_query.date_range.first, last) - 1
 
-        {query.date_range.first |> Timex.shift(days: diff),
-         now |> Timex.to_date() |> Timex.shift(days: diff)}
-      else
-        diff = Timex.diff(query.date_range.first, query.date_range.last, :days) - 1
+    new_first = Date.add(source_query.date_range.first, diff_in_days)
+    new_last = Date.add(last, diff_in_days)
 
-        {query.date_range.first |> Timex.shift(days: diff),
-         query.date_range.last |> Timex.shift(days: diff)}
-      end
-
-    Map.put(query, :date_range, Date.range(new_first, new_last))
-  end
-
-  defp do_compare(_site, %Stats.Query{period: "month"} = query, "previous_period", now) do
-    # Querying current month to date
-    {new_first, new_last} =
-      if Timex.compare(now, query.date_range.first, :month) == 0 do
-        diff =
-          Timex.diff(
-            Timex.beginning_of_month(now),
-            now,
-            :days
-          ) - 1
-
-        {query.date_range.first |> Timex.shift(days: diff),
-         now |> Timex.to_date() |> Timex.shift(days: diff)}
-      else
-        diff = Timex.diff(query.date_range.first, query.date_range.last, :days) - 1
-
-        {query.date_range.first |> Timex.shift(days: diff),
-         query.date_range.last |> Timex.shift(days: diff)}
-      end
-
-    Map.put(query, :date_range, Date.range(new_first, new_last))
-  end
-
-  defp do_compare(_site, query, "previous_period", _now) do
-    diff = Timex.diff(query.date_range.first, query.date_range.last, :days) - 1
-    new_first = query.date_range.first |> Timex.shift(days: diff)
-    new_last = query.date_range.last |> Timex.shift(days: diff)
-    Map.put(query, :date_range, Date.range(new_first, new_last))
+    range = Date.range(new_first, new_last)
+    %Stats.Query{source_query | date_range: range}
   end
 
   defp earliest(a, b) do
