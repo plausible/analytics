ba561c48bcbc86b23a22e0d43cc33828e850bcbf
Adam Rutkowski <hq@mtod.org>
2023-03-20 11:26:51 +0100

    Remove obsolete migration config
    The compilation warning advises to use `Application.compile_env/3`
    however there is no `:url` environment setting under `:plausible` key
    anyway, so we might as well get rid of it completely.

--- priv/repo/migrations/20190618165016_add_public_sites.exs
+++ priv/repo/migrations/20190618165016_add_public_sites.exs
@@ -1,12 +1,9 @@
 defmodule Plausible.Repo.Migrations.AddPublicSites do
   use Ecto.Migration
-  @host Application.get_env(:plausible, :url, :host)
 
   def change do
     alter table(:sites) do
       add :public, :boolean, null: false, default: false
     end
-
-    execute "update sites set public=true where domain='#{@host}'"
   end
 end
