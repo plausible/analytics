30af66dca29f6cbc5ef88390d14c033d479cf22a
Adam Rutkowski <hq@mtod.org>
2023-03-21 10:20:47 +0100

    Adjust to Ch/Chto only

--- lib/plausible/data_migration/repo.ex
+++ lib/plausible/data_migration/repo.ex
@@ -2,42 +2,21 @@ defmodule Plausible.DataMigration.Repo do
   @moduledoc """
   Ecto.Repo for Clickhouse data migrations, to be started manually, 
   outside of the main application supervision tree.
-
-  Supports both Ch and Clickhousex
   """
-  if Code.ensure_loaded?(Ch) do
-    use Ecto.Repo,
-      otp_app: :plausible,
-      adapter: Ecto.Adapters.ClickHouse
-  else
-    use Ecto.Repo,
-      otp_app: :plausible,
-      adapter: ClickhouseEcto
-  end
+  use Ecto.Repo,
+    otp_app: :plausible,
+    adapter: Ecto.Adapters.ClickHouse
 
   def start(url, max_threads) when is_binary(url) and is_integer(max_threads) do
-    if Code.ensure_loaded?(Ch) do
-      start_link(
-        url: url,
-        queue_target: 500,
-        queue_interval: 2000,
-        pool_size: 1,
-        settings: [
-          max_insert_threads: max_threads,
-          send_progress_in_http_headers: 1
-        ]
-      )
-    else
-      start_link(
-        url: url,
-        queue_target: 500,
-        queue_interval: 2000,
-        pool_size: 1,
-        clickhouse_settings: [
-          max_insert_threads: max_threads,
-          send_progress_in_http_headers: 1
-        ]
-      )
-    end
+    start_link(
+      url: url,
+      queue_target: 500,
+      queue_interval: 2000,
+      pool_size: 1,
+      settings: [
+        max_insert_threads: max_threads,
+        send_progress_in_http_headers: 1
+      ]
+    )
   end
 end
