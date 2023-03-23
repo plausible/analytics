ddca9c80a7353cfe857c431eaae421835276a2d9
Adam Rutkowski <hq@mtod.org>
2023-03-23 10:13:13 +0100

    Add test_helper prompt

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -4,3 +4,11 @@ FunWithFlags.enable(:visits_metric)
 ExUnit.start(exclude: :slow)
 Application.ensure_all_started(:double)
 Ecto.Adapters.SQL.Sandbox.mode(Plausible.Repo, :manual)
+
+if Plausible.v2?() do
+  IO.puts("Running tests against v2 schema")
+else
+  IO.puts(
+    "Running tests against v1 schema. Use: `V2_MIGRATION_DONE=1 mix test` for secondary run."
+  )
+end
