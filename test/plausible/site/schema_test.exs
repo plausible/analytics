7a8fa76fa2eb2faee0041a1e190a802fa6dcde4e
Uku Taht <Uku.taht@gmail.com>
2023-03-23 16:49:14 +0200

    Format large numbers with underscore in tests
    Co-authored-by: Adam <hq@mtod.org>

--- test/plausible/site/schema_test.exs
+++ test/plausible/site/schema_test.exs
@@ -8,27 +8,27 @@ defmodule Plausible.SiteTest do
     test "returns offset from utc in seconds" do
       site = build(:site, timezone: "US/Eastern")
 
-      assert Site.tz_offset(site, ~U[2023-01-01 00:00:00Z]) == -18000
+      assert Site.tz_offset(site, ~U[2023-01-01 00:00:00Z]) == -18_000
     end
 
     test "returns correct offset from utc during summer time" do
       site = build(:site, timezone: "US/Eastern")
 
-      assert Site.tz_offset(site, ~U[2023-07-01 00:00:00Z]) == -14400
+      assert Site.tz_offset(site, ~U[2023-07-01 00:00:00Z]) == -14_400
     end
 
     test "returns correct offset when changing from winter to summer time" do
       site = build(:site, timezone: "US/Eastern")
 
-      assert Site.tz_offset(site, ~U[2023-03-12 06:59:59Z]) == -18000
-      assert Site.tz_offset(site, ~U[2023-03-12 07:00:00Z]) == -14400
+      assert Site.tz_offset(site, ~U[2023-03-12 06:59:59Z]) == -18_000
+      assert Site.tz_offset(site, ~U[2023-03-12 07:00:00Z]) == -14_400
     end
 
     test "returns correct offset when changing from summer to winter time" do
       site = build(:site, timezone: "US/Eastern")
 
-      assert Site.tz_offset(site, ~U[2023-11-05 05:59:59Z]) == -14400
-      assert Site.tz_offset(site, ~U[2023-11-05 06:00:00Z]) == -18000
+      assert Site.tz_offset(site, ~U[2023-11-05 05:59:59Z]) == -14_400
+      assert Site.tz_offset(site, ~U[2023-11-05 06:00:00Z]) == -18_000
     end
   end
 end
