f965909bff797d997c35f11675e0a04bb34a9f6a
Robert Joonas <robertjoonas16@gmail.com>
2023-03-29 20:51:03 +0100

    Refactor: extract FilterTypeSelector component

--- assets/js/dashboard/stats/modals/regular-filter-modal.js
+++ assets/js/dashboard/stats/modals/regular-filter-modal.js
@@ -1,15 +1,14 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { withRouter } from 'react-router-dom'
-import classNames from 'classnames'
-import { Menu, Transition } from '@headlessui/react'
-import { ChevronDownIcon } from '@heroicons/react/20/solid'
 
+import FilterTypeSelector from "../../components/filter-type-selector";
 import Combobox from '../../components/combobox'
 import { FILTER_GROUPS, parseQueryFilter, formatFilterGroup, formattedFilters, toFilterQuery, FILTER_TYPES } from '../../util/filters'
 import { parseQuery } from '../../query'
 import * as api from '../../api'
 import { apiPath, siteBasePath } from '../../util/url'
 import { shouldIgnoreKeypress } from '../../keybinding'
+import { isFreeChoiceFilter } from "../../util/filters";
 
 function getFormState(filterGroup, query) {
   if (filterGroup === 'props') {
@@ -34,10 +33,6 @@ function getFormState(filterGroup, query) {
   }, {})
 }
 
-function supportsIsNot(filterName) {
-  return !['goal', 'prop_key'].includes(filterName)
-}
-
 function withIndefiniteArticle(word) {
   if (word.startsWith('UTM')) {
     return `a ${word}`
@@ -45,7 +40,6 @@ function withIndefiniteArticle(word) {
     return `an ${word}`
   }
   return `a ${word}`
-
 }
 
 class RegularFilterModal extends React.Component {
@@ -95,7 +89,7 @@ class RegularFilterModal extends React.Component {
     this.selectFiltersAndCloseModal(filters)
   }
 
-  onChange(filterName) {
+  onComboboxSelect(filterName) {
     return (selection) => {
       this.setState(prevState => ({
         formState: Object.assign(prevState.formState, {
@@ -105,12 +99,14 @@ class RegularFilterModal extends React.Component {
     }
   }
 
-  setFilterType(filterName, newType) {
-    this.setState(prevState => ({
-      formState: Object.assign(prevState.formState, {
-        [filterName]: Object.assign(prevState.formState[filterName], { type: newType })
-      })
-    }))
+  onFilterTypeSelect(filterName) {
+    return (newType) => {
+      this.setState(prevState => ({
+        formState: Object.assign(prevState.formState, {
+          [filterName]: Object.assign(prevState.formState[filterName], { type: newType })
+        })
+      }))
+    }
   }
 
   fetchOptions(filter) {
@@ -178,87 +174,20 @@ class RegularFilterModal extends React.Component {
     this.props.history.replace({ pathname: siteBasePath(this.props.site), search: queryString.toString() })
   }
 
-  isFreeChoice() {
-    return ['page', 'utm'].includes(this.props.filterGroup)
-  }
-
-  renderSearchBox(filter) {
-    return <Combobox fetchOptions={this.fetchOptions(filter)} freeChoice={this.isFreeChoice()} values={this.state.formState[filter].clauses} onChange={this.onChange(filter)} placeholder={`Select ${withIndefiniteArticle(formattedFilters[filter])}`} />
-  }
-
   renderFilterInputs() {
-    const groups = FILTER_GROUPS[this.props.filterGroup]
+    const filtersInGroup = FILTER_GROUPS[this.props.filterGroup]
 
-    return groups.map((filter) => {
+    return filtersInGroup.map((filter) => {
       return (
         <div className="mt-4" key={filter}>
           <div className="text-sm font-medium text-gray-700 dark:text-gray-300">{formattedFilters[filter]}</div>
           <div className="flex items-start mt-1">
-            {this.renderFilterTypeSelector(filter)}
-            {this.renderSearchBox(filter)}
+            <FilterTypeSelector forFilter={filter} onSelect={this.onFilterTypeSelect(filter)} selectedType={this.selectedFilterType(filter)}/>
+            <Combobox fetchOptions={this.fetchOptions(filter)} freeChoice={isFreeChoiceFilter(filter)} values={this.state.formState[filter].clauses} onSelect={this.onComboboxSelect(filter)} placeholder={`Select ${withIndefiniteArticle(formattedFilters[filter])}`} />
           </div>
         </div>
       )
     })
-  }
-
-  renderFilterTypeSelector(filterName) {
-    return (
-      <Menu as="div" className="relative inline-block text-left">
-        {({ open }) => (
-          <>
-            <div className="w-24">
-              <Menu.Button className="inline-flex justify-between items-center w-full rounded-md border border-gray-300 dark:border-gray-500 shadow-sm px-4 py-2 bg-white dark:bg-gray-800 text-sm text-gray-700 dark:text-gray-200 hover:bg-gray-50 dark:hover:bg-gray-850 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 dark:focus:ring-offset-gray-900 focus:ring-indigo-500">
-                {this.selectedFilterType(filterName)}
-                <ChevronDownIcon className="-mr-2 ml-2 h-4 w-4 text-gray-500 dark:text-gray-400" aria-hidden="true" />
-              </Menu.Button>
-            </div>
-
-            <Transition
-              show={open}
-              as={Fragment}
-              enter="transition ease-out duration-100"
-              enterFrom="transform opacity-0 scale-95"
-              enterTo="transform opacity-100 scale-100"
-              leave="transition ease-in duration-75"
-              leaveFrom="transform opacity-100 scale-100"
-              leaveTo="transform opacity-0 scale-95"
-            >
-              <Menu.Items
-                static
-                className="z-10 origin-top-left absolute left-0 mt-2 w-24 rounded-md shadow-lg bg-white dark:bg-gray-800 ring-1 ring-black ring-opacity-5 focus:outline-none"
-              >
-                <div className="py-1">
-                  {this.renderTypeItem(filterName, FILTER_TYPES.is, true)}
-                  {this.renderTypeItem(filterName, FILTER_TYPES.isNot, supportsIsNot(filterName))}
-                  {this.renderTypeItem(filterName, FILTER_TYPES.contains, this.isFreeChoice())}
-                </div>
-              </Menu.Items>
-            </Transition>
-          </>
-        )}
-      </Menu>
-    )
-  }
-
-  renderTypeItem(filterName, type, shouldDisplay) {
-    return (
-      shouldDisplay && (
-        <Menu.Item>
-          {({ active }) => (
-            <span
-              onClick={() => this.setFilterType(filterName, type)}
-              className={classNames(
-                active ? "bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100" : "text-gray-700 dark:text-gray-200",
-                "cursor-pointer block px-4 py-2 text-sm"
-              )}
-            >
-              {type}
-            </span>
-          )}
-        </Menu.Item>
-      )
-    );
   }
 
   render() {
