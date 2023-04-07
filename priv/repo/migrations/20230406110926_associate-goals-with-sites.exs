6bbbb8cf84d7759c15b003f5976547ddb8b0b67b
Adam Rutkowski <hq@mtod.org>
2023-04-07 07:18:02 +0200

    Full migration (to be submitted separately)

---
+++ priv/repo/migrations/20230406110926_associate-goals-with-sites.exs
@@ -0,0 +1,27 @@
+defmodule :"Elixir.Plausible.Repo.Migrations.Associate-goals-with-sites" do
+  use Ecto.Migration
+
+  def up do
+    alter table(:goals) do
+      add :site_id, :integer, null: true
+    end
+
+    execute """
+    DELETE FROM goals g WHERE NOT EXISTS (
+      SELECT 1 FROM sites s
+      WHERE s.domain = g.domain
+    )
+    """
+
+    execute """
+    UPDATE goals g SET site_id = (
+      SELECT s.id FROM sites s WHERE s.domain = g.domain
+    )
+    """
+
+    alter table(:goals) do
+      modify :site_id, references(:sites, on_delete: :delete_all), null: false
+      remove :domain
+    end
+  end
+end
