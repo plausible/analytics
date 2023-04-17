d0973b51d6fd3c50d1a92ea128794e3e4abb68d0
Adam Rutkowski <hq@mtod.org>
2023-04-17 15:32:38 +0200

    !fixup

---
+++ lib/plausible_web/controllers/api/funnels_controller.ex
@@ -0,0 +1,25 @@
+defmodule PlausibleWeb.Api.FunnelsController do
+  use PlausibleWeb, :controller
+
+  def show(conn, _params) do
+    json(conn, %{
+      conversion_rate: "38%",
+      steps: [
+        %{
+          label: "Signup",
+          visitors: 31,
+          complete_percentage: "100%",
+          dropoff: 0,
+          dropoff_percentage: "61%"
+        },
+        %{
+          label: "Second goal",
+          visitors: 12,
+          complete_percentage: "38%",
+          dropoff: 19,
+          dropoff_percentage: "61%"
+        }
+      ]
+    })
+  end
+end
