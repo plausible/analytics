bd43ed4f7de063d120046d8d7d7d67d29f930fb7
Radek Janicki <13463966+rdk08@users.noreply.github.com>
2023-04-18 12:48:09 +0200

    Refactor if statement

--- lib/plausible_web/controllers/api/external_controller.ex
+++ lib/plausible_web/controllers/api/external_controller.ex
@@ -168,11 +168,7 @@ defmodule PlausibleWeb.Api.ExternalController do
   defp is_bot?(%UAInspector.Result.Bot{}), do: true
 
   defp is_bot?(%UAInspector.Result{client: %UAInspector.Result.Client{name: "Headless Chrome"}}) do
-    if Application.get_env(:plausible, :system_environment) in ["staging", "rc"] do
-      false
-    else
-      true
-    end
+    Application.get_env(:plausible, :system_environment) not in ["staging", "rc"]
   end
 
   defp is_bot?(_), do: false
