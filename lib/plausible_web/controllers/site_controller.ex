964718dbe2f7407891098888d8fa1a70e0cb2306
Adam Rutkowski <hq@mtod.org>
2023-04-19 10:55:50 +0200

    Mute warnings

--- lib/plausible_web/controllers/site_controller.ex
+++ lib/plausible_web/controllers/site_controller.ex
@@ -264,7 +264,7 @@ defmodule PlausibleWeb.SiteController do
 
     funnel_name = params["funnel_name"]
 
-    {:ok, funnel} = Plausible.Funnels.create(site, funnel_name, sanitized_params, nil)
+    {:ok, _funnel} = Plausible.Funnels.create(site, funnel_name, sanitized_params, nil)
 
     redirect(conn, to: Routes.site_path(conn, :settings_funnels, site.domain))
   end
