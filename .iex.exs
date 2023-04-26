dda013dee1c92f7068211547af29272d6faea256
Adam Rutkowski <hq@mtod.org>
2023-04-26 09:28:17 +0200

    Add .iex.exs with common aliases/imports

---
+++ .iex.exs
@@ -0,0 +1,19 @@
+alias Plausible.{Repo, ClickhouseRepo, IngestRepo}
+alias Plausible.{Site, Sites, Goal, Goals, Stats}
+
+import_if_available(Ecto.Query)
+import_if_available(Plausible.Factory)
+
+Logger.configure(level: :warn)
+
+IO.puts(
+  IO.ANSI.cyan() <>
+    ~S[
+        .*$$$$$$s.
+       *$$$$$$$$$$$,
+      :$$SSS#######S   *$$$$/. $$                      ** $$     l$:
+      ,$$SSS#######.   $$   ,$:$$  $$$$$ .S*  $: $$@$* $% $$$$@s :$: s$@$s
+      .**$$$#####`     $@$$$$' $l  s-' $:.@*  $| '$@s. $% $$  \$ :$:,$$ *$:
+      ,***/`           #$      `$$ %$$%$$ *$$$#`.*sss$ $% $#$$$'  %$ *$sss,
+      ,$$'] <> IO.ANSI.reset() <> "\n\n"
+)
