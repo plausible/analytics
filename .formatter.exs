69a2dbc514702c01c5bfcd15714e43ba43a4d294
Robert Joonas <robertjoonas16@gmail.com>
2023-04-26 18:17:37 +0300

    upgrade phoenix
    Co-authored-by: Vini Brasil <vini@hey.com>

--- .formatter.exs
+++ .formatter.exs
@@ -1,5 +1,6 @@
 [
-  import_deps: [:ecto, :phoenix],
-  inputs: ["*.{ex,exs}", "priv/*/seeds.exs", "{config,lib,test}/**/*.{ex,exs}"],
-  subdirectories: ["priv/*/migrations"]
+  import_deps: [:ecto, :ecto_sql, :phoenix],
+  subdirectories: ["priv/*/migrations"],
+  plugins: [Phoenix.LiveView.HTMLFormatter],
+  inputs: ["*.{heex,ex,exs}", "{config,lib,test}/**/*.{heex,ex,exs}", "priv/*/seeds.exs"]
 ]
--- test/plausible_web/controllers/auth_controller_test.exs
+++ test/plausible_web/controllers/auth_controller_test.exs
@@ -757,7 +757,7 @@ defmodule PlausibleWeb.AuthControllerTest do
       conn = get(conn, Routes.auth_path(conn, :google_auth_callback), callback_params)
 
       assert redirected_to(conn, 302) == Routes.site_path(conn, :settings_general, site.domain)
-      assert get_flash(conn, :error) =~ "unable to authenticate your Google Analytics"
+      assert Phoenix.Flash.get(conn.assigns.flash, :error) =~ "unable to authenticate your Google Analytics"
     end
   end
 
