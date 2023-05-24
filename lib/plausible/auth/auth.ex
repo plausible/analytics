801d7e7398c4b9865339dc52c06f667174d0de67
Uku Taht <uku.taht@gmail.com>
2023-05-24 13:46:11 +0300

    Wrap user deletion in DB transaction

--- lib/plausible/auth/auth.ex
+++ lib/plausible/auth/auth.ex
@@ -82,6 +82,26 @@ defmodule Plausible.Auth do
     Enum.any?(sites, &Plausible.Sites.has_stats?/1)
   end
 
+  def delete_user(user) do
+    Repo.transaction(fn ->
+      user =
+        user
+        |> Repo.preload(site_memberships: :site)
+        |> Repo.preload(:subscription)
+
+      for membership <- user.site_memberships do
+        Repo.delete!(membership)
+
+        if membership.role == :owner do
+          Plausible.Site.Removal.run(membership.site.domain)
+        end
+      end
+
+      if user.subscription, do: Repo.delete!(user.subscription)
+      Repo.delete!(user)
+    end)
+  end
+
   def user_owns_sites?(user) do
     Repo.exists?(
       from(s in Plausible.Site,
