3136e4d5f45200370c584405577ef216abfbc11a
Vinicius Brasil <vini@hey.com>
2023-05-24 14:11:09 +0200

    Remove LowCardinality from currency fields

--- priv/ingest_repo/migrations/20230417104025_add_revenue_to_events.exs
+++ priv/ingest_repo/migrations/20230417104025_add_revenue_to_events.exs
@@ -4,10 +4,10 @@ defmodule Plausible.IngestRepo.Migrations.AddRevenueToEvents do
   def change do
     alter table(:events_v2) do
       add :revenue_reporting_amount, :"Decimal64(4)"
-      add :revenue_reporting_currency, :"LowCardinality(FixedString(3))"
+      add :revenue_reporting_currency, :"FixedString(3)"
 
       add :revenue_source_amount, :"Decimal64(4)"
-      add :revenue_source_currency, :"LowCardinality(FixedString(3))"
+      add :revenue_source_currency, :"FixedString(3)"
     end
   end
 end
