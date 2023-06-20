8e5c701f14d7ec38ec0c25460f5cfbdf9c00b49f
Adam Rutkowski <hq@mtod.org>
2023-06-20 11:20:44 +0200

    Revert "Fix combobox input length w/ WebKit"
    This reverts commit 3c653a6d85d5000167631e10ef45a93c13b41ed1.

--- lib/plausible_web/live/funnel_settings/combo_box.ex
+++ lib/plausible_web/live/funnel_settings/combo_box.ex
@@ -41,14 +41,14 @@ defmodule PlausibleWeb.Live.FunnelSettings.ComboBox do
     ~H"""
     <div
       id={"input-picker-main-#{@id}"}
-      class="mb-3 flex"
+      class="mb-3"
       x-data={"window.suggestionsDropdown('#{@id}')"}
       x-on:keydown.arrow-up="focusPrev"
       x-on:keydown.arrow-down="focusNext"
       x-on:keydown.enter="select()"
       x-on:keydown.tab="close"
     >
-      <div class="w-full">
+      <div class="relative w-full">
         <div
           @click.away="close"
           class="pl-2 pr-8 py-1 w-full dark:bg-gray-900 dark:text-gray-300 rounded-md shadow-sm border border-gray-300 dark:border-gray-700 focus-within:border-indigo-500 focus-within:ring-1 focus-within:ring-indigo-500"
