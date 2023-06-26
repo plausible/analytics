8c7898284c6ce27b9772a50a58913ac297f4d262
Adam Rutkowski <hq@mtod.org>
2023-06-26 11:20:30 +0200

    Don't try to connect LV socket if there's no CRSF token set up
    This is perfectly okay for some of the templates/layouts.

--- assets/js/liveview/live_socket.js
+++ assets/js/liveview/live_socket.js
@@ -2,17 +2,20 @@ import "phoenix_html"
 import { Socket } from "phoenix"
 import { LiveSocket } from "phoenix_live_view"
 
-let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, {
-  params: { _csrf_token: csrfToken }, hooks: {}, dom: {
-    // for alpinejs integration
-    onBeforeElUpdated(from, to) {
-      if (from.__x) {
-        window.Alpine.clone(from.__x, to);
-      }
-    },
-  }
-})
+let csrfToken = document.querySelector("meta[name='csrf-token']")
+if (csrfToken) {
+  let token = csrfToken.getAttribute("content")
+  let liveSocket = new LiveSocket("/live", Socket, {
+    params: { _csrf_token: token }, hooks: {}, dom: {
+      // for alpinejs integration
+      onBeforeElUpdated(from, to) {
+        if (from.__x) {
+          window.Alpine.clone(from.__x, to);
+        }
+      },
+    }
+  })
 
-liveSocket.connect()
-window.liveSocket = liveSocket
+  liveSocket.connect()
+  window.liveSocket = liveSocket
+}
