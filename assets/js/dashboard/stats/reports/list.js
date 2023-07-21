b9d122c0c7fdad185556d58b3280dc4e2d833a46
RobertJoonas <56999674+RobertJoonas@users.noreply.github.com>
2023-07-21 07:35:41 +0300

    Refactor: Use ListReport component in Sources (#3153)
    * refactor SourceList to use ListReport
    
    * refactor SourceList into a fn comp
    
    * change referrer-drilldown API response format and remove dead code
    
    * use ListReport in referrer-list
    
    * fix CI
    
    * fix flaky test
    
    * remove IO.inspect

--- assets/js/dashboard/stats/reports/list.js
+++ assets/js/dashboard/stats/reports/list.js
@@ -7,6 +7,7 @@ import FadeIn from '../../fade-in'
 import MoreLink from '../more-link'
 import Bar from '../bar'
 import LazyLoader from '../../components/lazy-loader'
+import classNames from 'classnames'
 
 const MAX_ITEMS = 9
 const MIN_HEIGHT = 380
@@ -15,10 +16,29 @@ const ROW_GAP_HEIGHT = 4
 const DATA_CONTAINER_HEIGHT = (ROW_HEIGHT + ROW_GAP_HEIGHT) * (MAX_ITEMS - 1) + ROW_HEIGHT
 const COL_MIN_WIDTH = 70
 
-function ExternalLink({item, externalLinkDest}) {
-  if (externalLinkDest) {
-    const dest = externalLinkDest(item)
+function FilterLink({filterQuery, onClick, children}) {
+  const className = classNames('max-w-max w-full flex md:overflow-hidden', {
+    'hover:underline': !!filterQuery
+  })
+  
+  if (filterQuery) {
+    return (
+      <Link
+        to={{search: filterQuery.toString()}}
+        onClick={onClick}
+        className={className}
+        >
+        { children }
+      </Link>
+    )
+  } else {
+    return <span className={className}>{ children }</span>
+  }
+}
 
+function ExternalLink({item, externalLinkDest}) {
+  const dest = externalLinkDest && externalLinkDest(item)
+  if (dest) {
     return (
       <a
         target="_blank"
@@ -63,7 +83,8 @@ function ExternalLink({item, externalLinkDest}) {
 //     in realtime or goal-filtered views, we'll use `realtimeLabel` and `GoalFilterLabel`.
 
 //   * `getFilterFor` - a function that takes a list item and returns the query link (with
-//      the filter) to navigate to when this list item is clicked.
+//      the filter) to navigate to when this list item is clicked. If a list item is not
+//      supposed to be clickable, this function should return `null` for that list item.
 
 // OPTIONAL PROPS:
 
@@ -76,8 +97,8 @@ function ExternalLink({item, externalLinkDest}) {
 //     to navigate to. If this prop is given, an additional icon is rendered upon hovering
 //     the entry.
 
-//   * `renderIconFor` - a function that takes a list item and returns the
-//     HTML of an icon (such as a flag or screen size icon) for a listItem.
+//   * `renderIcon` - a function that takes a list item and returns the
+//     HTML of an icon (such as a flag, favicon, or a screen size icon) for a listItem.
 
 //   * `color` - color of the comparison bars in light-mode
 
@@ -168,14 +189,19 @@ export default function ListReport(props) {
     )
   }
 
-  function renderBarFor(listItem) {
-    const query = new URLSearchParams(window.location.search)
+  function getFilterQuery(listItem) {
     const filter = props.getFilterFor(listItem)
-
+    if (!filter) { return null }
+    
+    const query = new URLSearchParams(window.location.search)
     Object.entries(filter).forEach((([key, value]) => {
       query.set(key, value)
     }))
 
+    return query
+  }
+
+  function renderBarFor(listItem) {    
     const lightBackground = props.color || 'bg-green-50'
     const noop = () => {}
     const metricToPlot = metrics[0].name
@@ -189,14 +215,14 @@ export default function ListReport(props) {
           plot={metricToPlot}
         >
           <div className="flex justify-start px-2 py-1.5 group text-sm dark:text-gray-300 relative z-9 break-all w-full">
-            <Link onClick={props.onClick || noop} to={{search: query.toString()}} className="max-w-max w-full flex hover:underline md:overflow-hidden">
+            <FilterLink filterQuery={getFilterQuery(listItem)} onClick={props.onClick || noop}>
               {maybeRenderIconFor(listItem)}
 
               <span className="w-full md:truncate">
                 {listItem.name}
               </span>
-            </Link>
-            <ExternalLink item={listItem} externalLinkDest={props.externalLinkDest}  />
+            </FilterLink>
+            <ExternalLink item={listItem} externalLinkDest={props.externalLinkDest} />
           </div>
         </Bar>
       </div>
