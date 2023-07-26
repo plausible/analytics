82f356f38780b81a4945e13acacee0768fb4e60d
hq1 <hq@mtod.org>
2023-07-26 10:44:04 +0200

    Add debug_replay_info context to Sentry Stats reports (#3189)
    * Add debug_replay_info context to Sentry Stats reports
    
    * Test limit

--- lib/plausible/stats.ex
+++ lib/plausible/stats.ex
@@ -1,10 +1,42 @@
 defmodule Plausible.Stats do
-  defdelegate breakdown(site, query, prop, metrics, pagination), to: Plausible.Stats.Breakdown
-  defdelegate aggregate(site, query, metrics), to: Plausible.Stats.Aggregate
-  defdelegate timeseries(site, query, metrics), to: Plausible.Stats.Timeseries
-  defdelegate current_visitors(site), to: Plausible.Stats.CurrentVisitors
-  defdelegate funnel(site, query, funnel), to: Plausible.Stats.Funnel
+  alias Plausible.Stats.{
+    Breakdown,
+    Aggregate,
+    Timeseries,
+    CurrentVisitors,
+    Funnel,
+    FilterSuggestions
+  }
 
-  defdelegate filter_suggestions(site, query, filter_name, filter_search),
-    to: Plausible.Stats.FilterSuggestions
+  use Plausible.DebugReplayInfo
+
+  def breakdown(site, query, prop, metrics, pagination) do
+    include_sentry_replay_info()
+    Breakdown.breakdown(site, query, prop, metrics, pagination)
+  end
+
+  def aggregate(site, query, metrics) do
+    include_sentry_replay_info()
+    Aggregate.aggregate(site, query, metrics)
+  end
+
+  def timeseries(site, query, metrics) do
+    include_sentry_replay_info()
+    Timeseries.timeseries(site, query, metrics)
+  end
+
+  def current_visitors(site) do
+    include_sentry_replay_info()
+    CurrentVisitors.current_visitors(site)
+  end
+
+  def funnel(site, query, funnel) do
+    include_sentry_replay_info()
+    Funnel.funnel(site, query, funnel)
+  end
+
+  def filter_suggestions(site, query, filter_name, filter_search) do
+    include_sentry_replay_info()
+    FilterSuggestions.filter_suggestions(site, query, filter_name, filter_search)
+  end
 end
