821cbb2402edfd8ce9ca5219180cf04b60ba9495
Robert Joonas <robertjoonas16@gmail.com>
2023-08-03 15:29:50 +0100

    capture special props in an object instead (review suggestion)

--- assets/js/dashboard/stats/behaviours/goal-conversions.js
+++ assets/js/dashboard/stats/behaviours/goal-conversions.js
@@ -5,22 +5,20 @@ import { CR_METRIC } from "../reports/metrics"
 import * as url from "../../util/url"
 import * as api from "../../api"
 
+const SPECIAL_GOALS = {
+  '404': {title: '404 Pages', prop: 'path'},
+  'Outbound Link: Click': {title: 'Outbound Links', prop: 'url'},
+  'File Download': {title: 'File Downloads', prop: 'url'}
+}
+
 export function specialTitleWhenGoalFilter(query, defaultTitle) {
-  switch (query.filters.goal) {
-    case '404':
-      return '404 Pages'
-    case 'Outbound Link: Click':
-      return 'Outbound Links'
-    case 'File Download':
-      return 'File Downloads'
-    default:
-      return defaultTitle
-  }
+  return SPECIAL_GOALS[query.filters.goal]?.title || defaultTitle
 }
 
 function SpecialPropBreakdown(props) {
-  const { site, query, prop } = props
-
+  const { site, query } = props
+  const prop = SPECIAL_GOALS[query.filters.goal].prop
+  
   function fetchData() {
     return api.get(url.apiPath(site, `/custom-prop-values/${prop}`), query)
   }
@@ -49,10 +47,8 @@ function SpecialPropBreakdown(props) {
 export default function GoalConversions(props) {
   const {site, query} = props
 
-  if (query.filters.goal === '404') {
-    return <SpecialPropBreakdown site={site} query={props.query} prop="path"/>
-  } else if (['Outbound Link: Click', 'File Download'].includes(query.filters.goal)) {
-    return <SpecialPropBreakdown site={site} query={props.query} prop="url"/>
+  if (SPECIAL_GOALS[query.filters.goal]) {
+    return <SpecialPropBreakdown site={site} query={props.query} />
   } else {
     return <Conversions site={site} query={props.query} />
   }
