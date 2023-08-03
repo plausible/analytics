dbddec2fbe6b3d3c0298e2e024a67c448c5ce01e
Robert Joonas <robertjoonas16@gmail.com>
2023-08-03 01:34:54 +0100

    fix CI

--- assets/js/dashboard/stats/more-link.js
+++ assets/js/dashboard/stats/more-link.js
@@ -26,11 +26,11 @@ function moreIcon() {
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
-      stroke-width="1.5"
+      strokeWidth="1.5"
       stroke="currentColor"
-      class="w-6 h-6 feather mr-1"
+      className="w-6 h-6 feather mr-1"
     >
-      <path stroke-linecap="round" stroke-linejoin="round" d="M3 4.5h14.25M3 9h9.75M3 13.5h9.75m4.5-4.5v12m0 0l-3.75-3.75M17.25 21L21 17.25" />
+      <path strokeLinecap="round" strokeLinejoin="round" d="M3 4.5h14.25M3 9h9.75M3 13.5h9.75m4.5-4.5v12m0 0l-3.75-3.75M17.25 21L21 17.25" />
     </svg>
   )
 }
