82c3b5c90001d9398d2e9e50191858af8f6c9700
Adam Rutkowski <hq@mtod.org>
2023-08-21 15:12:24 +0200

    Revert "Reconfigure session cookie (#3274)"
    This reverts commit 63fabcbb5f1da302fa13ce9e4017dea56cccee85.

--- lib/plausible_web/endpoint.ex
+++ lib/plausible_web/endpoint.ex
@@ -3,10 +3,9 @@ defmodule PlausibleWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :plausible
 
   @session_options [
-    # key to be patched
-    key: "",
     store: :cookie,
-    signing_salt: "I45i0SKHEku2f3tJh6y4v8gztrb/eG5KGCOe/o/AwFb7VHeuvDOn7AAq6KsdmOFM",
+    key: "_plausible_key",
+    signing_salt: "3IL0ob4k",
     # 5 years, this is super long but the SlidingSessionTimeout will log people out if they don't return for 2 weeks
     max_age: 60 * 60 * 24 * 365 * 5,
     extra: "SameSite=Lax"
@@ -17,62 +16,58 @@ defmodule PlausibleWeb.Endpoint do
   #
   # You should set gzip to true if you are running phx.digest
   # when deploying your static files in production.
-  plug(PlausibleWeb.Tracker)
-  plug(PlausibleWeb.Favicon)
+  plug PlausibleWeb.Tracker
+  plug PlausibleWeb.Favicon
 
-  plug(Plug.Static,
+  plug Plug.Static,
     at: "/",
     from: :plausible,
     gzip: false,
     only: ~w(css images js favicon.ico robots.txt)
-  )
 
-  plug(Plug.Static,
+  plug Plug.Static,
     at: "/kaffy",
     from: :kaffy,
     gzip: false,
     only: ~w(assets)
-  )
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
   if code_reloading? do
-    socket("/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket)
-    plug(Phoenix.LiveReloader)
-    plug(Phoenix.CodeReloader)
+    socket "/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket
+    plug Phoenix.LiveReloader
+    plug Phoenix.CodeReloader
   end
 
-  plug(Plug.RequestId)
-  plug(PromEx.Plug, prom_ex_module: Plausible.PromEx)
-  plug(Plug.Telemetry, event_prefix: [:phoenix, :endpoint])
+  plug Plug.RequestId
+  plug PromEx.Plug, prom_ex_module: Plausible.PromEx
+  plug Plug.Telemetry, event_prefix: [:phoenix, :endpoint]
 
-  plug(Plug.Parsers,
+  plug Plug.Parsers,
     parsers: [:urlencoded, :multipart, :json],
     pass: ["*/*"],
     json_decoder: Phoenix.json_library()
-  )
 
-  plug(Sentry.PlugContext)
+  plug Sentry.PlugContext
 
-  plug(Plug.MethodOverride)
-  plug(Plug.Head)
+  plug Plug.MethodOverride
+  plug Plug.Head
 
-  plug(PlausibleWeb.Plugs.RuntimeSessionAdapter, @session_options)
+  plug PlausibleWeb.Plugs.RuntimeSessionAdapter, @session_options
 
-  socket("/live", PlausibleWeb.LiveSocket,
+  socket "/live", Phoenix.LiveView.Socket,
     websocket: [
       check_origin: true,
       connect_info: [session: {__MODULE__, :patch_session_opts, []}]
     ]
-  )
 
-  plug(CORSPlug)
-  plug(PlausibleWeb.Router)
-
-  def secure_cookie?, do: config!(:secure_cookie)
+  plug CORSPlug
+  plug PlausibleWeb.Router
 
   def websocket_url() do
-    config!(:websocket_url)
+    :plausible
+    |> Application.fetch_env!(__MODULE__)
+    |> Keyword.fetch!(:websocket_url)
   end
 
   def patch_session_opts() do
@@ -80,15 +75,6 @@ defmodule PlausibleWeb.Endpoint do
     # is used to inject the domain - this way we can authenticate
     # websocket requests within single root domain, in case websocket_url()
     # returns a ws{s}:// scheme (in which case SameSite=Lax is not applicable).
-    @session_options
-    |> Keyword.put(:domain, host())
-    |> Keyword.put(:key, "_plausible_#{Application.fetch_env!(:plausible, :environment)}")
-    |> Keyword.put(:secure, secure_cookie?())
-  end
-
-  defp config!(key) do
-    :plausible
-    |> Application.fetch_env!(__MODULE__)
-    |> Keyword.fetch!(key)
+    Keyword.put(@session_options, :domain, host())
   end
 end
