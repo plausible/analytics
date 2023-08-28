4da2eacf62cb42e04670a63200654992768a5e8b
Adam Rutkowski <hq@mtod.org>
2023-08-28 10:21:56 +0200

    Use Heroicons.trash icon

--- lib/plausible_web/live/goal_settings/list.ex
+++ lib/plausible_web/live/goal_settings/list.ex
@@ -79,22 +79,7 @@ defmodule PlausibleWeb.Live.GoalSettings.List do
                 class="text-sm text-red-600"
                 data-confirm={delete_confirmation_text(goal)}
               >
-                <svg
-                  class="feather feather-sm"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                >
-                  <polyline points="3 6 5 6 21 6"></polyline>
-                  <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2">
-                  </path>
-                  <line x1="10" y1="11" x2="10" y2="17"></line>
-                  <line x1="14" y1="11" x2="14" y2="17"></line>
-                </svg>
+                <Heroicons.trash class="feather feather-sm" />
               </button>
             </div>
           <% end %>
