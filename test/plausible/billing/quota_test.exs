9b713a72b80ab7ed5d08f011024fc0745172357d
Vinicius Brasil <vini@hey.com>
2023-08-29 18:02:14 -0300

    Create usage and limits functions for team members

--- test/plausible/billing/quota_test.exs
+++ test/plausible/billing/quota_test.exs
@@ -41,9 +41,6 @@ defmodule Plausible.Billing.QuotaTest do
     test "returns 50 when user in on trial" do
       user = insert(:user, trial_expiry_date: Timex.shift(Timex.now(), days: 7))
       assert 50 == Quota.site_limit(user)
-
-      user = insert(:user, trial_expiry_date: Timex.shift(Timex.now(), days: -7))
-      assert 50 == Quota.site_limit(user)
     end
 
     test "returns the subscription limit for enterprise users who have not paid yet" do
@@ -191,6 +188,157 @@ defmodule Plausible.Billing.QuotaTest do
       )
 
       assert Quota.monthly_pageview_usage(user) == 0
+    end
+  end
+
+  describe "team_member_usage/1" do
+    test "returns the number of members in all of the sites the user owns" do
+      me = insert(:user)
+
+      _site_i_own_1 =
+        insert(:site,
+          memberships: [
+            build(:site_membership, user: me, role: :owner),
+            build(:site_membership, user: build(:user), role: :viewer)
+          ]
+        )
+
+      _site_i_own_2 =
+        insert(:site,
+          memberships: [
+            build(:site_membership, user: me, role: :owner),
+            build(:site_membership, user: build(:user), role: :admin),
+            build(:site_membership, user: build(:user), role: :viewer)
+          ]
+        )
+
+      _site_i_own_3 =
+        insert(:site,
+          memberships: [
+            build(:site_membership, user: me, role: :owner)
+          ]
+        )
+
+      _site_i_have_access =
+        insert(:site,
+          memberships: [
+            build(:site_membership, user: me, role: :viewer),
+            build(:site_membership, user: build(:user), role: :viewer),
+            build(:site_membership, user: build(:user), role: :viewer),
+            build(:site_membership, user: build(:user), role: :viewer)
+          ]
+        )
+
+      assert Quota.team_member_usage(me) == 3
+    end
+
+    test "counts the same email address as one team member" do
+      me = insert(:user)
+      joe = insert(:user, email: "joe@plausible.test")
+
+      _site_i_own_1 =
+        insert(:site,
+          memberships: [
+            build(:site_membership, user: me, role: :owner),
+            build(:site_membership, user: joe, role: :viewer)
+          ]
+        )
+
+      _site_i_own_2 =
+        insert(:site,
+          memberships: [
+            build(:site_membership, user: me, role: :owner),
+            build(:site_membership, user: build(:user), role: :admin),
+            build(:site_membership, user: joe, role: :viewer)
+          ]
+        )
+
+      site_i_own_3 = insert(:site, memberships: [build(:site_membership, user: me, role: :owner)])
+
+      insert(:invitation, site: site_i_own_3, inviter: me, email: "joe@plausible.test")
+
+      assert Quota.team_member_usage(me) == 2
+    end
+
+    test "counts pending invitations as team members" do
+      me = insert(:user)
+      member = insert(:user)
+
+      site_i_own =
+        insert(:site,
+          memberships: [
+            build(:site_membership, user: me, role: :owner),
+            build(:site_membership, user: member, role: :admin)
+          ]
+        )
+
+      site_i_have_access =
+        insert(:site, memberships: [build(:site_membership, user: me, role: :admin)])
+
+      insert(:invitation, site: site_i_own, inviter: me)
+      insert(:invitation, site: site_i_own, inviter: member)
+      insert(:invitation, site: site_i_have_access, inviter: me)
+
+      assert Quota.team_member_usage(me) == 3
+    end
+
+    test "returns zero when user does not have any site" do
+      me = insert(:user)
+      assert Quota.team_member_usage(me) == 0
+    end
+
+    test "does not count email report recipients as team members" do
+      me = insert(:user)
+      site = insert(:site, memberships: [build(:site_membership, user: me, role: :owner)])
+
+      insert(:weekly_report,
+        site: site,
+        recipients: ["adam@plausible.test", "vini@plausible.test"]
+      )
+
+      assert Quota.team_member_usage(me) == 0
+    end
+  end
+
+  describe "team_member_limit/1" do
+    test "returns unlimited when user is on an old plan" do
+      user_on_v1 = insert(:user, subscription: build(:subscription, paddle_plan_id: @v1_plan_id))
+      user_on_v2 = insert(:user, subscription: build(:subscription, paddle_plan_id: @v2_plan_id))
+      user_on_v3 = insert(:user, subscription: build(:subscription, paddle_plan_id: @v3_plan_id))
+
+      assert :unlimited == Quota.team_member_limit(user_on_v1)
+      assert :unlimited == Quota.team_member_limit(user_on_v2)
+      assert :unlimited == Quota.team_member_limit(user_on_v3)
+    end
+
+    test "returns unlimited when user is on free_10k plan" do
+      user = insert(:user, subscription: build(:subscription, paddle_plan_id: "free_10k"))
+      assert :unlimited == Quota.team_member_limit(user)
+    end
+
+    test "returns unlimited when user is on an enterprise plan" do
+      user = insert(:user)
+      enterprise_plan = insert(:enterprise_plan, user_id: user.id)
+
+      _subscription =
+        insert(:subscription, user_id: user.id, paddle_plan_id: enterprise_plan.paddle_plan_id)
+
+      assert :unlimited == Quota.team_member_limit(user)
+    end
+
+    test "returns 5 when user in on trial" do
+      user = insert(:user, trial_expiry_date: Timex.shift(Timex.now(), days: 7))
+      assert 5 == Quota.team_member_limit(user)
+    end
+
+    test "is unlimited for enterprise customers" do
+      user =
+        insert(:user,
+          enterprise_plan: build(:enterprise_plan, paddle_plan_id: "123321"),
+          subscription: build(:subscription, paddle_plan_id: "123321")
+        )
+
+      assert :unlimited == Quota.team_member_limit(user)
     end
   end
 end
