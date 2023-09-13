03e220ec66dbd6fa05b0a9e5d603ee7104ff6065
Adam Rutkowski <hq@mtod.org>
2023-09-13 15:52:48 +0200

    Get rid of the funnels flag completely

--- lib/plausible/funnels.ex
+++ lib/plausible/funnels.ex
@@ -13,11 +13,6 @@ defmodule Plausible.Funnels do
 
   import Ecto.Query
 
-  @spec enabled_for?(any()) :: boolean()
-  def enabled_for?(actor) do
-    FunWithFlags.enabled?(:funnels, for: actor)
-  end
-
   @spec create(Plausible.Site.t(), String.t(), [map()]) ::
           {:ok, Funnel.t()}
           | {:error, Ecto.Changeset.t() | :invalid_funnel_size}
