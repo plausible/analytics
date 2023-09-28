aeb3c9bc5974fea48b8f6db286ae818955812005
Robert Joonas <robertjoonas16@gmail.com>
2023-09-28 20:00:04 +0300

    credo - another moduledoc

--- lib/mix/tasks/cancel_subscription.ex
+++ lib/mix/tasks/cancel_subscription.ex
@@ -1,15 +1,16 @@
 defmodule Mix.Tasks.CancelSubscription do
-  use Mix.Task
-  use Plausible.Repo
-  alias Plausible.{Repo, Billing.Subscription}
-  require Logger
-
-  @doc """
+  @moduledoc """
   This task is meant to replicate the behavior of cancelling
   a subscription. On production, this action is initiated by
   a Paddle webhook. Currently, only the subscription status
   is changed with that action.
   """
+
+  use Mix.Task
+  use Plausible.Repo
+  alias Plausible.{Repo, Billing.Subscription}
+  require Logger
+
   def run([paddle_subscription_id]) do
     Mix.Task.run("app.start")
 
