b7f65fec967a1a35afa415c89688c6db8962cd65
Adam Rutkowski <hq@mtod.org>
2023-10-02 10:56:29 +0200

    Simplify with statement

--- lib/plausible/plugins/api/tokens.ex
+++ lib/plausible/plugins/api/tokens.ex
@@ -11,9 +11,8 @@ defmodule Plausible.Plugins.API.Tokens do
 
   @spec create(Site.t(), String.t()) ::
           {:ok, Token.t(), String.t(), String.t()} | {:error, Ecto.Changeset.t()}
-  def create(%Site{} = site, description, generate \\ Token.generate()) do
-    with generated_token <- generate,
-         changeset <- Token.insert_changeset(site, generated_token, %{description: description}),
+  def create(%Site{} = site, description, generated_token \\ Token.generate()) do
+    with changeset <- Token.insert_changeset(site, generated_token, %{description: description}),
          {:ok, saved_token} <- Repo.insert(changeset) do
       {:ok, saved_token, generated_token.raw}
     end
