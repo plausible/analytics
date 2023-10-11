a1ab00478d0507d106dccba6d26916ce91ed5c68
Adam Rutkowski <hq@mtod.org>
2023-10-11 14:56:53 +0200

    No longer require domain to seek Plugins API Tokens

--- lib/plausible_web/plugs/authorize_plugins_api.ex
+++ lib/plausible_web/plugs/authorize_plugins_api.ex
@@ -10,14 +10,14 @@ defmodule PlausibleWeb.Plugs.AuthorizePluginsAPI do
   def init(opts), do: opts
 
   def call(conn, _opts \\ []) do
-    with {:ok, domain, token} <- extract_token(conn),
-         {:ok, conn} <- authorize(conn, domain, token) do
+    with {:ok, token} <- extract_token(conn),
+         {:ok, conn} <- authorize(conn, token) do
       conn
     end
   end
 
-  defp authorize(conn, domain, token_value) do
-    case Tokens.find(domain, token_value) do
+  defp authorize(conn, token_value) do
+    case Tokens.find(token_value) do
       {:ok, token} ->
         {:ok, Plug.Conn.assign(conn, :authorized_site, token.site)}
 
@@ -28,8 +28,8 @@ defmodule PlausibleWeb.Plugs.AuthorizePluginsAPI do
 
   defp extract_token(conn) do
     case Plug.BasicAuth.parse_basic_auth(conn) do
-      {token_identifier, token_value} ->
-        {:ok, token_identifier, token_value}
+      {_token_identifier, token_value} ->
+        {:ok, token_value}
 
       :error ->
         Errors.unauthorized(conn)
