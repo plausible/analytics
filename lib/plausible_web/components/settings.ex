3be122bb6ca58437129f160f76cb63aa4115c3b0
Adam Rutkowski <hq@mtod.org>
2023-10-16 16:26:58 +0200

    Remove unnecessary wildcards

--- lib/plausible_web/components/settings.ex
+++ lib/plausible_web/components/settings.ex
@@ -5,6 +5,6 @@ defmodule PlausibleWeb.Components.Settings do
   use Phoenix.Component
   use Phoenix.HTML
 
-  embed_templates("../templates/site/settings_search_console*")
-  embed_templates("../templates/site/settings_google_import*")
+  embed_templates("../templates/site/settings_search_console.html")
+  embed_templates("../templates/site/settings_google_import.html")
 end
