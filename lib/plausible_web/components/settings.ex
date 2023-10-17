366f370a1c269210ae94dd331e6ee242cdb7222e
Adam Rutkowski <hq@mtod.org>
2023-10-17 14:47:24 +0200

    Tweak Google integrations layout

--- lib/plausible_web/components/settings.ex
+++ lib/plausible_web/components/settings.ex
@@ -5,6 +5,8 @@ defmodule PlausibleWeb.Components.Settings do
   use Phoenix.Component
   use Phoenix.HTML
 
+  import PlausibleWeb.Components.Generic
+
   embed_templates("../templates/site/settings_search_console.html")
   embed_templates("../templates/site/settings_google_import.html")
 end
