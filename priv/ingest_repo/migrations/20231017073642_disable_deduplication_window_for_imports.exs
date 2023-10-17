4cdf843adecce3d377eaa88a26b3cca708865a94
Adrian Gruntkowski <adrian.gruntkowski@gmail.com>
2023-10-17 12:51:13 +0200

    Disable deduplication for improted_* tables in CH (#3429)
    * Disable deduplication for improted_* tables in CH
    
    * Make migration cluster-aware
    
    * Bump cache to avoid problems
    
    * Fix cache dump
    
    * Remove cache bump

---
+++ priv/ingest_repo/migrations/20231017073642_disable_deduplication_window_for_imports.exs
@@ -0,0 +1,34 @@
+defmodule Plausible.IngestRepo.Migrations.DisableDeduplicationWindowForImports do
+  use Ecto.Migration
+
+  @import_tables ~w(
+    imported_visitors 
+    imported_sources 
+    imported_pages 
+    imported_entry_pages 
+    imported_exit_pages 
+    imported_locations 
+    imported_devices 
+    imported_browsers 
+    imported_operating_systems
+  )
+
+  def change do
+    cluster_query = "SELECT 1 FROM system.replicas WHERE table = 'imported_visitors'"
+
+    cluster? =
+      case Ecto.Adapters.SQL.query(Plausible.IngestRepo, cluster_query) |> IO.inspect() do
+        {:ok, %{rows: []}} -> false
+        {:ok, _} -> true
+      end
+
+    for table <- @import_tables do
+      execute """
+              ALTER TABLE #{table} #{if cluster?, do: "ON CLUSTER '{cluster}'"}  MODIFY SETTING replicated_deduplication_window = 0
+              """,
+              """
+              ALTER TABLE #{table} #{if cluster?, do: "ON CLUSTER '{cluster}'"} MODIFY SETTING replicated_deduplication_window = 100
+              """
+    end
+  end
+end
