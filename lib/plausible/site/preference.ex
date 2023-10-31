26af88e7abf2aa997de840af6bec86787331b7f3
Adam Rutkowski <hq@mtod.org>
2023-10-31 16:32:54 +0100

    Implement basic changesets for user preferences

--- lib/plausible/site/preference.ex
+++ lib/plausible/site/preference.ex
@@ -4,14 +4,20 @@ defmodule Plausible.Site.Preference do
   """
 
   use Ecto.Schema
+  import Ecto.Changeset
 
   @type t() :: %__MODULE__{}
 
   defmodule Preferences do
     use Ecto.Schema
+    import Ecto.Changeset
 
     embedded_schema do
       field :is_pinned, :boolean, default: false
+    end
+
+    def changeset(attrs \\ %{}) do
+      cast(%__MODULE__{}, attrs, [:is_pinned])
     end
   end
 
@@ -22,5 +28,15 @@ defmodule Plausible.Site.Preference do
     belongs_to :site, Plausible.Site
 
     timestamps()
+  end
+
+  def changeset(user, site, attrs \\ %{}) do
+    embed_changeset = Preferences.changeset(attrs)
+
+    %__MODULE__{}
+    |> change()
+    |> put_embed(:preferences, embed_changeset)
+    |> put_assoc(:user, user)
+    |> put_assoc(:site, site)
   end
 end
