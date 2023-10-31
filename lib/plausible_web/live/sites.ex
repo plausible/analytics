05bcb796161837a3a33c9011e830cdb307e6a5e4
Adrian Gruntkowski <adrian.gruntkowski@gmail.com>
2023-10-31 12:01:35 +0100

    Swap inlined svg for Heroicons component in invitation modal

--- lib/plausible_web/live/sites.ex
+++ lib/plausible_web/live/sites.ex
@@ -313,22 +313,7 @@ defmodule PlausibleWeb.Live.Sites do
                 class="bg-white dark:bg-gray-800 rounded-md text-gray-400 dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               >
                 <span class="sr-only">Close</span>
-                <!-- Heroicon name: outline/x -->
-                <svg
-                  class="h-6 w-6"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
+                <Heroicons.x_mark class="h-6 w-6" />
               </button>
             </div>
             <div class="sm:flex sm:items-start">
