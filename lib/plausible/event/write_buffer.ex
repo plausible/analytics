6ec516fb84278e734f91f440d81ab7f933421dae
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2023-11-12 22:01:10 +0900

    encode early

--- lib/plausible/event/write_buffer.ex
+++ lib/plausible/event/write_buffer.ex
@@ -20,6 +20,8 @@ defmodule Plausible.Event.WriteBuffer do
      %{
        buffer: buffer,
        timer: timer,
+       insert_sql: insert_sql(Plausible.ClickhouseEventV2),
+       header: row_binary_header(Plausible.ClickhouseEventV2),
        buffer_size: length(buffer),
        max_buffer_size: max_buffer_size,
        flush_interval_ms: flush_interval_ms
@@ -27,7 +29,7 @@ defmodule Plausible.Event.WriteBuffer do
   end
 
   def insert(server \\ __MODULE__, event) do
-    GenServer.cast(server, {:insert, event})
+    GenServer.cast(server, {:insert, encode_row(Plausible.ClickhouseEventV2, event)})
     {:ok, event}
   end
 
@@ -37,47 +39,49 @@ defmodule Plausible.Event.WriteBuffer do
   end
 
   def handle_cast({:insert, event}, %{buffer: buffer} = state) do
-    new_buffer = [event | buffer]
-    new_buffer_size = state.buffer_size + 1
+    state = %{state | buffer: [event | buffer], buffer_size: state.buffer_size + 1}
 
-    if new_buffer_size >= state.max_buffer_size do
+    if state.buffer_size >= state.max_buffer_size do
       Logger.info("Buffer full, flushing to disk")
       Process.cancel_timer(state.timer)
-      do_flush(new_buffer)
+      do_flush(state)
       new_timer = Process.send_after(self(), :tick, state.flush_interval_ms)
       {:noreply, %{state | buffer: [], timer: new_timer, buffer_size: 0}}
     else
-      {:noreply, %{state | buffer: new_buffer, buffer_size: new_buffer_size}}
+      {:noreply, state}
     end
   end
 
-  def handle_info(:tick, %{buffer: buffer} = state) do
-    do_flush(buffer)
+  def handle_info(:tick, state) do
+    do_flush(state)
     timer = Process.send_after(self(), :tick, state.flush_interval_ms)
     {:noreply, %{state | buffer: [], buffer_size: 0, timer: timer}}
   end
 
-  def handle_call(:flush, _from, %{buffer: buffer} = state) do
+  def handle_call(:flush, _from, state) do
     Process.cancel_timer(state.timer)
-    do_flush(buffer)
+    do_flush(state)
     new_timer = Process.send_after(self(), :tick, state.flush_interval_ms)
     {:reply, :ok, %{state | buffer: [], buffer_size: 0, timer: new_timer}}
   end
 
-  def terminate(_reason, %{buffer: buffer}) do
+  def terminate(_reason, state) do
     Logger.info("Flushing event buffer before shutdown...")
-    do_flush(buffer)
+    do_flush(state)
   end
 
-  defp do_flush(buffer) do
+  defp do_flush(%{buffer: buffer} = state) do
     case buffer do
       [] ->
         nil
 
-      events ->
-        Logger.info("Flushing #{length(events)} events")
-        events = Enum.map(events, &(Map.from_struct(&1) |> Map.delete(:__meta__)))
-        IngestRepo.insert_all(Plausible.ClickhouseEventV2, events)
+      _not_empty ->
+        Logger.info("Flushing #{state.buffer_size} events")
+
+        IngestRepo.query!(state.insert_sql, [state.header | buffer],
+          command: :insert,
+          encode: false
+        )
     end
   end
 
@@ -87,5 +91,28 @@ defmodule Plausible.Event.WriteBuffer do
 
   defp default_max_buffer_size do
     Keyword.fetch!(Application.get_env(:plausible, IngestRepo), :max_buffer_size)
+  end
+
+  defp insert_sql(schema) do
+    "INSERT INTO #{schema.__schema__(:source)} FORMAT RowBinaryWithNamesAndTypes"
+  end
+
+  defp row_binary_header(schema) do
+    fields = schema.__schema__(:fields)
+    types = Enum.map(fields, fn field -> extract_type(schema, field) end)
+    names = Enum.map(fields, &String.Chars.Atom.to_string/1)
+    Ch.RowBinary.encode_names_and_types(names, types)
+  end
+
+  defp extract_type(schema, field) do
+    type = schema.__schema__(:type, field) || raise "missing type for #{field}"
+    type |> Ecto.Type.type() |> Ecto.Adapters.ClickHouse.Schema.remap_type(schema, field)
+  end
+
+  def encode_row(schema, struct) do
+    fields = schema.__schema__(:fields)
+    types = Enum.map(fields, fn field -> extract_type(schema, field) end)
+    values = Enum.map(fields, fn field -> Map.fetch!(struct, field) end)
+    Ch.RowBinary.encode_row(values, types)
   end
 end
