c4a3be9aa38b22b5d75af3ad719a5bc155d3c000
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2023-11-12 20:43:35 +0900

    fewer length/1 calls

--- lib/plausible/event/write_buffer.ex
+++ lib/plausible/event/write_buffer.ex
@@ -8,10 +8,22 @@ defmodule Plausible.Event.WriteBuffer do
     GenServer.start_link(__MODULE__, [], name: opts[:name] || __MODULE__)
   end
 
-  def init(buffer) do
+  def init(opts) do
+    buffer = opts[:buffer] || []
+    max_buffer_size = opts[:max_buffer_size] || default_max_buffer_size()
+    flush_interval_ms = opts[:flush_interval_ms] || default_flush_interval_ms()
+
     Process.flag(:trap_exit, true)
-    timer = Process.send_after(self(), :tick, flush_interval_ms())
-    {:ok, %{buffer: buffer, timer: timer}}
+    timer = Process.send_after(self(), :tick, flush_interval_ms)
+
+    {:ok,
+     %{
+       buffer: buffer,
+       timer: timer,
+       buffer_size: length(buffer),
+       max_buffer_size: max_buffer_size,
+       flush_interval_ms: flush_interval_ms
+     }}
   end
 
   def insert(server \\ __MODULE__, event) do
@@ -26,29 +38,30 @@ defmodule Plausible.Event.WriteBuffer do
 
   def handle_cast({:insert, event}, %{buffer: buffer} = state) do
     new_buffer = [event | buffer]
+    new_buffer_size = state.buffer_size + 1
 
-    if length(new_buffer) >= max_buffer_size() do
+    if new_buffer_size >= state.max_buffer_size do
       Logger.info("Buffer full, flushing to disk")
-      Process.cancel_timer(state[:timer])
+      Process.cancel_timer(state.timer)
       do_flush(new_buffer)
-      new_timer = Process.send_after(self(), :tick, flush_interval_ms())
-      {:noreply, %{buffer: [], timer: new_timer}}
+      new_timer = Process.send_after(self(), :tick, state.flush_interval_ms)
+      {:noreply, %{state | buffer: [], timer: new_timer, buffer_size: 0}}
     else
-      {:noreply, %{state | buffer: new_buffer}}
+      {:noreply, %{state | buffer: new_buffer, buffer_size: new_buffer_size}}
     end
   end
 
-  def handle_info(:tick, %{buffer: buffer}) do
+  def handle_info(:tick, %{buffer: buffer} = state) do
     do_flush(buffer)
-    timer = Process.send_after(self(), :tick, flush_interval_ms())
-    {:noreply, %{buffer: [], timer: timer}}
+    timer = Process.send_after(self(), :tick, state.flush_interval_ms)
+    {:noreply, %{state | buffer: [], buffer_size: 0, timer: timer}}
   end
 
   def handle_call(:flush, _from, %{buffer: buffer} = state) do
-    Process.cancel_timer(state[:timer])
+    Process.cancel_timer(state.timer)
     do_flush(buffer)
-    new_timer = Process.send_after(self(), :tick, flush_interval_ms())
-    {:reply, nil, %{buffer: [], timer: new_timer}}
+    new_timer = Process.send_after(self(), :tick, state.flush_interval_ms)
+    {:reply, :ok, %{state | buffer: [], buffer_size: 0, timer: new_timer}}
   end
 
   def terminate(_reason, %{buffer: buffer}) do
@@ -64,16 +77,15 @@ defmodule Plausible.Event.WriteBuffer do
       events ->
         Logger.info("Flushing #{length(events)} events")
         events = Enum.map(events, &(Map.from_struct(&1) |> Map.delete(:__meta__)))
-
         IngestRepo.insert_all(Plausible.ClickhouseEventV2, events)
     end
   end
 
-  defp flush_interval_ms() do
+  defp default_flush_interval_ms do
     Keyword.fetch!(Application.get_env(:plausible, IngestRepo), :flush_interval_ms)
   end
 
-  defp max_buffer_size() do
+  defp default_max_buffer_size do
     Keyword.fetch!(Application.get_env(:plausible, IngestRepo), :max_buffer_size)
   end
 end
