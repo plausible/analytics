6eb3dd68db0b84624ef59188d99b517e91066864
Adam Rutkowski <hq@mtod.org>
2023-11-14 15:25:23 +0100

    Clone community config

---
+++ config/community.exs
@@ -0,0 +1,25 @@
+import Config
+
+config :plausible, PlausibleWeb.Endpoint,
+  server: true,
+  debug_errors: true,
+  code_reloader: false,
+  check_origin: false,
+  watchers: [
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]},
+    npm: [
+      "run",
+      "deploy",
+      cd: Path.expand("../tracker", __DIR__)
+    ]
+  ],
+  live_reload: [
+    patterns: [
+      ~r{priv/static/.*(js|css|png|jpeg|jpg|gif|svg)$},
+      ~r"lib/plausible_web/(controllers|live|components|templates|views|plugs)/.*(ex|heex)$"
+    ]
+  ]
+
+config :phoenix, :stacktrace_depth, 20
+config :phoenix, :plug_init_mode, :runtime
