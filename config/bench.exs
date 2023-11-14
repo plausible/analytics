a26fcf96998df9c1d4b58ef387ce86252e4b3c9d
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2023-11-14 17:06:43 +0900

    rm benches

--- config/bench.exs
+++
@@ -1,9 +0,0 @@
-import Config
-
-config :plausible, PlausibleWeb.Endpoint,
-  server: false,
-  debug_errors: false,
-  code_reloader: false,
-  check_origin: false,
-  watchers: [],
-  live_reload: []
