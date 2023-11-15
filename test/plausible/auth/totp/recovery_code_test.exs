1067b8d8379e08a82c72aeb56037976a70015fa9
Adrian Gruntkowski <adrian.gruntkowski@gmail.com>
2023-11-15 16:39:34 +0100

    Implement schema and stateless logic for TOTP recovery codes

---
+++ test/plausible/auth/totp/recovery_code_test.exs
@@ -0,0 +1,75 @@
+defmodule Plausible.Auth.TOTP.RecoveryCodeTest do
+  use Plausible.DataCase, async: true
+
+  alias Plausible.Auth.TOTP.RecoveryCode
+
+  describe "generate_codes/1" do
+    test "generates random codes conforming agreed upon format" do
+      codes = RecoveryCode.generate_codes(3)
+
+      Enum.each(codes, fn code ->
+        assert Regex.match?(~r/[A-Z0-9]{10}/, code)
+      end)
+
+      assert codes == Enum.uniq(codes)
+    end
+  end
+
+  describe "match?/1" do
+    test "verifies that provided code matches against a digest of stored recovery code" do
+      [plain_code] = RecoveryCode.generate_codes(1)
+
+      recovery_code =
+        build(:user)
+        |> RecoveryCode.changeset(plain_code)
+        |> Ecto.Changeset.apply_changes()
+
+      assert RecoveryCode.match?(recovery_code, plain_code)
+      refute RecoveryCode.match?(recovery_code, "INVALID")
+    end
+  end
+
+  describe "changeset/2" do
+    test "builds a valid changeset when provided valid code format" do
+      [plain_code] = RecoveryCode.generate_codes(1)
+
+      changeset = RecoveryCode.changeset(build(:user), plain_code)
+
+      assert changeset.valid?
+      assert changeset.changes.user
+      assert changeset.changes.code_digest
+
+      assert RecoveryCode.match?(Ecto.Changeset.apply_changes(changeset), plain_code)
+    end
+
+    test "crashes when code in invalid format is passed" do
+      user = build(:user)
+
+      assert_raise FunctionClauseError, fn ->
+        RecoveryCode.changeset(user, "INVALID")
+      end
+
+      assert_raise FunctionClauseError, fn ->
+        RecoveryCode.changeset(user, 123)
+      end
+    end
+  end
+
+  describe "changeset_to_map/2" do
+    test "converts changeset to a map suitable for Repo.insert_all/3" do
+      user = %{id: user_id} = insert(:user)
+      [plain_code] = RecoveryCode.generate_codes(1)
+
+      changeset =
+        %{changes: %{code_digest: code_digest}} = RecoveryCode.changeset(user, plain_code)
+
+      now = NaiveDateTime.utc_now() |> NaiveDateTime.truncate(:second)
+
+      assert %{
+               user_id: ^user_id,
+               code_digest: ^code_digest,
+               inserted_at: ^now
+             } = RecoveryCode.changeset_to_map(changeset, now)
+    end
+  end
+end
