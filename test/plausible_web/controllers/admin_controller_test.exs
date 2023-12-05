a116c8c45e40683979d8a7446d1bd63c0f5e3a77
Robert Joonas <robertjoonas16@gmail.com>
2023-12-05 19:16:56 +0000

    add a test for admin route authorization

---
+++ test/plausible_web/controllers/admin_controller_test.exs
@@ -0,0 +1,12 @@
+defmodule PlausibleWeb.AdminControllerTest do
+  use PlausibleWeb.ConnCase
+
+  describe "GET /crm/auth/user/:user_id/usage" do
+    setup [:create_user, :log_in]
+
+    test "returns 403 if the logged in user is not a super admin", %{conn: conn} do
+      conn = get(conn, "/crm/auth/user/1/usage")
+      assert response(conn, 403) == "Not allowed"
+    end
+  end
+end
