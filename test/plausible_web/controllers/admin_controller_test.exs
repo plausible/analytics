d05acbfac36f3c17d5efd10e3ba82460942639f6
Robert Joonas <robertjoonas16@gmail.com>
2023-12-05 19:23:46 +0000

    add full_build_only tag

--- test/plausible_web/controllers/admin_controller_test.exs
+++ test/plausible_web/controllers/admin_controller_test.exs
@@ -4,6 +4,7 @@ defmodule PlausibleWeb.AdminControllerTest do
   describe "GET /crm/auth/user/:user_id/usage" do
     setup [:create_user, :log_in]
 
+    @tag :full_build_only
     test "returns 403 if the logged in user is not a super admin", %{conn: conn} do
       conn = get(conn, "/crm/auth/user/1/usage")
       assert response(conn, 403) == "Not allowed"
