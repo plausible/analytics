7a4cafac16ac5702ebc70f9d70d8ff9972312e8a
ruslandoga <doga.ruslan@gmail.com>
2023-12-11 22:56:18 +0800

    dump buffer on invalid insert (#3613)
    * dump buffer on invalid insert
    
    * dump to PERSISTENT_CACHE_DIR
    
    * fix warning
    
    * ensure dir is not nil

--- config/runtime.exs
+++ config/runtime.exs
@@ -291,7 +291,8 @@ config :plausible,
   super_admin_user_ids: super_admin_user_ids,
   is_selfhost: is_selfhost,
   custom_script_name: custom_script_name,
-  log_failed_login_attempts: log_failed_login_attempts
+  log_failed_login_attempts: log_failed_login_attempts,
+  persistent_cache_dir: maxmind_cache_dir
 
 config :plausible, :selfhost,
   enable_email_verification: enable_email_verification,
