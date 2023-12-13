5a044a6c5a5cb2c9d8053d097128ea21aefa43fd
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2023-12-13 13:16:17 +0800

    add session collapse order test

--- test/plausible/session/cache_store_test.exs
+++ test/plausible/session/cache_store_test.exs
@@ -116,4 +116,63 @@ defmodule Plausible.Session.CacheStoreTest do
     assert_receive({WriteBuffer, :insert, [[session, _negative_record]]})
     assert session.duration == 10
   end
+
+  describe "collapse order" do
+    defp flush(events) do
+      for e <- events, do: CacheStore.on_event(e, nil)
+      Plausible.Session.WriteBuffer.flush()
+    end
+
+    test "across parts" do
+      e = build(:event, name: "pageview")
+
+      flush([%{e | pathname: "/"}])
+      flush([%{e | pathname: "/exit"}])
+
+      session_q = from s in Plausible.ClickhouseSessionV2, where: s.site_id == ^e.site_id
+      session = Plausible.ClickhouseRepo.one!(session_q, settings: [final: true])
+
+      refute session.is_bounce
+      assert session.entry_page == "/"
+      assert session.exit_page == "/exit"
+    end
+
+    test "within parts" do
+      e = build(:event, name: "pageview")
+
+      flush([
+        %{e | pathname: "/"},
+        %{e | pathname: "/exit"}
+      ])
+
+      session_q = from s in Plausible.ClickhouseSessionV2, where: s.site_id == ^e.site_id
+      session = Plausible.ClickhouseRepo.one!(session_q)
+
+      refute session.is_bounce
+      assert session.entry_page == "/"
+      assert session.exit_page == "/exit"
+    end
+
+    test "across and within parts" do
+      e = build(:event, name: "pageview")
+
+      flush([
+        %{e | pathname: "/"},
+        %{e | pathname: "/about"}
+      ])
+
+      flush([
+        %{e | pathname: "/login"},
+        %{e | pathname: "/exit"}
+      ])
+
+      session_q = from s in Plausible.ClickhouseSessionV2, where: s.site_id == ^e.site_id
+      session = Plausible.ClickhouseRepo.one!(session_q, settings: [final: true])
+
+      refute session.is_bounce
+      assert session.entry_page == "/"
+      assert session.exit_page == "/exit"
+      assert session.events == 4
+    end
+  end
 end
