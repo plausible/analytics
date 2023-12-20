64993fce8204bf146f11a9c01bfa324e260ee1fc
Adam Rutkowski <hq@mtod.org>
2023-12-20 11:16:53 +0100

    Fix typo

--- priv/repo/migrations/20231220072829_add_accept_traffic_until_to_user.exs
+++ priv/repo/migrations/20231220072829_add_accept_traffic_until_to_user.exs
@@ -2,7 +2,7 @@ defmodule Plausible.Repo.Migrations.AddAcceptTrafficUntilToUser do
   use Ecto.Migration
 
   def change do
-    # deletin accept_traffic_until from sites table will come later, not to crash anything live right now
+    # deleting accept_traffic_until from sites table will come later, not to crash anything live right now
     alter table(:users) do
       add :accept_traffic_until, :date
     end
