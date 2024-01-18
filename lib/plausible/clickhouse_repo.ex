d4d86637e40c3a58a9f22af4618fcccab1156592
Adam Rutkowski <hq@mtod.org>
2024-01-18 07:40:20 +0100

    PoC: keep log_comment metadata in ClickHouse queries

--- lib/plausible/clickhouse_repo.ex
+++ lib/plausible/clickhouse_repo.ex
@@ -12,6 +12,51 @@ defmodule Plausible.ClickhouseRepo do
     end
   end
 
+  defmodule KeepRouteContext do
+    alias Plausible.ClickhouseRepo
+
+    def init(_opts), do: nil
+
+    def call(conn, _) do
+      ClickhouseRepo.set_context(%{request_path: conn.request_path})
+      conn
+    end
+  end
+
+  @logger_metadata_key __MODULE__
+  def set_context(new) when is_map(new) do
+    metadata =
+      case :logger.get_process_metadata() do
+        %{@logger_metadata_key => ctx} -> Map.update(ctx, :log_comment, new, &Map.merge(&1, new))
+        _ -> %{:log_comment => new}
+      end
+
+    :logger.update_process_metadata(%{@logger_metadata_key => metadata})
+  end
+
+  def get_context() do
+    case :logger.get_process_metadata() do
+      %{@logger_metadata_key => ctx} ->
+        ctx
+
+      %{} ->
+        %{}
+
+      :undefined ->
+        %{}
+    end
+  end
+
+  @impl true
+  def prepare_query(_operation, query, opts) do
+    setting = {:log_comment, Jason.encode!(get_context())}
+
+    opts =
+      Keyword.update(opts, :settings, [setting], fn settings -> [setting | settings] end)
+
+    {query, opts}
+  end
+
   @task_timeout 60_000
   def parallel_tasks(queries) do
     ctx = OpenTelemetry.Ctx.get_current()
