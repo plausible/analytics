c57c114eff55a67289b7c0a079bffe0b5210cb02
Robert Joonas <robertjoonas16@gmail.com>
2024-01-25 18:32:13 +0000

    move calculate_cr function into Stats.Util

--- lib/plausible/stats/util.ex
+++ lib/plausible/stats/util.ex
@@ -20,4 +20,12 @@ defmodule Plausible.Stats.Util do
   def remove_internal_visits_metric(result) when is_map(result) do
     Map.delete(result, :__internal_visits)
   end
+
+  def calculate_cr(nil, _converted_visitors), do: nil
+
+  def calculate_cr(unique_visitors, converted_visitors) do
+    if unique_visitors > 0,
+      do: Float.round(converted_visitors / unique_visitors * 100, 1),
+      else: 0.0
+  end
 end
--- lib/plausible_web/controllers/api/stats_controller.ex
+++ lib/plausible_web/controllers/api/stats_controller.ex
@@ -3,6 +3,7 @@ defmodule PlausibleWeb.Api.StatsController do
   use PlausibleWeb, :controller
   use Plausible.Repo
   use PlausibleWeb.Plugs.ErrorHandler
+  import Plausible.Stats.Util
   alias Plausible.Stats
   alias Plausible.Stats.{Query, Comparisons}
   alias PlausibleWeb.Api.Helpers, as: H
@@ -1116,14 +1117,6 @@ defmodule PlausibleWeb.Api.StatsController do
     else
       json(conn, sizes)
     end
-  end
-
-  defp calculate_cr(nil, _converted_visitors), do: nil
-
-  defp calculate_cr(unique_visitors, converted_visitors) do
-    if unique_visitors > 0,
-      do: Float.round(converted_visitors / unique_visitors * 100, 1),
-      else: 0.0
   end
 
   def conversions(conn, params) do
