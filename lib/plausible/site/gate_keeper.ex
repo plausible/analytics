18ddf1e7a05bf9fc6f765621b55c5698f73b29ed
hq1 <hq@mtod.org>
2024-01-29 13:37:30 +0100

    Re enable accept traffic until notifications (#3737)
    * Revert "Remove 0-sized queue"
    
    This reverts commit 6d229b3f70cf567c3df0abfe4de7bc1c882b0355.
    
    * Revert "Temporarily remove notification cron worker (#3732)"
    
    This reverts commit 447e3929cb2d4964cd79ea00617271c8f0cffd85.
    
    * Revert "Temporarily disable accept_traffic_until actions (#3730)"
    
    This reverts commit 58afe3376f248a8746b5e2b91f33ac072c0d9fa1.
    
    * Send accept_traffic_until notifications at 8 UTC rather than 5
    
    * Add option for dry-run
    
    * Expose dry_run in a dedicated function

--- lib/plausible/site/gate_keeper.ex
+++ lib/plausible/site/gate_keeper.ex
@@ -43,10 +43,17 @@ defmodule Plausible.Site.GateKeeper do
   end
 
   defp policy(domain, opts) when is_binary(domain) do
-    if site = Cache.get(domain, Keyword.get(opts, :cache_opts, [])) do
-      check_rate_limit(site, opts)
+    with from_cache <- Cache.get(domain, Keyword.get(opts, :cache_opts, [])),
+         site = %Site{owner: %{accept_traffic_until: accept_traffic_until}} <- from_cache do
+      if not is_nil(accept_traffic_until) and
+           Date.after?(Date.utc_today(), accept_traffic_until) do
+        :payment_required
+      else
+        check_rate_limit(site, opts)
+      end
     else
-      @policy_for_non_existing_sites
+      _ ->
+        @policy_for_non_existing_sites
     end
   end
 
