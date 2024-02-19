b30069a8d563811ef46cfcbfa141c8f5b3e8a37e
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2024-02-19 20:58:59 +0800

    more tests

--- lib/plausible/imported/csv_importer.ex
+++ lib/plausible/imported/csv_importer.ex
@@ -15,6 +15,41 @@ defmodule Plausible.Imported.CSVImporter do
   @impl true
   def email_template(), do: "google_analytics_import.html"
 
+  @doc ~S"""
+  Extracts min/max dates from the date ranges specified in the upload files.
+
+  Examples:
+
+      iex> extract_min_max_date_range([])
+      ** (ArgumentError) filenames cannot be empty
+
+      iex> extract_min_max_date_range(["my_data.csv"])
+      ** (ArgumentError) filename "my_data.csv"  does not conform to the expected "#{table}_#{start_date}_#{end_date}.csv" format
+
+      iex> extract_min_max_date_range(["imported_devices_00010101_20250101.csv"])
+      Date.range(~D[0001-01-01], ~D[2025-01-01])
+
+      iex> extract_min_max_date_range(["imported_devices_00010101_09990101.csv", "imported_browsers_10010101_20250101.csv"])
+      Date.range(~D[0001-01-01], ~D[2025-01-01])
+
+  """
+  @spec extract_min_max_date_range([String.t()]) :: Date.Range.t()
+  def extract_min_max_date_range(filenames) do
+    if Enum.empty?(filenames) do
+      raise ArgumentError, message: "filenames cannot be empty"
+    end
+
+    ranges =
+      Enum.map(filenames, fn filename ->
+        [_table, start_date, end_date] = parse_filename!(filename)
+        Date.range(start_date, end_date)
+      end)
+
+    min = Enum.min_by(ranges, & &1.first)
+    max = Enum.max_by(ranges, & &1.last)
+    Date.range(min.first, max.last)
+  end
+
   @impl true
   def parse_args(%{"uploads" => uploads}), do: [uploads: uploads]
 
@@ -86,9 +121,25 @@ defmodule Plausible.Imported.CSVImporter do
   for {table, input_structure} <- input_structures do
     defp input_structure!(unquote(table)), do: unquote(input_structure)
     defp ensure_importable_table!(unquote(table)), do: :ok
+
+    defp parse_filename!(
+           <<unquote(table)::bytes, ?_, start_date::8-bytes, ?_, end_date::8-bytes, ".csv">>
+         ) do
+      [
+        unquote(table),
+        Timex.parse!(start_date, "{YYYY}{0M}{0D}"),
+        Timex.parse!(end_date, "{YYYY}{0M}{0D}")
+      ]
+    end
   end
 
   defp ensure_importable_table!(table) do
     raise ArgumentError, "table #{table} is not supported for data import"
+  end
+
+  defp parse_filename!(filename) do
+    raise ArgumentError,
+          "filename #{inspect(filename)} " <>
+            ~S[ does not conform to the expected "#{table}_#{start_date}_#{end_date}.csv" format]
   end
 end
---
+++ test/plausible/imported/csv_importer_test.exs
@@ -0,0 +1,78 @@
+defmodule Plausible.Imported.CSVImporterTest do
+  use Plausible.DataCase, async: true
+
+  doctest Plausible.Imported.CSVImporter, import: true
+
+  @moduletag :minio
+
+  # uses https://min.io
+  # docker run -d --rm -p 9000:9000 -p 9001:9001 --name minio minio/minio server /data --console-address ":9001"
+  # docker exec minio mc alias set local http://localhost:9000 minioadmin minioadmin
+  # docker exec minio mc mb local/imports
+
+  alias Plausible.Imported.{CSVImporter, SiteImport}
+  require SiteImport
+
+  describe "new_import/3 and parse_args/1" do
+    setup [:create_user, :create_new_site]
+
+    test "parses job args properly", %{user: user, site: site} do
+      tables = [
+        "imported_browsers",
+        "imported_devices",
+        "imported_entry_pages",
+        "imported_exit_pages",
+        "imported_locations",
+        "imported_operating_systems",
+        "imported_pages",
+        "imported_sources",
+        "imported_visitors"
+      ]
+
+      start_date = "20231001"
+      end_date = "20240102"
+
+      uploads =
+        Enum.map(tables, fn table ->
+          filename = "#{table}_#{start_date}_#{end_date}.csv"
+          s3_path = "#{site.id}/#{filename}"
+          %{"filename" => filename, "s3_path" => s3_path}
+        end)
+
+      min_max_date_range =
+        uploads
+        |> Enum.map(& &1["filename"])
+        |> CSVImporter.extract_min_max_date_range()
+
+      assert {:ok, job} =
+               CSVImporter.new_import(site, user,
+                 start_date: min_max_date_range.first,
+                 end_date: min_max_date_range.last,
+                 uploads: uploads
+               )
+
+      assert %Oban.Job{args: %{"import_id" => import_id, "uploads" => ^uploads} = args} =
+               Repo.reload!(job)
+
+      assert [
+               %{
+                 id: ^import_id,
+                 source: :csv,
+                 start_date: ~D[2023-10-01],
+                 end_date: ~D[2024-01-02],
+                 status: SiteImport.pending()
+               }
+             ] = Plausible.Imported.list_all_imports(site)
+
+      assert %{imported_data: nil} = Repo.reload!(site)
+      assert CSVImporter.parse_args(args) == [uploads: uploads]
+    end
+  end
+
+  describe "import_data/2" do
+    setup [:create_user, :create_new_site]
+
+    test "imports tables from S3"
+    test "invalid CSV"
+  end
+end
