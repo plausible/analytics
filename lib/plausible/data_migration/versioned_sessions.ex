b0b68fb40ab74b717644ed6f876491a8d192cb9f
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2024-02-20 13:42:54 +0200

    Add an early exit to migration

--- lib/plausible/data_migration/versioned_sessions.ex
+++ lib/plausible/data_migration/versioned_sessions.ex
@@ -6,6 +6,10 @@ defmodule Plausible.DataMigration.VersionedSessions do
   use Plausible.DataMigration, dir: "VersionedSessions", repo: Plausible.IngestRepo
 
   @suffix_format "{YYYY}{0M}{0D}{h24}{m}{s}"
+  @versioned_table_engines [
+    "ReplicatedVersionedCollapsingMergeTree",
+    "VersionedCollapsingMergeTree"
+  ]
 
   def run(opts \\ []) do
     run_exchange? = Keyword.get(opts, :run_exchange?, true)
@@ -21,22 +25,55 @@ defmodule Plausible.DataMigration.VersionedSessions do
     {:ok, %{rows: partitions}} = run_sql("list-partitions")
     partitions = Enum.map(partitions, fn [part] -> part end)
 
-    {:ok, %{rows: [[table_settings]]}} = run_sql("get-sessions-table-settings")
+    {:ok, %{rows: [[current_table_engine, table_settings]]}} =
+      run_sql("get-sessions-table-settings")
 
-    run_sql("drop-sessions-tmp-table", cluster?: cluster?)
+    if Enum.member?(@versioned_table_engines, current_table_engine) do
+      IO.puts("sessions_v2 table is already versioned, no migration needed")
+    else
+      {:ok, _} = run_sql("drop-sessions-tmp-table", cluster?: cluster?)
 
-    run_sql("create-sessions-tmp-table",
-      cluster?: cluster?,
-      table_settings: table_settings,
-      unique_suffix: unique_suffix
-    )
+      {:ok, _} =
+        run_sql("create-sessions-tmp-table",
+          cluster?: cluster?,
+          table_settings: table_settings,
+          unique_suffix: unique_suffix
+        )
 
-    for partition <- partitions do
-      run_sql("attach-partition", partition: partition)
+      for partition <- partitions do
+        {:ok, _} = run_sql("attach-partition", partition: partition)
+      end
+
+      if run_exchange? do
+        run_exchange(cluster?)
+      end
+
+      IO.puts("Migration done!")
     end
+  end
 
-    if run_exchange? do
-      run_sql("exchange-sessions-tables", cluster?: cluster?)
+  defp run_exchange(cluster?) do
+    case run_sql("exchange-sessions-tables", cluster?: cluster?) do
+      {:ok, _} ->
+        nil
+
+      # Docker containers don't seem to support EXCHANGE TABLE, hack around this with a non-atomic swap
+      {:error, %Ch.Error{code: 1}} ->
+        IO.puts("Exchanging sessions_v2 and sessions_v2_tmp_versioned non-atomically")
+
+        {:ok, _} =
+          run_sql("rename-table",
+            from: "sessions_v2",
+            to: "sessions_v2_backup",
+            cluster?: cluster?
+          )
+
+        {:ok, _} =
+          run_sql("rename-table",
+            from: "sessions_v2_tmp_versioned",
+            to: "sessions_v2",
+            cluster?: cluster?
+          )
     end
   end
 end
