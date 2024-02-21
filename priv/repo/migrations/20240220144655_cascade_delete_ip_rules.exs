fbf2748e5334f3ec038ac1a18d2229013eb66ffd
Adam Rutkowski <hq@mtod.org>
2024-02-21 08:31:25 +0100

    drop_if_exists

--- priv/repo/migrations/20240220144655_cascade_delete_ip_rules.exs
+++ priv/repo/migrations/20240220144655_cascade_delete_ip_rules.exs
@@ -2,7 +2,7 @@ defmodule Plausible.Repo.Migrations.CascadeDeleteIpRules do
   use Ecto.Migration
 
   def up do
-    drop constraint(:shield_rules_ip, "shield_rules_ip_site_id_fkey")
+    drop_if_exists constraint(:shield_rules_ip, "shield_rules_ip_site_id_fkey")
 
     alter table(:shield_rules_ip) do
       modify :site_id, references(:sites, on_delete: :delete_all), null: false
@@ -10,7 +10,7 @@ defmodule Plausible.Repo.Migrations.CascadeDeleteIpRules do
   end
 
   def down do
-    drop constraint(:shield_rules_ip, "shield_rules_ip_site_id_fkey")
+    drop_if_exists constraint(:shield_rules_ip, "shield_rules_ip_site_id_fkey")
 
     alter table(:shield_rules_ip) do
       modify :site_id, references(:sites), null: false
