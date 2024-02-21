3f7e68f2b42129b64f8a638c7daa0d1299961652
Adrian Gruntkowski <adrian.gruntkowski@gmail.com>
2024-02-21 10:05:46 +0100

    Implement LV date input using flatpickr

--- assets/js/liveview/live_socket.js
+++ assets/js/liveview/live_socket.js
@@ -1,4 +1,5 @@
 import "phoenix_html"
+import flatpickr from "flatpickr"
 import Alpine from 'alpinejs'
 import { Socket } from "phoenix"
 import { LiveSocket } from "phoenix_live_view"
@@ -16,6 +17,19 @@ if (csrfToken && websocketUrl) {
         setTimeout(afterMetrics, 5000)
         params.callback = afterMetrics
         window.plausible(event_name, params)
+      })
+    }
+  }
+  Hooks.DatePicker = {
+    mounted() {
+      flatpickr(this.el, {
+        maxDate: 'today',
+        showMonths: 1,
+        static: true,
+        animate: true,
+        onChange: (_selected, dateStr) => {
+          document.getElementById(this.el.id + "-display").innerHTML = dateStr
+        }
       })
     }
   }
