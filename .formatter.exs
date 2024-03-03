1033334279b977414b038eedc7a204f4d8567926
Adam Rutkowski <hq@mtod.org>
2024-03-03 14:17:18 +0100

    wip

--- .formatter.exs
+++ .formatter.exs
@@ -2,5 +2,11 @@
   plugins: [Phoenix.LiveView.HTMLFormatter],
   import_deps: [:ecto, :ecto_sql, :phoenix],
   subdirectories: ["priv/*/migrations"],
-  inputs: ["*.{heex,ex,exs}", "{config,lib,test,extra}/**/*.{heex,ex,exs}", "priv/*/seeds.exs"]
+  inputs: ["*.{heex,ex,exs}", "{config,lib,test,extra}/**/*.{heex,ex,exs}", "priv/*/seeds.exs"],
+  locals_without_parens: [
+    pageview: 1,
+    pageview: 2,
+    custom_event: 1,
+    custom_event: 2
+  ]
 ]
