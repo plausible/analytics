48e795ce98b5272e4b7b3182c6420c5df22d15d0
Adam Rutkowski <hq@mtod.org>
2024-03-03 14:17:18 +0100

    wip

--- test/plausible/funnels_test.exs
+++ test/plausible/funnels_test.exs
@@ -173,28 +173,19 @@ defmodule Plausible.FunnelsTest do
           )
 
         journey site do
-          pageview("/irrelevant/page/not/in/funnel")
+          pageview "/irrelevant/page/not/in/funnel"
         end
 
         journey site do
-          pageview("/go/to/blog/foo")
-          custom_event("Signup")
-          pageview("/checkout")
+          pageview "/go/to/blog/foo"
+          custom_event "Signup"
+          pageview "/checkout"
         end
 
         journey site do
-          pageview("/go/to/blog/bar")
-          custom_event("Signup")
+          pageview "/go/to/blog/bar"
+          custom_event "Signup"
         end
-
-        # populate_stats(site, [
-        #   build(:pageview, pathname: "/irrelevant/page/not/in/funnel", user_id: 999),
-        #   build(:pageview, pathname: "/go/to/blog/foo", user_id: 123),
-        #   build(:event, name: "Signup", user_id: 123),
-        #   build(:pageview, pathname: "/checkout", user_id: 123),
-        #   build(:pageview, pathname: "/go/to/blog/bar", user_id: 666),
-        #   build(:event, name: "Signup", user_id: 666)
-        # ])
 
         query = Plausible.Stats.Query.from(site, %{"period" => "all"})
 
