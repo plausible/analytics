bac7fde2f7185a8dc2a1d0067139481acd3fd795
Adam Rutkowski <hq@mtod.org>
2024-03-11 13:32:27 +0100

    Try less

--- lib/plausible/cache/adapter.ex
+++ lib/plausible/cache/adapter.ex
@@ -29,93 +29,79 @@ defmodule Plausible.Cache.Adapter do
 
   @spec size(atom()) :: non_neg_integer() | nil
   def size(cache_name) do
-    try do
-      ConCache.size(cache_name)
-    catch
-      :exit, _ -> nil
-    end
+    ConCache.size(cache_name)
+  catch
+    :exit, _ -> nil
   end
 
   @spec get(atom(), any()) :: any()
   def get(cache_name, key) do
-    try do
-      cache_name
-      |> ConCache.get(key)
-      |> Plausible.Cache.Stats.track(cache_name)
-    catch
-      :exit, _ ->
-        Logger.error("Error retrieving key from '#{inspect(cache_name)}'")
-        nil
-    end
+    cache_name
+    |> ConCache.get(key)
+    |> Plausible.Cache.Stats.track(cache_name)
+  catch
+    :exit, _ ->
+      Logger.error("Error retrieving key from '#{inspect(cache_name)}'")
+      nil
   end
 
   @spec get(atom(), any(), (-> any())) :: any()
   def get(cache_name, key, fallback_fn) do
-    try do
-      cache_name
-      |> ConCache.get_or_store(key, fn ->
-        {:from_fallback, fallback_fn.()}
-      end)
-      |> Plausible.Cache.Stats.track(cache_name)
-    catch
-      :exit, _ ->
-        Logger.error("Error retrieving key from '#{inspect(cache_name)}'")
-        nil
-    end
+    cache_name
+    |> ConCache.get_or_store(key, fn ->
+      {:from_fallback, fallback_fn.()}
+    end)
+    |> Plausible.Cache.Stats.track(cache_name)
+  catch
+    :exit, _ ->
+      Logger.error("Error retrieving key from '#{inspect(cache_name)}'")
+      nil
   end
 
   @spec put(atom(), any(), any()) :: any()
   def put(cache_name, key, value) do
-    try do
-      :ok = ConCache.put(cache_name, key, value)
-      value
-    catch
-      :exit, _ ->
-        Logger.error("Error putting a key to '#{cache_name}'")
-        nil
-    end
+    :ok = ConCache.put(cache_name, key, value)
+    value
+  catch
+    :exit, _ ->
+      Logger.error("Error putting a key to '#{cache_name}'")
+      nil
   end
 
   @spec put_many(atom(), [any()]) :: :ok
   def put_many(cache_name, items) when is_list(items) do
-    try do
-      true = :ets.insert(ConCache.ets(cache_name), items)
+    true = :ets.insert(ConCache.ets(cache_name), items)
+    :ok
+  catch
+    :exit, _ ->
+      Logger.error("Error putting keys to '#{cache_name}'")
       :ok
-    catch
-      :exit, _ ->
-        Logger.error("Error putting keys to '#{cache_name}'")
-        :ok
-    end
   end
 
   @spec delete(atom(), any()) :: :ok
   def delete(cache_name, key) do
-    try do
-      ConCache.dirty_delete(cache_name, key)
-    catch
-      :exit, _ ->
-        Logger.error("Error deleting a key in '#{cache_name}'")
-        :ok
-    end
+    ConCache.dirty_delete(cache_name, key)
+  catch
+    :exit, _ ->
+      Logger.error("Error deleting a key in '#{cache_name}'")
+      :ok
   end
 
   @spec keys(atom()) :: Enumerable.t()
   def keys(cache_name) do
-    try do
-      ets = ConCache.ets(cache_name)
+    ets = ConCache.ets(cache_name)
 
-      Stream.resource(
-        fn -> :ets.first(ets) end,
-        fn
-          :"$end_of_table" -> {:halt, nil}
-          prev_key -> {[prev_key], :ets.next(ets, prev_key)}
-        end,
-        fn _ -> :ok end
-      )
-    catch
-      :exit, _ ->
-        Logger.error("Error retrieving key from '#{inspect(cache_name)}'")
-        []
-    end
+    Stream.resource(
+      fn -> :ets.first(ets) end,
+      fn
+        :"$end_of_table" -> {:halt, nil}
+        prev_key -> {[prev_key], :ets.next(ets, prev_key)}
+      end,
+      fn _ -> :ok end
+    )
+  catch
+    :exit, _ ->
+      Logger.error("Error retrieving key from '#{inspect(cache_name)}'")
+      []
   end
 end
