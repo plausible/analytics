0618d7fabbc7e070a8bb00a57d3692b91b3e5cde
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2024-03-17 17:44:04 +0800

    fix typo

--- rel/overlays/import_extra_config.exs
+++ rel/overlays/import_extra_config.exs
@@ -3,6 +3,6 @@ import Plausible.ConfigHelpers
 
 config_dir = System.get_env("CONFIG_DIR", "/run/secrets")
 
-if extra_config_path = get_var_from_path_or_env(config_dir, "EXTRA_CONIFG_PATH") do
+if extra_config_path = get_var_from_path_or_env(config_dir, "EXTRA_CONFIG_PATH") do
   import_config extra_config_path
 end
