fd879eeb165698ad2ca429f3992facc77e5969f5
Uku Taht <Uku.taht@gmail.com>
2024-03-21 17:45:34 +0200

    Store referrers from android apps (#3715)
    * Store referrers from android apps
    
    * Add test for unknown referrer protocol
    
    * Store android referrer protocol

--- lib/plausible_web/refinspector.ex
+++ lib/plausible_web/refinspector.ex
@@ -7,7 +7,7 @@ defmodule PlausibleWeb.RefInspector do
         uri = URI.parse(String.trim(ref.referer))
 
         if right_uri?(uri) do
-          String.replace_leading(uri.host, "www.", "")
+          format_referrer_host(uri)
         end
 
       source ->
@@ -15,11 +15,23 @@ defmodule PlausibleWeb.RefInspector do
     end
   end
 
+  def format_referrer(uri) do
+    path = String.trim_trailing(uri.path || "", "/")
+    format_referrer_host(uri) <> path
+  end
+
   def right_uri?(%URI{host: nil}), do: false
 
   def right_uri?(%URI{host: host, scheme: scheme})
-      when scheme in ["http", "https"] and byte_size(host) > 0,
+      when scheme in ["http", "https", "android-app"] and byte_size(host) > 0,
       do: true
 
   def right_uri?(_), do: false
+
+  defp format_referrer_host(uri) do
+    protocol = if uri.scheme == "android-app", do: "android-app://", else: ""
+    host = String.replace_prefix(uri.host, "www.", "")
+
+    protocol <> host
+  end
 end
