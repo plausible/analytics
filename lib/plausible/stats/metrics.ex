440988373eaa5b0d205837e66b49a53f29e451c9
Robert Joonas <robertjoonas16@gmail.com>
2024-03-21 11:06:30 +0000

    Add revenue metrics into metrics.ex

--- lib/plausible/stats/metrics.ex
+++ lib/plausible/stats/metrics.ex
@@ -1,23 +1,25 @@
 defmodule Plausible.Stats.Metrics do
   @moduledoc """
-  A module keeping the context of all available metrics in Plausible.
+  A module listing all available metrics in Plausible.
 
   Useful for an explicit string to atom conversion.
   """
 
-  @metrics [
-    :visitors,
-    :visits,
-    :pageviews,
-    :views_per_visit,
-    :bounce_rate,
-    :visit_duration,
-    :events,
-    :conversion_rate,
-    :time_on_page
-  ]
+  use Plausible
 
-  @metric_mappings Enum.into(@metrics, %{}, fn metric -> {to_string(metric), metric} end)
+  @all_metrics [
+                 :visitors,
+                 :visits,
+                 :pageviews,
+                 :views_per_visit,
+                 :bounce_rate,
+                 :visit_duration,
+                 :events,
+                 :conversion_rate,
+                 :time_on_page
+               ] ++ on_full_build(do: Plausible.Stats.Goal.Revenue.revenue_metrics(), else: [])
+
+  @metric_mappings Enum.into(@all_metrics, %{}, fn metric -> {to_string(metric), metric} end)
 
   def from_string!(str) do
     Map.fetch!(@metric_mappings, str)
