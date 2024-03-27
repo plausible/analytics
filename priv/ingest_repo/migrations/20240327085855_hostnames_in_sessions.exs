8b6ec60d118e500bae0186336c621a14132b7622
Adam Rutkowski <hq@mtod.org>
2024-03-27 13:51:16 +0100

    Use ClickHouse DDL in favour of ecto so that cluster is included

--- priv/ingest_repo/migrations/20240327085855_hostnames_in_sessions.exs
+++ priv/ingest_repo/migrations/20240327085855_hostnames_in_sessions.exs
@@ -1,9 +1,19 @@
 defmodule Plausible.ClickhouseRepo.Migrations.HostnamesInSessions do
   use Ecto.Migration
 
-  def change do
-    alter table(:sessions_v2) do
-      add(:exit_page_hostname, :string)
-    end
+  def up do
+    execute """
+    ALTER TABLE sessions_v2
+    #{Plausible.MigrationUtils.on_cluster_statement("sessions_v2")}
+    ADD COLUMN exit_page_hostname String
+    """
+  end
+
+  def down do
+    execute """
+    ALTER TABLE sessions_v2
+    #{Plausible.MigrationUtils.on_cluster_statement("sessions_v2")}
+    DROP COLUMN exit_page_hostname
+    """
   end
 end
