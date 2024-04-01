62b61b1094b852b2badeb06a289dd5a63914ac48
Adam Rutkowski <hq@mtod.org>
2024-04-01 13:58:42 +0200

    Remove `:window_time_on_page` flag
    It seems that we can remove it after all?

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -161,7 +161,7 @@ native_stats_range
 
     [
       site_id: site.id,
-      hostname: site.domain,
+      hostname: Enum.random(["en.dummy.site", "es.dummy.site", "dummy.site"]),
       timestamp: put_random_time.(date, index),
       referrer_source: Enum.random(["", "Facebook", "Twitter", "DuckDuckGo", "Google"]),
       browser: Enum.random(["Edge", "Chrome", "Safari", "Firefox", "Vivaldi"]),
@@ -196,7 +196,7 @@ native_stats_range
     [
       name: goal4.event_name,
       site_id: site.id,
-      hostname: site.domain,
+      hostname: Enum.random(["en.dummy.site", "es.dummy.site", "dummy.site"]),
       timestamp: put_random_time.(date, index),
       referrer_source: Enum.random(["", "Facebook", "Twitter", "DuckDuckGo", "Google"]),
       browser: Enum.random(["Edge", "Chrome", "Safari", "Firefox", "Vivaldi"]),
