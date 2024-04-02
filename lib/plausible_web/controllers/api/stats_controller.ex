211b45895a30e23a6d88e709cf46d3773746e169
Adam Rutkowski <hq@mtod.org>
2024-04-02 12:58:08 +0200

    Untested yet: also apply experimental filtering to sources

--- lib/plausible_web/controllers/api/stats_controller.ex
+++ lib/plausible_web/controllers/api/stats_controller.ex
@@ -428,6 +428,14 @@ defmodule PlausibleWeb.Api.StatsController do
     query = Query.from(site, params)
     pagination = parse_pagination(params)
 
+    # untested yet
+    query =
+      if query.experimental_hostname_filter? and query.filters["event:hostname"] do
+        Query.put_filter(query, "visit:entry_page_hostname", query.filters["event:hostname"])
+      else
+        query
+      end
+
     extra_metrics =
       if params["detailed"], do: [:bounce_rate, :visit_duration], else: []
 
