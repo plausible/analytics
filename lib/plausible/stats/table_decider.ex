fba617c6f4096fd3388d5cb6c11939f925080772
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2024-04-03 10:23:42 +0300

    Handle percentage separately from other metrics

--- lib/plausible/stats/table_decider.ex
+++ lib/plausible/stats/table_decider.ex
@@ -70,6 +70,7 @@ defmodule Plausible.Stats.TableDecider do
   # Calculated metrics - handled on callsite separately from other metrics.
   defp metric_partitioner(_, :time_on_page), do: :other
   defp metric_partitioner(_, :total_visitors), do: :other
+  defp metric_partitioner(_, :percentage), do: :other
   # Sample percentage is included in both tables if queried.
   defp metric_partitioner(_, :sample_percent), do: :both
 
