0a99f958e7954292b168ffaacea62fb38c92d305
Adrian Gruntkowski <adrian.gruntkowski@gmail.com>
2024-04-03 16:44:47 +0200

    Populate newly added `Imported*` fields in GA4 imports

--- test/plausible/imported/google_analytics4_test.exs
+++ test/plausible/imported/google_analytics4_test.exs
@@ -82,15 +82,15 @@ defmodule Plausible.Imported.GoogleAnalytics4Test do
       Enum.each(Plausible.Imported.tables(), fn table ->
         count =
           case table do
-            "imported_sources" -> 3
-            "imported_visitors" -> 3
-            "imported_pages" -> 8
-            "imported_entry_pages" -> 4
+            "imported_sources" -> 8
+            "imported_visitors" -> 5
+            "imported_pages" -> 10
+            "imported_entry_pages" -> 6
             "imported_exit_pages" -> 0
-            "imported_locations" -> 4
-            "imported_devices" -> 4
-            "imported_browsers" -> 5
-            "imported_operating_systems" -> 4
+            "imported_locations" -> 6
+            "imported_devices" -> 6
+            "imported_browsers" -> 7
+            "imported_operating_systems" -> 7
           end
 
         query = from(imported in table, where: imported.site_id == ^site.id)
