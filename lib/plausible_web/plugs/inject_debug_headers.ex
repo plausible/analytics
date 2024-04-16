6fd0931ab3c1e2e1ac8ed272a08f3873b0771bfa
Adam Rutkowski <hq@mtod.org>
2024-04-16 09:19:15 +0200

    Move headers injection to a separate plug module

---
+++ lib/plausible_web/plugs/inject_debug_headers.ex
@@ -0,0 +1,20 @@
+defmodule PlausibleWeb.Plugs.InjectDebugHeaders do
+  def init(opts), do: opts
+
+  def call(conn, _opts) do
+    Plug.Conn.register_before_send(conn, fn conn ->
+      Plausible.DebugReplayInfo.get_queries_from_context()
+      |> Enum.reverse()
+      |> Enum.with_index()
+      |> Enum.reduce(conn, fn {q, index}, conn ->
+        {[label], [value]} = Enum.unzip(q)
+
+        conn
+        |> Plug.Conn.put_resp_header(
+          "x-plausible-query-#{String.pad_leading("#{index}", 3, "0")}-#{label}",
+          String.replace(value, ["\n", "\r"], "")
+        )
+      end)
+    end)
+  end
+end
