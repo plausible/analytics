e14cc43dad2f06fc3a394e21f38e3b228c0934fc
Adam Rutkowski <hq@mtod.org>
2024-04-16 11:06:40 +0200

    Wait longer

--- test/plausible/clickhouse_repo_test.exs
+++ test/plausible/clickhouse_repo_test.exs
@@ -29,10 +29,9 @@ defmodule Plausible.ClickhouseRepoTest do
   end
 
   test "queries are logged with sentry metadata" do
-    Plausible.ClickhouseRepo.query!("""
-    select * from system.query_log where type = 1 order by event_time desc limit 50
-    """)
-    |> IO.inspect(label: :wtaf)
+    Sentry.Context.set_user_context(%{id: 1})
+    Sentry.Context.set_request_context(%{url: "http://example.com"})
+    Sentry.Context.set_extra_context(%{domain: "example.com", site_id: 1})
 
     Plausible.ClickhouseRepo.all(from(u in "sessions_v2", select: true, limit: 0),
       debug_label: "log_all"
@@ -46,21 +45,25 @@ defmodule Plausible.ClickhouseRepoTest do
              [q1, c1],
              [q2, c2]
            ] =
-             eventually(fn ->
-               result =
-                 Plausible.ClickhouseRepo.query!("""
-                 SELECT
-                 query,
-                 log_comment
-                 FROM system.query_log
-                 WHERE (type = 1) AND (query LIKE '%sessions_v2%')
-                 AND JSONExtractString(log_comment, 'debug_label') IN ('log_all', 'log_one')
-                 ORDER BY event_time DESC
-                 LIMIT 2
-                 """)
+             eventually(
+               fn ->
+                 result =
+                   Plausible.ClickhouseRepo.query!("""
+                   SELECT
+                   query,
+                   log_comment
+                   FROM system.query_log
+                   WHERE (type = 1) AND (query LIKE '%sessions_v2%')
+                   AND JSONExtractString(log_comment, 'debug_label') IN ('log_all', 'log_one')
+                   ORDER BY event_time DESC
+                   LIMIT 2
+                   """)
 
-               {length(result.rows) == 2, result.rows}
-             end)
+                 {length(result.rows) == 2, result.rows}
+               end,
+               500,
+               10
+             )
 
     assert q1 == "SELECT true FROM \"sessions_v2\" AS s0 LIMIT 0"
     assert q1 == q2
