0578911d498b4f47980af3fb2d3818e49bf170ba
Adam Rutkowski <hq@mtod.org>
2024-04-16 10:06:37 +0200

    Update repo test

--- test/plausible/clickhouse_repo_test.exs
+++ test/plausible/clickhouse_repo_test.exs
@@ -4,30 +4,54 @@ defmodule Plausible.ClickhouseRepoTest do
   test "queries are kept in context for debugging purposes for superadmins" do
     Sentry.Context.set_user_context(%{id: 1, super_admin?: true})
 
-    Plausible.ClickhouseRepo.all(from(u in "events_v2", select: true))
-    Plausible.ClickhouseRepo.one(from(u in "events_v2", select: true), debug_label: "one")
+    Plausible.ClickhouseRepo.all(from(u in "events_v2", select: true, limit: 0))
+
+     
+
+    
+    Plausible.ClickhouseRepo.one(from(u in "events_v2", select: true, limit: 0),
+      debug_label: "one"
+    )
 
     queries = Plausible.DebugReplayInfo.get_queries_from_context()
 
     assert [
-             %{"one" => "SELECT true FROM \"events_v2\" AS e0"},
-             %{"unlabelled" => "SELECT true FROM \"events_v2\" AS e0"}
+             %{"one" => "SELECT true FROM \"events_v2\" AS e0 LIMIT 0"},
+             %{"unlabelled" => "SELECT true FROM \"events_v2\" AS e0 LIMIT 0"}
            ] = queries
   end
 
-  test "queries are not kept in context for debugging purposes for non-superadmins" do
-    Plausible.ClickhouseRepo.all(from(u in "events_v2", select: true))
-    Plausible.ClickhouseRepo.one(from(u in "events_v2", select: true), debug_label: "one")
-    assert Plausible.DebugReplayInfo.get_queries_from_context() == []
-  end
+     
 
+    
+
+  test "queries are not kept in context for debugging purposes for non-superadmins" do
+    Plausible.ClickhouseRepo.all(from(u in "events_v2", select: true, limit: 0))
+
+    Plausible.ClickhouseRepo.one(from(u in "events_v2", select: true, limit: 0),
+      debug_label: "one"
+    )
+
+    assert Plausible.DebugReplayInfo.get_queries_from_context() == []
+  end     
+
+    
+
+     
+
+    
   test "queries are logged with sentry metadata" do
     Sentry.Context.set_user_context(%{id: 1})
     Sentry.Context.set_request_context(%{url: "http://example.com"})
     Sentry.Context.set_extra_context(%{domain: "example.com", site_id: 1})
 
-    Plausible.ClickhouseRepo.all(from(u in "sessions_v2", select: true), debug_label: "log_all")
-    Plausible.ClickhouseRepo.one(from(u in "sessions_v2", select: true), debug_label: "log_one")
+    Plausible.ClickhouseRepo.all(from(u in "sessions_v2", select: true, limit: 0),
+      debug_label: "log_all"
+    )
+
+    Plausible.ClickhouseRepo.one(from(u in "sessions_v2", select: true, limit: 0),
+      debug_label: "log_one"
+    )
 
     logged =
       Plausible.ClickhouseRepo.query!("""
@@ -46,7 +70,7 @@ defmodule Plausible.ClickhouseRepoTest do
              [q2, c2]
            ] = logged
 
-    assert q1 == "SELECT true FROM \"sessions_v2\" AS s0"
+    assert q1 == "SELECT true FROM \"sessions_v2\" AS s0 LIMIT 0"
     assert q1 == q2
 
     assert Jason.decode!(c1) == %{
