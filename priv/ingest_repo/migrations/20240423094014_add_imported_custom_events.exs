b7b0edcac65d22d7d6686f0a5e34f920f41c96b5
Adrian Gruntkowski <adrian.gruntkowski@gmail.com>
2024-04-23 11:58:35 +0200

    Add `imported_custom_events` to CH

---
+++ priv/ingest_repo/migrations/20240423094014_add_imported_custom_events.exs
@@ -0,0 +1,40 @@
+defmodule Plausible.IngestRepo.Migrations.AddImportedCustomEvents do
+  use Ecto.Migration
+
+  def change do
+    # NOTE: Using another table for determining cluster presence
+    on_cluster = Plausible.MigrationUtils.on_cluster_statement("imported_pages")
+
+    settings =
+      if Plausible.MigrationUtils.clustered_table?("imported_pages") do
+        """
+        ENGINE = ReplicatedMergeTree('/clickhouse/{cluster}/tables/{shard}/{database}/imported_custom_events', '{replica}')
+        ORDER BY (site_id, import_id, date, name)
+        SETTINGS index_granularity = 8192, replicated_deduplication_window = 0, storage_policy = 'tiered'
+        """
+      else
+        """
+        ENGINE = MergeTree()
+        ORDER BY (site_id, import_id, date, name)
+        SETTINGS index_granularity = 8192, replicated_deduplication_window = 0
+        """
+      end
+
+    execute """
+            CREATE TABLE IF NOT EXISTS imported_custom_events #{on_cluster}
+                (
+                `site_id` UInt64,
+                `import_id` UInt64,
+                `date` Date,
+                `name` String CODEC(ZSTD(3)),
+                `link_url` String CODEC(ZSTD(3)),
+                `visitors` UInt64,
+                `events` UInt64
+            )
+            #{settings}
+            """,
+            """
+            DROP TABLE IF EXISTS imported_custom_events #{on_cluster} SYNC
+            """
+  end
+end
