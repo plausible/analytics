50ce8c0435f3d6bd6180c8631adf0c4120c07485
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2024-05-05 20:17:18 +0300

    Fix default

--- lib/plausible/google/api.ex
+++ lib/plausible/google/api.ex
@@ -73,7 +73,7 @@ defmodule Plausible.Google.API do
     end
   end
 
-  def fetch_stats(site, %{filters: %{} = filters, date_range: date_range}, limit) do
+  def fetch_stats(site, %{filters: [] = filters, date_range: date_range}, limit) do
     with site <- Plausible.Repo.preload(site, :google_auth),
          {:ok, access_token} <- maybe_refresh_token(site.google_auth),
          {:ok, stats} <-
