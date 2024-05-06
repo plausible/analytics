652be42f60c2c5ac98b72ce81ce8571c733c2b7a
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2024-05-06 09:50:37 +0300

    Filter suggestions fix

--- lib/plausible/stats/filter_suggestions.ex
+++ lib/plausible/stats/filter_suggestions.ex
@@ -152,7 +152,7 @@ defmodule Plausible.Stats.FilterSuggestions do
   def filter_suggestions(site, query, "prop_value", filter_search) do
     filter_query = if filter_search == nil, do: "%", else: "%#{filter_search}%"
 
-    {"event:props:" <> key, _filter} = Query.get_filter_by_prefix(query, "event:props")
+    [_op, "event:props:" <> key | _rest] = Query.get_filter_by_prefix(query, "event:props")
 
     none_q =
       from(e in base_event_query(site, Query.remove_event_filters(query, [:props])),
