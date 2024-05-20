dcfe149c14713f56d561e56ac2df7abc5abe3534
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2024-05-20 14:32:42 +0300

    Fix sources/index.js

--- assets/js/dashboard/stats/sources/index.js
+++ assets/js/dashboard/stats/sources/index.js
@@ -9,7 +9,7 @@ export default function Sources(props) {
   if (isFilteringOnFixedValue(props.query, 'source', 'Google')) {
     return <SearchTerms {...props} />
   } else if (isFilteringOnFixedValue(props.query, 'source')) {
-    const [[_operation, _filterKey, clauses]] = getFiltersByKeyPrefix("source")
+    const [[_operation, _filterKey, clauses]] = getFiltersByKeyPrefix(props.query, "source")
     return <ReferrerList {...props} source={clauses[0]} />
   } else  {
     return <SourceList {...props} />
