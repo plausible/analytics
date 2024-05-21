b2860d160276531242fffc2be478ce5e71d32d36
Uku Taht <Uku.taht@gmail.com>
2024-05-21 10:23:57 +0300

    Fix unselecting filter from filter modal (#4109)

--- assets/js/dashboard/stats/modals/regular-filter-modal.js
+++ assets/js/dashboard/stats/modals/regular-filter-modal.js
@@ -57,7 +57,10 @@ class RegularFilterModal extends React.Component {
     const { formState } = this.state;
 
     const filters = Object.entries(formState).reduce((res, [filterKey, { type, clauses }]) => {
-      if (clauses.length === 0) { return res }
+      if (clauses.length === 0) {
+        res.push({filter: filterKey, value: null})
+        return res
+      }
       if (filterKey === 'country') { res.push({ filter: 'country_labels', value: clauses.map(clause => clause.label).join('|') }) }
       if (filterKey === 'region') { res.push({ filter: 'region_labels', value: clauses.map(clause => clause.label).join('|') }) }
       if (filterKey === 'city') { res.push({ filter: 'city_labels', value: clauses.map(clause => clause.label).join('|') }) }
