87d7180717932a896e98ab2bacd941de9d45c00f
Robert Joonas <robertjoonas16@gmail.com>
2024-06-05 14:52:41 +0300

    fix tab switch pills jumping due to bubble height

--- assets/js/dashboard/stats/imported-query-unsupported-warning.js
+++ assets/js/dashboard/stats/imported-query-unsupported-warning.js
@@ -5,10 +5,10 @@ import FadeIn from "../fade-in";
 export default function ImportedQueryUnsupportedWarning({query, loading, skipImportedReason, alt_condition, message}) {
   const tooltipMessage = message || "Imported data is excluded due to applied filters"
   const show = query && query.with_imported && skipImportedReason === "unsupported_query"
-  
+
   if (show || alt_condition) {
     return (
-      <FadeIn show={!loading}>
+      <FadeIn show={!loading} className="h-6">
         <span tooltip={tooltipMessage}>
           <ExclamationCircleIcon className="w-6 h-6 dark:text-gray-100" />
         </span>
