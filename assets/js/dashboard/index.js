825e3121a30900a58c5f664e8ab103070a48da42
Robert Joonas <robertjoonas16@gmail.com>
2024-06-07 16:32:36 +0300

    turn Dashboard and CurrentVisitors into fn components

--- assets/js/dashboard/index.js
+++ assets/js/dashboard/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { withRouter } from 'react-router-dom'
 
 import Historical from './historical'
@@ -8,47 +8,50 @@ import * as api from './api'
 
 export const statsBoxClass = "stats-item relative w-full mt-6 p-4 flex flex-col bg-white dark:bg-gray-825 shadow-xl rounded"
 
-class Dashboard extends React.Component {
-  constructor(props) {
-    super(props)
-    this.updateLastLoadTimestamp = this.updateLastLoadTimestamp.bind(this)
-    this.updateImportedDataInView = this.updateImportedDataInView.bind(this)
-    this.state = {
-      query: parseQuery(props.location.search, this.props.site),
-      importedDataInView: false,
-      lastLoadTimestamp: new Date()
+function Dashboard(props) {
+  const { location, site, loggedIn, currentUserRole } = props
+  const [query, setQuery] = useState(parseQuery(location.search, site))
+  const [importedDataInView, setImportedDataInView] = useState(false)
+  const [lastLoadTimestamp, setLastLoadTimestamp] = useState(new Date())
+  const updateLastLoadTimestamp = () => { setLastLoadTimestamp(new Date()) }
+
+  useEffect(() => {
+    document.addEventListener('tick', updateLastLoadTimestamp)
+
+    return () => {
+      document.removeEventListener('tick', updateLastLoadTimestamp)
     }
-  }
+  }, [])
 
-  componentDidMount() {
-    document.addEventListener('tick', this.updateLastLoadTimestamp)
-  }
+  useEffect(() => {
+    api.cancelAll()
+    setQuery(parseQuery(location.search, site))
+    updateLastLoadTimestamp()
+  }, [location.search])
 
-  componentDidUpdate(prevProps) {
-    if (prevProps.location.search !== this.props.location.search) {
-      api.cancelAll()
-      this.setState({query: parseQuery(this.props.location.search, this.props.site)})
-      this.updateLastLoadTimestamp()
-    }
-  }
 
-  updateLastLoadTimestamp() {
-    this.setState({lastLoadTimestamp: new Date()})
-  }
-
-  updateImportedDataInView(newBoolean) {
-    this.setState({importedDataInView: newBoolean})
-  }
-
-  render() {
-    const { site, loggedIn, currentUserRole } = this.props
-    const { query, lastLoadTimestamp } = this.state
-
-    if (this.state.query.period === 'realtime') {
-      return <Realtime site={site} loggedIn={loggedIn} currentUserRole={currentUserRole} query={query} lastLoadTimestamp={lastLoadTimestamp}/>
-    } else {
-      return <Historical site={site} loggedIn={loggedIn} currentUserRole={currentUserRole} query={query} lastLoadTimestamp={lastLoadTimestamp} importedDataInView={this.state.importedDataInView} updateImportedDataInView={this.updateImportedDataInView}/>
-    }
+  if (query.period === 'realtime') {
+    return (
+      <Realtime
+        site={site}
+        loggedIn={loggedIn}
+        currentUserRole={currentUserRole}
+        query={query}
+        lastLoadTimestamp={lastLoadTimestamp}
+      />
+    )
+  } else {
+    return (
+      <Historical
+        site={site}
+        loggedIn={loggedIn}
+        currentUserRole={currentUserRole}
+        query={query}
+        lastLoadTimestamp={lastLoadTimestamp}
+        importedDataInView={importedDataInView}
+        updateImportedDataInView={setImportedDataInView}
+      />
+    )
   }
 }
 
