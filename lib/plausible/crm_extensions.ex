3cc736425eb88089e509db8cbe9c990147e5ccb2
Adrian Gruntkowski <adrian.gruntkowski@gmail.com>
2024-06-14 17:33:38 +0200

    Apply `raw/1` only to the one (and only) element of the list

--- lib/plausible/crm_extensions.ex
+++ lib/plausible/crm_extensions.ex
@@ -7,25 +7,22 @@ defmodule Plausible.CrmExtensions do
 
   on_ee do
     def javascripts(%{assigns: %{context: "auth", resource: "user", entry: %{} = user}}) do
-      Enum.map(
-        [
-          """
-          <script type="text/javascript">
-            (async () => {
-              const response = await fetch("/crm/auth/user/#{user.id}/usage?embed=true")
-              const usageHTML = await response.text()
-              const cardBody = document.querySelector(".card-body")
-              if (cardBody) {
-                const usageDOM = document.createElement("div")
-                usageDOM.innerHTML = usageHTML
-                cardBody.prepend(usageDOM)
-              }
-            })()
-          </script>
-          """
-        ],
-        &Phoenix.HTML.raw/1
-      )
+      [
+        Phoenix.HTML.raw("""
+        <script type="text/javascript">
+          (async () => {
+            const response = await fetch("/crm/auth/user/#{user.id}/usage?embed=true")
+            const usageHTML = await response.text()
+            const cardBody = document.querySelector(".card-body")
+            if (cardBody) {
+              const usageDOM = document.createElement("div")
+              usageDOM.innerHTML = usageHTML
+              cardBody.prepend(usageDOM)
+            }
+          })()
+        </script>
+        """)
+      ]
     end
   end
 
