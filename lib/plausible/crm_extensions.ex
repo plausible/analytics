f9397e645b89f53950c7b39c12ca9174b2e99674
Adrian Gruntkowski <adrian.gruntkowski@gmail.com>
2024-06-14 15:25:56 +0200

    Implement autoprefill of enterprise plan fields on user change

--- lib/plausible/crm_extensions.ex
+++ lib/plausible/crm_extensions.ex
@@ -24,6 +24,64 @@ defmodule Plausible.CrmExtensions do
         """)
       ]
     end
+
+    def javascripts(%{assigns: %{context: "billing", resource: "enterprise_plan", changeset: %{}}}) do
+      [
+        Phoenix.HTML.raw("""
+        <script type="text/javascript">
+          (async () => {
+            const userIdField = document.getElementById("enterprise_plan_user_id")
+            let planRequest
+            let lastValue = Number(userIdField.value)
+            let scheduledCheck
+
+            userIdField.addEventListener("change", async () => {
+              if (scheduledCheck) clearTimeout(scheduledCheck)
+
+              scheduledCheck = setTimeout(async () => {
+                const currentValue = Number(userIdField.value)
+                if (Number.isInteger(currentValue)
+                      && currentValue > 0
+                      && currentValue != lastValue
+                      && !planRequest) {
+                  planRequest = await fetch("/crm/billing/user/" + currentValue + "/current_plan")
+                  const result = await planRequest.json()
+
+                  fillForm(result)
+
+                  lastValue = currentValue
+                  planRequest = null
+                }
+              }, 300)
+            })
+
+            userIdField.dispatchEvent(new Event("change"))
+
+            function fillForm(result) {
+              [
+                'billing_interval',
+                'monthly_pageview_limit',
+                'site_limit',
+                'team_member_limit',
+                'hourly_api_request_limit'
+              ].forEach(name => {
+                const prefillValue = result[name] || ""
+                const field = document.getElementById('enterprise_plan_' + name)
+
+                field.value = prefillValue
+                field.dispatchEvent(new Event("change"))
+              });
+
+              ['stats_api', 'props', 'funnels', 'revenue_goals'].forEach(feature => {
+                const checked = result.features.includes(feature)
+                document.getElementById('enterprise_plan_features_' + feature).checked = checked
+              });
+            }
+          })()
+        </script>
+        """)
+      ]
+    end
   end
 
   def javascripts(_) do
--- test/plausible_web/controllers/admin_controller_test.exs
+++ test/plausible_web/controllers/admin_controller_test.exs
@@ -62,4 +62,59 @@ defmodule PlausibleWeb.AdminControllerTest do
       assert site.stats_start_date == nil
     end
   end
+
+  describe "GET /crm/billing/user/:user_id/current_plan" do
+    setup [:create_user, :log_in]
+
+    @tag :ee_only
+    test "returns 403 if the logged in user is not a super admin", %{conn: conn} do
+      conn = get(conn, "/crm/billing/user/0/current_plan")
+      assert response(conn, 403) == "Not allowed"
+    end
+
+    @tag :ee_only
+    test "returns empty state for non-existent user", %{conn: conn, user: user} do
+      patch_env(:super_admin_user_ids, [user.id])
+
+      conn = get(conn, "/crm/billing/user/0/current_plan")
+      assert json_response(conn, 200) == %{"features" => []}
+    end
+
+    @tag :ee_only
+    test "returns empty state for user without subscription", %{conn: conn, user: user} do
+      patch_env(:super_admin_user_ids, [user.id])
+
+      conn = get(conn, "/crm/billing/user/#{user.id}/current_plan")
+      assert json_response(conn, 200) == %{"features" => []}
+    end
+
+    @tag :ee_only
+    test "returns empty state for user with subscription with non-existent paddle plan ID", %{
+      conn: conn,
+      user: user
+    } do
+      patch_env(:super_admin_user_ids, [user.id])
+
+      insert(:subscription, user: user)
+
+      conn = get(conn, "/crm/billing/user/#{user.id}/current_plan")
+      assert json_response(conn, 200) == %{"features" => []}
+    end
+
+    @tag :ee_only
+    test "returns plan data for user with subscription", %{conn: conn, user: user} do
+      patch_env(:super_admin_user_ids, [user.id])
+
+      insert(:subscription, user: user, paddle_plan_id: "857104")
+
+      conn = get(conn, "/crm/billing/user/#{user.id}/current_plan")
+
+      assert json_response(conn, 200) == %{
+               "features" => ["goals"],
+               "monthly_pageview_limit" => 10_000_000,
+               "site_limit" => 10,
+               "team_member_limit" => 3
+             }
+    end
+  end
 end
