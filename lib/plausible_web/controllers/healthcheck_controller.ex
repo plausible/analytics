e8136b1652e2aefcd3276f8ad8ab98ddb9cdc3d5
karlo.smid <karlo.smid@tentamen.hr>
2024-06-18 14:19:20 +0200

    health check version as string

--- lib/plausible_web/controllers/healthcheck_controller.ex
+++ lib/plausible_web/controllers/healthcheck_controller.ex
@@ -2,6 +2,6 @@ defmodule PlausibleWeb.HealthCheckController do
   use PlausibleWeb, :controller
 
   def index(conn, _params) do
-    json(conn, %{status: "RUNNING", version: Application.spec(:plausible)[:vsn], timestamp: DateTime.to_unix(DateTime.utc_now()), name: "plausible"})
+    json(conn, %{status: "RUNNING", version: List.to_string(Application.spec(:plausible)[:vsn]), timestamp: DateTime.to_unix(DateTime.utc_now()), name: "plausible"})
   end
 end
