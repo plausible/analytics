db18209b6a00177b1b7726648e59c1c46d3edabf
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2024-06-20 14:11:55 +0300

    inline load_goals

--- lib/plausible/stats/filters/utils.ex
+++ lib/plausible/stats/filters/utils.ex
@@ -67,14 +67,6 @@ defmodule Plausible.Stats.Filters.Utils do
   def unwrap_goal_value({:page, page}), do: "Visit " <> page
   def unwrap_goal_value({:event, event}), do: event
 
-  def load_goals(site) do
-    Plausible.Goals.for_site(site)
-    |> Enum.map(fn
-      %{page_path: path} when is_binary(path) -> {:page, path}
-      %{event_name: event_name} -> {:event, event_name}
-    end)
-  end
-
   def split_goals(goals) do
     Enum.split_with(goals, fn {type, _} -> type == :event end)
   end
