ed8abf426bcb41a3ac98ccf3540816e0b29e877a
Karl-Aksel Puulmann <macobo@users.noreply.github.com>
2024-06-21 11:31:02 +0300

    Revert "Revert "Re-apply multiple filters in FE (#4219)" (#4223)" (#4227)
    This reverts commit 2c05676d8edfd77d71e708812a8a8b4a2f307575.

--- assets/js/dashboard/stats/modals/filter-modal-group.js
+++ assets/js/dashboard/stats/modals/filter-modal-group.js
@@ -17,18 +17,17 @@ export default function FilterModalGroup({
     () => Object.entries(filterState).filter(([_, filter]) => getFilterGroup(filter) == filterGroup).map(([id, filter]) => ({ id, filter })),
     [filterGroup, filterState]
   )
-
   const disabledOptions = useMemo(
     () => (filterGroup == 'props') ? rows.map(({ filter }) => ({ value: getPropertyKeyFromFilterKey(filter[1]) })) : null,
     [filterGroup, rows]
   )
 
-  const showAddRow = filterGroup == 'props'
+  const showAddRow = site.flags.multiple_filters ? !['goal', 'hostname'].includes(filterGroup) : filterGroup == 'props'
   const showTitle = filterGroup != 'props'
 
   return (
     <>
-      <div className="mt-4">
+      <div className="mt-6">
         {showTitle && (<div className="text-sm font-medium text-gray-700 dark:text-gray-300">{formattedFilters[filterGroup]}</div>)}
         {rows.map(({ id, filter }) =>
           filterGroup === 'props' ? (
@@ -55,7 +54,7 @@ export default function FilterModalGroup({
         )}
       </div>
       {showAddRow && (
-        <div className="mt-6">
+        <div className="mt-2">
           <a className="underline text-indigo-500 text-sm cursor-pointer" onClick={() => onAddRow(filterGroup)}>
             + Add another
           </a>
