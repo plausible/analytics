fa065c85a31453970f720007e00252cf8fe3bd45
hq1 <hq@mtod.org>
2024-06-25 14:41:09 +0200

    Make new tab dashboard links work (#4276)
    * Make new tab dashboard links work
    
    * Urlencode domain
    
    * Recognize shared links

--- assets/js/dashboard/router.js
+++ assets/js/dashboard/router.js
@@ -27,9 +27,10 @@ function ScrollToTop() {
 }
 
 export default function Router({ site, loggedIn, currentUserRole }) {
+  console.info('xxx', site);
   return (
-    <BrowserRouter>
-      <Route path="/:domain">
+    <BrowserRouter basename={site.shared ? `/share/${encodeURI(site.domain)}` : encodeURI(site.domain)}>
+      <Route path="/">
         <ScrollToTop />
         <Dash site={site} loggedIn={loggedIn} currentUserRole={currentUserRole} />
         <Switch>
@@ -52,7 +53,7 @@ export default function Router({ site, loggedIn, currentUserRole }) {
             <ExitPagesModal site={site} />
           </Route>
           <Route path="/:domain/countries">
-            <ModalTable title="Top countries" site={site} endpoint={url.apiPath(site, '/countries')} filterKey="country" keyLabel="Country" renderIcon={renderCountryIcon} showPercentage={true}/>
+            <ModalTable title="Top countries" site={site} endpoint={url.apiPath(site, '/countries')} filterKey="country" keyLabel="Country" renderIcon={renderCountryIcon} showPercentage={true} />
           </Route>
           <Route path="/:domain/regions">
             <ModalTable title="Top regions" site={site} endpoint={url.apiPath(site, '/regions')} filterKey="region" keyLabel="Region" renderIcon={renderRegionIcon} />
@@ -71,7 +72,7 @@ export default function Router({ site, loggedIn, currentUserRole }) {
           </Route>
         </Switch>
       </Route>
-    </BrowserRouter>
+    </BrowserRouter >
   );
 }
 
