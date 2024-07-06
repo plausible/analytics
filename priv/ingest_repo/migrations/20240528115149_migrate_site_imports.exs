d39c2ed28ff87a35928b37016cd2ee0c9631a175
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2024-07-06 17:26:35 +0700

    don't run twice

--- priv/ingest_repo/migrations/20240528115149_migrate_site_imports.exs
+++ priv/ingest_repo/migrations/20240528115149_migrate_site_imports.exs
@@ -1,14 +1,26 @@
 defmodule Plausible.IngestRepo.Migrations.MigrateSiteImports do
-  use Plausible
   use Ecto.Migration
 
   def up do
-    if ce?() do
+    if Plausible.ce?() do
       Ecto.Migrator.with_repo(Plausible.Repo, fn _repo ->
-        {:ok, _, _} =
-          Ecto.Migrator.with_repo(Plausible.ClickhouseRepo, fn _repo ->
-            Plausible.DataMigration.SiteImports.run(dry_run?: false)
-          end)
+        %Postgrex.Result{rows: rows} =
+          Plausible.Repo.query!(
+            "select inserted_at from schema_migrations where version=20240528115149"
+          )
+
+        case rows do
+          [[already_ran_at]] ->
+            IO.puts(
+              "skipping site_imports migration since it has already been run at #{already_ran_at}"
+            )
+
+          [] ->
+            {:ok, _, _} =
+              Ecto.Migrator.with_repo(Plausible.ClickhouseRepo, fn _repo ->
+                Plausible.DataMigration.SiteImports.run(dry_run?: false)
+              end)
+        end
       end)
     end
   end
