9f31843b405cfec4010e50579099f9b3baf59a5b
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2024-07-10 10:34:08 +0300

    Migration comment

--- priv/ingest_repo/migrations/20240709181437_populate_location_data.exs
+++ priv/ingest_repo/migrations/20240709181437_populate_location_data.exs
@@ -2,6 +2,7 @@ defmodule Plausible.IngestRepo.Migrations.PopulateLocationData do
   use Ecto.Migration
 
   def up do
+    # Location data may not be loaded, so _try_ to load it. Failure is OK - it means it's loaded.
     try do
       Location.load_all()
     rescue
