fe207269fb1c2f65bd50d32ae6e808a2ca910de3
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2024-07-10 10:53:42 +0300

    Add error when already loaded

--- priv/ingest_repo/migrations/20240709181437_populate_location_data.exs
+++ priv/ingest_repo/migrations/20240709181437_populate_location_data.exs
@@ -2,11 +2,11 @@ defmodule Plausible.IngestRepo.Migrations.PopulateLocationData do
   use Ecto.Migration
 
   def up do
-    # Location data may not be loaded, so _try_ to load it. Failure is OK - it means it's loaded.
     try do
       Location.load_all()
     rescue
-      _ -> nil
+      # Already loaded
+      ArgumentError -> nil
     end
 
     Plausible.DataMigration.LocationsSync.run()
