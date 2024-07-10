ec1a6742cb5e0cb2b7ff8860c89a20e4688fc53c
Robert Joonas <robertjoonas16@gmail.com>
2024-07-10 14:27:30 +0300

    do not delay the first function call in useDebounceEffect

--- assets/js/dashboard/custom-hooks.js
+++ assets/js/dashboard/custom-hooks.js
@@ -14,14 +14,20 @@ export function useMountedEffect(fn, deps) {
   }, deps)
 }
 
-// A custom hook that debounces the function calls by
-// a given delay. Cancels all function calls that have
-// a following call within `delay_ms`.
+// A custom hook that debounces the function calls by a given delay.
+// The first function call is not delayed. Every subsequent call is
+// delayed by `delay_ms`, and if another function is called before
+// the delay timeout, the previous function call gets cancelled.
 export function useDebouncedEffect(fn, deps, delay_ms) {
   const callback = useCallback(fn, deps)
+  const delay = useRef(0)
 
   useEffect(() => {
-    const timeout = setTimeout(callback, delay_ms)
+    const timeout = setTimeout(() => {
+      delay.current = delay_ms
+      callback()
+    }, delay.current)
+
     return () => clearTimeout(timeout)
   }, [callback, delay_ms])
 }
