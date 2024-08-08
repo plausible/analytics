1451b153153ce7f68415b10beb6c6461dea99ed7
Adam Rutkowski <hq@mtod.org>
2024-08-08 08:32:56 +0200

    Display names: 1st stage migration

---
+++ priv/repo/migrations/20240801052902_add_goal_display_name.exs
@@ -0,0 +1,24 @@
+defmodule Plausible.Repo.Migrations.AddGoalDisplayName do
+  use Ecto.Migration
+
+  def change do
+    alter table(:goals) do
+      add :display_name, :text
+    end
+
+    create unique_index(:goals, [:site_id, :display_name])
+
+    fill_display_names()
+  end
+
+  def fill_display_names do
+    execute """
+    UPDATE goals
+    SET display_name = 
+      CASE
+        WHEN page_path IS NOT NULL THEN 'Visit ' || page_path
+        WHEN event_name IS NOT NULL THEN event_name
+      END
+    """
+  end
+end
