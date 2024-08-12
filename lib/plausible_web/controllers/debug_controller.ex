11e36534d1e1bb03ebac6e89e107cb219d625026
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2024-08-12 16:46:34 +0300

    Handle clustered setups

--- lib/plausible_web/controllers/debug_controller.ex
+++ lib/plausible_web/controllers/debug_controller.ex
@@ -8,39 +8,65 @@ defmodule PlausibleWeb.DebugController do
   plug(PlausibleWeb.RequireAccountPlug)
   plug(PlausibleWeb.SuperAdminOnlyPlug)
 
+  @columns [
+    "query",
+    "log_comment",
+    "type",
+    "event_time",
+    "query_duration_ms",
+    "query_id",
+    "result_rows",
+    "memory_usage",
+    "read_bytes",
+    "result_bytes"
+  ]
+
   def clickhouse(conn, params) do
     user_id = Map.get(params, "user_id", conn.assigns.current_user.id)
 
+    cluster? = Plausible.MigrationUtils.clustered_table?("events_v2")
+    on_cluster = if(cluster?, do: "ON CLUSTER '{cluster}'", else: "")
+
     # Ensure last logs are flushed
-    IngestRepo.query("SYSTEM FLUSH LOGS")
+    IngestRepo.query("SYSTEM FLUSH LOGS #{on_cluster}")
+
+    table_expression =
+      if(cluster?,
+        do: "clusterAllReplicas('{cluster}', system.query_log)",
+        else: "system.query_log"
+      )
+
+    %Ch.Result{rows: rows} =
+      IngestRepo.query!(
+        """
+          SELECT
+            formatQuery(query) AS query,
+            log_comment,
+            type,
+            event_time,
+            query_duration_ms,
+            query_id,
+            result_rows,
+            formatReadableSize(memory_usage) AS memory_usage,
+            formatReadableSize(read_bytes) AS read_bytes,
+            formatReadableSize(result_bytes) AS result_bytes
+          FROM #{table_expression}
+          WHERE type > 1
+            AND JSONExtractString(log_comment, 'user_id') = {$0:String}
+            AND event_time > now() - toIntervalMinute(15)
+          ORDER BY event_time DESC
+        """,
+        [user_id]
+      )
 
     queries =
-      from(
-        l in "query_log",
-        prefix: "system",
-        select: %{
-          query: fragment("formatQuery(?)", l.query),
-          log_comment: l.log_comment,
-          type: l.type,
-          event_time: l.event_time,
-          query_duration_ms: l.query_duration_ms,
-          query_id: l.query_id,
-          memory_usage: fragment("formatReadableSize(?)", l.memory_usage),
-          read_bytes: fragment("formatReadableSize(?)", l.read_bytes),
-          result_bytes: fragment("formatReadableSize(?)", l.result_bytes),
-          result_rows: l.result_rows
-        },
-        where:
-          l.type > 1 and
-            fragment("JSONExtractInt(?, \'user_id\') = ?", l.log_comment, ^user_id) and
-            fragment("event_time > now() - toIntervalMinute(15)"),
-        order_by: [desc: l.event_time]
-      )
-      |> IngestRepo.all()
-      |> Enum.map(fn data ->
-        Jason.decode!(data.log_comment)
-        |> Map.merge(data)
-        |> Map.delete(:log_comment)
+      rows
+      |> Enum.map(fn row ->
+        data = Enum.zip(@columns, row) |> Enum.into(%{})
+
+        data
+        |> Map.merge(Jason.decode!(data["log_comment"]))
+        |> Map.delete("log_comment")
       end)
       |> IO.inspect()
 
