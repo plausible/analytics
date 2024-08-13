6d8cfe0ce94670811028f1756fa3fc1ce63d2009
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2024-08-13 13:00:35 +0300

    fragment trick to use ecto querying, filtering

--- lib/plausible_web/controllers/debug_controller.ex
+++ lib/plausible_web/controllers/debug_controller.ex
@@ -3,75 +3,72 @@ defmodule PlausibleWeb.DebugController do
   use Plausible.IngestRepo
   use Plausible
 
+  import Ecto.Query
+
   plug(PlausibleWeb.RequireAccountPlug)
   plug(PlausibleWeb.SuperAdminOnlyPlug)
 
-  @columns [
-    "query",
-    "log_comment",
-    "type",
-    "event_time",
-    "query_duration_ms",
-    "query_id",
-    "result_rows",
-    "memory_usage",
-    "read_bytes",
-    "result_bytes"
-  ]
-
   def clickhouse(conn, params) do
-    user_id = Map.get(params, "user_id", conn.assigns.current_user.id)
-
     cluster? = Plausible.MigrationUtils.clustered_table?("events_v2")
     on_cluster = if(cluster?, do: "ON CLUSTER '{cluster}'", else: "")
 
     # Ensure last logs are flushed
     IngestRepo.query("SYSTEM FLUSH LOGS #{on_cluster}")
 
-    table_expression =
-      if(cluster?,
-        do: "clusterAllReplicas('{cluster}', system.query_log)",
-        else: "system.query_log"
-      )
-
-    %Ch.Result{rows: rows} =
-      IngestRepo.query!(
-        """
-          SELECT
-            formatQuery(query) AS query,
-            log_comment,
-            type,
-            event_time,
-            query_duration_ms,
-            query_id,
-            result_rows,
-            formatReadableSize(memory_usage) AS memory_usage,
-            formatReadableSize(read_bytes) AS read_bytes,
-            formatReadableSize(result_bytes) AS result_bytes
-          FROM #{table_expression}
-          WHERE type > 1
-            AND JSONExtractString(log_comment, 'user_id') = {$0:String}
-            AND event_time > now() - toIntervalMinute(15)
-          ORDER BY event_time DESC
-        """,
-        [user_id]
-      )
-
     queries =
-      rows
-      |> Enum.map(fn row ->
-        data = Enum.zip(@columns, row) |> Enum.into(%{})
-
-        data
-        |> Map.merge(Jason.decode!(data["log_comment"]))
-        |> Map.delete("log_comment")
+      from(
+        l in from_query_log(cluster?),
+        select: %{
+          query: fragment("formatQuery(?)", l.query),
+          log_comment: l.log_comment,
+          type: l.type,
+          event_time: l.event_time,
+          query_duration_ms: l.query_duration_ms,
+          query_id: l.query_id,
+          memory_usage: fragment("formatReadableSize(?)", l.memory_usage),
+          read_bytes: fragment("formatReadableSize(?)", l.read_bytes),
+          result_bytes: fragment("formatReadableSize(?)", l.result_bytes),
+          result_rows: l.result_rows
+        },
+        where: l.type > 1 and fragment("event_time > now() - toIntervalMinute(15)"),
+        order_by: [desc: l.event_time]
+      )
+      |> filter_by_params(conn, params)
+      |> IngestRepo.all()
+      |> Enum.map(fn data ->
+        Jason.decode!(data.log_comment)
+        |> Map.merge(data)
+        |> Map.delete(:log_comment)
       end)
 
     conn
     |> render("clickhouse.html",
       queries: queries,
-      user_id: user_id,
       layout: {PlausibleWeb.LayoutView, "focus.html"}
     )
   end
+
+  defp from_query_log(cluster?) do
+    case cluster? do
+      true -> from(l in fragment("clusterAllReplicas('{cluster}', system.query_log)"))
+      false -> from(l in fragment("system.query_log"))
+    end
+  end
+
+  defp filter_by_params(q, _conn, %{"user_id" => user_id}),
+    do: where(q, [l], fragment("JSONExtractInt(?, \'user_id\') = ?", l.log_comment, ^user_id))
+
+  defp filter_by_params(q, _conn, %{"site_id" => site_id}),
+    do: where(q, [l], fragment("JSONExtractInt(?, \'site_id\') = ?", l.log_comment, ^site_id))
+
+  defp filter_by_params(q, _conn, %{"site_domain" => site_domain}),
+    do:
+      where(
+        q,
+        [l],
+        fragment("JSONExtractInt(?, \'site_domain\') = ?", l.log_comment, ^site_domain)
+      )
+
+  defp filter_by_params(q, conn, _),
+    do: filter_by_params(q, conn, %{"user_id" => conn.assigns.current_user.id})
 end
