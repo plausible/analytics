40a33cad483e5a12d3ae47354cc04da8ce167ea6
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2024-08-20 14:07:26 +0300

    query_result ordering test

---
+++ test/plausible/stats/query_result_test.exs
@@ -0,0 +1,68 @@
+defmodule Plausible.Stats.QueryTest do
+  use Plausible.DataCase, async: true
+  alias Plausible.Stats.{Query, QueryResult, QueryOptimizer}
+
+  setup do
+    user = insert(:user)
+
+    site =
+      insert(:site,
+        members: [user],
+        inserted_at: ~N[2020-01-01T00:00:00],
+        stats_start_date: ~D[2020-01-01]
+      )
+
+    {:ok, site: site}
+  end
+
+  test "serializing query to JSON keeps keys ordered" do
+    site = insert(:site)
+
+    {:ok, query} =
+      Query.build(
+        site,
+        %{
+          "site_id" => site.domain,
+          "metrics" => ["pageviews"],
+          "date_range" => ["2024-01-01", "2024-02-01"],
+          "include" => %{"imports" => true}
+        },
+        %{}
+      )
+
+    query = QueryOptimizer.optimize(query)
+
+    query_result_json =
+      QueryResult.from([], site, query)
+      |> Jason.encode!(pretty: true)
+      |> String.replace(site.domain, "dummy.site")
+
+    assert query_result_json == """
+           {
+             "results": [],
+             "meta": {},
+             "query": {
+               "site_id": "dummy.site",
+               "metrics": [
+                 "pageviews"
+               ],
+               "date_range": [
+                 "2024-01-01",
+                 "2024-02-01"
+               ],
+               "filters": [],
+               "dimensions": [],
+               "order_by": [
+                 [
+                   "pageviews",
+                   "desc"
+                 ]
+               ],
+               "include": {
+                 "imports": true
+               }
+             }
+           }\
+           """
+  end
+end
