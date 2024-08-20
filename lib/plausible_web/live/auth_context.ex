92499e61e3b8d1757a427dc9891629f675dcaa56
Adrian Gruntkowski <adrian.gruntkowski@gmail.com>
2024-08-20 11:36:29 +0200

    Introduce LV auth context populating user data from session on mount

---
+++ lib/plausible_web/live/auth_context.ex
@@ -0,0 +1,37 @@
+defmodule PlausibleWeb.Live.AuthContext do
+  @moduledoc """
+  This module supplies LiveViews with currently logged in user data _if_ session
+  data contains a valid token.
+  """
+
+  import Phoenix.Component
+
+  alias PlausibleWeb.UserAuth
+
+  defmacro __using__(_) do
+    quote do
+      on_mount unquote(__MODULE__)
+    end
+  end
+
+  def on_mount(:default, _params, session, socket) do
+    socket =
+      socket
+      |> assign_new(:current_user_session, fn ->
+        case UserAuth.get_user_session(session) do
+          {:ok, user_session} -> user_session
+          _ -> nil
+        end
+      end)
+      |> assign_new(:current_user, fn context ->
+        with %{} = user_session <- context.current_user_session,
+             {:ok, user} <- UserAuth.get_user(user_session) do
+          user
+        else
+          _ -> nil
+        end
+      end)
+
+    {:cont, socket}
+  end
+end
