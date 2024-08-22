20fea637c049c0eb135164a5fbfbce75dcf084c2
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2024-08-22 10:57:37 +0300

    Update QueryOptimizer tests

--- test/plausible/stats/query_optimizer_test.exs
+++ test/plausible/stats/query_optimizer_test.exs
@@ -1,7 +1,7 @@
 defmodule Plausible.Stats.QueryOptimizerTest do
   use Plausible.DataCase, async: true
 
-  alias Plausible.Stats.{Query, QueryOptimizer}
+  alias Plausible.Stats.{DateTimeRange, Query, QueryOptimizer}
 
   @default_params %{metrics: [:visitors]}
 
@@ -24,7 +24,7 @@ defmodule Plausible.Stats.QueryOptimizerTest do
 
     test "adds time and first metric to order_by if order_by not specified" do
       assert perform(%{
-               date_range: Date.range(~N[2022-01-01 00:00:00], ~N[2022-02-01 00:00:00]),
+               date_range: DateTimeRange.new(~N[2022-01-01 00:00:00], ~N[2022-02-01 00:00:00]),
                metrics: [:pageviews, :visitors],
                dimensions: ["time", "event:page"]
              }).order_by ==
@@ -35,69 +35,69 @@ defmodule Plausible.Stats.QueryOptimizerTest do
   describe "update_group_by_time" do
     test "does nothing if `time` dimension not passed" do
       assert perform(%{
-               date_range: Date.range(~N[2022-01-01 00:00:00], ~N[2022-01-05 00:00:00]),
+               date_range: DateTimeRange.new(~N[2022-01-01 00:00:00], ~N[2022-01-05 00:00:00]),
                dimensions: ["time:month"]
              }).dimensions == ["time:month"]
     end
 
     test "updating time dimension" do
       assert perform(%{
-               date_range: Date.range(~N[2022-01-01 00:00:00], ~N[2022-01-01 05:00:00]),
+               date_range: DateTimeRange.new(~N[2022-01-01 00:00:00], ~N[2022-01-01 05:00:00]),
                dimensions: ["time"]
              }).dimensions == ["time:hour"]
 
       assert perform(%{
-               date_range: Date.range(~N[2022-01-01 00:00:00], ~N[2022-01-02 00:00:00]),
+               date_range: DateTimeRange.new(~N[2022-01-01 00:00:00], ~N[2022-01-02 00:00:00]),
                dimensions: ["time"]
              }).dimensions == ["time:hour"]
 
       assert perform(%{
-               date_range: Date.range(~N[2022-01-01 00:00:00], ~N[2022-01-02 16:00:00]),
+               date_range: DateTimeRange.new(~N[2022-01-01 00:00:00], ~N[2022-01-02 16:00:00]),
                dimensions: ["time"]
              }).dimensions == ["time:hour"]
 
       assert perform(%{
-               date_range: Date.range(~D[2022-01-01], ~D[2022-01-04]),
+               date_range: DateTimeRange.new(~D[2022-01-01], ~D[2022-01-04]),
                dimensions: ["time"]
              }).dimensions == ["time:day"]
 
       assert perform(%{
-               date_range: Date.range(~D[2022-01-01], ~D[2022-01-10]),
+               date_range: DateTimeRange.new(~D[2022-01-01], ~D[2022-01-10]),
                dimensions: ["time"]
              }).dimensions == ["time:day"]
 
       assert perform(%{
-               date_range: Date.range(~D[2022-01-01], ~D[2022-01-16]),
+               date_range: DateTimeRange.new(~D[2022-01-01], ~D[2022-01-16]),
                dimensions: ["time"]
              }).dimensions == ["time:day"]
 
       assert perform(%{
-               date_range: Date.range(~D[2022-01-01], ~D[2022-02-16]),
+               date_range: DateTimeRange.new(~D[2022-01-01], ~D[2022-02-16]),
                dimensions: ["time"]
              }).dimensions == ["time:week"]
 
       assert perform(%{
-               date_range: Date.range(~D[2022-01-01], ~D[2022-03-16]),
+               date_range: DateTimeRange.new(~D[2022-01-01], ~D[2022-03-16]),
                dimensions: ["time"]
              }).dimensions == ["time:week"]
 
       assert perform(%{
-               date_range: Date.range(~D[2022-01-01], ~D[2022-03-16]),
+               date_range: DateTimeRange.new(~D[2022-01-01], ~D[2022-03-16]),
                dimensions: ["time"]
              }).dimensions == ["time:week"]
 
       assert perform(%{
-               date_range: Date.range(~D[2022-01-01], ~D[2023-11-16]),
+               date_range: DateTimeRange.new(~D[2022-01-01], ~D[2023-11-16]),
                dimensions: ["time"]
              }).dimensions == ["time:month"]
 
       assert perform(%{
-               date_range: Date.range(~D[2022-01-01], ~D[2024-01-16]),
+               date_range: DateTimeRange.new(~D[2022-01-01], ~D[2024-01-16]),
                dimensions: ["time"]
              }).dimensions == ["time:month"]
 
       assert perform(%{
-               date_range: Date.range(~D[2022-01-01], ~D[2026-01-01]),
+               date_range: DateTimeRange.new(~D[2022-01-01], ~D[2026-01-01]),
                dimensions: ["time"]
              }).dimensions == ["time:month"]
     end
@@ -106,7 +106,7 @@ defmodule Plausible.Stats.QueryOptimizerTest do
   describe "update_time_in_order_by" do
     test "updates explicit time dimension in order_by" do
       assert perform(%{
-               date_range: Date.range(~N[2022-01-01 00:00:00], ~N[2022-01-01 05:00:00]),
+               date_range: DateTimeRange.new(~N[2022-01-01 00:00:00], ~N[2022-01-01 05:00:00]),
                dimensions: ["time:hour"],
                order_by: [{"time", :asc}]
              }).order_by == [{"time:hour", :asc}]
@@ -116,7 +116,7 @@ defmodule Plausible.Stats.QueryOptimizerTest do
   describe "extend_hostname_filters_to_visit" do
     test "updates filters it filtering by event:hostname and visit:referrer and visit:exit_page dimensions" do
       assert perform(%{
-               date_range: Date.range(~N[2022-01-01 00:00:00], ~N[2022-01-01 05:00:00]),
+               date_range: DateTimeRange.new(~N[2022-01-01 00:00:00], ~N[2022-01-01 05:00:00]),
                filters: [
                  [:is, "event:hostname", ["example.com"]],
                  [:matches, "event:hostname", ["*.com"]]
@@ -134,7 +134,7 @@ defmodule Plausible.Stats.QueryOptimizerTest do
 
     test "does not update filters if not needed" do
       assert perform(%{
-               date_range: Date.range(~N[2022-01-01 00:00:00], ~N[2022-01-01 05:00:00]),
+               date_range: DateTimeRange.new(~N[2022-01-01 00:00:00], ~N[2022-01-01 05:00:00]),
                filters: [
                  [:is, "event:hostname", ["example.com"]]
                ],
