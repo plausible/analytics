dce2995b2092536b333a464e914bafe9c4f52700
Adrian Gruntkowski <adrian.gruntkowski@gmail.com>
2024-08-22 15:43:00 +0200

    Turn `Plausible.Auth.UserSession` into full schema

--- lib/plausible/auth/user_session.ex
+++ lib/plausible/auth/user_session.ex
@@ -5,9 +5,45 @@ defmodule Plausible.Auth.UserSession do
 
   use Ecto.Schema
 
+  import Ecto.Changeset
+
+  alias Plausible.Auth
+
   @type t() :: %__MODULE__{}
 
-  embedded_schema do
-    field :user_id, :integer
+  @rand_size 32
+  @timeout Duration.new!(day: 14)
+
+  schema "user_sessions" do
+    field :token, :binary
+    field :device, :string
+    field :last_used_at, :naive_datetime
+    field :timeout_at, :naive_datetime
+
+    belongs_to :user, Plausible.Auth.User
+
+    timestamps(updated_at: false)
+  end
+
+  @spec new_session(Auth.User.t(), String.t(), NaiveDateTime.t()) :: Ecto.Changeset.t()
+  def new_session(user, device, now \\ NaiveDateTime.utc_now(:second)) do
+    %__MODULE__{}
+    |> cast(%{device: device}, [:device])
+    |> generate_token()
+    |> put_assoc(:user, user)
+    |> touch_session(now)
+  end
+
+  @spec touch_session(t() | Ecto.Changeset.t(), NaiveDateTime.t()) :: Ecto.Changeset.t()
+  def touch_session(session, now \\ NaiveDateTime.utc_now(:second)) do
+    session
+    |> change()
+    |> put_change(:last_used_at, now)
+    |> put_change(:timeout_at, NaiveDateTime.shift(now, @timeout))
+  end
+
+  defp generate_token(changeset) do
+    token = :crypto.strong_rand_bytes(@rand_size)
+    put_change(changeset, :token, token)
   end
 end
