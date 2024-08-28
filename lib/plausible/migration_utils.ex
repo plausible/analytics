23bb6d7f2ac808823d134a4abd6afd542d7b8b1d
karlo.smid <karlo.smid@tentamen.hr>
2024-08-28 10:35:36 +0200

    feat: 🎸 plausible migrations on cluster

--- lib/plausible/migration_utils.ex
+++ lib/plausible/migration_utils.ex
@@ -4,13 +4,21 @@ defmodule Plausible.MigrationUtils do
   """
 
   def on_cluster_statement(table) do
-    if(clustered_table?(table), do: "ON CLUSTER '{cluster}'", else: "")
+    if(clustered_table?(table), do: "ON CLUSTER #{cluster_name()}", else: "")
   end
 
   def clustered_table?(table) do
     case Plausible.IngestRepo.query("SELECT 1 FROM system.replicas WHERE table = '#{table}'") do
       {:ok, %{rows: []}} -> false
       {:ok, _} -> true
+    end
+  end
+
+  def cluster_name do
+    case Plausible.IngestRepo.query("SELECT cluster FROM system.clusters where cluster != 'default' limit 1;") do
+      {:ok, %{rows: [[cluster]]}} -> cluster
+      {:ok, _} -> raise "No cluster defined."
+      {:error, _} -> raise "Cluster not found"
     end
   end
 end
