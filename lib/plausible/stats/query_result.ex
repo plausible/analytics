7731ef1582f826ab7a619119a927a12f7c095652
Robert Joonas <robertjoonas16@gmail.com>
2024-08-29 13:51:06 +0300

    return query_result.date_range as iso8601 timestamps with timezone

--- lib/plausible/stats/query_result.ex
+++ lib/plausible/stats/query_result.ex
@@ -7,7 +7,7 @@ defmodule Plausible.Stats.QueryResult do
   produced by Jason.encode(query_result) is ordered.
   """
 
-  alias Plausible.Stats.{Util, Filters, DateTimeRange}
+  alias Plausible.Stats.{Util, Filters}
 
   defstruct results: [],
             meta: %{},
@@ -23,8 +23,6 @@ defmodule Plausible.Stats.QueryResult do
         }
       end)
 
-    %{first: first, last: last} = DateTimeRange.to_date_range(query.date_range)
-
     struct!(
       __MODULE__,
       results: results_list,
@@ -33,7 +31,10 @@ defmodule Plausible.Stats.QueryResult do
         Jason.OrderedObject.new(
           site_id: site.domain,
           metrics: query.metrics,
-          date_range: [first, last],
+          date_range: [
+            to_iso_8601_with_timezone(query.date_range.first),
+            to_iso_8601_with_timezone(query.date_range.last)
+          ],
           filters: query.filters,
           dimensions: query.dimensions,
           order_by: query.order_by |> Enum.map(&Tuple.to_list/1),
@@ -79,6 +80,15 @@ defmodule Plausible.Stats.QueryResult do
     }
     |> Enum.reject(fn {_, value} -> is_nil(value) end)
     |> Enum.into(%{})
+  end
+
+  defp to_iso_8601_with_timezone(%DateTime{time_zone: timezone} = datetime) do
+    naive_iso8601 =
+      datetime
+      |> DateTime.to_naive()
+      |> NaiveDateTime.to_iso8601()
+
+    naive_iso8601 <> " " <> timezone
   end
 end
 
