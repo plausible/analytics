e92004ea08869eb69790ad0436e9805f17f94663
Artur Pata <artur.pata@gmail.com>
2024-09-05 12:13:18 +0300

    Remove unneeded order by from total pageviews query

--- lib/plausible_web/controllers/api/stats_controller.ex
+++ lib/plausible_web/controllers/api/stats_controller.ex
@@ -926,6 +926,7 @@ defmodule PlausibleWeb.Api.StatsController do
 
       total_pageviews_query =
         query
+        |> struct!(order_by: [])
         |> Query.remove_top_level_filters(["visit:exit_page"])
         |> Query.add_filter([:is, "event:page", pages])
         |> Query.set(dimensions: ["event:page"])
