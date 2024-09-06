4e2d800848cbed545c853df5b6d0659302dff0dc
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2024-09-06 15:11:49 +0700

    format

--- config/runtime.exs
+++ config/runtime.exs
@@ -537,7 +537,9 @@ case mailer_adapter do
     config :plausible, Plausible.Mailer, adapter: Bamboo.Mua
 
     # prevents common problems with Erlang's TLS v1.3
-    middlebox_comp_mode = get_var_from_path_or_env(config_dir, "SMTP_MIDDLEBOX_COMP_MODE", "false")
+    middlebox_comp_mode =
+      get_var_from_path_or_env(config_dir, "SMTP_MIDDLEBOX_COMP_MODE", "false")
+
     middlebox_comp_mode = String.to_existing_atom(middlebox_comp_mode)
     config :plausible, Plausible.Mailer, ssl: [middlebox_comp_mode: middlebox_comp_mode]
 
