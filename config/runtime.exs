c919ff018239ddb671f7d58f4259d4f1cd1ccb52
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2024-09-06 14:47:34 +0700

    fix comma

--- config/runtime.exs
+++ config/runtime.exs
@@ -767,7 +767,7 @@ else
     traces_exporter: :none
 end
 
-config :tzdata, :data_dir, Path.join(persistent_cache_dir || System.tmp_dir!(), "tzdata_data"),
+config :tzdata, :data_dir, Path.join(persistent_cache_dir || System.tmp_dir!(), "tzdata_data")
 config :tzdata, :autoupdate, :disabled
 
 promex_disabled? =
