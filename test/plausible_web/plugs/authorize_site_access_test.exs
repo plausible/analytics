c1bb70f68c81fd5174a28b6c5b4c922e2260de07
Adrian Gruntkowski <adrian.gruntkowski@gmail.com>
2024-09-10 22:59:35 +0200

    Add regression test for shared link auth case

--- test/plausible_web/plugs/authorize_site_access_test.exs
+++ test/plausible_web/plugs/authorize_site_access_test.exs
@@ -2,19 +2,16 @@ defmodule PlausibleWeb.Plugs.AuthorizeSiteAccessTest do
   use PlausibleWeb.ConnCase, async: true
   alias PlausibleWeb.Plugs.AuthorizeSiteAccess
 
-  setup [:create_user, :log_in]
+  setup [:create_user, :log_in, :create_site]
 
-  test "doesn't allow :website bypass with :domain in body", %{conn: conn, user: me} do
-    my_site = insert(:site, memberships: [build(:site_membership, user: me, role: :owner)])
-
-    other_site =
-      insert(:site, memberships: [build(:site_membership, user: insert(:user), role: :owner)])
+  test "doesn't allow :website bypass with :domain in body", %{conn: conn, site: site} do
+    other_site = insert(:site, members: [build(:user)])
 
     conn =
       conn
       |> bypass_through(PlausibleWeb.Router)
-      |> get("/#{other_site.domain}/settings", %{"domain" => my_site.domain})
-      |> AuthorizeSiteAccess.call([:admin, :owner])
+      |> get("/#{other_site.domain}/settings", %{"domain" => site.domain})
+      |> AuthorizeSiteAccess.call(_allowed_roles = [:admin, :owner])
 
     assert conn.halted
     assert conn.status == 404
@@ -35,5 +32,23 @@ defmodule PlausibleWeb.Plugs.AuthorizeSiteAccessTest do
 
     assert conn.resp_body ==
              "{\"error\":\"Site does not exist or user does not have sufficient access.\"}"
+  end
+
+  test "rejects unrelated shared link slug even if user is permitted for site", %{
+    conn: conn,
+    site: site
+  } do
+    shared_link_other_site = insert(:shared_link, site: build(:site))
+
+    params = %{"shared_link" => %{"name" => "some name"}}
+
+    conn =
+      conn
+      |> bypass_through(PlausibleWeb.Router)
+      |> put("/sites/#{site.domain}/shared-links/#{shared_link_other_site.slug}", params)
+      |> AuthorizeSiteAccess.call(_allowed_roles = [:super_admin, :admin, :owner])
+
+    assert conn.halted
+    assert conn.status == 404
   end
 end
