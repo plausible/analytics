59c7ce2ef163a3062c5a60ad5613a804c396c301
Artur Pata <artur.pata@gmail.com>
2024-09-18 18:46:05 +0300

    Fix auth issue with POST /api/docs/query (#4593)

--- lib/plausible_web/router.ex
+++ lib/plausible_web/router.ex
@@ -50,9 +50,7 @@ defmodule PlausibleWeb.Router do
     plug :accepts, ["json"]
     plug :fetch_session
     plug PlausibleWeb.AuthPlug
-
     plug PlausibleWeb.Plugs.AuthorizeSiteAccess, [:admin, :super_admin, :owner]
-
     plug PlausibleWeb.Plugs.NoRobots
   end
 
