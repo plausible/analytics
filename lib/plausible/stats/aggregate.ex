03b5b73575a2459a85a2426b4d300124112261a1
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2024-09-18 15:21:37 +0300

    Refactor aggregate.ex to be more straight-forward in output format building

--- lib/plausible/stats/aggregate.ex
+++ lib/plausible/stats/aggregate.ex
@@ -7,7 +7,7 @@ defmodule Plausible.Stats.Aggregate do
 
   use Plausible.ClickhouseRepo
   use Plausible
-  alias Plausible.Stats.{Query, QueryExecutor, Util}
+  alias Plausible.Stats.{Query, QueryExecutor}
 
   def aggregate(site, query, metrics) do
     {currency, metrics} =
@@ -19,39 +19,39 @@ defmodule Plausible.Stats.Aggregate do
 
     Query.trace(query, metrics)
 
-    query_with_metrics = %Query{query | metrics: metrics}
-
-    run_query(site, query_with_metrics)
-    |> cast_revenue_metrics_to_money(currency)
-    |> Enum.map(&maybe_round_value/1)
-    |> Enum.map(fn {metric, value} -> {metric, %{value: value}} end)
-    |> Enum.into(%{})
-  end
-
-  def run_query(site, query) do
+    query = %Query{query | metrics: metrics}
     query_result = QueryExecutor.execute(site, query)
 
-    [%{metrics: metrics_values}] = query_result.results
+    [entry] = query_result.results
 
-    Enum.zip(query.metrics, metrics_values)
-    |> Map.new()
+    query.metrics
+    |> Enum.with_index()
+    |> Enum.map(fn {metric, index} ->
+      {
+        metric,
+        %{
+          value:
+            entry.metrics
+            |> Enum.at(index)
+            |> maybe_round_value(metric)
+            |> maybe_cast_metric_to_money(metric, currency)
+        }
+      }
+    end)
+    |> Enum.into(%{})
   end
 
   @metrics_to_round [:bounce_rate, :time_on_page, :visit_duration, :sample_percent]
 
-  defp maybe_round_value({metric, nil}), do: {metric, nil}
-
-  defp maybe_round_value({metric, value}) when metric in @metrics_to_round do
-    {metric, round(value)}
-  end
-
-  defp maybe_round_value(entry), do: entry
+  defp maybe_round_value(nil, _metric), do: nil
+  defp maybe_round_value(value, metric) when metric in @metrics_to_round, do: round(value)
+  defp maybe_round_value(value, _metric), do: value
 
   on_ee do
-    defp cast_revenue_metrics_to_money(results, revenue_goals) do
-      Plausible.Stats.Goal.Revenue.cast_revenue_metrics_to_money(results, revenue_goals)
+    defp maybe_cast_metric_to_money(value, metric, currency) do
+      Plausible.Stats.Goal.Revenue.maybe_cast_metric_to_money(value, metric, currency)
     end
   else
-    defp cast_revenue_metrics_to_money(results, _revenue_goals), do: results
+    defp maybe_cast_metric_to_money(value, _metric, _currency), do: value
   end
 end
