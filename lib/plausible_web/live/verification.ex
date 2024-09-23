71fa38709f5cc2ac0352d2d510b62a353f615014
ruslandoga <doga.ruslan@gmail.com>
2024-09-23 19:17:32 +0700

    skip verification in ce (#4604)

--- lib/plausible_web/live/verification.ex
+++ lib/plausible_web/live/verification.ex
@@ -1,9 +1,10 @@
 defmodule PlausibleWeb.Live.Verification do
   @moduledoc """
   LiveView coordinating the site verification process.
-  Onboarding new sites, renders a standalone component. 
+  Onboarding new sites, renders a standalone component.
   Embedded modal variant is available for general site settings.
   """
+  use Plausible
   use PlausibleWeb, :live_view
   use Phoenix.HTML
 
@@ -28,13 +29,14 @@ defmodule PlausibleWeb.Live.Verification do
     private = Map.get(socket.private.connect_info, :private, %{})
 
     super_admin? = Plausible.Auth.is_super_admin?(socket.assigns.current_user)
+    has_pageviews? = has_pageviews?(site)
 
     socket =
       assign(socket,
         site: site,
         super_admin?: super_admin?,
         domain: domain,
-        has_pageviews?: has_pageviews?(site),
+        has_pageviews?: has_pageviews?,
         component: @component,
         installation_type: params["installation_type"],
         report_to: self(),
@@ -45,28 +47,65 @@ defmodule PlausibleWeb.Live.Verification do
         attempts: 0
       )
 
-    if connected?(socket) do
-      launch_delayed(socket)
+    on_ee do
+      if connected?(socket) do
+        launch_delayed(socket)
+      end
     end
 
-    {:ok, socket}
+    on_ee do
+      {:ok, socket}
+    else
+      # on CE we skip the verification process and instead,
+      # we just wait for the first pageview to be recorded
+      socket =
+        if has_pageviews? do
+          redirect_to_stats(socket)
+        else
+          schedule_pageviews_check(socket)
+        end
+
+      {:ok, socket}
+    end
   end
 
-  def render(assigns) do
-    ~H"""
-    <PlausibleWeb.Components.FlowProgress.render flow={@flow} current_step="Verify installation" />
+  on_ee do
+    def render(assigns) do
+      ~H"""
+      <PlausibleWeb.Components.FlowProgress.render flow={@flow} current_step="Verify installation" />
 
-    <.live_component
-      module={@component}
-      installation_type={@installation_type}
-      domain={@domain}
-      id="verification-standalone"
-      attempts={@attempts}
-      flow={@flow}
-      awaiting_first_pageview?={not @has_pageviews?}
-      super_admin?={@super_admin?}
-    />
-    """
+      <.live_component
+        module={@component}
+        installation_type={@installation_type}
+        domain={@domain}
+        id="verification-standalone"
+        attempts={@attempts}
+        flow={@flow}
+        awaiting_first_pageview?={not @has_pageviews?}
+        super_admin?={@super_admin?}
+      />
+      """
+    end
+  else
+    def render(assigns) do
+      ~H"""
+      <PlausibleWeb.Components.FlowProgress.render flow={@flow} current_step="Verify installation" />
+      <.awaiting_pageviews />
+      """
+    end
+  end
+
+  on_ce do
+    defp awaiting_pageviews(assigns) do
+      ~H"""
+      <PlausibleWeb.Components.Generic.focus_box>
+        <div class="flex items-center">
+          <div class="block pulsating-circle"></div>
+          <p class="ml-8">Awaiting your first pageview …</p>
+        </div>
+      </PlausibleWeb.Components.Generic.focus_box>
+      """
+    end
   end
 
   def handle_event("launch-verification", _, socket) do
@@ -116,7 +155,7 @@ defmodule PlausibleWeb.Live.Verification do
     interpretation = Checks.interpret_diagnostics(state)
 
     if not socket.assigns.has_pageviews? do
-      Process.send_after(self(), :check_pageviews, socket.assigns.delay * 2)
+      schedule_pageviews_check(socket)
     end
 
     update_component(socket,
@@ -132,15 +171,22 @@ defmodule PlausibleWeb.Live.Verification do
   def handle_info(:check_pageviews, socket) do
     socket =
       if has_pageviews?(socket.assigns.site) do
-        redirect(socket,
-          external: Routes.stats_url(PlausibleWeb.Endpoint, :stats, socket.assigns.domain, [])
-        )
+        redirect_to_stats(socket)
       else
-        Process.send_after(self(), :check_pageviews, socket.assigns.delay * 2)
-        socket
+        schedule_pageviews_check(socket)
       end
 
     {:noreply, socket}
+  end
+
+  defp schedule_pageviews_check(socket) do
+    Process.send_after(self(), :check_pageviews, socket.assigns.delay * 2)
+    socket
+  end
+
+  defp redirect_to_stats(socket) do
+    stats_url = Routes.stats_url(PlausibleWeb.Endpoint, :stats, socket.assigns.domain, [])
+    redirect(socket, external: stats_url)
   end
 
   defp reset_component(socket) do
