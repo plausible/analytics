23d1f7e53a57ba34ca79717279b16d5249792e47
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2024-09-30 17:24:01 +0700

    handle cross-device file move

--- test/plausible/imported/csv_importer_test.exs
+++ test/plausible/imported/csv_importer_test.exs
@@ -551,7 +551,7 @@ defmodule Plausible.Imported.CSVImporterTest do
           )
         )
       else
-        File.rename!(local_path, Path.join(tmp_dir, "plausible-export.zip"))
+        Plausible.File.mv!(local_path, Path.join(tmp_dir, "plausible-export.zip"))
       end
 
       # unzip archive
