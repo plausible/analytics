df37b8718fa2679bc9f05256338e6184bbfce6f6
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2024-10-23 11:19:15 +0300

    not variable stroke width

--- assets/js/dashboard/stats/reports/change-arrow.tsx
+++ assets/js/dashboard/stats/reports/change-arrow.tsx
@@ -25,7 +25,7 @@ export function ChangeArrow({
     : ` ${numberShortFormatter(Math.abs(change))}%`
 
   let icon = null
-  const arrowClassName = classNames(color(change, metric), strokeClass(change), "inline-block h-3 w-3 stroke-current")
+  const arrowClassName = classNames(color(change, metric), "inline-block h-3 w-3 stroke-[1px] stroke-current")
 
   if (change > 0) {
     icon = (<ArrowUpRightIcon className={arrowClassName} />)
@@ -47,14 +47,4 @@ function color(change: number, metric: Metric) {
   const invert = metric === 'bounce_rate'
 
   return (change > 0) != invert ? 'text-green-500' : 'text-red-400'
-}
-
-function strokeClass(change: number) {
-  if (Math.abs(change) < 5) {
-    return "stroke-[0.5px]"
-  } else if (Math.abs(change) < 25) {
-    return "stroke-[1px]"
-  } else {
-    return "stroke-[2px]"
-  }
 }
