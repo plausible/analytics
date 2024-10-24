c341b6eae81fabb6485decc391422245d4e166a0
Robert Joonas <robertjoonas16@gmail.com>
2024-10-24 18:09:07 +0200

    test pageleaves in SPAs

--- tracker/test/pageleave.spec.js
+++ tracker/test/pageleave.spec.js
@@ -5,15 +5,37 @@ const { test } = require('@playwright/test');
 const { LOCAL_SERVER_ADDR } = require('./support/server');
 
 test.describe('pageleave extension', () => {
-  test('sends a pageleave when navigating to the next page', async ({ page, browserName }) => {
-    test.skip(browserName === 'webkit', 'Not testable on Webkit');
+  test.skip(({browserName}) => browserName === 'webkit', 'Not testable on Webkit');
 
+  test('sends a pageleave when navigating to the next page', async ({ page }) => {
     const pageviewRequestMock = mockRequest(page, '/api/event')
     await page.goto('/pageleave.html');
     await pageviewRequestMock;
 
     await clickPageElementAndExpectEventRequests(page, '#navigate-away', [
       {n: 'pageleave', u: `${LOCAL_SERVER_ADDR}/pageleave.html`}
+    ])
+  });
+
+  test('sends pageleave and pageview on hash-based SPA navigation', async ({ page }) => {
+    const pageviewRequestMock = mockRequest(page, '/api/event')
+    await page.goto('/pageleave-hash.html');
+    await pageviewRequestMock;
+
+    await clickPageElementAndExpectEventRequests(page, '#hash-nav', [
+      {n: 'pageleave', u: `${LOCAL_SERVER_ADDR}/pageleave-hash.html`},
+      {n: 'pageview', u: `${LOCAL_SERVER_ADDR}/pageleave-hash.html#some-hash`}
+    ])
+  });
+
+  test('sends pageleave and pageview on history-based SPA navigation', async ({ page }) => {
+    const pageviewRequestMock = mockRequest(page, '/api/event')
+    await page.goto('/pageleave.html');
+    await pageviewRequestMock;
+
+    await clickPageElementAndExpectEventRequests(page, '#history-nav', [
+      {n: 'pageleave', u: `${LOCAL_SERVER_ADDR}/pageleave.html`},
+      {n: 'pageview', u: `${LOCAL_SERVER_ADDR}/another-page`}
     ])
   });
 });
