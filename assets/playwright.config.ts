17e4b984fa2b6261dc95584d06ad8c5d8f7cd594
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2024-10-28 09:58:01 +0200

    Update playwright reporter

--- assets/playwright.config.ts
+++ assets/playwright.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: process.env.CI ? 'github' : 'list',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
