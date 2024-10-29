1bb8a031c755e911932b0d637b3f62199f5bdf8c
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2024-10-29 12:20:08 +0200

    docs for util

--- assets/playwright-tests/playwright.ts
+++ assets/playwright-tests/playwright.ts
@@ -4,7 +4,12 @@ import { test as playwrightTest, expect } from '@playwright/test'
 
 export { expect, Page } from '@playwright/test'
 
-// Test wrapper which fails if any JS errors are logged
+// Test wrapper which fails if any JS errors are logged.
+//
+// This is a catch-all error handling tool - all errors which
+// are not handled in JS trigger errors, such as 500s or other
+// frontend bugs.
+//
 // Idea from https://www.checklyhq.com/blog/track-frontend-javascript-exceptions-with-playwright/
 export const test = playwrightTest.extend<{ page: void }>({
   page: async ({ page }: { page: any }, use: any) => {
