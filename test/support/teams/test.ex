5d9e94770d966a22efecb73502539b968a4e1e61
Adrian Gruntkowski <adrian.gruntkowski@gmail.com>
2024-10-31 14:17:31 +0100

    Run full test suite against `read_team_schemas` FF on CI (#4763)
    * Run all tests with `read_team_schemas` flag up and down on CI
    
    Replacing the existing setup for `experimental_reduced_joins` which
    wasn't actively used and will be removed some time in the future.
    This way we don't make CI run longer than necessary.
    
    * Add one more test case for listing invitations on /sites LV
    
    * Intentionally reintroduce regression to test CI
    
    * Revert "Intentionally reintroduce regression to test CI"
    
    This reverts commit cdb83c1494935d44a9ab2133c9890a015a6a7b82.
    
    * Run tests for `experimental_reduced_joins` together with `read_team_schemas`

--- test/support/teams/test.ex
+++ test/support/teams/test.ex
@@ -78,7 +78,13 @@ defmodule Plausible.Teams.Test do
       )
 
     team_invitation =
-      insert(:team_invitation, team: team, email: email, inviter: inviter, role: :guest)
+      insert(:team_invitation,
+        invitation_id: old_model_invitation.invitation_id,
+        team: team,
+        email: email,
+        inviter: inviter,
+        role: :guest
+      )
 
     insert(:guest_invitation, team_invitation: team_invitation, site: site, role: role)
 
