cdb83c1494935d44a9ab2133c9890a015a6a7b82
Adrian Gruntkowski <adrian.gruntkowski@gmail.com>
2024-10-31 12:41:47 +0100

    Intentionally reintroduce regression to test CI

--- lib/plausible_web/live/sites.ex
+++ lib/plausible_web/live/sites.ex
@@ -35,7 +35,8 @@ defmodule PlausibleWeb.Live.Sites do
       |> assign_new(:needs_to_upgrade, fn %{current_user: current_user, sites: sites} ->
         user_owns_sites =
           Enum.any?(sites.entries, fn site ->
-            length(site.invitations) > 0 && List.first(site.invitations).role == :owner
+            # length(site.invitations) > 0 && List.first(site.invitations).role == :owner
+            List.first(site.invitations).role == :owner
           end) ||
             Auth.user_owns_sites?(current_user)
 
