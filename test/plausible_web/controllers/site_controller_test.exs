cea368c0940bf88759a729d24b96619e2e50c405
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2024-11-03 15:24:42 +0700

    remove @plausible.io contacts from CE

--- test/plausible_web/controllers/site_controller_test.exs
+++ test/plausible_web/controllers/site_controller_test.exs
@@ -408,6 +408,12 @@ defmodule PlausibleWeb.SiteControllerTest do
 
       assert html_response(conn, 200) =~
                "This domain cannot be registered. Perhaps one of your colleagues registered it?"
+
+      if Plausible.ee?() do
+        assert html_response(conn, 200) =~ "support@plausible.io"
+      else
+        refute html_response(conn, 200) =~ "support@plausible.io"
+      end
     end
 
     test "renders form again when domain was changed from elsewhere", %{conn: conn} do
@@ -425,6 +431,12 @@ defmodule PlausibleWeb.SiteControllerTest do
 
       assert html_response(conn, 200) =~
                "This domain cannot be registered. Perhaps one of your colleagues registered it?"
+
+      if Plausible.ee?() do
+        assert html_response(conn, 200) =~ "support@plausible.io"
+      else
+        refute html_response(conn, 200) =~ "support@plausible.io"
+      end
     end
 
     test "allows creating the site if domain was changed by the owner", %{
