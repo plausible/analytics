726a45d1e4e44f5bcb1ecaf851bbab05582630ac
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2024-11-07 12:13:49 +0200

    Speed up calculating acquisition_channel in clickhouse
    The previous `has` queries proved to be problematic and causing a lot of
    CPU overhead.
    
    Benchmarked via this query:
    
    ```sql
    SELECT
      channel,
      count(),
      countIf(acquisition_channel(referrer_source, utm_medium, utm_campaign, utm_source, click_id_param) = channel) AS matches
    FROM events_v2
    WHERE timestamp > now() - toIntervalHour(48)
    GROUP BY channel
    ORDER BY count() desc
    ```
    
    Before this fix:
    ```
    query_duration_ms:                                                57960
    DiskReadElapsedMs:                                                374.712
    RealTimeMs:                                                       2891200.667
    UserTimeMs:                                                       2704024.783
    SystemTimeMs:                                                     1693.265
    OSCPUWaitMs:                                                      90.253
    OSCPUVirtualTimeMs:                                               2705709.58
    ```
    
    After this fix:
    ```
    query_duration_ms:                                                4367
    DiskReadElapsedMs:                                                454.356
    RealTimeMs:                                                       213892.207
    UserTimeMs:                                                       199363.485
    SystemTimeMs:                                                     1479.364
    OSCPUWaitMs:                                                      13.739
    OSCPUVirtualTimeMs:                                               200837.37
    ```
    
    Note that the new tables are not tracked in our schema as usual as
    they're pretty much temporary tables to create the dictionary without
    needing to upload files to clickhouse servers.

--- lib/plausible/data_migration/acquisition_channel.ex
+++ lib/plausible/data_migration/acquisition_channel.ex
@@ -1,40 +1,26 @@
 defmodule Plausible.DataMigration.AcquisitionChannel do
   @moduledoc """
-  Creates functions to calculate acquisition channel in ClickHouse
+  Creates dictionaries and functions to calculate acquisition channel in ClickHouse
 
   SQL files available at: priv/data_migrations/AcquisitionChannel/sql
   """
   use Plausible.DataMigration, dir: "AcquisitionChannel", repo: Plausible.IngestRepo
 
   def run(opts \\ []) do
-    source_categories =
-      Plausible.Ingestion.Acquisition.source_categories()
-      |> invert_map()
-
     on_cluster_statement = Plausible.MigrationUtils.on_cluster_statement("sessions_v2")
 
     run_sql_multi(
       "acquisition_channel_functions",
       [
-        on_cluster_statement: on_cluster_statement
+        on_cluster_statement: on_cluster_statement,
+        dictionary_connection_params: Plausible.MigrationUtils.dictionary_connection_params()
       ],
       params: %{
-        "source_category_shopping" => source_categories["SOURCE_CATEGORY_SHOPPING"],
-        "source_category_social" => source_categories["SOURCE_CATEGORY_SOCIAL"],
-        "source_category_video" => source_categories["SOURCE_CATEGORY_VIDEO"],
-        "source_category_search" => source_categories["SOURCE_CATEGORY_SEARCH"],
-        "source_category_email" => source_categories["SOURCE_CATEGORY_EMAIL"],
+        "source_categories" =>
+          Plausible.Ingestion.Acquisition.source_categories() |> Map.to_list(),
         "paid_sources" => Plausible.Ingestion.Source.paid_sources()
       },
       quiet: Keyword.get(opts, :quiet, false)
-    )
-  end
-
-  defp invert_map(source_categories) do
-    source_categories
-    |> Enum.group_by(
-      fn {_source, category} -> category end,
-      fn {source, _category} -> source end
     )
   end
 end
