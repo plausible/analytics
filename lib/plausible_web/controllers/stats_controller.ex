abdd22500824175a675a8a6ed34e0ac1a271bd72
Uku Taht <uku.taht@gmail.com>
2024-11-07 11:51:31 +0200

    Feature flag channels in CSV

--- lib/plausible_web/controllers/stats_controller.ex
+++ lib/plausible_web/controllers/stats_controller.ex
@@ -121,7 +121,6 @@ defmodule PlausibleWeb.StatsController do
       csvs = %{
         ~c"visitors.csv" => fn -> main_graph_csv(site, query) end,
         ~c"sources.csv" => fn -> Api.StatsController.sources(conn, params) end,
-        ~c"channels.csv" => fn -> Api.StatsController.channels(conn, params) end,
         ~c"utm_mediums.csv" => fn -> Api.StatsController.utm_mediums(conn, params) end,
         ~c"utm_sources.csv" => fn -> Api.StatsController.utm_sources(conn, params) end,
         ~c"utm_campaigns.csv" => fn -> Api.StatsController.utm_campaigns(conn, params) end,
@@ -144,6 +143,14 @@ defmodule PlausibleWeb.StatsController do
         ~c"referrers.csv" => fn -> Api.StatsController.referrers(conn, params) end,
         ~c"custom_props.csv" => fn -> Api.StatsController.all_custom_prop_values(conn, params) end
       }
+
+      csvs =
+        if FunWithFlags.enabled?(:channels, for: site) ||
+             FunWithFlags.enabled?(:channels, for: conn.assigns[:current_user]) do
+          Map.put(csvs, ~c"channels.csv", fn -> Api.StatsController.channels(conn, params) end)
+        else
+          csvs
+        end
 
       csv_values =
         Map.values(csvs)
