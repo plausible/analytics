ee954354bd2af4da1ba818b67322ff0f32cda6b8
Artur Pata <artur.pata@gmail.com>
2024-11-11 15:53:40 +0200

    Refactor to explicit segment type

--- priv/repo/migrations/20241107120000_create_segments.exs
+++ priv/repo/migrations/20241107120000_create_segments.exs
@@ -2,14 +2,16 @@ defmodule Plausible.Repo.Migrations.CreateSegments do
   use Ecto.Migration
 
   def change do
+    execute(
+      "CREATE TYPE segment_type AS ENUM ('personal', 'site')",
+      "DROP TYPE segment_type"
+    )
+
     create table(:segments) do
       add :name, :string, null: false
-      add :personal, :boolean, default: true, null: false
+      add :type, :segment_type, null: false, default: "personal"
       add :segment_data, :map, null: false
       add :site_id, references(:sites, on_delete: :delete_all), null: false
-
-      # owner_id is null (aka segment is dangling) when the original owner is deassociated from the site
-      # the segment is dangling until another user edits it: the editor becomes the new owner
       add :owner_id, references(:users, on_delete: :nothing), null: true
 
       timestamps()
