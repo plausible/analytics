1b4b8fbf648c6fbc74dfedd2683861fa5255eaf7
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2024-11-11 10:43:04 +0200

    Channels: Migration to add column, backfill code
    This change adds `acqusition_channel` columns to events_v2 and
    sessions_v2 tables. These columns are materialized - we don't ingest
    into them directly. Instead they're calculated based on other columns.
    
    The data migration changes now allow to also backfill the column.
    
    Tested the ability to change definitions by changing the function
    definitions and re-running the migration with backfill. Confirmed that
    the underlying data changed as expected.

--- lib/plausible/data_migration/acquisition_channel.ex
+++ lib/plausible/data_migration/acquisition_channel.ex
@@ -2,6 +2,15 @@ defmodule Plausible.DataMigration.AcquisitionChannel do
   @moduledoc """
   Creates dictionaries and functions to calculate acquisition channel in ClickHouse
 
+  Creates `acquisition_channel` columns in `events_v2` and `sessions_v2` tables.
+  Run via `Plausible.DataMigration.AcquisitionChannel.run(options)`
+
+  Options:
+  - `add_column` - creates the materialized column. Already done in a migration
+  - `update_column` - Updates the column definition to use new function definitions. Defaults to true.
+      Note that historical data is only updated if `backfill` is set to true or if it was never materialized.
+  - `backfill` - backfills the data for the column. Speeds up calculations on historical data.
+
   SQL files available at: priv/data_migrations/AcquisitionChannel/sql
   """
   use Plausible.DataMigration, dir: "AcquisitionChannel", repo: Plausible.IngestRepo
@@ -11,19 +20,53 @@ defmodule Plausible.DataMigration.AcquisitionChannel do
     # In distributed environments, wait for insert to all temporary tables.
     insert_quorum = Plausible.IngestRepo.replica_count("sessions_v2")
 
-    run_sql_multi(
-      "acquisition_channel_functions",
-      [
+    :ok =
+      run_sql_multi(
+        "acquisition_channel_functions",
+        [
+          on_cluster_statement: on_cluster_statement,
+          dictionary_connection_params: Plausible.MigrationUtils.dictionary_connection_params(),
+          insert_quorum: insert_quorum
+        ],
+        params: %{
+          "source_categories" =>
+            Plausible.Ingestion.Acquisition.source_categories() |> Map.to_list(),
+          "paid_sources" => Plausible.Ingestion.Source.paid_sources()
+        },
+        quiet: Keyword.get(opts, :quiet, false)
+      )
+
+    cond do
+      Keyword.get(opts, :add_column) ->
+        alter_data_tables("acquisition_channel_add_materialized_column", on_cluster_statement)
+
+      Keyword.get(opts, :update_column, true) ->
+        alter_data_tables("acquisition_channel_update_materialized_column", on_cluster_statement)
+
+      true ->
+        nil
+    end
+
+    if Keyword.get(opts, :backfill) do
+      alter_data_tables("acquisition_channel_backfill_materialized_column", on_cluster_statement)
+    end
+
+    :ok
+  end
+
+  defp alter_data_tables(sql_name, on_cluster_statement) do
+    {:ok, _} =
+      run_sql(
+        sql_name,
         on_cluster_statement: on_cluster_statement,
-        dictionary_connection_params: Plausible.MigrationUtils.dictionary_connection_params(),
-        insert_quorum: insert_quorum
-      ],
-      params: %{
-        "source_categories" =>
-          Plausible.Ingestion.Acquisition.source_categories() |> Map.to_list(),
-        "paid_sources" => Plausible.Ingestion.Source.paid_sources()
-      },
-      quiet: Keyword.get(opts, :quiet, false)
-    )
+        table: "events_v2"
+      )
+
+    {:ok, _} =
+      run_sql(
+        sql_name,
+        on_cluster_statement: on_cluster_statement,
+        table: "sessions_v2"
+      )
   end
 end
