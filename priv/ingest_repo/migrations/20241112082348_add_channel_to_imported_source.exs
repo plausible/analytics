1800eae77257e7724fb44d6bfb6d964b2487721c
Uku Taht <uku.taht@gmail.com>
2024-11-14 19:54:28 +0200

    Revert "Revert "Remove migration""
    This reverts commit 7958a46c5c96339f4d83f8274a2e80033317c087.

--- priv/ingest_repo/migrations/20241112082348_add_channel_to_imported_source.exs
+++
@@ -1,9 +0,0 @@
-defmodule Plausible.IngestRepo.Migrations.AddChannelToImportedSource do
-  use Ecto.Migration
-
-  def change do
-    alter table(:imported_sources) do
-      add :channel, :"LowCardinality(String)"
-    end
-  end
-end
