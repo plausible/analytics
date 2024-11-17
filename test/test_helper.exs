80fd441f94f4c95984961cd6de02077717467195
Robert Joonas <robertjoonas16@gmail.com>
2024-11-17 21:06:04 +0100

    enable scroll_depth flag in tests

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -7,6 +7,7 @@ Mox.defmock(Plausible.HTTPClient.Mock, for: Plausible.HTTPClient.Interface)
 Application.ensure_all_started(:double)
 
 FunWithFlags.enable(:channels)
+FunWithFlags.enable(:scroll_depth)
 # Temporary flag to test `read_team_schemas` flag on all tests.
 if System.get_env("TEST_READ_TEAM_SCHEMAS") == "1" do
   IO.puts("READS TEAM SCHEMAS")
