7ee1254d6f3ddd45d17524980cab46a3387f6013
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2024-11-19 17:50:54 +0200

    ce behavior

--- test/plausible/stats/sampling_cache_test.exs
+++ test/plausible/stats/sampling_cache_test.exs
@@ -1,32 +1,64 @@
 defmodule Plausible.Stats.SamplingCacheTest do
   use Plausible.DataCase, async: true
-  alias Plausible.Stats.SamplingCache
 
-  test "returns cached values for traffic in past 30 days", %{test: test} do
-    now = DateTime.utc_now()
+  use Plausible
 
-    Plausible.IngestRepo.insert_all(Plausible.Ingestion.Counters.Record, [
-      %{site_id: 2, value: 11_000_000, event_timebucket: add(now, -1, :day), metric: "buffered"},
-      %{site_id: 2, value: 11_000_000, event_timebucket: add(now, -5, :day), metric: "buffered"},
-      %{site_id: 2, value: 11_000_000, event_timebucket: add(now, -35, :day), metric: "buffered"},
-      %{site_id: 3, value: 44_000_000, event_timebucket: add(now, -35, :day), metric: "buffered"},
-      %{site_id: 4, value: 11_000_000, event_timebucket: add(now, -35, :day), metric: "buffered"}
-    ])
+  on_ee do
+    alias Plausible.Stats.SamplingCache
 
-    {:ok, _} = start_test_cache(test)
+    describe "getter" do
+      test "returns cached values for traffic in past 30 days", %{test: test} do
+        now = DateTime.utc_now()
 
-    assert SamplingCache.get(1) == nil
-    assert SamplingCache.get(2) == 22_000_000
-    assert SamplingCache.get(3) == nil
-    assert SamplingCache.get(4) == nil
-  end
+        Plausible.IngestRepo.insert_all(Plausible.Ingestion.Counters.Record, [
+          %{
+            site_id: 2,
+            value: 11_000_000,
+            event_timebucket: add(now, -1, :day),
+            metric: "buffered"
+          },
+          %{
+            site_id: 2,
+            value: 11_000_000,
+            event_timebucket: add(now, -5, :day),
+            metric: "buffered"
+          },
+          %{
+            site_id: 2,
+            value: 11_000_000,
+            event_timebucket: add(now, -35, :day),
+            metric: "buffered"
+          },
+          %{
+            site_id: 3,
+            value: 44_000_000,
+            event_timebucket: add(now, -35, :day),
+            metric: "buffered"
+          },
+          %{
+            site_id: 4,
+            value: 11_000_000,
+            event_timebucket: add(now, -35, :day),
+            metric: "buffered"
+          }
+        ])
 
-  def add(datetime, n, unit) do
-    DateTime.add(datetime, n, unit) |> DateTime.truncate(:second)
-  end
+        {:ok, _} = start_test_cache(test)
 
-  defp start_test_cache(cache_name) do
-    %{start: {m, f, a}} = SamplingCache.child_spec(cache_name: cache_name)
-    apply(m, f, a)
+        assert SamplingCache.get(1) == nil
+        assert SamplingCache.get(2) == 22_000_000
+        assert SamplingCache.get(3) == nil
+        assert SamplingCache.get(4) == nil
+      end
+    end
+
+    def add(datetime, n, unit) do
+      DateTime.add(datetime, n, unit) |> DateTime.truncate(:second)
+    end
+
+    defp start_test_cache(cache_name) do
+      %{start: {m, f, a}} = SamplingCache.child_spec(cache_name: cache_name)
+      apply(m, f, a)
+    end
   end
 end
