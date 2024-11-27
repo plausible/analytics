031ddde40b664a213b44598834f72809baa095fa
Adam Rutkowski <hq@mtod.org>
2024-11-27 10:00:18 +0100

    Fix regions test

--- test/plausible_web/controllers/api/stats_controller/regions_test.exs
+++ test/plausible_web/controllers/api/stats_controller/regions_test.exs
@@ -1,4 +1,5 @@
 defmodule PlausibleWeb.Api.StatsController.RegionsTest do
+  use Plausible.Teams.Test
   use PlausibleWeb.ConnCase
 
   describe "GET /api/stats/:domain/regions" do
@@ -99,7 +100,7 @@ defmodule PlausibleWeb.Api.StatsController.RegionsTest do
       # Given it's 28th Nov and there's 30 day range, the starting day falls on 29th Oct
       # which coincides with daylight savings time change there:
       # https://www.timeanddate.com/time/change/portugal/ponta-delgada-azores.
-      site = insert(:site, members: [user], timezone: "Atlantic/Azores")
+      site = new_site(owner: user, timezone: "Atlantic/Azores")
 
       populate_stats(site, [
         build(:pageview, timestamp: relative_time(minutes: -5))
