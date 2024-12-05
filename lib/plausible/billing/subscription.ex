6cf6314be6296177e06e8ed038f84edb5f7af781
Adrian Gruntkowski <adrian.gruntkowski@gmail.com>
2024-12-05 13:43:11 +0100

    Comment out legacy fields and relationships

--- lib/plausible/billing/subscription.ex
+++ lib/plausible/billing/subscription.ex
@@ -33,7 +33,7 @@ defmodule Plausible.Billing.Subscription do
     field :last_bill_date, :date
     field :currency_code, :string
 
-    belongs_to :user, Plausible.Auth.User
+    ## belongs_to :user, Plausible.Auth.User
     belongs_to :team, Plausible.Teams.Team
 
     timestamps()
