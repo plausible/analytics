d9c15740855afaef98b2671a72bd91767620ab27
Adrian Gruntkowski <adrian.gruntkowski@gmail.com>
2024-12-05 15:59:46 +0100

    WIP

--- lib/plausible/billing/site_locker.ex
+++ lib/plausible/billing/site_locker.ex
@@ -1,6 +1,8 @@
 defmodule Plausible.Billing.SiteLocker do
   use Plausible.Repo
 
+  alias Plausible.Teams
+
   @type update_opt() :: {:send_email?, boolean()}
 
   @type lock_reason() ::
@@ -9,28 +11,23 @@ defmodule Plausible.Billing.SiteLocker do
           | :no_trial
           | :no_active_subscription
 
-  @spec update_sites_for(Plausible.Auth.User.t(), [update_opt()]) ::
+  @spec update_sites_for(Teams.Team.t(), [update_opt()]) ::
           {:locked, lock_reason()} | :unlocked
-  def update_sites_for(user, opts \\ []) do
+  def update_sites_for(team, opts \\ []) do
     send_email? = Keyword.get(opts, :send_email?, true)
 
-    user = Plausible.Users.with_subscription(user)
-
-    team =
-      case Plausible.Teams.get_by_owner(user) do
-        {:ok, team} -> team
-        _ -> nil
-      end
+    team = Teams.with_subscription(team)
 
     case Plausible.Teams.Billing.check_needs_to_upgrade(team) do
       {:needs_to_upgrade, :grace_period_ended} ->
-        set_lock_status_for(user, true)
+        set_lock_status_for(team, true)
 
-        if user.grace_period.is_over != true do
-          Plausible.Users.end_grace_period(user)
+        if team.grace_period.is_over != true do
+          Plausible.Teams.end_grace_period(team)
 
           if send_email? do
-            send_grace_period_end_email(user)
+            team = Repo.preload(team, :owner)
+            send_grace_period_end_email(team)
           end
 
           {:locked, :grace_period_ended_now}
@@ -39,25 +36,18 @@ defmodule Plausible.Billing.SiteLocker do
         end
 
       {:needs_to_upgrade, reason} ->
-        set_lock_status_for(user, true)
+        set_lock_status_for(team, true)
         {:locked, reason}
 
       :no_upgrade_needed ->
-        set_lock_status_for(user, false)
+        set_lock_status_for(team, false)
         :unlocked
     end
   end
 
-  @spec set_lock_status_for(Plausible.Auth.User.t(), boolean()) :: {:ok, non_neg_integer()}
-  def set_lock_status_for(user, status) do
-    site_ids =
-      Repo.all(
-        from(s in Plausible.Site.Membership,
-          where: s.user_id == ^user.id,
-          where: s.role == :owner,
-          select: s.site_id
-        )
-      )
+  @spec set_lock_status_for(Teams.Team.t(), boolean()) :: {:ok, non_neg_integer()}
+  def set_lock_status_for(team, status) do
+    site_ids = Teams.owned_sites_ids(team)
 
     site_q =
       from(
@@ -70,18 +60,11 @@ defmodule Plausible.Billing.SiteLocker do
     {:ok, num_updated}
   end
 
-  @spec send_grace_period_end_email(Plausible.Auth.User.t()) :: Plausible.Mailer.result()
-  def send_grace_period_end_email(user) do
-    team =
-      case Plausible.Teams.get_by_owner(user) do
-        {:ok, team} -> team
-        _ -> nil
-      end
-
-    usage = Plausible.Teams.Billing.monthly_pageview_usage(team)
+  defp send_grace_period_end_email(team) do
+    usage = Teams.Billing.monthly_pageview_usage(team)
     suggested_plan = Plausible.Billing.Plans.suggest(team, usage.last_cycle.total)
 
-    user
+    team.owner
     |> PlausibleWeb.Email.dashboard_locked(usage, suggested_plan)
     |> Plausible.Mailer.send()
   end
--- lib/plausible/users.ex
+++ lib/plausible/users.ex
@@ -3,7 +3,6 @@ defmodule Plausible.Users do
   User context
   """
   use Plausible
-  @accept_traffic_until_free ~D[2135-01-01]
 
   import Ecto.Query
 
@@ -12,33 +11,9 @@ defmodule Plausible.Users do
   alias Plausible.Billing.Subscription
   alias Plausible.Repo
 
-  @spec on_trial?(Auth.User.t()) :: boolean()
-  on_ee do
-    def on_trial?(%Auth.User{trial_expiry_date: nil}), do: false
-
-    def on_trial?(user) do
-      user = with_subscription(user)
-      not Plausible.Billing.Subscriptions.active?(user.subscription) && trial_days_left(user) >= 0
-    end
-  else
-    def on_trial?(_), do: true
-  end
-
   @spec trial_days_left(Auth.User.t()) :: integer()
   def trial_days_left(user) do
     Date.diff(user.trial_expiry_date, Date.utc_today())
-  end
-
-  @spec update_accept_traffic_until(Auth.User.t()) :: Auth.User.t()
-  def update_accept_traffic_until(user) do
-    user =
-      user
-      |> Auth.User.changeset(%{accept_traffic_until: accept_traffic_until(user)})
-      |> Repo.update!()
-
-    Plausible.Teams.sync_team(user)
-
-    user
   end
 
   @spec bump_last_seen(Auth.User.t() | pos_integer(), NaiveDateTime.t()) :: :ok
@@ -52,35 +27,6 @@ defmodule Plausible.Users do
     Repo.update_all(q, set: [last_seen: now])
 
     :ok
-  end
-
-  @spec accept_traffic_until(Auth.User.t()) :: Date.t()
-  on_ee do
-    def accept_traffic_until(user) do
-      user = with_subscription(user)
-
-      cond do
-        Plausible.Users.on_trial?(user) ->
-          Date.shift(user.trial_expiry_date,
-            day: Auth.User.trial_accept_traffic_until_offset_days()
-          )
-
-        user.subscription && user.subscription.paddle_plan_id == "free_10k" ->
-          @accept_traffic_until_free
-
-        user.subscription && user.subscription.next_bill_date ->
-          Date.shift(user.subscription.next_bill_date,
-            day: Auth.User.subscription_accept_traffic_until_offset_days()
-          )
-
-        true ->
-          raise "This user is neither on trial or has a valid subscription. Manual intervention required."
-      end
-    end
-  else
-    def accept_traffic_until(_user) do
-      @accept_traffic_until_free
-    end
   end
 
   def with_subscription(%Auth.User{} = user) do
