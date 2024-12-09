f161bf37bb5a2e120df6b0e352eb8ade484493b3
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2024-12-09 10:54:28 +0200

    Inline utils

--- lib/plausible/stats/filters/utils.ex
+++ lib/plausible/stats/filters/utils.ex
@@ -1,27 +1,5 @@
 defmodule Plausible.Stats.Filters.Utils do
-  @moduledoc """
-  Contains utility functions for `StatsAPIFilterParser`.
-  """
-
-  @non_escaped_pipe_regex ~r/(?<!\\)\|/
-
-  def list_expression?(expression) do
-    Regex.match?(@non_escaped_pipe_regex, expression)
-  end
-
-  def wildcard_expression?(expression) do
-    String.contains?(expression, "*")
-  end
-
-  def parse_member_list(raw_value) do
-    raw_value
-    |> String.split(@non_escaped_pipe_regex)
-    |> Enum.map(&remove_escape_chars/1)
-  end
-
-  def remove_escape_chars(value) do
-    String.replace(value, "\\|", "|")
-  end
+  @moduledoc false
 
   def split_goals(goals) do
     Enum.split_with(goals, fn goal -> Plausible.Goal.type(goal) == :event end)
