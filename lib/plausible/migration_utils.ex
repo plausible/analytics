6f30a4f7d4db34ced2049fcefaf714f5e0dff283
karlo.smid <karlo.smid@tentamen.hr>
2025-01-08 14:20:48 +0100

    fix: 🐛 cluster detection
    not using table because table on first migration never exists!

--- lib/plausible/migration_utils.ex
+++ lib/plausible/migration_utils.ex
@@ -3,14 +3,10 @@ defmodule Plausible.MigrationUtils do
   Base module for to use in Clickhouse migrations
   """
 
-  def on_cluster_statement(table) do
-    if(clustered_table?(table), do: "ON CLUSTER '#{cluster_name()}'", else: "")
-  end
-
-  def clustered_table?(table) do
-    case Plausible.IngestRepo.query("SELECT 1 FROM system.replicas WHERE table = '#{table}'") do
-      {:ok, %{rows: []}} -> false
-      {:ok, _} -> true
+  def on_cluster_statement(_table) do
+    case cluster_name() do
+      {:ok, cluster} -> "ON CLUSTER '#{cluster}'"
+      _ -> ""
     end
   end
 
