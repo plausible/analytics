011b140440a93c15b6818e7fa3194873dc8e81c1
Marko Saric <34340819+metmarkosaric@users.noreply.github.com>
2025-01-10 13:02:14 +0100

    Update list.ex

--- lib/plausible_web/live/goal_settings/list.ex
+++ lib/plausible_web/live/goal_settings/list.ex
@@ -39,7 +39,7 @@ defmodule PlausibleWeb.Live.GoalSettings.List do
                       <%= goal %>
                       <br />
                       <span class="text-red-600">
-                        Upgrade to a Business plan to access this
+                        Upgrade to Business to access this
                       </span>
                     </div>
                   <% else %>
