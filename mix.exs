6f5df63ddc20c829f0b2d4988f70965deb04164f
Uku Taht <uku.taht@gmail.com>
2025-01-10 12:06:23 +0200

    Update storybook dependency

--- mix.exs
+++ mix.exs
@@ -148,7 +148,7 @@ defmodule Plausible.MixProject do
       {:odgn_json_pointer, "~> 3.0.1"},
       {:phoenix_bakery, "~> 0.1.2", only: [:ce, :ce_dev, :ce_test]},
       {:site_encrypt, github: "sasa1977/site_encrypt", only: [:ce, :ce_dev, :ce_test]},
-      {:phoenix_storybook, "~> 0.7.2"}
+      {:phoenix_storybook, "~> 0.8"}
     ]
   end
 
