06e47bd3df23e16449ce406bfedcfed57205e193
Adrian Gruntkowski <adrian.gruntkowski@gmail.com>
2025-01-13 15:33:58 +0100

    Add sample test touching on combobox in team setup

--- lib/plausible_web/live/teams.ex
+++ lib/plausible_web/live/teams.ex
@@ -121,7 +121,7 @@ defmodule PlausibleWeb.Live.Teams do
 
   def member(assigns) do
     ~H"""
-    <div class="mt-4">
+    <div class="member mt-4">
       <div class="flex items-center gap-x-5">
         <img src={User.profile_img_url(@user)} class="w-7 rounded-full" />
         <span class="text-sm">
@@ -135,7 +135,7 @@ defmodule PlausibleWeb.Live.Teams do
         </span>
         <div class="flex-1 text-right">
           <.dropdown class="relative">
-            <:button class="bg-transparent text-gray-800 dark:text-gray-100 hover:bg-gray-50 dark:hover:bg-gray-700 focus-visible:outline-gray-100 whitespace-nowrap truncate inline-flex items-center gap-x-2 font-medium rounded-md px-3.5 py-2.5 text-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 disabled:bg-gray-400 dark:disabled:text-white dark:disabled:text-gray-400 dark:disabled:bg-gray-700">
+            <:button class="role bg-transparent text-gray-800 dark:text-gray-100 hover:bg-gray-50 dark:hover:bg-gray-700 focus-visible:outline-gray-100 whitespace-nowrap truncate inline-flex items-center gap-x-2 font-medium rounded-md px-3.5 py-2.5 text-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 disabled:bg-gray-400 dark:disabled:text-white dark:disabled:text-gray-400 dark:disabled:bg-gray-700">
               <%= @role |> to_string() |> String.capitalize() %>
               <Heroicons.chevron_down mini class="size-4 mt-0.5" />
             </:button>
