0e6d43ecbcf4b366191ba76ea4d3a7c9e321a2b2
Adam Rutkowski <hq@mtod.org>
2025-01-20 12:03:28 +0100

    Add team management notices on /settings/people

--- lib/plausible_web/components/team/notice.ex
+++ lib/plausible_web/components/team/notice.ex
@@ -1,6 +1,33 @@
 defmodule PlausibleWeb.Team.Notice do
   use PlausibleWeb, :component
 
+  def inviting_banner(assigns) do
+    ~H"""
+    <aside class="mt-4 mb-4">
+      <.notice title="Inviting people to your team" class="shadow-md dark:shadow-none mt-4">
+        <p>
+          You can also invite people to your team and give them different roles like admin, editor, viewer or billing. Team members can have full access to all sites.
+        </p>
+      </.notice>
+    </aside>
+    """
+  end
+
+  def team_members_notice(assigns) do
+    ~H"""
+    <aside class="mt-4 mb-4">
+      <.notice theme={:gray} class="rounded border border-gray-300 text-sm mt-4">
+        <p>
+          Team members automatically have access to this site.
+          <.styled_link href={Routes.settings_path(PlausibleWeb.Endpoint, :team_general)}>
+            View team members
+          </.styled_link>
+        </p>
+      </.notice>
+    </aside>
+    """
+  end
+
   def team_invitations(assigns) do
     ~H"""
     <aside class="mt-4 mb-4">
@@ -11,12 +38,16 @@ defmodule PlausibleWeb.Team.Notice do
         class="shadow-md dark:shadow-none mt-4"
       >
         {i.inviter.name} has invited you to join the "{i.team.name}" as {i.role} member.
-        <.link method="post" href={Routes.invitation_path(PlausibleWeb.Endpoint, :accept_invitation, i.invitation_id)} class="whitespace-nowrap font-semibold">
+        <.link
+          method="post"
+          href={Routes.invitation_path(PlausibleWeb.Endpoint, :accept_invitation, i.invitation_id)}
+          class="whitespace-nowrap font-semibold"
+        >
           Accept
         </.link>
         or
         <.link
-    method="post"
+          method="post"
           href={Routes.invitation_path(PlausibleWeb.Endpoint, :reject_invitation, i.invitation_id)}
           phx-value-invitation-id={i.invitation_id}
           class="whitespace-nowrap font-semibold"
