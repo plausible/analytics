422ed9a660bca32dded45e1f125fa2f73783bc43
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2025-01-27 08:42:11 +0200

    Update migration

--- priv/repo/migrations/20250109114738_add_engagement_metrics_enabled_at_to_site.exs
+++
@@ -1,9 +0,0 @@
-defmodule Plausible.Repo.Migrations.AddEngagementMetricsEnabledAtToSite do
-  use Ecto.Migration
-
-  def change do
-    alter table(:sites) do
-      add :engagement_metrics_enabled_at, :naive_datetime, null: true
-    end
-  end
-end
