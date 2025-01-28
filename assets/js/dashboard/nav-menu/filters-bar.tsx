111e9044917f5c6bd4e13bf4c78f75b2c8480ad7
Artur Pata <artur.pata@gmail.com>
2025-01-28 11:21:38 +0200

    Remove extraneous class and prevent wrap

--- assets/js/dashboard/nav-menu/filters-bar.tsx
+++ assets/js/dashboard/nav-menu/filters-bar.tsx
@@ -223,7 +223,7 @@ const ClearAction = () => (
   <AppNavigationLink
     title="Clear all filters"
     className={classNames(
-      'self-start button h-9 !px-3 !py-2 flex !bg-red-500 dark:!bg-red-500 hover:!bg-red-600 dark:hover:!bg-red-700'
+      'self-start button h-9 !px-3 !py-2 flex !bg-red-500 dark:!bg-red-500 hover:!bg-red-600 dark:hover:!bg-red-700 whitespace-nowrap'
     )}
     search={(search) => ({
       ...search,
