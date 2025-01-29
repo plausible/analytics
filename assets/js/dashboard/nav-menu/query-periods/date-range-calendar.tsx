688c947006907382eb1186d3573e7314588d978b
Artur Pata <artur.pata@gmail.com>
2025-01-29 22:10:11 +0200

    Remove extraneous Calendar component, better props

--- assets/js/dashboard/nav-menu/query-periods/date-range-calendar.tsx
+++ assets/js/dashboard/nav-menu/query-periods/date-range-calendar.tsx
@@ -2,6 +2,15 @@
 import React, { useEffect, useRef } from 'react'
 import DatePicker from 'react-flatpickr'
 
+export interface DateRangeCalendarProps {
+  id: string
+  minDate?: string
+  maxDate?: string
+  defaultDates?: [string, string]
+  onCloseWithNoSelection?: () => void
+  onCloseWithSelection?: ([selectionStart, selectionEnd]: [Date, Date]) => void
+}
+
 export function DateRangeCalendar({
   id,
   minDate,
@@ -9,14 +18,7 @@ export function DateRangeCalendar({
   defaultDates,
   onCloseWithNoSelection,
   onCloseWithSelection
-}: {
-  id: string
-  minDate?: string
-  maxDate?: string
-  defaultDates?: [string, string]
-  onCloseWithNoSelection?: () => void
-  onCloseWithSelection?: ([selectionStart, selectionEnd]: [Date, Date]) => void
-}) {
+}: DateRangeCalendarProps) {
   const calendarRef = useRef<DatePicker>(null)
 
   useEffect(() => {
@@ -60,78 +62,6 @@ export function DateRangeCalendar({
             : undefined
         }
         className="invisible"
-      />
-    </div>
-  )
-}
-
-export function OtherDateRangeCalendar({
-  open,
-  minDate,
-  maxDate,
-  defaultDates,
-  onCloseWithNoSelection,
-  onCloseWithSelection
-}: {
-  open: boolean
-  minDate?: string
-  maxDate?: string
-  defaultDates?: [string, string]
-  onCloseWithNoSelection?: () => void
-  onCloseWithSelection?: ([selectionStart, selectionEnd]: [Date, Date]) => void
-}) {
-  const calendarRef = useRef<DatePicker>(null)
-
-  useEffect(() => {
-    const calendar = calendarRef.current
-    // console.log('calendar', calendar)
-    // if (calendar) {
-    //   calendar.flatpickr.open()
-    // }
-
-    return () => {
-      calendar?.flatpickr?.destroy()
-    }
-  }, [])
-
-  useEffect(() => {
-    const calendar = calendarRef.current
-
-    if (open && calendar) {
-      calendar.flatpickr.open()
-    }
-  }, [open])
-
-  return (
-    <div className="h-0 w-0">
-      <DatePicker
-        className="invisible"
-        id="calendar"
-        options={{
-          mode: 'range',
-          maxDate,
-          minDate,
-          defaultDate: defaultDates,
-          showMonths: 1,
-          static: true,
-          animate: true
-        }}
-        ref={calendarRef}
-        onClose={
-          onCloseWithSelection || onCloseWithNoSelection
-            ? ([selectionStart, selectionEnd]) => {
-                if (selectionStart && selectionEnd) {
-                  if (onCloseWithSelection) {
-                    onCloseWithSelection([selectionStart, selectionEnd])
-                  }
-                } else {
-                  if (onCloseWithNoSelection) {
-                    onCloseWithNoSelection()
-                  }
-                }
-              }
-            : undefined
-        }
       />
     </div>
   )
