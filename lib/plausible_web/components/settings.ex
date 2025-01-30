1ae350a4e0ac8a3862815fc01774df11510afcef
RobertJoonas <56999674+RobertJoonas@users.noreply.github.com>
2025-01-30 17:16:03 +0100

    Looker studio (#5032)
    * add very basic info box in site settings > integrations
    
    * add looker studio as plan benefit
    
    * add paragraph of text
    
    * remove unwanted comma
    
    * hide new settings section on ce
    
    * moduledoc
    
    * organize files and modules better + call ee-only condition in template
    
    * move everything into a single template

--- lib/plausible_web/components/settings.ex
+++
@@ -1,8 +0,0 @@
-defmodule PlausibleWeb.Components.Settings do
-  @moduledoc """
-  An umbrella module for the Integrations settings section
-  """
-  use PlausibleWeb, :component
-
-  embed_templates("../templates/site/settings_search_console.html")
-end
