7bc91dc3543b1c187ca2e0b5826742a2f987b140
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2025-02-03 11:30:17 +0200

    Fix a test

--- test/plausible/ingestion/scroll_depth_visible_at_test.exs
+++ test/plausible/ingestion/scroll_depth_visible_at_test.exs
@@ -6,9 +6,10 @@ defmodule Plausible.Ingestion.ScrollDepthVisibleAtTest do
     site = new_site()
     Plausible.Ingestion.ScrollDepthVisibleAt.mark_scroll_depth_visible(site.id)
 
-    Plausible.TestUtils.eventually(fn ->
-      site = Plausible.Repo.reload!(site)
-      {not is_nil(site.scroll_depth_visible_at), site.scroll_depth_visible_at}
-    end)
+    assert Plausible.TestUtils.eventually(fn ->
+             site = Plausible.Repo.reload!(site)
+             success = not is_nil(site.scroll_depth_visible_at)
+             {success, success}
+           end)
   end
 end
