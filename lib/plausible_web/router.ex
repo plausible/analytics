4411139c2fdca8f0cf616eeff310bfa5d9401b80
Adrian Gruntkowski <adrian.gruntkowski@gmail.com>
2025-02-04 14:47:04 +0100

    Extend Teams CRM

--- lib/plausible_web/router.ex
+++ lib/plausible_web/router.ex
@@ -101,6 +101,7 @@ defmodule PlausibleWeb.Router do
     scope "/crm", PlausibleWeb do
       pipe_through :flags
       get "/auth/user/:user_id/usage", AdminController, :usage
+      get "/teams/team/:team_id/usage", AdminController, :usage
       get "/billing/user/:user_id/current_plan", AdminController, :current_plan
       get "/billing/search/user-by-id/:user_id", AdminController, :user_by_id
       post "/billing/search/user", AdminController, :user_search
