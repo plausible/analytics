5e950ab31ea5a47d7d0ab4bfff7dc689b38aaa60
Adam Rutkowski <hq@mtod.org>
2025-02-06 10:57:34 +0100

    Remove `class="relative"` from `.dropdown` instances

--- lib/plausible_web/live/components/team.ex
+++ lib/plausible_web/live/components/team.ex
@@ -42,7 +42,7 @@ defmodule PlausibleWeb.Live.Components.Team do
           <br /><span class="text-gray-500 text-xs">{@user.email}</span>
         </span>
         <div class="flex-1 text-right">
-          <.dropdown class="relative" id={"role-dropdown-#{@user.email}"}>
+          <.dropdown id={"role-dropdown-#{@user.email}"}>
             <:button class="role bg-transparent text-gray-800 dark:text-gray-100 hover:bg-gray-50 dark:hover:bg-gray-700 focus-visible:outline-gray-100 whitespace-nowrap truncate inline-flex items-center gap-x-2 font-medium rounded-md px-3.5 py-2.5 text-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 disabled:bg-gray-400 dark:disabled:text-white dark:disabled:text-gray-400 dark:disabled:bg-gray-700">
               {@role |> to_string() |> String.capitalize()}
               <Heroicons.chevron_down mini class="size-4 mt-0.5" />
--- lib/plausible_web/live/team_management.ex
+++ lib/plausible_web/live/team_management.ex
@@ -75,7 +75,7 @@ defmodule PlausibleWeb.Live.TeamManagement do
             />
           </div>
 
-          <.dropdown class="relative" id="input-role-picker">
+          <.dropdown id="input-role-picker">
             <:button class="role border rounded border-indigo-700 bg-transparent text-gray-800 dark:text-gray-100 hover:bg-gray-50 dark:hover:bg-gray-700 focus-visible:outline-gray-100 whitespace-nowrap truncate inline-flex items-center gap-x-2 font-medium rounded-md px-3 py-2 text-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 disabled:bg-gray-400 dark:disabled:text-white dark:disabled:text-gray-400 dark:disabled:bg-gray-700">
               {@input_role |> Atom.to_string() |> String.capitalize()}
               <Heroicons.chevron_down mini class="size-4 mt-0.5" />
