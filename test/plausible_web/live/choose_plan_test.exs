166746d05abba4e5c9539a807c4480fdbefe2694
Adrian Gruntkowski <adrian.gruntkowski@gmail.com>
2025-02-06 10:08:38 +0100

    Remove unused virtual fields from User schema

--- test/plausible_web/live/choose_plan_test.exs
+++ test/plausible_web/live/choose_plan_test.exs
@@ -999,7 +999,8 @@ defmodule PlausibleWeb.Live.ChoosePlanTest do
       user: user
     } do
       user
-      |> Plausible.Auth.User.changeset(%{trial_expiry_date: nil})
+      |> team_of()
+      |> Ecto.Changeset.change(trial_expiry_date: nil)
       |> Repo.update!()
 
       {:ok, lv, _doc} = get_liveview(conn)
