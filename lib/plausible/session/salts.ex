6b9ebb683b954d5f361d983daf5e25d550ffa629
Adam Rutkowski <hq@mtod.org>
2025-02-12 11:50:30 +0100

    Speed up salts lookup by serving them off ets

--- lib/plausible/session/salts.ex
+++ lib/plausible/session/salts.ex
@@ -7,37 +7,54 @@ defmodule Plausible.Session.Salts do
       fn ->
         clean_old_salts()
 
+        __MODULE__ =
+          :ets.new(__MODULE__, [
+            :named_table,
+            :set,
+            :protected,
+            {:read_concurrency, true}
+          ])
+
         salts =
           Repo.all(from s in "salts", select: s.salt, order_by: [desc: s.inserted_at], limit: 2)
 
-        case salts do
-          [current, prev] ->
-            %{previous: prev, current: current}
+        state =
+          case salts do
+            [current, prev] ->
+              %{previous: prev, current: current}
 
-          [current] ->
-            %{previous: nil, current: current}
+            [current] ->
+              %{previous: nil, current: current}
 
-          [] ->
-            new = generate_and_persist_new_salt()
-            %{previous: nil, current: new}
-        end
+            [] ->
+              new = generate_and_persist_new_salt()
+              %{previous: nil, current: new}
+          end
+
+        true = :ets.insert(__MODULE__, {:state, state})
+        :ok
       end,
       name: __MODULE__
     )
   end
 
   def fetch() do
-    Agent.get(__MODULE__, & &1)
+    [state: state] = :ets.lookup(__MODULE__, :state)
+    state
   end
 
   def rotate() do
-    Agent.update(__MODULE__, fn %{current: current} ->
+    Agent.update(__MODULE__, fn :ok ->
+      current = fetch().current
       clean_old_salts()
 
-      %{
+      state = %{
         current: generate_and_persist_new_salt(),
         previous: current
       }
+
+      true = :ets.insert(__MODULE__, {:state, state})
+      :ok
     end)
   end
 
