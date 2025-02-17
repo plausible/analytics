e0af2e00dc2388597d44cccb2dfc3e7ef48d93a4
Adam Rutkowski <hq@mtod.org>
2025-02-17 08:02:02 +0100

    Remove unused alias

--- test/plausible/cache_test.exs
+++ test/plausible/cache_test.exs
@@ -2,7 +2,6 @@ defmodule Plausible.CacheTest do
   use Plausible.DataCase, async: true
 
   alias Plausible.Cache
-  alias Plausible.Cache.Adapter
   import ExUnit.CaptureLog
 
   defmodule ExampleCache do
