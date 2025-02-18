2d3f60ccfbc1848f41c4c1545cfc9394068e3417
Adrian Gruntkowski <adrian.gruntkowski@gmail.com>
2025-02-18 17:21:59 +0100

    Restrict public CSV export access

--- lib/plausible_web/plugs/authorize_site_access.ex
+++ lib/plausible_web/plugs/authorize_site_access.ex
@@ -40,6 +40,8 @@ defmodule PlausibleWeb.Plugs.AuthorizeSiteAccess do
 
   @all_roles [:public, :viewer, :admin, :editor, :super_admin, :owner]
 
+  def all_roles(), do: @all_roles
+
   def init([]), do: {@all_roles, nil}
 
   def init(:all_roles), do: {@all_roles, nil}
