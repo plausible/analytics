213fafe041af79500e9dff67c09b65b4b08fd68d
Adam Rutkowski <hq@mtod.org>
2025-02-19 07:43:24 +0100

    Ensure no export button is rendered for public roles
    This enforces the UI to show it only for site members.

--- assets/js/dashboard/stats/graph/stats-export.js
+++ assets/js/dashboard/stats/graph/stats-export.js
@@ -3,7 +3,7 @@ import * as api from '../../api';
 import { getCurrentInterval } from "./interval-picker";
 import { useSiteContext } from "../../site-context";
 import { useQueryContext } from "../../query-context";
-import { useUserContext } from '../../user-context';
+import { useUserContext, Role } from '../../user-context'
 
 export default function StatsExport() {
   const site = useSiteContext();
@@ -11,8 +11,8 @@ export default function StatsExport() {
   const [exporting, setExporting] = useState(false)
   const user = useUserContext();
 
-  if (!user.loggedIn) {
-    return null;
+  if (!user.loggedIn || user.role == Role.public) {
+    return null
   }
 
   function startExport() {
