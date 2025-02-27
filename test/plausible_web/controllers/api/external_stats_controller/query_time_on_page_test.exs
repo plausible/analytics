75ad96ae5efe1448223cf695957165556cf99890
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2025-02-27 10:24:53 +0200

    timeseries test for metric

--- test/plausible_web/controllers/api/external_stats_controller/query_time_on_page_test.exs
+++ test/plausible_web/controllers/api/external_stats_controller/query_time_on_page_test.exs
@@ -86,6 +86,70 @@ defmodule PlausibleWeb.Api.ExternalStatsController.QueryImportedTest do
                    } = json_response(conn, 200)
   end
 
+  test "new_time_on_page time series", %{conn: conn, site: site, site_import: site_import} do
+    populate_stats(site, site_import.id, [
+      build(:pageview, user_id: 12, pathname: "/", timestamp: ~N[2021-01-01 00:00:00]),
+      build(:engagement,
+        user_id: 12,
+        pathname: "/",
+        timestamp: ~N[2021-01-01 00:00:00],
+        engagement_time: 100_000
+      ),
+      build(:pageview, user_id: 12, pathname: "/", timestamp: ~N[2021-01-02 00:00:00]),
+      build(:engagement,
+        user_id: 12,
+        pathname: "/",
+        timestamp: ~N[2021-01-02 00:00:00],
+        engagement_time: 100_000
+      ),
+      build(:pageview, user_id: 13, pathname: "/", timestamp: ~N[2021-01-02 00:00:00]),
+      build(:engagement,
+        user_id: 13,
+        pathname: "/",
+        timestamp: ~N[2021-01-02 00:00:00],
+        engagement_time: 300_000
+      ),
+      build(:pageview, user_id: 12, pathname: "/", timestamp: ~N[2021-01-03 00:00:00]),
+      build(:engagement,
+        user_id: 12,
+        pathname: "/",
+        timestamp: ~N[2021-01-03 00:00:00],
+        engagement_time: 100_000
+      ),
+      build(:pageview, user_id: 12, pathname: "/", timestamp: ~N[2021-01-04 00:00:00]),
+      build(:engagement,
+        user_id: 12,
+        pathname: "/",
+        timestamp: ~N[2021-01-04 00:00:00],
+        engagement_time: 100_000
+      ),
+      build(:imported_pages,
+        page: "/blog",
+        date: ~D[2021-01-01],
+        visitors: 9,
+        total_time_on_page: 9 * 20,
+        total_time_on_page_visits: 9
+      )
+    ])
+
+    conn =
+      post(conn, "/api/v2/query-internal-test", %{
+        "site_id" => site.domain,
+        "metrics" => ["new_time_on_page"],
+        "date_range" => ["2021-01-01", "2021-01-04"],
+        "dimensions" => ["time:day", "event:page"],
+        "include" => %{"imports" => true}
+      })
+
+    assert json_response(conn, 200)["results"] == [
+             %{"dimensions" => ["2021-01-01", "/"], "metrics" => [100]},
+             %{"dimensions" => ["2021-01-01", "/blog"], "metrics" => [20]},
+             %{"dimensions" => ["2021-01-02", "/"], "metrics" => [200]},
+             %{"dimensions" => ["2021-01-03", "/"], "metrics" => [100]},
+             %{"dimensions" => ["2021-01-04", "/"], "metrics" => [100]}
+           ]
+  end
+
   describe "legacy time_on_page metric" do
     test "aggregated", %{conn: conn, site: site, site_import: site_import} do
       populate_stats(site, site_import.id, [
