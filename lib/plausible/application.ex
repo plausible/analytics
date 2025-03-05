2eef3d99697dbc75b7d6e2138dea9f4f9b6c22aa
hq1 <hq@mtod.org>
2025-03-05 10:18:45 +0100

    Use compile-time Ingestion.Source lookups (#5141)
    * Use Source compile-time source lookups
    
    * skip a test

--- lib/plausible/application.ex
+++ lib/plausible/application.ex
@@ -149,7 +149,6 @@ defmodule Plausible.Application do
 
     setup_geolocation()
     Location.load_all()
-    Plausible.Ingestion.Source.init()
     Plausible.Geo.await_loader()
 
     Supervisor.start_link(List.flatten(children), opts)
