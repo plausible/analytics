4078e762d36129aeb632eb4552d63da7da47a0b1
Karl-Aksel Puulmann <macobo@users.noreply.github.com>
2025-03-10 10:12:26 +0200

    Stop counting engagement events into current visitors (#5163)
    This was unexpected breakage from releasing new tracker script with engagement tracking to the world. Users are seeing inflated current visitors counts compared to top sources report.

--- test/plausible_web/controllers/api/stats_controller/current_visitors_test.exs
+++ test/plausible_web/controllers/api/stats_controller/current_visitors_test.exs
@@ -5,9 +5,14 @@ defmodule PlausibleWeb.Api.StatsController.CurrentVisitorsTest do
     setup [:create_user, :log_in, :create_site]
 
     test "returns unique users in the last 5 minutes", %{conn: conn, site: site} do
+      now = DateTime.utc_now()
+
       populate_stats(site, [
-        build(:pageview, user_id: 123),
-        build(:pageview, user_id: 456)
+        build(:pageview, user_id: 123, timestamp: now |> DateTime.shift(minute: -3)),
+        build(:pageview, user_id: 456, timestamp: now |> DateTime.shift(minute: -3)),
+        build(:pageview, user_id: 123, timestamp: now |> DateTime.shift(minute: -1)),
+        build(:pageview, user_id: 789, timestamp: now |> DateTime.shift(minute: -7)),
+        build(:engagement, user_id: 789, timestamp: now |> DateTime.shift(minute: -3))
       ])
 
       conn = get(conn, "/api/stats/#{site.domain}/current-visitors")
