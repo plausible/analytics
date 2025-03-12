3722cda188ff4ad4b27402d69a9512a3e7e0c93f
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2025-03-12 13:02:12 +0200

    Fix typo in variable name

--- tracker/src/plausible.js
+++ tracker/src/plausible.js
@@ -51,7 +51,7 @@
 
   // Timestamp indicating when this particular page last became visible.
   // Reset during pageviews, set to null when page is closed.
-  var runningEnagementStart = null
+  var runningEngagementStart = null
   // When page is hidden, this 'engaged' time is saved to this variable
   var currentEngagementTime = 0
 
@@ -78,8 +78,8 @@
   }
 
   function getEngagementTime() {
-    if (runningEnagementStart) {
-      return currentEngagementTime + (Date.now() - runningEnagementStart)
+    if (runningEngagementStart) {
+      return currentEngagementTime + (Date.now() - runningEngagementStart)
     } else {
       return currentEngagementTime
     }
@@ -139,7 +139,7 @@
       }
 
       // Reset current engagement time metrics. They will restart upon when page becomes visible or the next SPA pageview
-      runningEnagementStart = null
+      runningEngagementStart = null
       currentEngagementTime = 0
 
       {{#if hash}}
@@ -157,11 +157,11 @@
         if (document.visibilityState === 'hidden') {
           // Tab went back to background. Save the engaged time so far
           currentEngagementTime = getEngagementTime()
-          runningEnagementStart = null
+          runningEngagementStart = null
 
           triggerEngagement()
-        } else if (document.visibilityState === 'visible' && runningEnagementStart === null) {
-          runningEnagementStart = Date.now()
+        } else if (document.visibilityState === 'visible' && runningEngagementStart === null) {
+          runningEngagementStart = Date.now()
         }
       })
       listeningOnEngagement = true
@@ -259,7 +259,7 @@
       currentEngagementProps = payload.p
       currentEngagementMaxScrollDepth = -1
       currentEngagementTime = 0
-      runningEnagementStart = Date.now()
+      runningEngagementStart = Date.now()
       registerEngagementListener()
     }
 
