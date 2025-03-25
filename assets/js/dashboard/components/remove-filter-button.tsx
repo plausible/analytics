f485f973a8f98b8e6f8715b797c9331497210542
Artur Pata <artur.pata@gmail.com>
2025-03-25 16:04:03 +0200

    Remove filter to indigo

--- assets/js/dashboard/components/remove-filter-button.tsx
+++ assets/js/dashboard/components/remove-filter-button.tsx
@@ -5,5 +5,5 @@ import classNames from 'classnames'
 export const removeFilterButtonClassname = classNames(
   'flex items-center py-1',
   'text-sm font-medium whitespace-nowrap',
-  'text-gray-700 hover:text-gray-800 dark:text-gray-100 dark:hover:text-gray-200'
+  'text-indigo-600 dark:text-indigo-500 hover:text-indigo-700 dark:hover:text-indigo-600'
 )
