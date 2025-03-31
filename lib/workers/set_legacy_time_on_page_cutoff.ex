247c252ce97767eb8613c3230ab3fed1f3302576
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2025-03-31 09:01:24 +0300

    Make cutoff date in job configurable - useful for backfilling

--- lib/workers/set_legacy_time_on_page_cutoff.ex
+++ lib/workers/set_legacy_time_on_page_cutoff.ex
@@ -13,13 +13,14 @@ defmodule Plausible.Workers.SetLegacyTimeOnPageCutoff do
   require Logger
 
   @impl Oban.Worker
-  def perform(_job) do
+  def perform(%Oban.Job{args: args}) do
+    cutoff_date = Map.get(args, "cutoff_date", Date.utc_today())
+
     {small_sites, large_sites} =
-      sites_with_engagement_data()
+      sites_with_engagement_data(cutoff_date)
       |> filter_sites_needing_update()
 
     to_update = small_sites ++ large_sites
-    today = Date.utc_today()
 
     if length(to_update) > 0 do
       Logger.info(
@@ -32,10 +33,10 @@ defmodule Plausible.Workers.SetLegacyTimeOnPageCutoff do
             where: s.id in ^to_update,
             where: is_nil(s.legacy_time_on_page_cutoff)
           ),
-          set: [legacy_time_on_page_cutoff: today]
+          set: [legacy_time_on_page_cutoff: cutoff_date]
         )
 
-      Logger.info("Successfully set legacy_time_on_page_cutoff=#{today} for #{count} sites")
+      Logger.info("Successfully set legacy_time_on_page_cutoff=#{cutoff_date} for #{count} sites")
     else
       Logger.debug("No sites legacy_time_on_page_cutoff needs updating")
     end
@@ -43,12 +44,12 @@ defmodule Plausible.Workers.SetLegacyTimeOnPageCutoff do
     :ok
   end
 
-  defp sites_with_engagement_data() do
+  defp sites_with_engagement_data(cutoff_date) do
     site_info_q =
       from(
         e in "events_v2",
-        where: e.timestamp > fragment("toStartOfHour(now() - toIntervalHour(48))"),
-        where: e.timestamp < fragment("toStartOfHour(now() - toIntervalHour(24))"),
+        where: e.timestamp >= fragment("toStartOfHour(? - toIntervalHour(48))", ^cutoff_date),
+        where: e.timestamp < fragment("toStartOfHour(? - toIntervalHour(24))", ^cutoff_date),
         group_by: e.site_id,
         select: %{
           site_id: e.site_id,
--- test/workers/set_legacy_time_on_page_cutoff_test.exs
+++ test/workers/set_legacy_time_on_page_cutoff_test.exs
@@ -9,23 +9,23 @@ defmodule Plausible.Workers.SetLegacyTimeOnPageCutoffTest do
     site = create_site_with_cutoff(nil)
 
     populate_stats(site, [
-      build(:pageview, user_id: 13, timestamp: hours_ago(25)),
-      build(:engagement, user_id: 13, engagement_time: 10, timestamp: hours_ago(25))
+      build(:pageview, user_id: 13, timestamp: ~U[2025-03-03 23:05:00Z]),
+      build(:engagement, user_id: 13, engagement_time: 10, timestamp: ~U[2025-03-03 23:05:00Z])
     ])
 
-    SetLegacyTimeOnPageCutoff.perform(nil)
-    assert Repo.reload!(site).legacy_time_on_page_cutoff == Date.utc_today()
+    perform(~D[2025-03-05])
+    assert Repo.reload!(site).legacy_time_on_page_cutoff == ~D[2025-03-05]
   end
 
   test "does not update an already set cutoff" do
     site = create_site_with_cutoff(~D[1970-01-01])
 
     populate_stats(site, [
-      build(:pageview, user_id: 13, timestamp: hours_ago(25)),
-      build(:engagement, user_id: 13, engagement_time: 10, timestamp: hours_ago(25))
+      build(:pageview, user_id: 13, timestamp: ~U[2025-03-03 23:05:00Z]),
+      build(:engagement, user_id: 13, engagement_time: 10, timestamp: ~U[2025-03-03 23:05:00Z])
     ])
 
-    SetLegacyTimeOnPageCutoff.perform(nil)
+    perform(~D[2025-03-05])
     assert Repo.reload!(site).legacy_time_on_page_cutoff == ~D[1970-01-01]
   end
 
@@ -35,16 +35,17 @@ defmodule Plausible.Workers.SetLegacyTimeOnPageCutoffTest do
     1..2500
     |> Enum.flat_map(fn i ->
       hour = 25 + rem(i, 24)
+      timestamp = ~U[2025-03-05 00:00:00Z] |> DateTime.add(-hour, :hour)
 
       [
-        build(:pageview, user_id: i, timestamp: hours_ago(hour)),
-        build(:engagement, user_id: i, engagement_time: 10, timestamp: hours_ago(hour))
+        build(:pageview, user_id: i, timestamp: timestamp),
+        build(:engagement, user_id: i, engagement_time: 10, timestamp: timestamp)
       ]
     end)
     |> then(&populate_stats(site, &1))
 
-    SetLegacyTimeOnPageCutoff.perform(nil)
-    assert Repo.reload!(site).legacy_time_on_page_cutoff == Date.utc_today()
+    perform(~D[2025-03-05])
+    assert Repo.reload!(site).legacy_time_on_page_cutoff == ~D[2025-03-05]
   end
 
   test "does not update large site with incomplete engagement" do
@@ -53,13 +54,13 @@ defmodule Plausible.Workers.SetLegacyTimeOnPageCutoffTest do
     1..2500
     |> Enum.flat_map(fn i ->
       [
-        build(:pageview, user_id: i, timestamp: hours_ago(25)),
-        build(:engagement, user_id: i, engagement_time: 10, timestamp: hours_ago(25))
+        build(:pageview, user_id: i, timestamp: ~U[2025-03-03 23:05:00Z]),
+        build(:engagement, user_id: i, engagement_time: 10, timestamp: ~U[2025-03-03 23:05:00Z])
       ]
     end)
     |> then(&populate_stats(site, &1))
 
-    SetLegacyTimeOnPageCutoff.perform(nil)
+    perform(~D[2025-03-05])
     assert Repo.reload!(site).legacy_time_on_page_cutoff == nil
   end
 
@@ -67,11 +68,11 @@ defmodule Plausible.Workers.SetLegacyTimeOnPageCutoffTest do
     site = create_site_with_cutoff(~D[2025-01-01])
 
     populate_stats(site, [
-      build(:pageview, user_id: 13, timestamp: hours_ago(25)),
-      build(:engagement, user_id: 13, engagement_time: 10, timestamp: hours_ago(25))
+      build(:pageview, user_id: 13, timestamp: ~U[2025-03-03 23:05:00Z]),
+      build(:engagement, user_id: 13, engagement_time: 10, timestamp: ~U[2025-03-03 23:05:00Z])
     ])
 
-    SetLegacyTimeOnPageCutoff.perform(nil)
+    perform(~D[2025-03-05])
     assert Repo.reload!(site).legacy_time_on_page_cutoff == ~D[2025-01-01]
   end
 
@@ -79,12 +80,16 @@ defmodule Plausible.Workers.SetLegacyTimeOnPageCutoffTest do
     site = create_site_with_cutoff(nil)
 
     populate_stats(site, [
-      build(:pageview, user_id: 13, timestamp: hours_ago(1)),
-      build(:engagement, user_id: 13, engagement_time: 10, timestamp: hours_ago(1))
+      build(:pageview, user_id: 13, timestamp: ~U[2025-03-04 22:05:00Z]),
+      build(:engagement, user_id: 13, engagement_time: 10, timestamp: ~U[2025-03-04 22:05:00Z])
     ])
 
-    SetLegacyTimeOnPageCutoff.perform(nil)
+    perform(~D[2025-03-05])
     assert Repo.reload!(site).legacy_time_on_page_cutoff == nil
+  end
+
+  defp perform(date) do
+    SetLegacyTimeOnPageCutoff.perform(%Oban.Job{args: %{"cutoff_date" => date}})
   end
 
   defp create_site_with_cutoff(cutoff) do
@@ -96,9 +101,5 @@ defmodule Plausible.Workers.SetLegacyTimeOnPageCutoffTest do
     |> Plausible.Repo.update()
 
     site
-  end
-
-  defp hours_ago(hours) do
-    DateTime.utc_now() |> DateTime.add(-hours, :hour)
   end
 end
