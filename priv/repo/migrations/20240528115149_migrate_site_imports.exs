847353d17e9be28f4f4cedea934246d328f9939f
Adrian Gruntkowski <adrian.gruntkowski@gmail.com>
2025-04-07 11:03:01 +0200

    Fix determining CE/EE in migrations (#5283)
    * Exclude MigrationUtils in validation check
    
    * Expose runtime versions of CE/EE checks for migrations
    
    * Fix migrations relying on CE/EE checks

--- priv/repo/migrations/20240528115149_migrate_site_imports.exs
+++ priv/repo/migrations/20240528115149_migrate_site_imports.exs
@@ -1,9 +1,10 @@
 defmodule Plausible.Repo.Migrations.MigrateSiteImports do
-  use Plausible
   use Ecto.Migration
 
+  import Plausible.MigrationUtils
+
   def up do
-    if ce?() do
+    if community_edition?() do
       {:ok, _, _} =
         Ecto.Migrator.with_repo(Plausible.ClickhouseRepo, fn _repo ->
           Plausible.DataMigration.SiteImports.run(dry_run?: false)
