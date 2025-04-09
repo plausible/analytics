c18961b2d2adcde1199745ec4324eeadeccd2e02
Karl-Aksel Puulmann <macobo@users.noreply.github.com>
2025-04-09 12:41:27 +0300

    Remove sampling flag code and reduce sampling threshold (#5299)
    * Remove old flagged code
    
    * Reduce sampling threshold
    
    We're seeing slow queries for users near the current threshold, so
    reducing it.

--- test/plausible/stats/sampling_test.exs
+++ test/plausible/stats/sampling_test.exs
@@ -5,37 +5,39 @@ defmodule Plausible.Stats.SamplingTest do
 
   on_ee do
     import Plausible.Stats.Sampling, only: [fractional_sample_rate: 2]
-    alias Plausible.Stats.{Query, DateTimeRange}
+    alias Plausible.Stats.{Query, DateTimeRange, Sampling}
 
     describe "&fractional_sample_rate/2" do
+      @threshold Sampling.default_sample_threshold()
+
       test "no traffic estimate" do
         assert fractional_sample_rate(nil, query(30)) == :no_sampling
       end
 
       test "scales sampling rate according to query duration" do
-        assert fractional_sample_rate(40_000_000, query(30)) == :no_sampling
-        assert fractional_sample_rate(40_000_000, query(60)) == 0.25
-        assert fractional_sample_rate(40_000_000, query(100)) == 0.15
+        assert fractional_sample_rate(@threshold * 2, query(30)) == :no_sampling
+        assert fractional_sample_rate(@threshold * 2, query(60)) == 0.25
+        assert fractional_sample_rate(@threshold * 2, query(100)) == 0.15
 
-        assert fractional_sample_rate(100_000_000, query(1)) == :no_sampling
-        assert fractional_sample_rate(100_000_000, query(5)) == :no_sampling
-        assert fractional_sample_rate(100_000_000, query(10)) == :no_sampling
-        assert fractional_sample_rate(100_000_000, query(15)) == 0.40
-        assert fractional_sample_rate(100_000_000, query(30)) == 0.20
-        assert fractional_sample_rate(100_000_000, query(60)) == 0.10
-        assert fractional_sample_rate(100_000_000, query(100)) == 0.06
+        assert fractional_sample_rate(@threshold * 5, query(1)) == :no_sampling
+        assert fractional_sample_rate(@threshold * 5, query(5)) == :no_sampling
+        assert fractional_sample_rate(@threshold * 5, query(10)) == :no_sampling
+        assert fractional_sample_rate(@threshold * 5, query(15)) == 0.40
+        assert fractional_sample_rate(@threshold * 5, query(30)) == 0.20
+        assert fractional_sample_rate(@threshold * 5, query(60)) == 0.10
+        assert fractional_sample_rate(@threshold * 5, query(100)) == 0.06
 
-        assert fractional_sample_rate(300_000_000, query(2)) == :no_sampling
-        assert fractional_sample_rate(300_000_000, query(5)) == 0.40
-        assert fractional_sample_rate(300_000_000, query(10)) == 0.20
+        assert fractional_sample_rate(@threshold * 15, query(2)) == :no_sampling
+        assert fractional_sample_rate(@threshold * 15, query(5)) == 0.40
+        assert fractional_sample_rate(@threshold * 15, query(10)) == 0.20
       end
 
       test "short durations" do
-        assert fractional_sample_rate(300_000_000_000, query(1, :hour)) == :no_sampling
+        assert fractional_sample_rate(@threshold * 15, query(1, :hour)) == :no_sampling
       end
 
       test "very low sampling rate" do
-        assert fractional_sample_rate(300_000_000_000, query(30)) == 0.01
+        assert fractional_sample_rate(@threshold * 500, query(30)) == 0.01
       end
     end
 
