1afa67b45672021ea4deed43b9583f0eb4713944
Artur Pata <artur.pata@gmail.com>
2025-04-17 18:42:49 +0300

    Register open dropmenus globally
    This is needed to prevent invalid state when navigating with site hotkeys with Firefox while a dropdown is open and coming back using browser

--- assets/js/dashboard/navigation/routeless-modals-context.tsx
+++ assets/js/dashboard/navigation/routeless-modals-context.tsx
@@ -1,14 +1,26 @@
-import React, { createContext, ReactNode, useContext, useState } from 'react'
+import React, {
+  createContext,
+  ReactNode,
+  useCallback,
+  useContext,
+  useState
+} from 'react'
 import { RoutelessSegmentModal } from '../segments/routeless-segment-modals'
 
 type ActiveModal = null | RoutelessSegmentModal
 
 const routelessModalsContextDefaultValue: {
+  /** List of menu IDs that are open, [] if none are */
+  openDropmenus: string[]
+  registerDropmenuState: (options: { id: string; isOpen: boolean }) => void
+  /** Only one modal can be open at a time, this value shows which is */
   modal: ActiveModal
   setModal: (modal: ActiveModal) => void
 } = {
+  openDropmenus: [],
   modal: null,
-  setModal: () => {}
+  setModal: () => {},
+  registerDropmenuState: () => {}
 }
 
 const RoutelessModalsContext = createContext<
@@ -24,6 +36,20 @@ export function RoutelessModalsContextProvider({
 }: {
   children: ReactNode
 }) {
+  const [openDropmenus, setOpenDropmenus] = useState<string[]>([])
+
+  const registerDropmenuState = useCallback(
+    ({ id, isOpen }: { id: string; isOpen: boolean }) => {
+      setOpenDropmenus((prevState) => {
+        if (!isOpen) {
+          return prevState.filter((i) => i !== id)
+        }
+
+        return prevState.includes(id) ? prevState : prevState.concat(id)
+      })
+    },
+    []
+  )
   const [modal, setModal] = useState<ActiveModal>(
     routelessModalsContextDefaultValue.modal
   )
@@ -31,6 +57,8 @@ export function RoutelessModalsContextProvider({
   return (
     <RoutelessModalsContext.Provider
       value={{
+        openDropmenus,
+        registerDropmenuState,
         modal,
         setModal
       }}
