415abaf32c7ac45f708053cd048243a2b60f8969
Artur Pata <artur.pata@gmail.com>
2025-05-21 14:46:07 +0300

    Fix format

--- tracker/test/support/types.ts
+++ tracker/test/support/types.ts
@@ -1,18 +1,17 @@
 export type Options = {
-    hash: boolean
-    local: boolean
-    exclusions: boolean
-    manual: boolean
-    revenue: boolean
-    pageviewProps: boolean
-    outboundLinks: boolean
-    fileDownloads: boolean
-    taggedEvents: boolean
-    formSubmissions: boolean
-  }
+  hash: boolean;
+  local: boolean;
+  exclusions: boolean;
+  manual: boolean;
+  revenue: boolean;
+  pageviewProps: boolean;
+  outboundLinks: boolean;
+  fileDownloads: boolean;
+  taggedEvents: boolean;
+  formSubmissions: boolean;
+};
 
 export type ScriptConfig = {
-    domain: string
-    endpoint: string
-  } & Partial<Options>
-  
+  domain: string;
+  endpoint: string;
+} & Partial<Options>;
