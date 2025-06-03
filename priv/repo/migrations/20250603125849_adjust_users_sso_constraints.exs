3e381232edbef8c8745fe5b06f2f2a89713d7227
Adrian Gruntkowski <adrian.gruntkowski@gmail.com>
2025-06-03 15:21:24 +0200

    Add `users.sso_domain_id` FK and alter `users.sso_integration_id` FK

---
+++ priv/repo/migrations/20250603125849_adjust_users_sso_constraints.exs
@@ -0,0 +1,13 @@
+defmodule Plausible.Repo.Migrations.AdjustUsersSsoConstraints do
+  use Ecto.Migration
+
+  def change do
+    alter table(:users) do
+      add :sso_domain_id, references(:sso_domains, on_delete: :nothing), null: true
+
+      modify :sso_integration_id, references(:sso_integrations, on_delete: :nothing),
+        from: references(:sso_integrations, on_delete: :nilify_all),
+        null: true
+    end
+  end
+end
