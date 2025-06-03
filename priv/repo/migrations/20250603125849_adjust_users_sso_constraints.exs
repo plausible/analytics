19c40b72062ede18defe8ac115427fcb1573918d
Adrian Gruntkowski <adrian.gruntkowski@gmail.com>
2025-06-03 15:25:12 +0200

    Run the migration only on EE

--- priv/repo/migrations/20250603125849_adjust_users_sso_constraints.exs
+++ priv/repo/migrations/20250603125849_adjust_users_sso_constraints.exs
@@ -1,13 +1,17 @@
 defmodule Plausible.Repo.Migrations.AdjustUsersSsoConstraints do
   use Ecto.Migration
 
-  def change do
-    alter table(:users) do
-      add :sso_domain_id, references(:sso_domains, on_delete: :nothing), null: true
+  import Plausible.MigrationUtils
 
-      modify :sso_integration_id, references(:sso_integrations, on_delete: :nothing),
-        from: references(:sso_integrations, on_delete: :nilify_all),
-        null: true
+  def change do
+    if enterprise_edition?() do
+      alter table(:users) do
+        add :sso_domain_id, references(:sso_domains, on_delete: :nothing), null: true
+
+        modify :sso_integration_id, references(:sso_integrations, on_delete: :nothing),
+          from: references(:sso_integrations, on_delete: :nilify_all),
+          null: true
+      end
     end
   end
 end
