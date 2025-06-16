6bcc104e03d75ecdb2fe0ab764d3cc8d434f9b86
Robert Joonas <robertjoonas16@gmail.com>
2025-06-16 10:37:34 +0300

    dogfood with script v2 + disable tracking in tests

--- extra/lib/plausible_web/dogfood.ex
+++ extra/lib/plausible_web/dogfood.ex
@@ -3,29 +3,63 @@ defmodule PlausibleWeb.Dogfood do
   Plausible tracking itself functions
   """
 
-  @doc """
-  Temporary override to do more testing of the new ingest.plausible.io endpoint for accepting events. In staging and locally
-  will fall back to staging.plausible.io/api/event and localhost:8000/api/event respectively.
-  """
-  def api_destination() do
+  def script_params(assigns) do
+    %{
+      script_url: script_url(assigns),
+      api_destination: api_destination(),
+      location_override: location_override(assigns),
+      custom_properties: custom_properties(assigns),
+      capture_on_localhost: Application.get_env(:plausible, :environment) == "dev"
+    }
+  end
+
+  defp api_destination() do
+    # Temporary override to do more testing of the new ingest.plausible.io endpoint
+    # for accepting events. In staging and locally will fall back to
+    # staging.plausible.io/api/event and localhost:8000/api/event respectively.
     if Application.get_env(:plausible, :environment) == "prod" do
       "https://ingest.plausible.io/api/event"
-    end
-  end
-
-  def script_url() do
-    if Application.get_env(:plausible, :environment) in ["prod", "staging"] do
-      "#{PlausibleWeb.Endpoint.url()}/js/script.manual.pageview-props.tagged-events.pageleave.js"
     else
-      "#{PlausibleWeb.Endpoint.url()}/js/script.local.manual.pageview-props.tagged-events.pageleave.js"
+      "#{PlausibleWeb.Endpoint.url()}/api/event"
     end
   end
 
-  def domain(conn) do
-    cond do
-      Application.get_env(:plausible, :is_selfhost) -> "ee.plausible.io"
-      conn.assigns[:embedded] -> "embed." <> PlausibleWeb.Endpoint.host()
-      true -> PlausibleWeb.Endpoint.host()
-    end
+  defp script_url(assigns) do
+    env = Application.get_env(:plausible, :environment)
+    selfhost? = Application.get_env(:plausible, :is_selfhost)
+
+    tracker_script_config_id =
+      cond do
+        env == "prod" and selfhost? ->
+          "V5OUguy5m04s95qHnmGbH"
+
+        env == "prod" and assigns[:embedded] ->
+          "Qo3A7Ksnbn-wYQWMijuR3"
+
+        env == "prod" ->
+          "6_srOGVV9SLMWJ1ZpUAbG"
+
+        env == "staging" ->
+          "egYOCIzzYzPL9v6GHLc-7"
+
+        env in ["dev", "ce_dev"] ->
+          PlausibleWeb.Tracker.get_or_create_tracker_script_configuration!(1).id
+      end
+
+    "#{PlausibleWeb.Endpoint.url()}/js/s-#{tracker_script_config_id}.js"
+  end
+
+  defp location_override(%{dogfood_page_path: path}) when is_binary(path) do
+    Path.join(PlausibleWeb.Endpoint.url(), path)
+  end
+
+  defp location_override(_), do: nil
+
+  defp custom_properties(%{current_user: user}) when is_map(user) do
+    %{logged_in: true, theme: user.theme}
+  end
+
+  defp custom_properties(_) do
+    %{logged_in: false}
   end
 end
