801314dfe256093b22d1f606c4860a20b9922c4d
Adam Rutkowski <hq@mtod.org>
2025-06-16 14:39:35 +0200

    Galaxy brain

--- priv/repo/migrations/20250616121812_sso_domains_validation_to_verification_rename.exs
+++ priv/repo/migrations/20250616121812_sso_domains_validation_to_verification_rename.exs
@@ -1,8 +1,14 @@
 defmodule Plausible.Repo.Migrations.SsoDomainsValidationToVerificationRename do
   use Ecto.Migration
 
+  import Plausible.MigrationUtils
+
   def change do
-    rename table(:sso_domains), :validated_via, to: :verified_via
-    rename table(:sso_domains), :last_validated_at, to: :last_verified_at
+    if enterprise_edition?() do
+      alter table(:sso_domains) do
+        add :verified_via, :string, null: true
+        add :last_verified_at, :naive_datetime, null: true
+      end
+    end
   end
 end
