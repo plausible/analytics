295004158c1d5fc395f63e9971ad7518ef8394e3
Adrian Gruntkowski <adrian.gruntkowski@gmail.com>
2025-06-18 09:15:35 +0200

    Implement `Plausible.Users.type/1` for determining user type

--- lib/plausible/users.ex
+++ lib/plausible/users.ex
@@ -2,10 +2,22 @@ defmodule Plausible.Users do
   @moduledoc """
   User context
   """
+  use Plausible
+
   import Ecto.Query
 
   alias Plausible.Auth
   alias Plausible.Repo
+
+  on_ee do
+    @spec type(Auth.User.t()) :: :standard | :sso
+    def type(user) do
+      user.type
+    end
+  else
+    @spec type(Auth.User.t()) :: :standard
+    def type(_user), do: :standard
+  end
 
   @spec bump_last_seen(Auth.User.t() | pos_integer(), NaiveDateTime.t()) :: :ok
   def bump_last_seen(%Auth.User{id: user_id}, now) do
