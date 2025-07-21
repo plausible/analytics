cb3d7d4a4bca5fef21144fec4cecf85cb3edb45b
Adrian Gruntkowski <adrian.gruntkowski@gmail.com>
2025-07-21 11:38:34 +0200

    Fix test

--- test/plausible_web/live/customer_support/teams_test.exs
+++ test/plausible_web/live/customer_support/teams_test.exs
@@ -149,7 +149,7 @@ defmodule PlausibleWeb.Live.CustomerSupport.TeamsTest do
         })
 
         html = render(lv)
-        assert text_of_attr(html, ~s|#cost-estimate|, "value") == "10380.00"
+        assert text_of_attr(html, ~s|#cost-estimate|, "value") == "12380.00"
       end
 
       test "saves custom plan", %{conn: conn, user: user} do
