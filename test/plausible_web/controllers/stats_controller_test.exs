ece132d5325e437065e1ef910ff89840ed84bdaa
Adam Rutkowski <hq@mtod.org>
2025-08-05 19:14:32 +0200

    Update test to work with both CE/EE

--- test/plausible_web/controllers/stats_controller_test.exs
+++ test/plausible_web/controllers/stats_controller_test.exs
@@ -221,8 +221,7 @@ defmodule PlausibleWeb.StatsControllerTest do
     test "does not show CRM link to the site", %{conn: conn, site: site} do
       conn = get(conn, conn |> get("/" <> site.domain) |> redirected_to())
 
-      refute html_response(conn, 200) =~
-               Routes.customer_support_site_path(PlausibleWeb.Endpoint, :show, site.id)
+      refute html_response(conn, 200) =~ "/cs/sites"
     end
 
     test "all segments (personal or site) are stuffed into dataset, with their associated owner_id and owner_name",
