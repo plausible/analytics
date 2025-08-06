33639f4f0ce672b86674520adb7f7e67414a9729
Artur Pata <artur.pata@gmail.com>
2025-08-06 08:40:21 +0300

    Update ecto_ch and ch deps

--- mix.exs
+++ mix.exs
@@ -69,7 +69,7 @@ defmodule Plausible.MixProject do
       {:bamboo_mua, "~> 0.2.0"},
       {:bcrypt_elixir, "~> 3.0"},
       {:bypass, "~> 2.1", only: [:dev, :test, :ce_test]},
-      {:ecto_ch, "~> 0.6.0"},
+      {:ecto_ch, "~> 0.7.1"},
       {:cloak, "~> 1.1"},
       {:cloak_ecto, "~> 1.2"},
       {:combination, "~> 0.0.3"},
