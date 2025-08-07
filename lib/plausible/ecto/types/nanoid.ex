1d49a64e3738a80a18b91a3d4efc7f8ec94003af
Artur Pata <artur.pata@gmail.com>
2025-08-07 19:14:56 +0300

    Add NanoidBase behaviour

--- lib/plausible/ecto/types/nanoid.ex
+++ lib/plausible/ecto/types/nanoid.ex
@@ -1,6 +1,6 @@
-defmodule Plausible.Ecto.Types.Nanoid do
+defmodule Plausible.Ecto.Types.NanoidBase do
   @moduledoc """
-  Custom column type for nanoid strings
+  Base module for nanoid string types
   """
 
   use Ecto.Type
@@ -14,6 +14,21 @@ defmodule Plausible.Ecto.Types.Nanoid do
 
   def dump(value) when is_binary(value), do: {:ok, value}
   def dump(_), do: :error
+
+  @callback autogenerate() :: String.t()
+end
+
+defmodule Plausible.Ecto.Types.Nanoid do
+  @moduledoc """
+  Custom column type for nanoid strings
+  """
+
+  @behaviour Plausible.Ecto.Types.NanoidBase
+
+  def type(), do: Plausible.Ecto.Types.NanoidBase.type()
+  def cast(value), do: Plausible.Ecto.Types.NanoidBase.cast(value)
+  def load(value), do: Plausible.Ecto.Types.NanoidBase.load(value)
+  def dump(value), do: Plausible.Ecto.Types.NanoidBase.dump(value)
 
   @spec autogenerate() :: String.t()
   def autogenerate(), do: Nanoid.generate()
@@ -24,17 +39,12 @@ defmodule Plausible.Ecto.Types.TrackerScriptNanoid do
   Custom column type for tracker script configuration nanoid strings with pa- prefix
   """
 
-  use Ecto.Type
+  @behaviour Plausible.Ecto.Types.NanoidBase
 
-  def type(), do: :string
-
-  def cast(value) when is_binary(value), do: {:ok, value}
-  def cast(_), do: :error
-
-  def load(value), do: {:ok, value}
-
-  def dump(value) when is_binary(value), do: {:ok, value}
-  def dump(_), do: :error
+  def type(), do: Plausible.Ecto.Types.NanoidBase.type()
+  def cast(value), do: Plausible.Ecto.Types.NanoidBase.cast(value)
+  def load(value), do: Plausible.Ecto.Types.NanoidBase.load(value)
+  def dump(value), do: Plausible.Ecto.Types.NanoidBase.dump(value)
 
   @spec autogenerate() :: String.t()
   def autogenerate(), do: "pa-#{Nanoid.generate()}"
