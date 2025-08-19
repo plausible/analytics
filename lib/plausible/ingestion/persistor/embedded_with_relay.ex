4beb03004c18885b09137a446e5829f17b1cbfcd
Adrian Gruntkowski <adrian.gruntkowski@gmail.com>
2025-08-19 10:45:59 +0200

    Instrument relayed persistence

--- lib/plausible/ingestion/persistor/embedded_with_relay.ex
+++ lib/plausible/ingestion/persistor/embedded_with_relay.ex
@@ -7,9 +7,58 @@ defmodule Plausible.Ingestion.Persistor.EmbeddedWithRelay do
 
   def persist_event(event, session_attrs, previous_user_id, opts) do
     Task.start(fn ->
-      Persistor.Remote.persist_event(event, session_attrs, previous_user_id, opts)
+      Plausible.PromEx.Plugins.PlausibleMetrics.measure_duration(
+        telemetry_pipeline_step_duration(),
+        fn -> do_persist_event(event, session_attrs, previous_user_id, opts) end,
+        %{step: "register_session"}
+      )
     end)
 
     Persistor.Embedded.persist_event(event, session_attrs, previous_user_id, opts)
+  end
+
+  defp do_persist_event(event, session_attrs, previous_user_id, opts) do
+    result = Persistor.Remote.persist_event(event, session_attrs, previous_user_id, opts)
+
+    case result do
+      {:ok, event} ->
+        emit_telemetry_buffered(event)
+
+      {:error, reason} ->
+        emit_telemetry_dropped(event, reason)
+    end
+  end
+
+  def emit_telemetry_buffered(event) do
+    :telemetry.execute(telemetry_event_buffered(), %{}, %{
+      domain: event.domain,
+      request_timestamp: event.request.timestamp,
+      tracker_script_version: event.request.tracker_script_version
+    })
+  end
+
+  def emit_telemetry_dropped(event, reason) do
+    :telemetry.execute(
+      telemetry_event_dropped(),
+      %{},
+      %{
+        domain: event.domain,
+        reason: reason,
+        request_timestamp: event.request.timestamp,
+        tracker_script_version: event.request.tracker_script_version
+      }
+    )
+  end
+
+  def telemetry_event_buffered() do
+    [:plausible, :remote_ingest, :event, :buffered]
+  end
+
+  def telemetry_event_dropped() do
+    [:plausible, :remote_ingest, :event, :dropped]
+  end
+
+  def telemetry_pipeline_step_duration() do
+    [:plausible, :remote_ingest, :pipeline, :step]
   end
 end
