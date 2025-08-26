13f74ba6671757c120ca3dabd44b54ba8e953643
Robert Joonas <robertjoonas16@gmail.com>
2025-08-26 17:10:28 +0100

    Custom URL input + render attempted URL in error message

--- lib/plausible/installation_support/verification/diagnostics.ex
+++ lib/plausible/installation_support/verification/diagnostics.ex
@@ -198,27 +198,38 @@ defmodule Plausible.InstallationSupport.Verification.Diagnostics do
       ),
       do: error(@error_gtm_selected_maybe_cookie_banner)
 
-  @error_domain_not_found Error.new!(%{
-                            message: "We couldn't find your website",
-                            recommendation:
-                              "Please check that the domain you entered is correct and reachable publicly via either the naked domain, or with a \"www.\" prefix. If it's intentionally private, you'll need to verify that Plausible works manually",
-                            url:
-                              "https://plausible.io/docs/troubleshoot-integration#how-to-manually-check-your-integration"
-                          })
-  def interpret(%__MODULE__{service_error: service_error}, _expected_domain, _url)
-      when service_error in [:domain_not_found, :invalid_url],
-      do: error(@error_domain_not_found)
+  def interpret(%__MODULE__{service_error: service_error}, expected_domain, url)
+      when service_error in [:domain_not_found, :invalid_url] do
+    attempted_url = if url, do: url, else: "https://#{expected_domain}"
 
-  @error_browserless_network Error.new!(%{
-                               message: "We couldn't verify your website",
-                               recommendation:
-                                 "Our verification tool encountered a network error while trying to verify your website. Please verify your integration manually",
-                               url:
-                                 "https://plausible.io/docs/troubleshoot-integration#how-to-manually-check-your-integration"
-                             })
+    %Error{
+      message: "We couldn't find your website at #{attempted_url}",
+      recommendation:
+        "Please check that the domain you entered is correct and reachable publicly. If it's intentionally private, you'll need to verify that Plausible works manually",
+      url:
+        "https://plausible.io/docs/troubleshoot-integration#how-to-manually-check-your-integration"
+    }
+    |> error()
+    |> struct!(data: %{offer_custom_url_input: true})
+  end
 
-  def interpret(%__MODULE__{service_error: "net::" <> _}, _expected_domain, _url) do
-    error(@error_browserless_network)
+  def interpret(%__MODULE__{service_error: "net::" <> _}, expected_domain, url) do
+    attempted_url =
+      if url do
+        String.split(url, "?") |> List.first()
+      else
+        "https://#{expected_domain}"
+      end
+
+    %Error{
+      message: "We couldn't verify your website at #{attempted_url}",
+      recommendation:
+        "Our verification tool encountered a network error while trying to verify your website. Please verify your integration manually",
+      url:
+        "https://plausible.io/docs/troubleshoot-integration#how-to-manually-check-your-integration"
+    }
+    |> error()
+    |> struct!(data: %{offer_custom_url_input: true})
   end
 
   @unknown_error Error.new!(%{
--- lib/plausible_web/live/components/verification.ex
+++ lib/plausible_web/live/components/verification.ex
@@ -123,6 +123,21 @@ defmodule PlausibleWeb.Live.Components.Verification do
       </div>
       <:footer>
         <.focus_list>
+          <:item :if={
+            @interpretation && is_map(@interpretation.data) &&
+              @interpretation.data[:offer_custom_url_input]
+          }>
+            Is your website located at a different URL?
+            <.styled_link href={
+              Routes.site_path(PlausibleWeb.Endpoint, :verification, @domain,
+                flow: @flow,
+                installation_type: @installation_type,
+                custom_url: true
+              )
+            }>
+              Click here
+            </.styled_link>
+          </:item>
           <:item :if={ee?() and @attempts >= 3}>
             <b>Need further help with your installation?</b>
             <.styled_link href="https://plausible.io/contact">
