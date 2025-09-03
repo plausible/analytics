2e8b5e9d49dd72d1582a1bfdef6910372a2dfc31
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2025-09-03 11:05:58 +0300

    Revert "Revert "Trim `month`, `year`, `day` periods to local now on main graph (#5668)" (#5684)"
    This reverts commit 2d11681f25617c6faa8e2f2b06e0f8a137887ab2.

--- lib/plausible_web/controllers/api/stats_controller.ex
+++ lib/plausible_web/controllers/api/stats_controller.ex
@@ -101,6 +101,7 @@ defmodule PlausibleWeb.Api.StatsController do
          :ok <- validate_interval_granularity(site, params, dates),
          params <- realtime_period_to_30m(params),
          query = Query.from(site, params, debug_metadata(conn)),
+         query <- Query.set_include(query, :trim_relative_date_range, true),
          {:ok, metric} <- parse_and_validate_graph_metric(params, query) do
       {timeseries_result, comparison_result, _meta} = Stats.timeseries(site, query, [metric])
 
@@ -281,10 +282,10 @@ defmodule PlausibleWeb.Api.StatsController do
       toplevel_goal_filter?(query)
 
     cond do
-      query.period == "30m" && goal_filter? ->
+      query.input_date_range == "30m" && goal_filter? ->
         fetch_goal_realtime_top_stats(site, query)
 
-      query.period == "30m" ->
+      query.input_date_range == "30m" ->
         fetch_realtime_top_stats(site, query)
 
       goal_filter? ->
@@ -580,7 +581,7 @@ defmodule PlausibleWeb.Api.StatsController do
         Filters.filtering_on_dimension?(query, "event:page") ->
           {:error, {:invalid_funnel_query, "pages"}}
 
-        query.period == "realtime" ->
+        query.input_date_range == "realtime" ->
           {:error, {:invalid_funnel_query, "realtime period"}}
 
         true ->
