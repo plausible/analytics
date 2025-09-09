b215c348a1ec1a2db389c1f6c8f79cdae7bcaa36
Adrian Gruntkowski <adrian.gruntkowski@gmail.com>
2025-09-09 12:22:01 +0200

    Replace `Timex.compare` with `Date.compare`

--- lib/plausible/billing/subscriptions.ex
+++ lib/plausible/billing/subscriptions.ex
@@ -27,7 +27,7 @@ defmodule Plausible.Billing.Subscriptions do
 
   def expired?(%Subscription{next_bill_date: next_bill_date} = subscription) do
     deleted? = Subscription.Status.deleted?(subscription)
-    expired? = Timex.compare(next_bill_date, Date.utc_today()) < 0
+    expired? = Date.compare(next_bill_date, Date.utc_today()) == :lt
 
     deleted? && expired?
   end
