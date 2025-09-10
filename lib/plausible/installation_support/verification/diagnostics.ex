2f943c7b929521dec248a1516201ae9704040914
Artur Pata <artur.pata@gmail.com>
2025-09-10 10:42:50 +0300

    Better struct definition

--- lib/plausible/installation_support/verification/diagnostics.ex
+++ lib/plausible/installation_support/verification/diagnostics.ex
@@ -5,20 +5,22 @@ defmodule Plausible.InstallationSupport.Verification.Diagnostics do
   require Logger
 
   # In this struct
-  # - nil value means the result is indeterminate (e.g. we didn't even get to the part where response_status is set)
-  defstruct selected_installation_type: nil,
-            disallowed_by_csp: nil,
-            tracker_is_in_html: nil,
-            plausible_is_on_window: nil,
-            plausible_is_initialized: nil,
-            plausible_version: nil,
-            plausible_variant: nil,
-            diagnostics_are_from_cache_bust: nil,
-            test_event: nil,
-            cookies_consent_result: nil,
-            response_status: nil,
-            service_error: nil,
-            attempts: nil
+  # - the default nil value for each field means that the value is indeterminate (e.g. we didn't even get to the part where response_status is set)
+  defstruct [
+    :selected_installation_type,
+    :disallowed_by_csp,
+    :tracker_is_in_html,
+    :plausible_is_on_window,
+    :plausible_is_initialized,
+    :plausible_version,
+    :plausible_variant,
+    :diagnostics_are_from_cache_bust,
+    :test_event,
+    :cookies_consent_result,
+    :response_status,
+    :service_error,
+    :attempts
+  ]
 
   @type t :: %__MODULE__{}
 
