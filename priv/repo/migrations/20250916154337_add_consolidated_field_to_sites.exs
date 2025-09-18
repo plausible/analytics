29d645f8c63d14c95fd13b3f8691e626356d4659
Robert Joonas <robertjoonas16@gmail.com>
2025-09-18 10:57:15 +0100

    behind ee_only + index

--- priv/repo/migrations/20250916154337_add_consolidated_field_to_sites.exs
+++ priv/repo/migrations/20250916154337_add_consolidated_field_to_sites.exs
@@ -1,9 +1,15 @@
 defmodule Plausible.Repo.Migrations.AddConsolidatedFieldToSites do
   use Ecto.Migration
 
+  import Plausible.MigrationUtils
+
   def change do
-    alter table(:sites) do
-      add :consolidated, :boolean, null: false, default: false
+    if enterprise_edition?() do
+      alter table(:sites) do
+        add :consolidated, :boolean, null: false, default: false
+      end
+
+      create index(:sites, [:id], where: "consolidated = true")
     end
   end
 end
