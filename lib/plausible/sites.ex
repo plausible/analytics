d398384a5c4ee07737e58760695e3e4fbc959168
Robert Joonas <robertjoonas16@gmail.com>
2025-09-24 17:53:53 +0100

    fix CE again

--- lib/plausible/sites.ex
+++ lib/plausible/sites.ex
@@ -352,18 +352,20 @@ defmodule Plausible.Sites do
     date
   end
 
-  def stats_start_date(%Site{consolidated: true} = site) do
-    native_stats_start_at = Plausible.ConsolidatedView.native_stats_start_at(site.team)
+  on_ee do
+    def stats_start_date(%Site{consolidated: true} = site) do
+      native_stats_start_at = Plausible.ConsolidatedView.native_stats_start_at(site.team)
 
-    if native_stats_start_at do
-      start_date = NaiveDateTime.to_date(native_stats_start_at)
+      if native_stats_start_at do
+        start_date = NaiveDateTime.to_date(native_stats_start_at)
 
-      site
-      |> Site.set_native_stats_start_at(native_stats_start_at)
-      |> Site.set_stats_start_date(start_date)
-      |> Repo.update!()
+        site
+        |> Site.set_native_stats_start_at(native_stats_start_at)
+        |> Site.set_stats_start_date(start_date)
+        |> Repo.update!()
 
-      start_date
+        start_date
+      end
     end
   end
 
