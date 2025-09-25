3c127a93dd352ecb7ccde50130f038d01b4d6de1
Robert Joonas <robertjoonas16@gmail.com>
2025-09-25 11:49:48 +0100

    defensive where builder

--- lib/plausible/stats/sql/where_builder.ex
+++ lib/plausible/stats/sql/where_builder.ex
@@ -48,7 +48,7 @@ defmodule Plausible.Stats.SQL.WhereBuilder do
   defp filter_site_id(query) do
     case query.consolidated_site_ids do
       nil -> dynamic([x], x.site_id == ^query.site_id)
-      ids -> dynamic([x], fragment("? in ?", x.site_id, ^ids))
+      [_ | _] = ids -> dynamic([x], fragment("? in ?", x.site_id, ^ids))
     end
   end
 
