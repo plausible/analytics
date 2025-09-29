c47de1b2fddc5e86550799753684ccac9c2108f7
Adam Rutkowski <hq@mtod.org>
2025-09-29 08:43:43 +0200

    Ensure consolidated view is validated on site removal

--- lib/plausible/site/removal.ex
+++ lib/plausible/site/removal.ex
@@ -20,6 +20,10 @@ defmodule Plausible.Site.Removal do
       Teams.Invitations.prune_guest_invitations(site.team)
 
       on_ee do
+        if not Plausible.ConsolidatedView.has_sites_to_consolidate?(site.team) do
+          Plausible.ConsolidatedView.disable(site.team)
+        end
+
         Plausible.Billing.SiteLocker.update_for(site.team, send_email?: false)
       end
 
