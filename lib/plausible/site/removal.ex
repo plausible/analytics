5c95b1fa0ad3acd9160876560ad170041b28a7c7
Adam Rutkowski <hq@mtod.org>
2025-09-29 09:03:34 +0200

    Alias

--- lib/plausible/site/removal.ex
+++ lib/plausible/site/removal.ex
@@ -6,6 +6,7 @@ defmodule Plausible.Site.Removal do
 
   alias Plausible.Repo
   alias Plausible.Teams
+  alias Plausible.ConsolidatedView
 
   import Ecto.Query
 
@@ -20,9 +21,9 @@ defmodule Plausible.Site.Removal do
       Teams.Invitations.prune_guest_invitations(site.team)
 
       on_ee do
-        if Plausible.Sites.regular?(site) and Plausible.ConsolidatedView.enabled?(site.team) and
-             not Plausible.ConsolidatedView.has_sites_to_consolidate?(site.team) do
-          Plausible.ConsolidatedView.disable(site.team)
+        if Plausible.Sites.regular?(site) and ConsolidatedView.enabled?(site.team) and
+             not ConsolidatedView.has_sites_to_consolidate?(site.team) do
+          ConsolidatedView.disable(site.team)
         end
 
         Plausible.Billing.SiteLocker.update_for(site.team, send_email?: false)
