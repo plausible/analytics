aec03c774bddd9b80c0bd3418c2cdc8b95ce6633
Robert Joonas <robertjoonas16@gmail.com>
2025-09-30 12:33:54 +0100

    use enabled?/1 in tests

--- test/plausible_web/live/customer_support/teams_test.exs
+++ test/plausible_web/live/customer_support/teams_test.exs
@@ -204,7 +204,7 @@ defmodule PlausibleWeb.Live.CustomerSupport.TeamsTest do
 
         lv |> element(@create_consolidated_view_button) |> render_click()
 
-        assert not is_nil(Plausible.ConsolidatedView.get(team))
+        assert Plausible.ConsolidatedView.enabled?(team)
       end
 
       test "renders existing consolidated view", %{conn: conn, user: user} do
@@ -231,7 +231,7 @@ defmodule PlausibleWeb.Live.CustomerSupport.TeamsTest do
 
         lv |> element(@delete_consolidated_view_button) |> render_click()
 
-        assert is_nil(Plausible.ConsolidatedView.get(team))
+        assert not Plausible.ConsolidatedView.enabled?(team)
       end
     end
 
