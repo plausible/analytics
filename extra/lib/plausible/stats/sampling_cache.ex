8baecfcf3730da35c13adae4d81dcaa4fc6fa385
Adam Rutkowski <hq@mtod.org>
2025-10-07 10:01:20 +0200

    Sampling Cache: ensure consolidated values above threshold are reported

--- extra/lib/plausible/stats/sampling_cache.ex
+++ extra/lib/plausible/stats/sampling_cache.ex
@@ -36,8 +36,7 @@ defmodule Plausible.Stats.SamplingCache do
         selected_as(fragment("sumIf(value, metric = 'buffered')"), :events_ingested)
       },
       where: fragment("toDate(event_timebucket) >= ?", ^thirty_days_ago()),
-      group_by: r.site_id,
-      having: selected_as(:events_ingested) > ^Sampling.default_sample_threshold()
+      group_by: r.site_id
     )
   end
 
@@ -49,10 +48,29 @@ defmodule Plausible.Stats.SamplingCache do
     |> Map.get(site_id)
   end
 
+  @threshold Sampling.default_sample_threshold()
+
+  def get(key, opts) do
+    above_threshold_only? = Keyword.get(opts, :above_threshold_only?, true)
+
+    case super(key, opts) do
+      result when is_integer(result) and above_threshold_only? and result >= @threshold ->
+        result
+
+      # in case of consolidated gets we need to sum all values, in case only consolidated qualifies for threshold breach
+      result when is_integer(result) and not above_threshold_only? ->
+        result
+
+      _ ->
+        nil
+    end
+  end
+
   @spec consolidated_get(list(pos_integer()), Keyword.t()) :: pos_integer() | nil
   def consolidated_get(site_ids, opts \\ []) when is_list(site_ids) do
+    opts = Keyword.put(opts, :above_threshold_only?, false)
     events_ingested = Enum.sum_by(site_ids, &(get(&1, opts) || 0))
-    if events_ingested > 0, do: events_ingested
+    if events_ingested >= @threshold, do: events_ingested
   end
 
   defp thirty_days_ago() do
