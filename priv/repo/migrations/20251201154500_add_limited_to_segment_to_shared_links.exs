0fb303516b4bfca9028a48974d0ac0d14ce947ab
Artur Pata <artur.pata@gmail.com>
2025-12-02 19:32:26 +0200

    Format

--- priv/repo/migrations/20251201154500_add_limited_to_segment_to_shared_links.exs
+++ priv/repo/migrations/20251201154500_add_limited_to_segment_to_shared_links.exs
@@ -5,6 +5,7 @@ defmodule Plausible.Repo.Migrations.AddLimitedToSegmentToSharedLinks do
     alter table(:shared_links) do
       add :segment_id, references(:segments, on_delete: :delete_all)
     end
+
     create index(:shared_links, [:segment_id])
   end
 end
