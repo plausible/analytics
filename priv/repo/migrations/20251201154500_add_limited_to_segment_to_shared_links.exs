1ff2b52cbbcfcb00b3ccaf18e19510edf8205e6f
Artur Pata <artur.pata@gmail.com>
2025-12-04 12:50:07 +0200

    Add :segment_id field to shared_links schema (#5924)
    * Add :limited_to_segment_id field to shared_links schema
    
    * Refactor column name, add FK, index, and on delete cascade
    
    * Format

---
+++ priv/repo/migrations/20251201154500_add_limited_to_segment_to_shared_links.exs
@@ -0,0 +1,11 @@
+defmodule Plausible.Repo.Migrations.AddLimitedToSegmentToSharedLinks do
+  use Ecto.Migration
+
+  def change do
+    alter table(:shared_links) do
+      add :segment_id, references(:segments, on_delete: :delete_all)
+    end
+
+    create index(:shared_links, [:segment_id])
+  end
+end
