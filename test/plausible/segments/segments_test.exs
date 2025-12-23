f2766e099fe21192562d7251661ebd24d0208fe8
Artur Pata <artur.pata@gmail.com>
2025-12-23 08:53:00 +0200

    Allow creating and editing limited views (#5925)
    * Add :limited_to_segment_id field to shared_links schema
    
    * Refactor column name, add FK, index, and on delete cascade
    
    * Format
    
    * Add static UI for adding a segment to a shared link
    
    - Update password UI to use a toogle switch to emphasize the optional nature of the feature
    - Add UI for limiting the link to a segment
    - Add toggle_field component for full-width label and toggle switch layout
    - Add optional link to the input component, showing below the input, which can be used for documentation links
    - Add eye icon to shared links list that indicates limited view
    - Add tooltips to icons in the shared links list
    
    * Allow creating and editing shared links with limited views
    
    * Clarify search function arg names
    
    * Unify function to set segment filter
    
    * Enforce limited view segment on dashboard
    
    * Clarify type
    
    * WIP
    
    * Disable clear filter button for limited view segment
    
    * Load related shared links WIP
    
    * Add warning about shared links WIP
    
    * Finalize delete modal
    
    * Unify shared link password protection checks
    
    * WIP
    
    * Give random name for shared links in tests
    
    * Add tests for related shared links endpoint
    
    * Split components and remove nesting
    
    * Change limited view copy
    
    * Fix search ranking, make segment search input DRYer, add search test
    
    * Fix url search params test
    
    * Update changelog
    
    * Fix invalid accessor
    
    * Format
    
    * Add test for stuffing limited view segment
    
    * Simplify preload syntax
    
    * Add tests for validating internal stats API requests
    
    * Remove id_suffix default values
    
    * Implement limited_to_segment?/1
    
    * Refactor search_by_name
    
    * Clarify toggle components
    
    * Add tests for Shared Link form, remove unused slot
    
    ---------
    
    Co-authored-by: Sanne de Vries <sannedv@protonmail.com>

--- test/plausible/segments/segments_test.exs
+++ test/plausible/segments/segments_test.exs
@@ -1,4 +1,154 @@
 defmodule Plausible.SegmentsTest do
   use ExUnit.Case
+  use Plausible.DataCase, async: true
   doctest Plausible.Segments, import: true
+  alias Plausible.Segments.Segment
+
+  setup [:create_user, :create_site]
+
+  describe("searching segments by name") do
+    for input <- [nil, "", " ", "\n", " \t"] do
+      test "empty search input #{inspect(input)} yields site segments ordered by updated_at",
+           %{
+             site: site
+           } do
+        other_site = new_site()
+
+        segment_scandinavia =
+          insert(:segment,
+            name: "Scandinavia",
+            site: site,
+            type: :site,
+            updated_at: "2025-12-01T12:00:00"
+          )
+
+        segment_apac =
+          insert(:segment,
+            name: "APAC",
+            site: site,
+            type: :site,
+            updated_at: "2025-12-01T10:00:00"
+          )
+
+        _segment_other_site = insert(:segment, name: "any", site: other_site, type: :site)
+        _segment_personal = insert(:segment, name: "My segment", site: site, type: :personal)
+
+        assert {:ok,
+                [
+                  list_all_result(segment_scandinavia),
+                  list_all_result(segment_apac)
+                ]} ==
+                 Plausible.Segments.search_by_name(site, unquote(input))
+      end
+    end
+
+    for input <- ["", "emea"] do
+      test "search input #{inspect(input)} returns a maximum of 20 segments", %{site: site} do
+        insert_list(100, :segment,
+          name: "EMEA",
+          site: site,
+          type: :site
+        )
+
+        assert {:ok, segments} = Plausible.Segments.search_by_name(site, unquote(input))
+
+        assert 20 == length(segments)
+      end
+    end
+
+    test "search input \"A\" yields correctly ranked segments", %{
+      site: site
+    } do
+      segment_scandinavia =
+        insert(:segment,
+          name: "Scandinavia",
+          site: site,
+          type: :site,
+          updated_at: "2025-12-01T12:00:00"
+        )
+
+      segment_scandinavia_latest =
+        insert(:segment,
+          name: segment_scandinavia.name,
+          site: site,
+          type: :site,
+          updated_at: "2025-12-01T13:00:00"
+        )
+
+      segment_scandinavia_longer_name =
+        insert(:segment,
+          name: "#{segment_scandinavia.name} (but longer)",
+          site: site,
+          type: :site,
+          updated_at: "2025-12-01T12:00:00"
+        )
+
+      segment_a =
+        insert(:segment,
+          name: "A",
+          site: site,
+          type: :site,
+          updated_at: "2025-12-01T10:00:00"
+        )
+
+      segment_apac =
+        insert(:segment,
+          name: "APAC",
+          site: site,
+          type: :site,
+          updated_at: "2025-12-01T10:00:00"
+        )
+
+      segment_apac_copy =
+        insert(:segment,
+          name: "Copy of APAC",
+          site: site,
+          type: :site,
+          updated_at: "2025-12-01T10:00:00"
+        )
+
+      _segment_personal = insert(:segment, name: "My segment", site: site, type: :personal)
+
+      assert_matches {:ok,
+                      [
+                        # ranked here because it exactly matches input
+                        %{
+                          name: ^segment_a.name,
+                          id: ^segment_a.id
+                        },
+                        # ranked here because it starts with the input
+                        %{
+                          name: ^segment_apac.name,
+                          id: ^segment_apac.id
+                        },
+                        # ranked here because it has a substring starting with a space and the input
+                        %{
+                          name: ^segment_apac_copy.name,
+                          id: ^segment_apac_copy.id
+                        },
+                        # ranked over next one (that has the same name) because it's been updated more lately
+                        %{
+                          name: ^segment_scandinavia_latest.name,
+                          id: ^segment_scandinavia_latest.id
+                        },
+                        # ranked over next one because it has a shorter name
+                        %{
+                          name: ^segment_scandinavia.name,
+                          id: ^segment_scandinavia.id
+                        },
+                        %{
+                          name: ^segment_scandinavia_longer_name.name,
+                          id: ^segment_scandinavia_longer_name.id
+                        }
+                      ]} =
+                       Plausible.Segments.search_by_name(site, "A")
+    end
+
+    defp list_all_result(segment) do
+      Plausible.Repo.load(Segment, %{
+        name: segment.name,
+        id: segment.id
+      })
+    end
+  end
 end
