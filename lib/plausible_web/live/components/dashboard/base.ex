90d2d471f0ef74aa59d108005e6d32aa5d28e965
Adrian Gruntkowski <adrian.gruntkowski@gmail.com>
2026-01-14 12:26:54 +0100

    Implement rough version of dashboard-friendly Prima modal wrapper

--- lib/plausible_web/live/components/dashboard/base.ex
+++ lib/plausible_web/live/components/dashboard/base.ex
@@ -5,6 +5,8 @@ defmodule PlausibleWeb.Components.Dashboard.Base do
 
   use PlausibleWeb, :component
 
+  alias Prima.Modal
+
   attr :to, :string, required: true
   attr :class, :string, default: ""
   attr :rest, :global
@@ -46,6 +48,75 @@ defmodule PlausibleWeb.Components.Dashboard.Base do
       </div>
       {render_slot(@inner_block)}
     </div>
+    """
+  end
+
+  attr :id, :string, required: true
+  attr :on_close, JS, default: %JS{}
+  attr :show, :boolean, default: false
+  attr :ready, :boolean, default: true
+  slot :inner_block, required: true
+
+  def modal(assigns) do
+    ~H"""
+    <div
+      :if={@show and not @ready}
+      transition_enter={JS.show(transition: {"ease-out duration-300", "opacity-0", "opacity-100"})}
+      class="fixed inset-0 z-[9999] bg-gray-500/75 dark:bg-gray-800/75"
+    >
+    </div>
+    <div
+      :if={@show and not @ready}
+      class="fixed inset-0 z-[9999] w-screen overflow-y-auto sm:pt-[10vmin]"
+    >
+      <div class="flex min-h-full items-end justify-center p-4 sm:items-start sm:p-0">
+        <div class="mx-auto loading">
+          <div></div>
+        </div>
+      </div>
+    </div>
+    <Modal.modal portal={false} id={@id} on_close={@on_close} show={@show}>
+      <Modal.modal_overlay
+        transition_enter={{"ease-out duration-300", "opacity-0", "opacity-100"}}
+        transition_leave={{"ease-in duration-200", "opacity-100", "opacity-0"}}
+        class="fixed inset-0 z-[9999] bg-gray-500/75 dark:bg-gray-800/75"
+      />
+
+      <div class="fixed inset-0 z-[9999] w-screen overflow-y-auto sm:pt-[10vmin]">
+        <div class="flex min-h-full items-end justify-center p-4 sm:items-start sm:p-0">
+          <Modal.modal_loader>
+            <div :if={not @ready} class="mx-auto loading">
+              <div></div>
+            </div>
+          </Modal.modal_loader>
+          <Modal.modal_panel
+            :if={@ready}
+            id={@id <> "-panel"}
+            class="relative overflow-hidden rounded-lg bg-white dark:bg-gray-900 text-left shadow-xl sm:w-full sm:max-w-lg"
+            transition_enter={
+              {"ease-out duration-300", "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95",
+               "opacity-100 translate-y-0 sm:scale-100"}
+            }
+            transition_leave={
+              {"ease-in duration-200", "opacity-100 translate-y-0 sm:scale-100",
+               "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"}
+            }
+          >
+            {render_slot(@inner_block)}
+          </Modal.modal_panel>
+        </div>
+      </div>
+    </Modal.modal>
+    """
+  end
+
+  slot :inner_block, required: true
+
+  def modal_title(assigns) do
+    ~H"""
+    <Modal.modal_title as={&h2/1} class="text-lg font-semibold text-gray-900 dark:text-gray-100">
+      {render_slot(@inner_block)}
+    </Modal.modal_title>
     """
   end
 end
