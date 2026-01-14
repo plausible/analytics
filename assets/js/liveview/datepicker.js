dc923e3893079160c4d916b19ce069f4dc49e89a
Robert Joonas <robertjoonas16@gmail.com>
2026-01-14 08:50:26 +0000

    add debounce

--- assets/js/liveview/datepicker.js
+++ assets/js/liveview/datepicker.js
@@ -8,28 +8,36 @@ import { buildHook } from './hook_builder'
 
 function prevPeriod() {
   if (this.currentIndex === 0) {
-    // add disabled class if it doesn't exist already
-    console.log('prev disabled')
+    return false
   } else {
     this.currentIndex--
-    this.pushEventTo(this.target, 'set-relative-date', { date: this.dates[this.currentIndex] })
+    return true
   }
 }
 
 function nextPeriod() {
   if (this.currentIndex === this.dates.length - 1) {
-    // add disabled class if it doesn't exist already
-    console.log('next disabled')
+    return false
   } else {
     this.currentIndex++
-    this.pushEventTo(this.target, 'set-relative-date', { date: this.dates[this.currentIndex] })
+    return true
+  }
+}
+
+function debounce(fn, delay) {
+  let timer
+
+  return function (...args) {
+    clearTimeout(timer)
+
+    timer = setTimeout(() => {
+      fn.apply(this, args)
+    }, delay)
   }
 }
 
 export default buildHook({
   initialize() {
-    this.target = this.el.dataset.target
-
     this.currentIndex = parseInt(this.el.dataset.currentIndex)
     this.dates = JSON.parse(this.el.dataset.dates)
     this.labels = JSON.parse(this.el.dataset.labels)
@@ -38,16 +46,29 @@ export default buildHook({
     this.nextPeriodButton = this.el.querySelector('button#next-period')
     this.periodLabel = this.el.querySelector('#period-label')
 
+    this.debouncedPushEvent = debounce(() => {
+      this.pushEventTo(this.el.dataset.target, 'set-relative-date', {
+        date: this.dates[this.currentIndex]
+      })
+    }, 500)
+
     this.addListener('click', this.el, (e) => {
       if (this.dates.length) {
         const button = e.target.closest('button')
   
+        let updated = false
+
         if (button === this.prevPeriodButton) {
-          prevPeriod.bind(this)()
+          updated = prevPeriod.bind(this)()
         }
   
         if (button === this.nextPeriodButton) {
           nextPeriod.bind(this)()
+          updated = nextPeriod.bind(this)()
+        }
+
+        if (updated) {
+          this.debouncedPushEvent()
         }
   
         this.periodLabel.innerText = this.labels[this.currentIndex]
