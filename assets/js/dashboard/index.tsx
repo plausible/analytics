91a5c0d36afe809d75a23394915da72e9a211811
Adrian Gruntkowski <adrian.gruntkowski@gmail.com>
2026-01-27 11:21:24 +0100

    Revert LV dashboard implementation

--- assets/js/dashboard/index.tsx
+++ assets/js/dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState, useEffect, useCallback } from 'react'
+import React, { useMemo, useState } from 'react'
 import { LiveViewPortal } from './components/liveview-portal'
 import VisitorGraph from './stats/graph/visitor-graph'
 import Sources from './stats/sources'
@@ -8,11 +8,7 @@ import Devices from './stats/devices'
 import { TopBar } from './nav-menu/top-bar'
 import Behaviours from './stats/behaviours'
 import { useQueryContext } from './query-context'
-import { useSiteContext } from './site-context'
-import { hasConversionGoalFilter, isRealTimeDashboard } from './util/filters'
-import { useAppNavigate } from './navigation/use-app-navigate'
-import { parseSearch } from './util/url-search-params'
-import { getDomainScopedStorageKey } from './util/storage'
+import { isRealTimeDashboard } from './util/filters'
 
 function DashboardStats({
   importedDataInView,
@@ -21,61 +17,14 @@ function DashboardStats({
   importedDataInView?: boolean
   updateImportedDataInView?: (v: boolean) => void
 }) {
-  const navigate = useAppNavigate()
   const site = useSiteContext()
   const { query } = useQueryContext()
-
-  // Handler for navigation events delegated from LiveView dashboard.
-  // Necessary to emulate navigation events in LiveView with pushState
-  // manipulation disabled.
-  const onLiveNavigate = useCallback(
-    (e: CustomEvent) => {
-      navigate({
-        path: e.detail.path,
-        search: () => parseSearch(e.detail.search)
-      })
-    },
-    [navigate]
-  )
-
-  useEffect(() => {
-    window.addEventListener(
-      'dashboard:live-navigate',
-      onLiveNavigate as EventListener
-    )
-
-    return () => {
-      window.removeEventListener(
-        'dashboard:live-navigate',
-        onLiveNavigate as EventListener
-      )
-    }
-  }, [onLiveNavigate])
 
   return (
     <>
       <VisitorGraph updateImportedDataInView={updateImportedDataInView} />
       <Sources />
-      {site.flags.live_dashboard ? (
-        <LiveViewPortal
-          id="pages-breakdown-live"
-          tabs={[
-            {
-              label: hasConversionGoalFilter(query)
-                ? 'Conversion pages'
-                : 'Top pages',
-              value: 'pages'
-            },
-            { label: 'Entry pages', value: 'entry-pages' },
-            { label: 'Exit pages', value: 'exit-pages' }
-          ]}
-          storageKey={getDomainScopedStorageKey('pageTab', site.domain)}
-          className="w-full h-full border-0 overflow-hidden"
-        />
-      ) : (
-        <Pages />
-      )}
-
+      <Pages />
       <Locations />
       <Devices />
       <Behaviours importedDataInView={importedDataInView} />
