49651644d71b70179add46d3b578819c97128574
Adam Rutkowski <hq@mtod.org>
2026-02-04 16:48:28 +0100

    Add a failing test for mixed goals

--- test/plausible_web/controllers/api/stats_controller/conversions_test.exs
+++ test/plausible_web/controllers/api/stats_controller/conversions_test.exs
@@ -524,6 +524,38 @@ defmodule PlausibleWeb.Api.StatsController.ConversionsTest do
     end
 
     @tag :ee_only
+    test "handles mixed goals with and without custom props", %{
+      conn: conn,
+      site: site
+    } do
+      populate_stats(site, [
+        build(:event, name: "Signup"),
+        build(:event, name: "Purchase", "meta.key": ["product"], "meta.value": ["Shirt"])
+      ])
+
+      {:ok, _goal_with_props} =
+        Plausible.Goals.create(
+          site,
+          %{
+            "event_name" => "Purchase",
+            "custom_props" => %{"product" => "Shirt"}
+          }
+        )
+
+      insert(:goal, %{site: site, event_name: "Signup"})
+
+      conn = get(conn, "/api/stats/#{site.domain}/conversions?period=day")
+      response = json_response(conn, 200)
+      results = response["results"]
+
+      assert [
+               %{"conversion_rate" => 50.0, "events" => 1, "name" => "Purchase", "visitors" => 1},
+               %{"conversion_rate" => 50.0, "events" => 1, "name" => "Signup", "visitors" => 1}
+             ] =
+               results
+    end
+
+    @tag :ee_only
     test "returns revenue metrics as nil for non-revenue goals", %{
       conn: conn,
       site: site
