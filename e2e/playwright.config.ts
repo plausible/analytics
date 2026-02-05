dc2f594d6e7593afeca3189501009a012e164959
Adrian Gruntkowski <adrian.gruntkowski@gmail.com>
2026-02-05 12:24:43 +0100

    Clean up playwright config slightly

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -1,14 +1,6 @@
 import { defineConfig, devices } from '@playwright/test';
 
 /**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
-
-/**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
