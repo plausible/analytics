2b5a4f4fbee8f3c1c30ed85e02d93cccceb8e80b
Adrian Gruntkowski <adrian.gruntkowski@gmail.com>
2026-02-05 15:23:46 +0100

    Don't dump screenshots and don't upload them as GH artifacts

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -22,11 +22,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-
-    screenshot: 'only-on-failure',
   },
-
-  outputDir: './output',
 
   /* Configure projects for major browsers */
   projects: [
