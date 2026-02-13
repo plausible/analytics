ad82849d2a6163ddad2d20faf0619e94fa11b982
Robert Joonas <robertjoonas16@gmail.com>
2026-02-13 12:45:50 +0000

    grace period end in hours + remove unused fn

--- test/plausible/teams/grace_period_test.exs
+++ test/plausible/teams/grace_period_test.exs
@@ -112,4 +112,79 @@ defmodule Plausible.Teams.GracePeriodTest do
     refute Plausible.Teams.GracePeriod.active?(team)
     refute Plausible.Teams.GracePeriod.expired?(team)
   end
+
+  describe "expires_in/1" do
+    test "returns nil for no team" do
+      assert Plausible.Teams.GracePeriod.expires_in(nil) == nil
+    end
+
+    test "returns nil for team without grace period" do
+      team = new_user() |> team_of()
+      refute team.grace_period
+      assert Plausible.Teams.GracePeriod.expires_in(team) == nil
+    end
+
+    test "returns nil for team with a manual_lock grace period" do
+      team =
+        new_user(trial_expiry_date: Date.utc_today())
+        |> team_of()
+        |> Plausible.Teams.GracePeriod.start_manual_lock_changeset()
+        |> Ecto.Changeset.apply_changes()
+
+      assert team.grace_period.manual_lock
+      assert Plausible.Teams.GracePeriod.expires_in(team) == nil
+    end
+
+    test "returns diff in days when at least 48 hours left" do
+      now = ~N[2021-01-01 00:00:00]
+
+      grace_period = %Plausible.Teams.GracePeriod{
+        end_date: ~D[2021-01-03],
+        is_over: false
+      }
+
+      team = new_user(team: [grace_period: grace_period]) |> team_of()
+
+      assert Plausible.Teams.GracePeriod.expires_in(team, now) == {:days, 2}
+    end
+
+    test "returns diff in hours when less than 48 hours left" do
+      now = ~N[2021-01-01 00:00:01]
+
+      grace_period = %Plausible.Teams.GracePeriod{
+        end_date: ~D[2021-01-03],
+        is_over: false
+      }
+
+      team = new_user(team: [grace_period: grace_period]) |> team_of()
+
+      assert Plausible.Teams.GracePeriod.expires_in(team, now) == {:hours, 47}
+    end
+
+    test "returns {:hours, 0} when less than an hour left" do
+      now = ~N[2021-01-01 23:00:01]
+
+      grace_period = %Plausible.Teams.GracePeriod{
+        end_date: ~D[2021-01-02],
+        is_over: false
+      }
+
+      team = new_user(team: [grace_period: grace_period]) |> team_of()
+
+      assert Plausible.Teams.GracePeriod.expires_in(team, now) == {:hours, 0}
+    end
+
+    test "returns {:hours, 0} when end date in already in the past" do
+      now = ~N[2021-01-01 10:00:00]
+
+      grace_period = %Plausible.Teams.GracePeriod{
+        end_date: ~D[2021-01-01],
+        is_over: false
+      }
+
+      team = new_user(team: [grace_period: grace_period]) |> team_of()
+
+      assert Plausible.Teams.GracePeriod.expires_in(team, now) == {:hours, 0}
+    end
+  end
 end
