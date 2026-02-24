bca384a6fbc1c14bd27311b21f5ce49fc91d7bb9
Adrian Gruntkowski <adrian.gruntkowski@gmail.com>
2026-02-24 23:15:59 +0100

    Move some of the helpers to `test-utils`

--- e2e/tests/test-utils.ts
+++ e2e/tests/test-utils.ts
@@ -8,3 +8,27 @@ export async function expectLiveViewConnected(page: Page) {
 export function randomID() {
   return Math.random().toString(16).slice(2)
 }
+
+export const tabButton = (page, label) =>
+  page.getByTestId('tab-button').filter({ hasText: label })
+
+export const expectHeaders = async (report, headers) =>
+  expect(report.getByTestId('report-header')).toHaveText(headers)
+
+export const expectRows = async (report, labels) =>
+  expect(report.getByTestId('report-row').getByRole('link')).toHaveText(labels)
+
+export const rowLink = (report, label) =>
+  report.getByTestId('report-row').filter({ hasText: label }).getByRole('link')
+
+export const expectMetricValues = async (report, label, values) =>
+  expect(
+    report
+      .getByTestId('report-row')
+      .filter({ hasText: label })
+      .getByTestId('metric-value')
+  ).toHaveText(values)
+
+export const dropdown = (report) => report.getByTestId('dropdown-items')
+
+
