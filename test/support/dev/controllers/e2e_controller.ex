8e6df23a84a006e34b409fd4b4ae3ac292581278
Adrian Gruntkowski <adrian.gruntkowski@gmail.com>
2026-03-02 16:49:18 +0100

    Don't include backend E2E logic in CE

--- test/support/dev/controllers/e2e_controller.ex
+++ test/support/dev/controllers/e2e_controller.ex
@@ -1,90 +1,92 @@
 defmodule PlausibleWeb.E2EController do
   use Plausible
 
-  use PlausibleWeb, :controller
+  on_ee do
+    use PlausibleWeb, :controller
 
-  def populate_stats(conn, %{"domain" => domain, "events" => events}) do
-    site = Plausible.Repo.get_by!(Plausible.Site, domain: domain)
+    def populate_stats(conn, %{"domain" => domain, "events" => events}) do
+      site = Plausible.Repo.get_by!(Plausible.Site, domain: domain)
 
-    events =
-      events
-      |> Enum.map(&deserialize/1)
-      |> Enum.map(&build/1)
+      events =
+        events
+        |> Enum.map(&deserialize/1)
+        |> Enum.map(&build/1)
 
-    stats_start_time = Enum.min_by(events, & &1.timestamp, NaiveDateTime).timestamp
-    stats_start_date = NaiveDateTime.to_date(stats_start_time)
+      stats_start_time = Enum.min_by(events, & &1.timestamp, NaiveDateTime).timestamp
+      stats_start_date = NaiveDateTime.to_date(stats_start_time)
 
-    site
-    |> Plausible.Site.set_native_stats_start_at(stats_start_time)
-    |> Plausible.Site.set_stats_start_date(stats_start_date)
-    |> Plausible.Repo.update!()
+      site
+      |> Plausible.Site.set_native_stats_start_at(stats_start_time)
+      |> Plausible.Site.set_stats_start_date(stats_start_date)
+      |> Plausible.Repo.update!()
 
-    populate(events, site)
+      populate(events, site)
 
-    send_resp(conn, 200, Jason.encode!(%{"ok" => true}))
-  end
+      send_resp(conn, 200, Jason.encode!(%{"ok" => true}))
+    end
 
-  def create_funnel(conn, %{"domain" => domain, "name" => name, "steps" => steps}) do
-    site = Plausible.Repo.get_by!(Plausible.Site, domain: domain)
+    def create_funnel(conn, %{"domain" => domain, "name" => name, "steps" => steps}) do
+      site = Plausible.Repo.get_by!(Plausible.Site, domain: domain)
 
-    steps =
-      Enum.map(steps, fn step ->
-        goal = get_goal(site, step)
-        %{"goal_id" => goal.id}
+      steps =
+        Enum.map(steps, fn step ->
+          goal = get_goal(site, step)
+          %{"goal_id" => goal.id}
+        end)
+
+      {:ok, _} = Plausible.Funnels.create(site, name, steps)
+
+      send_resp(conn, 200, Jason.encode!(%{"ok" => true}))
+    end
+
+    defp get_goal(site, name) do
+      Plausible.Repo.get_by!(Plausible.Goal, site_id: site.id, display_name: name)
+    end
+
+    defp deserialize(event) do
+      Enum.map(event, fn
+        {"timestamp", value} ->
+          {:timestamp, to_timestamp(value)}
+
+        {"revenue_reporting_amount", value} ->
+          {:revenue_reporting_amount, Decimal.new(value)}
+
+        {key, value} ->
+          {String.to_existing_atom(key), value}
       end)
+    end
 
-    {:ok, _} = Plausible.Funnels.create(site, name, steps)
+    defp build(attrs) do
+      timestamp = NaiveDateTime.utc_now(:second) |> NaiveDateTime.add(-48, :hour)
 
-    send_resp(conn, 200, Jason.encode!(%{"ok" => true}))
-  end
+      attrs =
+        if attrs[:timestamp] do
+          attrs
+        else
+          Keyword.put(attrs, :timestamp, timestamp)
+        end
 
-  defp get_goal(site, name) do
-    Plausible.Repo.get_by!(Plausible.Goal, site_id: site.id, display_name: name)
-  end
+      Plausible.Factory.build(:event, attrs)
+    end
 
-  defp deserialize(event) do
-    Enum.map(event, fn
-      {"timestamp", value} ->
-        {:timestamp, to_timestamp(value)}
+    defp populate(events, site) do
+      Plausible.TestUtils.populate_stats(site, events)
+    end
 
-      {"revenue_reporting_amount", value} ->
-        {:revenue_reporting_amount, Decimal.new(value)}
+    defp to_timestamp(%{"daysAgo" => offset}) do
+      NaiveDateTime.utc_now(:second) |> NaiveDateTime.add(-offset, :day)
+    end
 
-      {key, value} ->
-        {String.to_existing_atom(key), value}
-    end)
-  end
+    defp to_timestamp(%{"hoursAgo" => offset}) do
+      NaiveDateTime.utc_now(:second) |> NaiveDateTime.add(-offset, :hour)
+    end
 
-  defp build(attrs) do
-    timestamp = NaiveDateTime.utc_now(:second) |> NaiveDateTime.add(-48, :hour)
+    defp to_timestamp(%{"minutesAgo" => offset}) do
+      NaiveDateTime.utc_now(:second) |> NaiveDateTime.add(-offset, :minute)
+    end
 
-    attrs =
-      if attrs[:timestamp] do
-        attrs
-      else
-        Keyword.put(attrs, :timestamp, timestamp)
-      end
-
-    Plausible.Factory.build(:event, attrs)
-  end
-
-  defp populate(events, site) do
-    Plausible.TestUtils.populate_stats(site, events)
-  end
-
-  defp to_timestamp(%{"daysAgo" => offset}) do
-    NaiveDateTime.utc_now(:second) |> NaiveDateTime.add(-offset, :day)
-  end
-
-  defp to_timestamp(%{"hoursAgo" => offset}) do
-    NaiveDateTime.utc_now(:second) |> NaiveDateTime.add(-offset, :hour)
-  end
-
-  defp to_timestamp(%{"minutesAgo" => offset}) do
-    NaiveDateTime.utc_now(:second) |> NaiveDateTime.add(-offset, :minute)
-  end
-
-  defp to_timestamp(ts) when is_binary(ts) do
-    NaiveDateTime.from_iso8601!(ts)
+    defp to_timestamp(ts) when is_binary(ts) do
+      NaiveDateTime.from_iso8601!(ts)
+    end
   end
 end
