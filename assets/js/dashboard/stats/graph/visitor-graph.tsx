c8be42344e1f91bf51aa2fa69511aa8d74c0e2c7
Artur Pata <artur.pata@gmail.com>
2026-03-03 19:06:44 +0200

    Remove unused sampling notice

--- assets/js/dashboard/stats/graph/visitor-graph.tsx
+++ assets/js/dashboard/stats/graph/visitor-graph.tsx
@@ -206,7 +206,6 @@ export default function VisitorGraph({
                 notices={
                   [
                     importedIntervalUnsupportedNotice
-                    // getSamplingNotice(topStatsQuery.data)
                   ].filter((n) => !!n) as string[]
                 }
               />
