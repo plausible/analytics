c73e0e213bbbaed77ddcad0112a2f1b9ce3116dc
Adrian Gruntkowski <adrian.gruntkowski@gmail.com>
2026-03-04 15:26:26 +0100

    Add alias for conveniently setting up E2E

--- mix.exs
+++ mix.exs
@@ -179,6 +179,10 @@ defmodule Plausible.MixProject do
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate", "test", "clean_clickhouse"],
+      "e2e.setup": [
+        "cmd npm install --prefix ./e2e",
+        "cmd npm exec playwright install --with-deps chromium --prefix ./e2e"
+      ],
       "test.e2e": [
         "esbuild default",
         "ecto.create --quiet",
