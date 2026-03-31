9f696d3ea6734df833ae4014fd41bd7ee844bed2
Uku Taht <uku.taht@gmail.com>
2026-03-31 15:33:23 +0300

    Run revenue tests in EE only

--- test/plausible/stats/table_decider_test.exs
+++ test/plausible/stats/table_decider_test.exs
@@ -217,8 +217,6 @@ defmodule Plausible.Stats.TableDeciderTest do
                 "Event metric(s) `scroll_depth` cannot be queried along with session dimension(s) `visit:exit_page`"
             }}},
           {[:bounce_rate, :scroll_depth], ["event:page"], :ok},
-          {[:total_revenue], ["visit:entry_page"], :ok},
-          {[:average_revenue], ["visit:exit_page"], :ok},
           {[:scroll_depth], ["visit:entry_page"],
            {:error,
             %QueryError{
@@ -239,6 +237,19 @@ defmodule Plausible.Stats.TableDeciderTest do
           make_query() |> Query.set(metrics: unquote(metrics), dimensions: unquote(dimensions))
 
         assert validate_no_metrics_dimensions_conflict(query) == unquote(Macro.escape(expected))
+      end
+    end
+
+    for {metrics, dimensions} <- [
+          {[:total_revenue], ["visit:entry_page"]},
+          {[:average_revenue], ["visit:exit_page"]}
+        ] do
+      @tag :ee_only
+      test "revenue metrics #{inspect(metrics)} and dimensions #{inspect(dimensions)} are allowed with session dimensions" do
+        query =
+          make_query() |> Query.set(metrics: unquote(metrics), dimensions: unquote(dimensions))
+
+        assert validate_no_metrics_dimensions_conflict(query) == :ok
       end
     end
   end
