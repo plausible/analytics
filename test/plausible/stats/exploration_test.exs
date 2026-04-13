cfa3155dcfdbf08d4d142e8662f7083f6891ff41
Adrian Gruntkowski <adrian.gruntkowski@gmail.com>
2026-04-13 09:46:48 +0200

    Revise tests for exploration slightly

--- test/plausible/stats/exploration_test.exs
+++ test/plausible/stats/exploration_test.exs
@@ -13,46 +13,55 @@ defmodule Plausible.Stats.ExplorationTest do
       build(:pageview,
         user_id: 123,
         pathname: "/home",
+        browser: "Chrome",
         timestamp: DateTime.shift(now, minute: -300)
       ),
       build(:pageview,
         user_id: 123,
         pathname: "/login",
+        browser: "Chrome",
         timestamp: DateTime.shift(now, minute: -270)
       ),
       build(:pageview,
         user_id: 123,
         pathname: "/home",
+        browser: "Chrome",
         timestamp: DateTime.shift(now, minute: -30)
       ),
       build(:pageview,
         user_id: 123,
         pathname: "/login",
+        browser: "Chrome",
         timestamp: DateTime.shift(now, minute: -25)
       ),
       build(:pageview,
         user_id: 123,
         pathname: "/logout",
+        browser: "Chrome",
         timestamp: DateTime.shift(now, minute: -20)
       ),
       build(:pageview,
         user_id: 124,
         pathname: "/home",
+        browser: "Firefox",
         timestamp: DateTime.shift(now, minute: -30)
       ),
       build(:pageview,
         user_id: 124,
         pathname: "/login",
+        browser: "Firefox",
         timestamp: DateTime.shift(now, minute: -25)
       ),
       build(:pageview,
         user_id: 124,
         pathname: "/docs",
+        browser: "Firefox",
         timestamp: DateTime.shift(now, minute: -20)
       ),
       build(:pageview,
         user_id: 124,
         pathname: "/logout",
+        browser: "Firefox",
         timestamp: DateTime.shift(now, minute: -15)
       )
     ])
@@ -61,7 +70,7 @@ defmodule Plausible.Stats.ExplorationTest do
   end
 
   describe "journey_funnel" do
-    test "it works", %{site: site} do
+    test "queries 3-step journey", %{site: site} do
       query = QueryBuilder.build!(site, input_date_range: :all)
 
       journey = [
@@ -85,10 +94,45 @@ defmodule Plausible.Stats.ExplorationTest do
       assert step3.dropoff == 1
       assert step3.dropoff_percentage == "50"
     end
+
+    test "respects filters in the query", %{site: site} do
+      query =
+        QueryBuilder.build!(site,
+          input_date_range: :all,
+          filters: [[:is, "visit:browser", ["Firefox"]]]
+        )
+
+      journey = [
+        %Exploration.Journey.Step{name: "pageview", pathname: "/home"},
+        %Exploration.Journey.Step{name: "pageview", pathname: "/login"},
+        %Exploration.Journey.Step{name: "pageview", pathname: "/logout"}
+      ]
+
+      assert {:ok, [step1, step2, step3]} = Exploration.journey_funnel(query, journey)
+
+      assert step1.step.pathname == "/home"
+      assert step1.visitors == 1
+      assert step1.dropoff == 0
+      assert step1.dropoff_percentage == "0"
+      assert step2.step.pathname == "/login"
+      assert step2.visitors == 1
+      assert step2.dropoff == 0
+      assert step2.dropoff_percentage == "0"
+      assert step3.step.pathname == "/logout"
+      assert step3.visitors == 0
+      assert step3.dropoff == 1
+      assert step3.dropoff_percentage == "100"
+    end
+
+    test "returns error on empty journey", %{site: site} do
+      query = QueryBuilder.build!(site, input_date_range: :all)
+
+      assert {:error, :empty_journey} = Exploration.journey_funnel(query, [])
+    end
   end
 
   describe "next_steps" do
-    test "it works", %{site: site} do
+    test "suggests the next step for a 2-step journey", %{site: site} do
       query = QueryBuilder.build!(site, input_date_range: :all)
 
       journey = [
@@ -104,7 +148,7 @@ defmodule Plausible.Stats.ExplorationTest do
       assert next_step2.visitors == 1
     end
 
-    test "it works for an empty journey", %{site: site} do
+    test "suggests the first step in the journey", %{site: site} do
       query = QueryBuilder.build!(site, input_date_range: :all)
 
       assert {:ok, [next_step1, next_step2, next_step3, next_step4]} =
@@ -120,7 +164,27 @@ defmodule Plausible.Stats.ExplorationTest do
       assert next_step4.visitors == 1
     end
 
-    test "it filters", %{site: site} do
+    test "respects filters in the query", %{site: site} do
+      query =
+        QueryBuilder.build!(site,
+          input_date_range: :all,
+          filters: [[:is, "visit:browser", ["Firefox"]]]
+        )
+
+      assert {:ok, [next_step1, next_step2, next_step3, next_step4]} =
+               Exploration.next_steps(query, [])
+
+      assert next_step1.step.pathname == "/docs"
+      assert next_step1.visitors == 1
+      assert next_step2.step.pathname == "/home"
+      assert next_step2.visitors == 1
+      assert next_step3.step.pathname == "/login"
+      assert next_step3.visitors == 1
+      assert next_step4.step.pathname == "/logout"
+      assert next_step4.visitors == 1
+    end
+
+    test "allows to filter the next step suggestions", %{site: site} do
       query = QueryBuilder.build!(site, input_date_range: :all)
 
       journey = [
