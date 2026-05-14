429aaf20a7a45ff7a7401fdd32c0024c888250ec
Adrian Gruntkowski <adrian.gruntkowski@gmail.com>
2026-05-14 19:01:14 +0200

    Add tests for featured funnel

--- e2e/tests/dashboard/exploration.spec.ts
+++ e2e/tests/dashboard/exploration.spec.ts
@@ -9,8 +9,8 @@ import {
 import { tabButton } from '../test-utils'
 
 /*
- * - Loading featured funnel
- * - Loading featured funnel when there are no events fora given set of conditions and time range
+ * V - Loading featured funnel
+ * V - Loading featured funnel when there are no events for a given set of conditions and time range
  * - Rendering entries for:
  *   - pageview
  *   - custom event
@@ -52,7 +52,10 @@ const getReport = (page: Page) => page.getByTestId('report-behaviours')
 const getExplorationTabButton = (report: Locator) =>
   tabButton(report, 'Explore')
 
-test('load featured tunnel with only a single step', async ({ page, request }) => {
+test('load featured tunnel with only a single step', async ({
+  page,
+  request
+}) => {
   const report = getReport(page)
   const explorationTabButton = getExplorationTabButton(report)
   const { domain } = await setupSite({ page, request })
@@ -91,12 +94,197 @@ test('load featured tunnel with only a single step', async ({ page, request }) =
     firstColumn.getByTestId('exploration-row').getByTestId('metric-label')
   ).toHaveText(['/page'])
 
-
   await expect(
     firstColumn.getByTestId('exploration-row').getByTestId('metric-value')
   ).toHaveText(['1'])
 
-
   await expect(secondColumn).toHaveText(/1 step after/)
   await expect(secondColumn).toHaveText(/No further steps found/)
+})
+
+// current version is often stuck loading if the timing is unlucky
+test('load featured tunnel and switch to a period with no events without waiting for load', async ({
+  page,
+  request
+}) => {
+  const report = getReport(page)
+  const explorationTabButton = getExplorationTabButton(report)
+  const { domain } = await setupSite({ page, request })
+
+  await populateStats({
+    request,
+    domain,
+    events: [
+      {
+        user_id: 123,
+        name: 'pageview',
+        pathname: '/page',
+        timestamp: { hoursAgo: 40 }
+      }
+    ]
+  })
+
+  await page.goto('/' + domain, { waitUntil: 'commit' })
+
+  await explorationTabButton.click()
+
+  await expect(report.getByTestId('exploration-title')).toHaveText(
+    'Explore user journeys'
+  )
+
+  // switch to 'Today'
+  await page.keyboard.press('d')
+
+  await expect(report).toHaveText(/No data yet/)
+})
+
+test('load featured tunnel and switch to a period with no events', async ({
+  page,
+  request
+}) => {
+  const report = getReport(page)
+  const explorationTabButton = getExplorationTabButton(report)
+  const { domain } = await setupSite({ page, request })
+
+  await populateStats({
+    request,
+    domain,
+    events: [
+      {
+        user_id: 123,
+        name: 'pageview',
+        pathname: '/page',
+        timestamp: { hoursAgo: 40 }
+      }
+    ]
+  })
+
+  await page.goto('/' + domain, { waitUntil: 'commit' })
+
+  await explorationTabButton.click()
+
+  await expect(report.getByTestId('exploration-title')).toHaveText(
+    'Explore user journeys'
+  )
+
+  const firstColumn = report.getByTestId('exploration-column-0')
+
+  await expect(firstColumn).toBeVisible()
+
+  await expect(
+    firstColumn.getByTestId('exploration-row').getByTestId('metric-label')
+  ).toHaveText(['/page'])
+
+  // switch to 'Today'
+  await page.keyboard.press('d')
+
+  await expect(report).toHaveText(/No data yet/)
+})
+
+test('load a 3-step featured funnel', async ({ page, request }) => {
+  const report = getReport(page)
+  const explorationTabButton = getExplorationTabButton(report)
+  const { domain } = await setupSite({ page, request })
+
+  await populateStats({
+    request,
+    domain,
+    events: [
+      {
+        user_id: 123,
+        name: 'pageview',
+        pathname: '/home',
+        timestamp: { minutesAgo: 40 }
+      },
+      {
+        user_id: 123,
+        name: 'pageview',
+        pathname: '/login',
+        timestamp: { minutesAgo: 35 }
+      },
+      {
+        user_id: 123,
+        name: 'pageview',
+        pathname: '/dashboard',
+        timestamp: { minutesAgo: 30 }
+      },
+      {
+        user_id: 124,
+        name: 'pageview',
+        pathname: '/home',
+        timestamp: { minutesAgo: 35 }
+      },
+      {
+        user_id: 124,
+        name: 'pageview',
+        pathname: '/login',
+        timestamp: { minutesAgo: 30 }
+      },
+      {
+        user_id: 125,
+        name: 'pageview',
+        pathname: '/home',
+        timestamp: { minutesAgo: 35 }
+      },
+      {
+        user_id: 126,
+        name: 'pageview',
+        pathname: '/another',
+        timestamp: { minutesAgo: 40 }
+      },
+      {
+        user_id: 126,
+        name: 'pageview',
+        pathname: '/journey',
+        timestamp: { minutesAgo: 35 }
+      }
+    ]
+  })
+
+  await page.goto('/' + domain, { waitUntil: 'commit' })
+
+  await explorationTabButton.click()
+
+  await expect(report.getByTestId('exploration-title')).toHaveText(
+    'Explore user journeys'
+  )
+
+  await expect(
+    report.getByTestId('exploration-direction-forward')
+  ).toBeVisible()
+
+  const firstColumn = report.getByTestId('exploration-column-0')
+  const secondColumn = report.getByTestId('exploration-column-1')
+  const thirdColumn = report.getByTestId('exploration-column-2')
+
+  await expect(firstColumn).toBeVisible()
+  await expect(secondColumn).toBeVisible()
+  await expect(thirdColumn).toBeVisible()
+
+  await expect(
+    firstColumn.getByTestId('exploration-row').getByTestId('metric-label')
+  ).toHaveText(['/home', '/login', '/another', '/dashboard', '/journey'])
+
+  await expect(
+    firstColumn.getByTestId('exploration-row').getByTestId('metric-value')
+  ).toHaveText(['3', '2', '1', '1', '1'])
+
+  await expect(
+    secondColumn.getByTestId('exploration-row').getByTestId('metric-label')
+  ).toHaveText(['/login', 'no further action'])
+
+  await expect(
+    secondColumn.getByTestId('exploration-row').getByTestId('metric-value')
+  ).toHaveText(['2', '1'])
+
+  await expect(
+    thirdColumn.getByTestId('exploration-row').getByTestId('metric-label')
+  ).toHaveText(['no further action', '/dashboard'])
+
+  await expect(
+    thirdColumn.getByTestId('exploration-row').getByTestId('metric-value')
+  ).toHaveText(['1', '1'])
+
+  await expect(secondColumn).toHaveText(/1 step after/)
+  await expect(thirdColumn).toHaveText(/2 steps after/)
 })
