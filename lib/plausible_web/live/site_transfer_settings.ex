f3d7a4e054aa0e18e028ff69876ee5154153ecd0
Sanne de Vries <sannedv@protonmail.com>
2026-05-28 14:30:35 +0200

    Consolidate and improve site transfer UX
    - Combine "Transfer site" and "Change team" into a single tile with a
      destination radio (Team / Another Plausible account)
    - Convert site transfer tile to `LiveView`
    - Show validation and server errors inline next to the relevant field
    - Always render the Team option, greyed out when the user has no other
      team
    - Add disabled cursor styling to the shared radio input
    - Add documentation links to all tiles in `Danger Zone`

---
+++ lib/plausible_web/live/site_transfer_settings.ex
@@ -0,0 +1,288 @@
+defmodule PlausibleWeb.Live.SiteTransferSettings do
+  @moduledoc """
+  LiveView for the "Transfer site" tile in the site Danger Zone.
+
+  Lets the site owner pick a destination (another team they own/admin or
+  another Plausible account), validates the form interactively, and
+  dispatches to the appropriate transfer action.
+  """
+
+  use PlausibleWeb, :live_view
+
+  alias Plausible.Repo
+  alias Plausible.Teams
+  alias PlausibleWeb.Router.Helpers, as: Routes
+
+  defmodule Form do
+    @moduledoc false
+    use Ecto.Schema
+    import Ecto.Changeset
+
+    @primary_key false
+    embedded_schema do
+      field :destination, :string
+      field :team_identifier, :string
+      field :email, :string
+    end
+
+    @valid_destinations ~w(team account)
+
+    def changeset(params, opts \\ []) do
+      %__MODULE__{}
+      |> cast(params, [:destination, :team_identifier, :email])
+      |> validate_required(:destination)
+      |> validate_inclusion(:destination, @valid_destinations)
+      |> validate_destination_fields(opts)
+    end
+
+    defp validate_destination_fields(changeset, opts) do
+      case get_field(changeset, :destination) do
+        "team" ->
+          allowed = Keyword.get(opts, :team_identifiers, [])
+
+          changeset
+          |> validate_required(:team_identifier, message: "Please select a team")
+          |> validate_inclusion(:team_identifier, allowed, message: "Please select a team")
+
+        "account" ->
+          changeset
+          |> validate_required(:email, message: "Please enter an email address")
+          |> validate_format(:email, ~r/^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+            message: "Please enter a valid email address"
+          )
+
+        _ ->
+          changeset
+      end
+    end
+  end
+
+  def mount(_params, %{"domain" => domain}, socket) do
+    user = socket.assigns.current_user
+
+    site =
+      Plausible.Sites.get_for_user!(user, domain, roles: [:owner, :admin, :super_admin])
+
+    transferable_teams =
+      user
+      |> Teams.Users.teams(roles: [:owner, :admin])
+      |> Enum.reject(&(&1.id == site.team_id))
+      |> Enum.map(&{&1.name, &1.identifier})
+
+    show_team? = transferable_teams != []
+    initial_destination = if show_team?, do: "team", else: "account"
+
+    socket =
+      socket
+      |> assign(
+        site: site,
+        transferable_teams: transferable_teams,
+        show_team?: show_team?
+      )
+      |> assign_form(%{"destination" => initial_destination})
+
+    {:ok, socket}
+  end
+
+  def render(assigns) do
+    ~H"""
+    <div>
+      <.flash_messages flash={@flash} />
+
+      <.tile docs="transfer-ownership">
+        <:title>Transfer site</:title>
+        <:subtitle>Move this site to another team or Plausible account.</:subtitle>
+
+        <.form
+          :let={f}
+          for={@form}
+          id="site-transfer-form"
+          phx-change="validate"
+          phx-submit="save"
+          novalidate
+        >
+          <fieldset class="max-w-lg flex flex-col gap-y-4">
+            <.label>Destination</.label>
+
+            <div class="flex flex-col">
+              <div class={not @show_team? && "opacity-40 cursor-not-allowed"}>
+                <.input
+                  type="radio"
+                  id="destination-team"
+                  name={f[:destination].name}
+                  value="team"
+                  checked={f[:destination].value == "team" and @show_team?}
+                  disabled={not @show_team?}
+                  label="Team"
+                />
+              </div>
+              <div
+                :if={f[:destination].value == "team" and @show_team?}
+                class="ml-7 mt-1 flex flex-col gap-y-2"
+              >
+                <p class="text-sm text-gray-500 dark:text-gray-400 text-pretty">
+                  The site will immediately move to the selected team. Billing does not transfer.
+                </p>
+                <.input
+                  type="select"
+                  field={f[:team_identifier]}
+                  options={@transferable_teams}
+                  prompt="Select a team"
+                  mt?={false}
+                />
+              </div>
+              <p
+                :if={not @show_team?}
+                class="ml-7 mt-1 text-sm text-gray-500/60 dark:text-gray-400/60 text-pretty"
+              >
+                You aren't a member of any other teams.
+              </p>
+            </div>
+
+            <div class="flex flex-col">
+              <.input
+                type="radio"
+                id="destination-account"
+                name={f[:destination].name}
+                value="account"
+                checked={f[:destination].value == "account"}
+                label="Another Plausible account"
+              />
+              <div
+                :if={f[:destination].value == "account"}
+                class="ml-7 mt-1 flex flex-col gap-y-2"
+              >
+                <p class="text-sm text-gray-500 dark:text-gray-400 text-pretty">
+                  The recipient will receive an email to accept the transfer within 48 hours. You'll keep Guest Editor access by default.
+                </p>
+                <.input
+                  type="email"
+                  field={f[:email]}
+                  label="Email address"
+                  placeholder="joe@example.com"
+                  mt?={false}
+                />
+              </div>
+            </div>
+          </fieldset>
+
+          <.button
+            type="submit"
+            theme="danger"
+            phx-disable-with="Transferring..."
+          >
+            {submit_label(f[:destination].value)}
+          </.button>
+        </.form>
+      </.tile>
+    </div>
+    """
+  end
+
+  def handle_event("validate", %{"form" => params}, socket) do
+    {:noreply, assign_form(socket, params)}
+  end
+
+  def handle_event("save", %{"form" => params}, socket) do
+    changeset =
+      Form.changeset(params,
+        team_identifiers: Enum.map(socket.assigns.transferable_teams, &elem(&1, 1))
+      )
+
+    case Ecto.Changeset.apply_action(changeset, :insert) do
+      {:ok, %Form{destination: "team", team_identifier: identifier}} ->
+        do_change_team(socket, identifier, params)
+
+      {:ok, %Form{destination: "account", email: email}} ->
+        do_transfer_ownership(socket, email, params)
+
+      {:error, changeset} ->
+        {:noreply, assign(socket, form: to_form(changeset, as: :form))}
+    end
+  end
+
+  defp do_change_team(socket, identifier, params) do
+    user = socket.assigns.current_user
+    site = socket.assigns.site
+
+    destination_team =
+      Repo.one!(Teams.Users.teams_query(user, roles: [:admin, :owner], identifier: identifier))
+
+    case Teams.Sites.Transfer.change_team(site, user, destination_team) do
+      :ok ->
+        {:noreply,
+         socket
+         |> put_flash(:success, "Site team was changed")
+         |> redirect(to: Routes.site_path(socket, :index, __team: identifier))}
+
+      {:error, reason} ->
+        {:noreply,
+         assign_form(socket, params,
+           action: :insert,
+           field_errors: [{:team_identifier, change_team_error_message(reason)}]
+         )}
+    end
+  end
+
+  defp do_transfer_ownership(socket, email, params) do
+    user = socket.assigns.current_user
+    site = socket.assigns.site
+
+    case Teams.Invitations.InviteToSite.invite(site, user, email, :owner) do
+      {:ok, _invitation} ->
+        {:noreply,
+         socket
+         |> put_flash(:success, "Site transfer request has been sent to #{email}")
+         |> redirect(to: Routes.site_path(socket, :settings_people, site.domain))}
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        message =
+          case Plausible.ChangesetHelpers.traverse_errors(changeset) do
+            %{invitation: ["already sent" | _]} -> "Invitation has already been sent"
+            _ -> "Site transfer request to #{email} has failed"
+          end
+
+        {:noreply,
+         assign_form(socket, params,
+           action: :insert,
+           field_errors: [{:email, message}]
+         )}
+    end
+  end
+
+  defp assign_form(socket, params, opts \\ []) do
+    changeset =
+      params
+      |> Form.changeset(
+        team_identifiers: Enum.map(socket.assigns.transferable_teams, &elem(&1, 1))
+      )
+
+    changeset =
+      Enum.reduce(Keyword.get(opts, :field_errors, []), changeset, fn {field, message}, cs ->
+        Ecto.Changeset.add_error(cs, field, message)
+      end)
+
+    changeset =
+      case opts[:action] do
+        nil -> changeset
+        action -> Map.put(changeset, :action, action)
+      end
+
+    assign(socket, form: to_form(changeset, as: :form))
+  end
+
+  defp submit_label("team"), do: "Move site"
+  defp submit_label(_), do: "Send transfer request"
+
+  defp change_team_error_message(:no_plan) do
+    "This team doesn't have a subscription. Please start a subscription for the team first and then try moving the site again."
+  end
+
+  defp change_team_error_message({:over_plan_limits, _}) do
+    "This site's usage exceeds the destination team's subscription limits. Upgrade the team's subscription to continue."
+  end
+
+  defp change_team_error_message(_) do
+    "Sorry, this team cannot be used"
+  end
+end
---
+++ test/plausible_web/live/site_transfer_settings_test.exs
@@ -0,0 +1,310 @@
+defmodule PlausibleWeb.Live.SiteTransferSettingsTest do
+  use PlausibleWeb.ConnCase, async: false
+  use Plausible.Repo
+  use Bamboo.Test, shared: :true
+
+  import Phoenix.LiveViewTest
+
+  @subject_prefix if ee?(), do: "[Plausible Analytics] ", else: "[Plausible CE] "
+
+  setup [:create_user, :log_in, :create_site]
+
+  describe "mount" do
+    test "renders the Team radio as disabled when user has no other team", %{
+      conn: conn,
+      site: site
+    } do
+      {:ok, _lv, html} = get_liveview(conn, site)
+
+      assert html =~ "Transfer site"
+      assert html =~ "Move this site to another team or Plausible account"
+      assert html =~ "Another Plausible account"
+
+      assert element_exists?(html, ~s|input[name="form[destination]"][value="account"]|)
+
+      assert element_exists?(
+               html,
+               ~s|input[name="form[destination]"][value="team"][disabled]|
+             )
+
+      assert text_of_element(html, "#site-transfer-form") =~
+               "You aren't a member of any other teams"
+    end
+
+    test "renders both destinations enabled when the user has another team", %{
+      conn: conn,
+      user: user,
+      site: site
+    } do
+      _team2 = join_2nd_team(user)
+
+      {:ok, _lv, html} = get_liveview(conn, site)
+
+      assert element_exists?(html, ~s|input[name="form[destination]"][value="team"]|)
+      assert element_exists?(html, ~s|input[name="form[destination]"][value="account"]|)
+
+      refute element_exists?(
+               html,
+               ~s|input[name="form[destination]"][value="team"][disabled]|
+             )
+
+      assert html =~ "The site will immediately move to the selected team"
+      refute html =~ "joe@example.com"
+      refute text_of_element(html, "#site-transfer-form") =~
+               "You aren't a member of any other teams"
+    end
+
+    test "Team destination is preselected when available", %{
+      conn: conn,
+      user: user,
+      site: site
+    } do
+      _team2 = join_2nd_team(user)
+
+      {:ok, _lv, html} = get_liveview(conn, site)
+
+      assert element_exists?(
+               html,
+               ~s|input[name="form[destination]"][value="team"][checked]|
+             )
+
+      refute element_exists?(
+               html,
+               ~s|input[name="form[destination]"][value="account"][checked]|
+             )
+    end
+  end
+
+  describe "switching destination" do
+    test "changing to Account hides team picker and shows email input", %{
+      conn: conn,
+      user: user,
+      site: site
+    } do
+      _team2 = join_2nd_team(user)
+
+      {:ok, lv, _html} = get_liveview(conn, site)
+
+      html =
+        lv
+        |> element("#site-transfer-form")
+        |> render_change(%{"form" => %{"destination" => "account"}})
+
+      assert html =~ "Email address"
+      assert html =~ "joe@example.com"
+      refute html =~ "The site will immediately move to the selected team"
+
+      assert text_of_attr(html, ~s|button[type=submit]|, "phx-disable-with") ==
+               "Transferring..."
+
+      assert text_of_element(html, ~s|button[type=submit]|) =~ "Send transfer request"
+    end
+
+    test "changing to Team hides email and shows team picker", %{
+      conn: conn,
+      user: user,
+      site: site
+    } do
+      _team2 = join_2nd_team(user)
+
+      {:ok, lv, _html} = get_liveview(conn, site)
+
+      lv
+      |> element("#site-transfer-form")
+      |> render_change(%{"form" => %{"destination" => "account"}})
+
+      html =
+        lv
+        |> element("#site-transfer-form")
+        |> render_change(%{"form" => %{"destination" => "team"}})
+
+      assert html =~ "Select a team"
+      refute html =~ "Email address"
+      assert text_of_element(html, ~s|button[type=submit]|) =~ "Move site"
+    end
+  end
+
+  describe "submitting (account destination)" do
+    test "creates a site transfer and redirects to settings/people", %{
+      conn: conn,
+      site: site
+    } do
+      {:ok, lv, _html} = get_liveview(conn, site)
+
+      lv
+      |> element("#site-transfer-form")
+      |> render_submit(%{
+        "form" => %{"destination" => "account", "email" => "john.doe@example.com"}
+      })
+
+      assert_redirect(lv, "/#{URI.encode_www_form(site.domain)}/settings/people")
+
+      assert_email_delivered_with(
+        to: [nil: "john.doe@example.com"],
+        subject: @subject_prefix <> "Request to transfer ownership of #{site.domain}"
+      )
+    end
+
+    test "renders an inline error when the user has already been invited", %{
+      conn: conn,
+      user: user,
+      site: site
+    } do
+      invited = "john.doe@example.com"
+
+      {:ok, _invitation} =
+        Plausible.Teams.Invitations.InviteToSite.invite(site, user, invited, :editor)
+
+      {:ok, lv, _html} = get_liveview(conn, site)
+
+      html =
+        lv
+        |> element("#site-transfer-form")
+        |> render_submit(%{
+          "form" => %{"destination" => "account", "email" => invited}
+        })
+
+      assert html =~ "Invitation has already been sent"
+    end
+
+    test "validates that an email is required", %{conn: conn, site: site} do
+      {:ok, lv, _html} = get_liveview(conn, site)
+
+      html =
+        lv
+        |> element("#site-transfer-form")
+        |> render_submit(%{"form" => %{"destination" => "account", "email" => ""}})
+
+      assert html =~ "Please enter an email address"
+    end
+
+    test "validates that the email is well-formed", %{conn: conn, site: site} do
+      {:ok, lv, _html} = get_liveview(conn, site)
+
+      html =
+        lv
+        |> element("#site-transfer-form")
+        |> render_submit(%{"form" => %{"destination" => "account", "email" => "not-an-email"}})
+
+      assert html =~ "Please enter a valid email address"
+    end
+  end
+
+  describe "submitting (team destination)" do
+    test "successfully changes the site's team and redirects to sites listing", %{
+      conn: conn,
+      user: user,
+      site: site
+    } do
+      team2 = join_2nd_team(user, subscribe?: true)
+
+      {:ok, lv, _html} = get_liveview(conn, site)
+
+      lv
+      |> element("#site-transfer-form")
+      |> render_submit(%{
+        "form" => %{"destination" => "team", "team_identifier" => team2.identifier}
+      })
+
+      assert_redirect(lv, "/sites?__team=#{team2.identifier}")
+
+      assert Plausible.Repo.reload!(site).team_id == team2.id
+    end
+
+    @tag :ee_only
+    test "renders an inline error when the destination team has no subscription", %{
+      conn: conn,
+      user: user,
+      site: site
+    } do
+      team2 = join_2nd_team(user)
+
+      {:ok, lv, _html} = get_liveview(conn, site)
+
+      html =
+        lv
+        |> element("#site-transfer-form")
+        |> render_submit(%{
+          "form" => %{"destination" => "team", "team_identifier" => team2.identifier}
+        })
+
+      assert text_of_element(html, "#site-transfer-form") =~
+               "This team doesn't have a subscription"
+    end
+
+    @tag :ee_only
+    test "renders an inline error when usage exceeds destination team's limits", %{
+      conn: conn,
+      user: user,
+      site: site
+    } do
+      team2 = join_2nd_team(user, subscribe?: true)
+
+      generate_usage_for(site, 11_000, NaiveDateTime.utc_now() |> NaiveDateTime.shift(day: -5))
+      generate_usage_for(site, 11_000, NaiveDateTime.utc_now() |> NaiveDateTime.shift(day: -35))
+
+      {:ok, lv, _html} = get_liveview(conn, site)
+
+      html =
+        lv
+        |> element("#site-transfer-form")
+        |> render_submit(%{
+          "form" => %{"destination" => "team", "team_identifier" => team2.identifier}
+        })
+
+      assert text_of_element(html, "#site-transfer-form") =~
+               "This site's usage exceeds the destination team's subscription limits"
+    end
+
+    test "validates that a team must be selected", %{conn: conn, user: user, site: site} do
+      _team2 = join_2nd_team(user)
+
+      {:ok, lv, _html} = get_liveview(conn, site)
+
+      html =
+        lv
+        |> element("#site-transfer-form")
+        |> render_submit(%{
+          "form" => %{"destination" => "team", "team_identifier" => ""}
+        })
+
+      assert html =~ "Please select a team"
+    end
+
+    test "rejects an unknown team identifier", %{conn: conn, user: user, site: site} do
+      _team2 = join_2nd_team(user)
+
+      {:ok, lv, _html} = get_liveview(conn, site)
+
+      html =
+        lv
+        |> element("#site-transfer-form")
+        |> render_submit(%{
+          "form" => %{
+            "destination" => "team",
+            "team_identifier" => Ecto.UUID.generate()
+          }
+        })
+
+      assert html =~ "Please select a team"
+    end
+  end
+
+  defp get_liveview(conn, site) do
+    conn = assign(conn, :live_module, PlausibleWeb.Live.SiteTransferSettings)
+    live(conn, "/#{URI.encode_www_form(site.domain)}/settings/danger-zone")
+  end
+
+  defp join_2nd_team(user, opts \\ []) do
+    another = new_user()
+    new_site(owner: another)
+    team2 = team_of(another)
+    add_member(team2, user: user, role: :admin)
+
+    if opts[:subscribe?] do
+      subscribe_to_growth_plan(another)
+    end
+
+    team2
+  end
+end
