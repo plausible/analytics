8b68a6700cba75b57684e8faa823d2d3a44a0509
Adrian Gruntkowski <adrian.gruntkowski@gmail.com>
2026-06-10 12:18:02 +0200

    Ignore a bogus dialyzer error

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -1,5 +1,6 @@
 [
   {"lib/plausible/goals/goals.ex", :call_without_opaque},
   {"lib/plausible/sites.ex", :call_without_opaque},
-  {"lib/plausible/teams/invitations.ex", :call_without_opaque}
+  {"lib/plausible/teams/invitations.ex", :call_without_opaque},
+  {"lib/plausible_web/live/components/modal.ex", :pattern_match, 1}
 ]
