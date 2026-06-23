dc941ff76d662955ed5e7a92adc8efc46e1c0375
Sanne de Vries <sannedv@protonmail.com>
2026-06-23 12:11:11 +0200

    Add `current_plan` as custom property to track
    - Having a `current_plan` custom property allows us to see an accurate conversion rate for plan-specific features, such as User journeys.
    - The prop is called `current_plan` to distinguish it from the `plan` custom property used to track CTA clicks on the landing page.

--- extra/lib/plausible_web/dogfood.ex
+++ extra/lib/plausible_web/dogfood.ex
@@ -68,11 +68,28 @@ defmodule PlausibleWeb.Dogfood do
 
   defp location_override(_), do: nil
 
-  defp custom_properties(%{current_user: user}) when is_map(user) do
-    %{logged_in: true, theme: user.theme}
+  defp custom_properties(%{current_user: user} = assigns) when is_map(user) do
+    %{
+      logged_in: true,
+      theme: user.theme,
+      current_plan: current_plan(assigns[:current_team])
+    }
   end
 
   defp custom_properties(_) do
     %{logged_in: false}
+  end
+
+  defp current_plan(nil), do: nil
+
+  defp current_plan(team) do
+    team = Plausible.Teams.with_subscription(team)
+
+    case Plausible.Billing.Plans.get_subscription_plan(team.subscription) do
+      %Plausible.Billing.Plan{kind: kind} -> Atom.to_string(kind)
+      %Plausible.Billing.EnterprisePlan{} -> "enterprise"
+      :free_10k -> "free_10k"
+      nil -> "no_subscription"
+    end
   end
 end
