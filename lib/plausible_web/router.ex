31f39a4f78c38e97816bd4a37290462851e48591
Artur Pata <artur.pata@gmail.com>
2026-07-02 14:01:41 +0300

    Block access to annotations API without FF

--- lib/plausible_web/router.ex
+++ lib/plausible_web/router.ex
@@ -306,6 +306,10 @@ defmodule PlausibleWeb.Router do
 
     scope "/:domain/annotations", PlausibleWeb.Api.Internal,
       private: %{allow_consolidated_views: true} do
+      pipeline :annotations_endpoints,
+        do: plug(PlausibleWeb.Plugs.FeatureFlagCheckPlug, [:annotations])
+
+      pipe_through :annotations_endpoints
       get "/", AnnotationsController, :index
       post "/", AnnotationsController, :create
       patch "/:annotation_id", AnnotationsController, :update
