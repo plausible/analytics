8a0d359aec1b4611511988d847c31e42959ddd44
Adam Rutkowski <hq@mtod.org>
2026-07-07 06:49:50 +0200

    Better error message change domain (#6462)
    * Migration: allow domain change within the same team
    
    * Better error message if trying to change a domain to already owned
    
    * Allow domain name swap within the same team

--- lib/plausible_web/live/change_domain/form.ex
+++ lib/plausible_web/live/change_domain/form.ex
@@ -4,12 +4,18 @@ defmodule PlausibleWeb.Live.ChangeDomain.Form do
   """
   use PlausibleWeb, :live_component
 
-  alias Plausible.Site
+  alias Plausible.{Repo, Site}
+
+  import Ecto.Query
 
   def update(assigns, socket) do
     changeset = Site.update_changeset(assigns.site)
 
-    {:ok, assign(socket, Map.put(assigns, :changeset, changeset))}
+    {:ok,
+     socket
+     |> assign(assigns)
+     |> assign(changeset: changeset)
+     |> assign(already_owned_domain: nil)}
   end
 
   def render(assigns) do
@@ -24,6 +30,16 @@ defmodule PlausibleWeb.Live.ChangeDomain.Form do
           label="Domain"
         />
 
+        <p
+          :if={@already_owned_domain}
+          class="mt-1 flex text-sm text-red-500 leading-4.5 text-pretty"
+        >
+          You already own this site. See its&nbsp;
+          <.styled_link href={Routes.site_path(@socket, :settings_general, @already_owned_domain)}>
+            settings
+          </.styled_link>
+        </p>
+
         <.button type="submit" class="mt-4 w-full">
           Change Domain
         </.button>
@@ -33,13 +49,30 @@ defmodule PlausibleWeb.Live.ChangeDomain.Form do
   end
 
   def handle_event("submit", %{"site" => %{"domain" => new_domain}}, socket) do
-    case Plausible.Site.Domain.change(socket.assigns.site, new_domain) do
-      {:ok, updated_site} ->
-        send(self(), {:domain_changed, updated_site})
-        {:noreply, socket}
+    site = socket.assigns.site
+    preview_changeset = Site.update_changeset(site, %{domain: new_domain})
+    cleaned_domain = Ecto.Changeset.get_field(preview_changeset, :domain)
 
-      {:error, changeset} ->
-        {:noreply, assign(socket, changeset: changeset)}
+    if same_team_owns_domain?(site, cleaned_domain) do
+      {:noreply,
+       assign(socket, changeset: preview_changeset, already_owned_domain: cleaned_domain)}
+    else
+      case Plausible.Site.Domain.change(site, new_domain) do
+        {:ok, updated_site} ->
+          send(self(), {:domain_changed, updated_site})
+          {:noreply, assign(socket, already_owned_domain: nil)}
+
+        {:error, changeset} ->
+          {:noreply, assign(socket, changeset: changeset, already_owned_domain: nil)}
+      end
     end
   end
+
+  defp same_team_owns_domain?(%Site{id: site_id, team_id: team_id}, domain) do
+    Repo.exists?(
+      from s in Site, where: s.domain == ^domain and s.team_id == ^team_id and s.id != ^site_id
+    )
+  end
+
+  defp same_team_owns_domain?(_, _), do: false
 end
