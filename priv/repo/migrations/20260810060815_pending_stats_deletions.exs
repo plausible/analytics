efaf5b7bff645cfd59df1b7e32c6a836c1a9d59d
Adam Rutkowski <hq@mtod.org>
2026-08-10 09:31:30 +0200

    Migration: dates non-nullable

--- priv/repo/migrations/20260810060815_pending_stats_deletions.exs
+++ priv/repo/migrations/20260810060815_pending_stats_deletions.exs
@@ -5,8 +5,8 @@ defmodule Plausible.Repo.Migrations.PendingStatsDeletions do
     create table(:pending_stats_deletions) do
       # site id may not in postgres anymore, so not a reference
       add :site_id, :integer, null: false
-      add :stats_start_date, :date
-      add :stats_end_date, :date
+      add :stats_start_date, :date, null: false
+      add :stats_end_date, :date, null: false
       add :reason, :text, null: false
 
       timestamps()
