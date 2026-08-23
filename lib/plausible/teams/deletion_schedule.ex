421b690f56199e91d251f0fc65f33810d5d88349
Adam Rutkowski <hq@mtod.org>
2026-08-23 06:13:53 +0200

    Implement base deletion schedule/intervals

---
+++ lib/plausible/teams/deletion_schedule.ex
@@ -0,0 +1,54 @@
+defmodule Plausible.Teams.DeletionSchedule do
+  @moduledoc """
+  Inactive team deletion schedule
+  """
+
+  @trial_deletion_offset_days 60
+  @subscription_deletion_offset_days 180
+
+  @first_notice_before_deletion_days 30
+  @reminder_before_deletion_days 5
+
+  @backlog_deletion_offset_days 30
+  @backlog_reminder_before_deletion_days 5
+  @backlog_release_window_days 30
+
+  def trial_deletion_offset_days, do: @trial_deletion_offset_days
+  def subscription_deletion_offset_days, do: @subscription_deletion_offset_days
+  def first_notice_before_deletion_days, do: @first_notice_before_deletion_days
+  def reminder_before_deletion_days, do: @reminder_before_deletion_days
+  def backlog_deletion_offset_days, do: @backlog_deletion_offset_days
+  def backlog_reminder_before_deletion_days, do: @backlog_reminder_before_deletion_days
+  def backlog_release_window_days, do: @backlog_release_window_days
+
+  @spec deletion_offset_days(:expired_trial | :expired_subscription) :: pos_integer()
+  def deletion_offset_days(:expired_trial), do: @trial_deletion_offset_days
+  def deletion_offset_days(:expired_subscription), do: @subscription_deletion_offset_days
+
+  @spec deletion_date(:expired_trial | :expired_subscription, Date.t()) :: Date.t()
+  def deletion_date(category, expiry_date) do
+    Date.add(expiry_date, deletion_offset_days(category))
+  end
+
+  @spec first_notice_due_date(Date.t()) :: Date.t()
+  def first_notice_due_date(deletion_date),
+    do: Date.add(deletion_date, -@first_notice_before_deletion_days)
+
+  @spec reminder_due_date(Date.t()) :: Date.t()
+  def reminder_due_date(deletion_date),
+    do: Date.add(deletion_date, -@reminder_before_deletion_days)
+
+  @spec backlog_deletion_date(NaiveDateTime.t()) :: Date.t()
+  def backlog_deletion_date(first_notice_sent_at) do
+    first_notice_sent_at
+    |> NaiveDateTime.to_date()
+    |> Date.add(@backlog_deletion_offset_days)
+  end
+
+  @spec backlog_reminder_due_date(NaiveDateTime.t()) :: Date.t()
+  def backlog_reminder_due_date(first_notice_sent_at) do
+    first_notice_sent_at
+    |> NaiveDateTime.to_date()
+    |> Date.add(@backlog_deletion_offset_days - @backlog_reminder_before_deletion_days)
+  end
+end
