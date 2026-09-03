b8fbc3906113f1e876b0daacd022cf9261e23464
Adam Rutkowski <hq@mtod.org>
2026-09-03 07:18:29 +0200

    Preload team using a join

--- lib/plausible/team_deletion_schedules.ex
+++ lib/plausible/team_deletion_schedules.ex
@@ -83,9 +83,10 @@ defmodule Plausible.TeamDeletionSchedules do
   def due_for_first_notice(today \\ Date.utc_today()) do
     Repo.all(
       from(sch in TeamDeletionSchedule,
+        inner_join: t in assoc(sch, :team),
         where: sch.status == :scheduled,
         where: sch.first_notice_due_date <= ^today,
-        preload: [team: [:owners, :billing_members]]
+        preload: [team: {t, [:owners, :billing_members]}]
       )
     )
   end
@@ -100,9 +101,10 @@ defmodule Plausible.TeamDeletionSchedules do
 
     Repo.all(
       from(sch in TeamDeletionSchedule,
+        inner_join: t in assoc(sch, :team),
         where: sch.status == :first_notice_sent,
         where: sch.deletion_date <= ^reminder_threshold,
-        preload: [team: [:owners, :billing_members]]
+        preload: [team: {t, [:owners, :billing_members]}]
       )
     )
   end
