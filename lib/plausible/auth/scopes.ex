46083bf0f218e556c81d6537e6dda75a94f7bf16
Artur Pata <artur.pata@gmail.com>
2026-09-08 14:39:42 +0300

    Rename scope attrs

--- lib/plausible/auth/scopes.ex
+++ lib/plausible/auth/scopes.ex
@@ -4,9 +4,9 @@ defmodule Plausible.Auth.Scopes do
   authorization grant can be limited to.
   """
 
-  @stats_read_star "stats:read:*"
-  def stats_read(), do: @stats_read_star
+  @stats_read_all "stats:read:*"
+  def stats_read(), do: @stats_read_all
 
-  @sites_read_star "sites:read:*"
-  def sites_read(), do: @sites_read_star
+  @sites_read_all "sites:read:*"
+  def sites_read(), do: @sites_read_all
 end
