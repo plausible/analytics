0ea7f0a54aaeeb50225861e3979d3b5cfd0595e3
Sanne de Vries <sannedv@protonmail.com>
2026-09-12 16:58:00 +0200

    Fix icon size issues

--- assets/js/dashboard/stats/devices/icons.tsx
+++ assets/js/dashboard/stats/devices/icons.tsx
@@ -1,12 +1,12 @@
-import React, { ReactNode } from 'react'
+import React, { ReactElement } from 'react'
 
-const ICON_CLASS = 'inline-block w-4 h-4 mr-2'
+const ICON_CLASS = 'shrink-0 inline-block size-4 mr-2'
 
 const SVG_ICON_COLOR_CLASS = 'text-gray-600 dark:text-gray-300'
 
-// The fallback icons are inlined so that they follow the current text color,
-// supporting light and dark mode.
-const SHARED_FALLBACK_SVG_PROPS = {
+// The icons that are not brand logos are inlined so that they follow the
+// current text color, supporting light and dark mode.
+const SHARED_SVG_PROPS = {
   xmlns: 'http://www.w3.org/2000/svg',
   viewBox: '0 0 24 24',
   fill: 'none',
@@ -40,10 +40,10 @@ const BROWSER_ICONS: Record<string, string> = {
 }
 
 const BrowserFallbackIconSvg = () => (
-  <svg {...SHARED_FALLBACK_SVG_PROPS}>
-    <path d="M12 23c2.43 0 4.4-4.925 4.4-11S14.43 1 12 1 7.6 5.925 7.6 12 9.57 23 12 23Z" />
-    <path d="M1 12h22" />
-    <path d="M12 23c6.075 0 11-4.925 11-11S18.075 1 12 1 1 5.925 1 12s4.925 11 11 11Z" />
+  <svg {...SHARED_SVG_PROPS}>
+    <path d="M12 22c2.21 0 4-4.477 4-10S14.21 2 12 2 8 6.477 8 12s1.79 10 4 10Z" />
+    <path d="M2 12h20" />
+    <path d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10Z" />
   </svg>
 )
 
@@ -84,9 +84,9 @@ const OS_ICONS: Record<string, string> = {
 }
 
 const OsFallbackIconSvg = () => (
-  <svg {...SHARED_FALLBACK_SVG_PROPS}>
-    <path d="M3.2 21.8h17.6a2.2 2.2 0 0 0 2.2-2.2v-5.204q0-.195-.035-.39l-2.62-9.996a2.2 2.2 0 0 0-2.164-1.81H5.819a2.2 2.2 0 0 0-2.165 1.81l-2.62 9.997a2 2 0 0 0-.034.39V19.6a2.2 2.2 0 0 0 2.2 2.2M23 14.2H1" />
-    <path strokeWidth={3} d="M5.5 17.961v-.011M10 17.961v-.011" />
+  <svg {...SHARED_SVG_PROPS}>
+    <path d="M4 20.818h16a2 2 0 0 0 2-2v-4.73q0-.18-.032-.355l-2.38-9.087A2 2 0 0 0 17.617 3H6.382a2 2 0 0 0-1.968 1.646l-2.381 9.087a2 2 0 0 0-.032.355v4.73a2 2 0 0 0 2 2M22 13.91H2" />
+    <path strokeWidth={3} d="M6.092 17.328v-.01M10.182 17.328v-.01" />
   </svg>
 )
 
@@ -102,28 +102,15 @@ export const OsIcon = ({ dimensionValue }: { dimensionValue: string }) => {
   )
 }
 
-const SHARED_SCREEN_SIZE_SVG_PROPS = {
-  xmlns: 'http://www.w3.org/2000/svg',
-  width: 24,
-  height: 24,
-  viewBox: '0 0 24 24',
-  fill: 'none',
-  stroke: 'currentColor',
-  strokeWidth: 2,
-  strokeLinecap: 'round',
-  strokeLinejoin: 'round',
-  className: `-mt-px feather inline-block ${SVG_ICON_COLOR_CLASS}`
-} as const
-
 const MobileScreenIconSvg = () => (
-  <svg {...SHARED_SCREEN_SIZE_SVG_PROPS}>
+  <svg {...SHARED_SVG_PROPS}>
     <rect x="5" y="2" width="14" height="20" rx="2" ry="2" />
     <line x1="12" y1="18" x2="12" y2="18" />
   </svg>
 )
 
 const TabletScreenIconSvg = () => (
-  <svg {...SHARED_SCREEN_SIZE_SVG_PROPS}>
+  <svg {...SHARED_SVG_PROPS}>
     <rect
       x="4"
       y="2"
@@ -138,14 +125,14 @@ const TabletScreenIconSvg = () => (
 )
 
 const LaptopScreenIconSvg = () => (
-  <svg {...SHARED_SCREEN_SIZE_SVG_PROPS}>
+  <svg {...SHARED_SVG_PROPS}>
     <rect x="2" y="3" width="20" height="14" rx="2" ry="2" />
     <line x1="2" y1="20" x2="22" y2="20" />
   </svg>
 )
 
 const DesktopScreenIconSvg = () => (
-  <svg {...SHARED_SCREEN_SIZE_SVG_PROPS}>
+  <svg {...SHARED_SVG_PROPS}>
     <rect x="2" y="3" width="20" height="14" rx="2" ry="2" />
     <line x1="8" y1="21" x2="16" y2="21" />
     <line x1="12" y1="17" x2="12" y2="21" />
@@ -153,7 +140,7 @@ const DesktopScreenIconSvg = () => (
 )
 
 const UltraWideScreenIconSvg = () => (
-  <svg {...SHARED_SCREEN_SIZE_SVG_PROPS}>
+  <svg {...SHARED_SVG_PROPS}>
     <rect x="1" y="4" width="22" height="12" rx="2" ry="2" />
     <line x1="6" y1="20" x2="18" y2="20" />
     <line x1="12" y1="16" x2="12" y2="20" />
@@ -161,14 +148,14 @@ const UltraWideScreenIconSvg = () => (
 )
 
 const NotSetScreenIconSvg = () => (
-  <svg {...SHARED_SCREEN_SIZE_SVG_PROPS}>
-    <path d="M12 23c6.075 0 11-4.925 11-11S18.075 1 12 1 1 5.925 1 12s4.925 11 11 11Z" />
-    <path d="M8.7 9.8a3.3 3.3 0 1 1 6.6 0c0 1.44-.923 2.322-2.21 2.628-.59.14-1.09.614-1.09 1.222" />
-    <path strokeWidth={3} d="M12 17.511V17.5" />
+  <svg {...SHARED_SVG_PROPS}>
+    <path d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10Z" />
+    <path d="M9 10a3 3 0 1 1 6 0c0 1.31-.839 2.11-2.008 2.389-.538.128-.992.559-.992 1.111" />
+    <path strokeWidth={3} d="M12 17.01V17" />
   </svg>
 )
 
-const SCREEN_SIZE_SVGS: Record<string, ReactNode> = {
+const SCREEN_SIZE_SVGS: Record<string, ReactElement> = {
   Mobile: <MobileScreenIconSvg />,
   Tablet: <TabletScreenIconSvg />,
   Laptop: <LaptopScreenIconSvg />,
@@ -181,4 +168,4 @@ export const ScreenSizeIcon = ({
   dimensionValue
 }: {
   dimensionValue: string
-}) => <span className="mr-1.5">{SCREEN_SIZE_SVGS[dimensionValue] ?? null}</span>
+}) => SCREEN_SIZE_SVGS[dimensionValue] ?? null
