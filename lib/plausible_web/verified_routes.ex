8ca614fd863fee0fa0d966dd7e91afefb9872718
Adrian Gruntkowski <adrian.gruntkowski@gmail.com>
2026-09-15 12:47:02 +0200

    Update `VerifiedRoutes` static paths config to be on par with `Plug.Static`

--- lib/plausible_web/verified_routes.ex
+++ lib/plausible_web/verified_routes.ex
@@ -1,10 +1,25 @@
 defmodule PlausibleWeb.VerifiedRoutes do
+  @moduledoc """
+  Convenience macro to be used everywhere where verified routes are used.
+  """
+  use Plausible
+
   defmacro __using__(_) do
+    # Follows the same logic as `Plug.Static` setup in `PlausibleWeb.Endpoint`
+    static_paths = ~w(css js images favicon.ico)
+
+    static_paths =
+      on_ee do
+        static_paths
+      else
+        static_paths ++ ["robots.txt"]
+      end
+
     quote do
       use Phoenix.VerifiedRoutes,
         router: PlausibleWeb.Router,
         endpoint: PlausibleWeb.Endpoint,
-        statics: ~w(css js images favicon.ico)
+        statics: unquote(static_paths)
     end
   end
 end
