7cbd134f34d6b521a70a9f0ffda617981762e0dd
Adam Rutkowski <hq@mtod.org>
2026-09-16 07:11:24 +0200

    Implement e-mail sending suppression

--- test/plausible/mailer_test.exs
+++ test/plausible/mailer_test.exs
@@ -30,4 +30,41 @@ defmodule Plausible.MailerTest do
       assert email.from == mailer_email
     end
   end
+
+  describe "suppression" do
+    test "refuses to send to a suppressed address" do
+      user = insert(:user, email: "bounced@example.com")
+
+      {:ok, _} =
+        Plausible.EmailSuppressions.create_from_bounce(%{
+          email: user.email,
+          reason: :hard_bounce,
+          source: :webhook
+        })
+
+      email = PlausibleWeb.Email.welcome_email(user)
+      assert {:error, :suppressed} = Plausible.Mailer.send(email)
+
+      refute_delivered_email(email)
+    end
+
+    test "sends normally once the address is no longer suppressed" do
+      user = insert(:user, email: "reactivated@example.com")
+      reviewer = insert(:user)
+
+      {:ok, _} =
+        Plausible.EmailSuppressions.create_from_bounce(%{
+          email: user.email,
+          reason: :hard_bounce,
+          source: :webhook
+        })
+
+      {:ok, _} = Plausible.EmailSuppressions.reactivate(user.email, reviewer)
+
+      email = PlausibleWeb.Email.welcome_email(user)
+      assert :ok = Plausible.Mailer.send(email)
+
+      assert_delivered_email(email)
+    end
+  end
 end
