44336037ee173419d3563f9dd1d9a8f6e83ca5b4
Adam <hq@mtod.org>
2026-09-21 12:50:27 +0000

    Implement e-mail sending suppression (#6681)
    * Implement e-mail sending suppression
    
    * Do not suppress priority email
    
    * Make supression work only on ee
    
    * No need for cc/bcc checks
    
    * Resuppress following another webhook
    
    * Format
    
    * ee only

--- lib/plausible/mailer.ex
+++ lib/plausible/mailer.ex
@@ -1,9 +1,24 @@
 defmodule Plausible.Mailer do
   use Bamboo.Mailer, otp_app: :plausible
+  use Plausible
   require Logger
 
-  @spec send(Bamboo.Email.t()) :: :ok | {:error, :unknown_error}
+  @spec send(Bamboo.Email.t()) :: :ok | {:error, :unknown_error | :suppressed}
   def send(email) do
+    case suppressed_recipients(email) do
+      [] ->
+        do_send(email)
+
+      suppressed ->
+        Logger.warning(
+          "Not sending e-mail, recipient(s) are suppressed: #{Enum.join(suppressed, ", ")}"
+        )
+
+        {:error, :suppressed}
+    end
+  end
+
+  defp do_send(email) do
     try do
       deliver_now!(email)
     rescue
@@ -17,6 +32,50 @@ defmodule Plausible.Mailer do
         {:error, :unknown_error}
     else
       _sent_email -> :ok
+    end
+  end
+
+  defp suppressed_recipients(email)
+
+  on_ce do
+    defp suppressed_recipients(_email) do
+      # trick the type checker
+      if always(true), do: [], else: ["unreachable"]
+    end
+  end
+
+  on_ee do
+    defp suppressed_recipients(email) do
+      address = sole_recipient(email)
+
+      cond do
+        # Priority-stream mail (password resets, 2FA, e-mail verification)
+        # has to go through even to an address we'd otherwise suppress -
+        # refusing it could lock someone out of their own account.
+        priority_stream?(email) ->
+          []
+
+        Plausible.EmailSuppressions.suppressed?(address) ->
+          [address]
+
+        true ->
+          []
+      end
+    end
+
+    defp sole_recipient(email) do
+      case Bamboo.Mailer.normalize_addresses(email).to do
+        [{_name, address}] ->
+          address
+
+        to ->
+          # this can be handled in the future, but currently isn't ever used
+          raise "Plausible.Mailer only supports a single `to` recipient, got: #{inspect(to)}"
+      end
+    end
+
+    defp priority_stream?(email) do
+      get_in(email.private, [:message_params, "MessageStream"]) == "priority"
     end
   end
 end
--- test/plausible/mailer_test.exs
+++ test/plausible/mailer_test.exs
@@ -30,4 +30,101 @@ defmodule Plausible.MailerTest do
       assert email.from == mailer_email
     end
   end
+
+  describe "suppression" do
+    @describetag :ee_only
+
+    test "refuses to send to a suppressed address" do
+      user = insert(:user, email: "bounced@example.com")
+
+      {:ok, _} =
+        Plausible.EmailSuppressions.create_from_bounce(%{
+          email: user.email,
+          reason: :hard_bounce,
+          source: :webhook
+        })
+
+      email = PlausibleWeb.Email.welcome_email(user)
+      assert {:error, :suppressed} = Plausible.Mailer.send(email)
+
+      refute_delivered_email(email)
+    end
+
+    test "sends normally once the address is no longer suppressed" do
+      user = insert(:user, email: "reactivated@example.com")
+      reviewer = insert(:user)
+
+      {:ok, _} =
+        Plausible.EmailSuppressions.create_from_bounce(%{
+          email: user.email,
+          reason: :hard_bounce,
+          source: :webhook
+        })
+
+      {:ok, _} = Plausible.EmailSuppressions.reactivate(user.email, reviewer)
+
+      email = PlausibleWeb.Email.welcome_email(user)
+      assert :ok = Plausible.Mailer.send(email)
+
+      assert_delivered_email(email)
+    end
+
+    test "still sends priority-stream e-mails to a suppressed address" do
+      address = "bounced@example.com"
+
+      {:ok, _} =
+        Plausible.EmailSuppressions.create_from_bounce(%{
+          email: address,
+          reason: :hard_bounce,
+          source: :webhook
+        })
+
+      email = PlausibleWeb.Email.password_reset_email(address, "http://example.com/reset")
+      assert :ok = Plausible.Mailer.send(email)
+
+      assert_delivered_email(email)
+    end
+
+    test "does not send a non-priority e-mail to a suppressed address, even alongside a priority one" do
+      address = "bounced@example.com"
+
+      {:ok, _} =
+        Plausible.EmailSuppressions.create_from_bounce(%{
+          email: address,
+          reason: :hard_bounce,
+          source: :webhook
+        })
+
+      base_email = PlausibleWeb.Email.welcome_email(insert(:user, email: address))
+
+      priority_email =
+        PlausibleWeb.Email.password_reset_email(address, "http://example.com/reset")
+
+      assert {:error, :suppressed} = Plausible.Mailer.send(base_email)
+      assert :ok = Plausible.Mailer.send(priority_email)
+
+      refute_delivered_email(base_email)
+      assert_delivered_email(priority_email)
+    end
+
+    test "crashes rather than silently under-checking when `to` has more than one address" do
+      email =
+        Bamboo.Email.new_email(
+          from: "from@example.com",
+          to: ["one@example.com", "two@example.com"]
+        )
+
+      assert_raise RuntimeError, ~r/only supports a single `to` recipient/, fn ->
+        Plausible.Mailer.send(email)
+      end
+    end
+
+    test "crashes rather than silently under-checking when `to` is empty" do
+      email = Bamboo.Email.new_email(from: "from@example.com", to: [])
+
+      assert_raise RuntimeError, ~r/only supports a single `to` recipient/, fn ->
+        Plausible.Mailer.send(email)
+      end
+    end
+  end
 end
