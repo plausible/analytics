fb38cae72eaa8e00fc58f201f74bade979f63a0c
Adam Rutkowski <hq@mtod.org>
2026-09-21 14:26:32 +0200

    ee only

--- priv/repo/migrations/20260916070000_create_email_suppressions.exs
+++ priv/repo/migrations/20260916070000_create_email_suppressions.exs
@@ -1,22 +1,26 @@
 defmodule Plausible.Repo.Migrations.CreateEmailSuppressions do
   use Ecto.Migration
 
+  import Plausible.MigrationUtils
+
   def change do
-    create table(:email_suppressions) do
-      add :email, :citext, null: false
-      add :reason, :string, null: false
-      add :source, :string, null: false
-      add :postmark_bounce_id, :bigint
-      add :postmark_inactive, :boolean, null: false, default: false
-      add :can_activate, :boolean, null: false, default: false
-      add :details, :text
-      add :reactivated_at, :naive_datetime
-      add :reactivated_by_user_id, references(:users, on_delete: :nilify_all)
+    if enterprise_edition?() do
+      create table(:email_suppressions) do
+        add :email, :citext, null: false
+        add :reason, :string, null: false
+        add :source, :string, null: false
+        add :postmark_bounce_id, :bigint
+        add :postmark_inactive, :boolean, null: false, default: false
+        add :can_activate, :boolean, null: false, default: false
+        add :details, :text
+        add :reactivated_at, :naive_datetime
+        add :reactivated_by_user_id, references(:users, on_delete: :nilify_all)
 
-      timestamps()
+        timestamps()
+      end
+
+      create unique_index(:email_suppressions, [:email])
+      create index(:email_suppressions, [:reason])
     end
-
-    create unique_index(:email_suppressions, [:email])
-    create index(:email_suppressions, [:reason])
   end
 end
